21f10a69471b9ebe4b974d61f4de3b7b3e20465f
Mark Neumann <markn@allenai.org>
2017-06-27 12:39:15 -0700

    see if conda install is breaking build

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -4,3 +4,4 @@ pip install -r requirements.txt
 python -m nltk.downloader punkt
 python -m spacy.en.download all
 pip install --no-cache-dir -q http://download.pytorch.org/whl/cu80/torch-0.1.11.post5-cp35-cp35m-linux_x86_64.whl
+conda install pytorch torchvision -c soumith
