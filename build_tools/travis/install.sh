cad4fbbb21e66eb20da996713cfb1a5ba642912c
Mark Neumann <markn@allenai.org>
2017-06-27 09:59:48 -0700

    use install_requirements bash script in travis build

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -42,15 +42,8 @@ else
     source activate testenv
 fi
 
-# Install requirements via pip in our conda environment
-pip install -U -r requirements.txt
-pip install -q http://download.pytorch.org/whl/cu80/torch-0.1.12.post2-cp35-cp35m-linux_x86_64.whl
+# Install requirements via pip and download data inside our conda environment.
+bash scripts/install_requirements.sh
 
 # List the packages to get their versions for debugging
 pip list
-
-# Install punkt tokenizer
-python -m nltk.downloader punkt
-
-# Install spacy data
-python -m spacy.en.download all
