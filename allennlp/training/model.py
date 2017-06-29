b8d693afb5b85c073def6e04ad0a20ef55712aaf
Mark Neumann <markn@allenai.org>
2017-06-28 17:32:03 -0700

    address some of matt's comments

--- allennlp/training/model.py
+++ allennlp/training/model.py
@@ -5,7 +5,12 @@ import torch
 class Model(torch.nn.Module):
     """
     This abstract class represents a model to be trained. Rather than relying on the
-    raw torch Module, we provide a slightly different API.
+    raw torch Module, we provide a slightly different API, particularly to align with
+    data processing code for textual data. As textual data pre-processing and deep NLP
+    model inputs tend to be more complicated than for other modalities, we use
+    dictionaries of tensors to pass inputs to the model, to keep on top of what's
+    going on. It also makes reading model implementations easier to connect to
+    data generation, because model inputs are named.
     """
 
     def forward(self,
@@ -16,7 +21,7 @@ class Model(torch.nn.Module):
         this method is designed to compute a loss function defined by a user.
 
         The input dictionary is comprised of everything required to perform a
-        training update, _including_ labels. It is down to the user to ensure
+        training update, `including` labels. It is down to the user to ensure
         that inference can be performed (using compute_loss = False) without the
         presence of these labels! Hence, any inputs not available at inference
         time should only be used inside an ``if compute_loss:`` conditional block.
@@ -38,10 +43,10 @@ class Model(torch.nn.Module):
         ----------
         inputs: Dict[str, torch.Tensor], required.
             A dictionary of tensors comprising everything needed to perform a
-            training update, _including_ labels. At inference time, simply
+            training update, `including` labels. At inference time, simply
             pass an input dictionary which does not include labels and
         compute_loss: bool, optional (default = False)
-            If true, this method should call `:func: compute_loss` and return
+            If true, this method should call :func:`compute_loss` and return
             both the model outputs and a scalar loss to optimise.
 
         Returns
@@ -62,14 +67,14 @@ class Model(torch.nn.Module):
                      model_state: Dict[str, Any] = None) -> float:
         """
         Computes a scalar loss function to optimise. This method is designed
-        to be called and returned by `:func: forward` when `compute_loss = True`.
+        to be called and returned by :func:`forward` when `compute_loss = True`.
         It is part of the public interface however, as there are scenarios in which
         direct evaluation may be useful, such as during model comparison.
 
         Parameters
         ----------
         model_output : torch.Tensor or List[torch.Tensor], required.
-            The output of the result of calling `:func: forward`. This
+            The output of the result of calling :func:`forward`. This
             can be a list, as complex models may have many outputs.
         targets : torch.Tensor or List[torch.Tensor], required.
             The gold targets for computing some loss function with respect to
