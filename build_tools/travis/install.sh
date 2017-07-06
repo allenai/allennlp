0901ddda5ec0e939eee6fa77586c77f9447e8795
Joel Grus <joelgrus@gmail.com>
2017-07-06 10:39:33 -0700

    remove conda / pytorch from install_requirements.sh (#15)
    * remove conda / pytorch from install_requirements.sh
    
    * remove superfluous conda install from scripts

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -37,6 +37,7 @@ source activate testenv
 
 # Install requirements via pip and download data inside our conda environment.
 bash scripts/install_requirements.sh
+pip install --no-cache-dir -q http://download.pytorch.org/whl/cu80/torch-0.1.11.post5-cp35-cp35m-linux_x86_64.whl
 
 # List the packages to get their versions for debugging
 pip list
