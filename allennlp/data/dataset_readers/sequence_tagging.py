b6a26ea621dd95b901c9f84b30dbeea1b3a2680c
Mark Neumann <markn@allenai.org>
2017-07-11 13:05:52 -0700

    switch sequence tagger reader to new api

--- allennlp/data/dataset_readers/sequence_tagging.py
+++ allennlp/data/dataset_readers/sequence_tagging.py
@@ -19,21 +19,18 @@ class SequenceTaggingDatasetReader(DatasetReader):
 
     Parameters
     ----------
-    filename : ``str``
     token_indexers : ``List[TokenIndexer]``, optional (default=``[SingleIdTokenIndexer()]``)
         We use this to define the input representation for the text.  See :class:`TokenIndexer`.
         Note that the `output` tags will always correspond to single token IDs based on how they
         are pre-tokenised in the data file.
     """
     def __init__(self,
-                 filename: str,
                  token_indexers: List[TokenIndexer] = None):
-        self._filename = filename
         self._token_indexers = token_indexers or [SingleIdTokenIndexer()]
 
     @overrides
-    def read(self):
-        with open(self._filename, "r") as data_file:
+    def read(self, file_path):
+        with open(file_path, "r") as data_file:
 
             instances = []
             for line in data_file:
@@ -52,12 +49,9 @@ class SequenceTaggingDatasetReader(DatasetReader):
         """
         Parameters
         ----------
-        filename : ``str``
         token_indexers: ``List[Params]``, optional
         """
-        filename = params.pop('filename')
         token_indexers = [TokenIndexer.from_params(p)
                           for p in params.pop('token_indexers', [Params({})])]
         params.assert_empty(cls.__name__)
-        return SequenceTaggingDatasetReader(filename=filename,
-                                            token_indexers=token_indexers)
+        return SequenceTaggingDatasetReader(token_indexers=token_indexers)
