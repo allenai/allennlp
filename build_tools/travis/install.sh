4d4cb9c59ba2613e82174086b5fa1aa3de742da4
Joel Grus <joelgrus@gmail.com>
2017-07-13 13:56:07 -0700

    split out test requirements into separate file

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -37,6 +37,7 @@ source activate testenv
 
 # Install requirements via pip and download data inside our conda environment.
 bash scripts/install_requirements.sh
+pip install -r requirements_test.txt
 pip install --no-cache-dir -q http://download.pytorch.org/whl/cu80/torch-0.1.12.post2-cp35-cp35m-linux_x86_64.whl
 
 # List the packages to get their versions for debugging
