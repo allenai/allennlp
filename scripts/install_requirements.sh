3ab6930c37b6e96724d5656a91e5a08f8a027bc1
Joel Grus <joelgrus@gmail.com>
2017-07-13 14:22:07 -0700

    add mypy to requirements.txt (#26)
    * add mypy to requirements.txt
    
    * update travis scripts to use mypy
    
    * split out test requirements into separate file
    
    * add test_requirements flag
    
    * flip RUN_TESTS flag

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -3,3 +3,8 @@
 pip install -r requirements.txt
 python -m nltk.downloader punkt
 python -m spacy.en.download all
+
+# only install test requirements if explicitly specified
+if [[ "$INSTALL_TEST_REQUIREMENTS" == "true" ]]; then
+    pip install -r requirements_test.txt
+fi
