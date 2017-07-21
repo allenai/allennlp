17b1c26f6ac3b812260c7a6d536274b5399a5183
Mark Neumann <markn@allenai.org>
2017-07-21 09:51:04 -0700

    merge tensor test

--- tests/common/tensor_test.py
+++ tests/common/tensor_test.py
@@ -5,6 +5,8 @@ import pytest
 
 from allennlp.testing.test_case import AllenNlpTestCase
 from allennlp.common.tensor import arrays_to_variables
+from allennlp.common.tensor import get_lengths_from_binary_sequence_mask
+from allennlp.common.tensor import sort_batch_by_length
 
 
 class TestTensor(AllenNlpTestCase):
@@ -63,3 +65,30 @@ class TestTensor(AllenNlpTestCase):
                 torch.cuda.IntTensor(numpy.ones([2, 5], dtype="int32")))
         assert torch_array_dict["tags"].data.equal(
                 torch.cuda.ByteTensor(numpy.ones([2, 3], dtype="uint8")))
+
+    def test_get_sequence_lengths_from_binary_mask(self):
+        binary_mask = torch.ByteTensor([[1, 1, 1, 0, 0, 0],
+                                        [1, 1, 0, 0, 0, 0],
+                                        [1, 1, 1, 1, 1, 1],
+                                        [1, 0, 0, 0, 0, 0]])
+        lengths = get_lengths_from_binary_sequence_mask(binary_mask)
+        numpy.testing.assert_array_equal(lengths.numpy(), numpy.array([3, 2, 6, 1]))
+
+    def test_sort_tensor_by_length(self):
+        tensor = torch.rand([5, 7, 9])
+        tensor[0, 3:, :] = 0
+        tensor[1, 4:, :] = 0
+        tensor[2, 1:, :] = 0
+        tensor[3, 5:, :] = 0
+
+        sequence_lengths = torch.LongTensor([3, 4, 1, 5, 7])
+        sorted_tensor, reverse_indices = sort_batch_by_length(tensor, sequence_lengths)
+
+        # Test sorted indices are padded correctly.
+        numpy.testing.assert_array_equal(sorted_tensor[1, 5:, :].numpy(), 0.0)
+        numpy.testing.assert_array_equal(sorted_tensor[2, 4:, :].numpy(), 0.0)
+        numpy.testing.assert_array_equal(sorted_tensor[3, 3:, :].numpy(), 0.0)
+        numpy.testing.assert_array_equal(sorted_tensor[4, 1:, :].numpy(), 0.0)
+
+        # Test restoration indices correctly recover the original tensor.
+        assert sorted_tensor[reverse_indices].equal(tensor)
