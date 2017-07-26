44a12811077dcaa7c0d1512899b59652904cc67c
Matt Gardner <mattg@allenai.org>
2017-07-26 23:17:43 +0000

    Fix spacy splitter to match docs

--- allennlp/data/tokenizers/word_splitter.py
+++ allennlp/data/tokenizers/word_splitter.py
@@ -127,7 +127,7 @@ class SpacyWordSplitter(WordSplitter):
 
     @overrides
     def split_words(self, sentence: str) -> List[str]:
-        return [str(token.lower_) for token in self.en_nlp.tokenizer(sentence)]
+        return [str(token) for token in self.en_nlp.tokenizer(sentence)]
 
 
 word_splitters['simple'] = SimpleWordSplitter
