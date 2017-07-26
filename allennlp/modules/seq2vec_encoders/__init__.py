00e8f1d32f235b2e6d952915f389aff9ea741258
Mark Neumann <markn@allenai.org>
2017-07-26 13:08:59 -0700

    Encoder Wrapper Improvements (#41)
    * initial fragile fix to see what Matt thinks
    
    * lint fixes
    
    * fix tests and deal with state properly
    
    * move batch_first check into Wrappers
    
    * also catch error in private Wrapper to have sensible defaults
    
    * better comments and concatenated test

--- allennlp/modules/seq2vec_encoders/__init__.py
+++ allennlp/modules/seq2vec_encoders/__init__.py
@@ -6,9 +6,10 @@ from allennlp.common import Params
 from allennlp.common.checks import ConfigurationError
 from allennlp.experiments import Registry
 from allennlp.modules.seq2vec_encoders.cnn_encoder import CnnEncoder
-from allennlp.modules.seq2vec_encoders.wrapped_pytorch_rnn import WrappedPytorchRnn
+from allennlp.modules.seq2vec_encoders.pytorch_seq2vec_wrapper import PytorchSeq2VecWrapper
 
-class _Wrapper:
+
+class _Seq2VecWrapper:
     """
     The :class:`Registry` needs to have a ``Type[Seq2VecEncoder]`` as the value registered for each
     key.  What that means is that we need to be able to ``__call__`` these values (as is done with
@@ -19,35 +20,35 @@ class _Wrapper:
     the registry; or (2) we wrap pytorch's RNNs with something that `mimics` the required
     ``Registry`` API.  We've gone with the second option here.
 
-    This is a two-step approach: first, we have the :class:`WrappedPytorchRnn` class that handles
-    the interface between a pytorch RNN and our ``Seq2VecEncoder`` API.  Our ``WrappedPytorchRnn``
+    This is a two-step approach: first, we have the :class:`PytorchSeq2VecWrapper` class that handles
+    the interface between a pytorch RNN and our ``Seq2VecEncoder`` API.  Our ``PytorchSeq2VecWrapper``
     takes an instantiated pytorch RNN and just does some interface changes.  Second, we need a way
-    to create one of these ``WrappedPytorchRnns``, with an instantiated pytorch RNN, from the
+    to create one of these ``PytorchSeq2VecWrappers``, with an instantiated pytorch RNN, from the
     registry.  That's what this ``_Wrapper`` does.  The only thing this class does is instantiate
     the pytorch RNN in a way that's compatible with the ``Registry``, then pass it off to the
-    ``WrappedPytorchRnn`` class.
+    ``PytorchSeq2VecWrapper`` class.
 
     When you instantiate a ``_Wrapper`` object, you give it an ``RNNBase`` subclass, which we save
     to ``self``.  Then when called (as if we were instantiating an actual encoder with
     ``Encoder(**params)``, or with ``Encoder.from_params(params)``), we pass those parameters
     through to the ``RNNBase`` constructor, then pass the instantiated pytorch RNN to the
-    ``WrappedPytorchRnn``.  This lets us use this class in the registry and have everything just
+    ``PytorchSeq2VecWrapper``.  This lets us use this class in the registry and have everything just
     work.
     """
     def __init__(self, module_class: Type[torch.nn.modules.RNNBase]) -> None:
         self._module_class = module_class
 
-    def __call__(self, **kwargs) -> WrappedPytorchRnn:
+    def __call__(self, **kwargs) -> PytorchSeq2VecWrapper:
         return self.from_params(Params(kwargs))
 
-    def from_params(self, params: Params) -> WrappedPytorchRnn:
+    def from_params(self, params: Params) -> PytorchSeq2VecWrapper:
         if not params.pop('batch_first', True):
             raise ConfigurationError("Our encoder semantics assumes batch is always first!")
         params['batch_first'] = True
         module = self._module_class(**params.as_dict())
-        return WrappedPytorchRnn(module)
+        return PytorchSeq2VecWrapper(module)
 
 # pylint: disable=protected-access
-Registry.register_seq2vec_encoder("gru")(_Wrapper(torch.nn.GRU))
-Registry.register_seq2vec_encoder("lstm")(_Wrapper(torch.nn.LSTM))
-Registry.register_seq2vec_encoder("rnn")(_Wrapper(torch.nn.RNN))
+Registry.register_seq2vec_encoder("gru")(_Seq2VecWrapper(torch.nn.GRU))
+Registry.register_seq2vec_encoder("lstm")(_Seq2VecWrapper(torch.nn.LSTM))
+Registry.register_seq2vec_encoder("rnn")(_Seq2VecWrapper(torch.nn.RNN))
