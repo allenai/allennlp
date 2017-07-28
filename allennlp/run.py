8d7379ee02ae6a46ede257c78ad8630cb57968fe
Mark Neumann <markn@allenai.org>
2017-07-27 17:14:33 -0700

    try to fix TeeLogger types

--- allennlp/run.py
+++ allennlp/run.py
@@ -88,8 +88,8 @@ def train_model(param_dict: Dict[str, Any]):
 
     log_dir = params.get("serialization_prefix", None)  # pylint: disable=no-member
     if log_dir is not None:
-        sys.stdout = TeeLogger(log_dir + "_stdout.log", sys.stdout)
-        sys.stderr = TeeLogger(log_dir + "_stderr.log", sys.stderr)
+        sys.stdout = TeeLogger(log_dir + "_stdout.log", sys.stdout)  # type: ignore
+        sys.stderr = TeeLogger(log_dir + "_stderr.log", sys.stderr)  # type: ignore
         handler = logging.FileHandler(log_dir + "_python_logging.log")
         handler.setLevel(logging.INFO)
         handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s'))
