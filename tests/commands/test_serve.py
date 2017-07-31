633fbe45c5c5a6c49a83f6c95b62575c3d6fc924
Michael Schmitz <michael@schmitztech.com>
2017-07-31 15:24:37 -0700

    Add a test case to serving the web demo.

---
+++ tests/commands/test_serve.py
@@ -0,0 +1,30 @@
+# pylint: disable=no-self-use,invalid-name
+import argparse
+import json
+import os
+import tempfile
+from unittest import TestCase
+
+from allennlp.__main__ import main
+from allennlp.commands.serve import add_serve_subparser, serve
+
+
+class TestServe(TestCase):
+
+    def test_add_serve(self):
+        parser = argparse.ArgumentParser(description="Testing")
+        subparsers = parser.add_subparsers(title='Commands', metavar='')
+        add_serve_subparser(subparsers)
+
+        raw_args = ["serve",
+                    "--backend",
+                    "flask",
+                    "--port",
+                    "--8000"]
+
+        args = parser.parse_args(raw_args)
+
+        assert args.func == serve
+        assert args.backend == "flask"
+        assert args.port == "8000"
+
