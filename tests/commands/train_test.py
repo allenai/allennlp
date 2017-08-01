217dcd7fcc592abcab617e1838fd045952e9fc3b
Joel Grus <joelgrus@gmail.com>
2017-08-01 15:28:21 -0700

    one more test

--- tests/commands/train_test.py
+++ tests/commands/train_test.py
@@ -1,7 +1,7 @@
-
+import argparse
 
 from allennlp.testing.test_case import AllenNlpTestCase
-from allennlp.commands.train import train_model
+from allennlp.commands.train import train_model, add_subparser, train_model_from_file
 
 
 class TestTrain(AllenNlpTestCase):
@@ -33,3 +33,19 @@ class TestTrain(AllenNlpTestCase):
                 "num_epochs": 2
         }
         train_model(trainer_params)
+
+    def test_train_args(self):
+        parser = argparse.ArgumentParser(description="Testing")
+        subparsers = parser.add_subparsers(title='Commands', metavar='')
+        add_subparser(subparsers)
+
+        raw_args = ["train", "path/to/params"]
+
+        args = parser.parse_args(raw_args)
+
+        assert args.func == train_model_from_file
+        assert args.param_path == "path/to/params"
+
+        with self.assertRaises(SystemExit) as cm:  # pylint: disable=invalid-name
+            args = parser.parse_args(["train"])
+            assert cm.exception.code == 2  # argparse code for incorrect usage
