b9eea07b22f9b9da6aee36159b71879f8350ef6a
Matt Gardner <mattg@allenai.org>
2017-08-02 14:22:45 -0700

    Implemented decomposable attention model (#67)
    * Implemented decomposable attention model
    
    * Moving SNLI data to fixtures, implemented inference
    
    * Fixing bug in BiDAF._get_best_span
    
    * Added tests for DecomposableAttention, made them pass
    
    * Fix pylint errors
    
    * Making bidaf training test smaller, to see if that speeds up CI
    
    * Fixes from Mark

--- tests/models/bidaf_test.py
+++ tests/models/bidaf_test.py
@@ -38,12 +38,67 @@ class BidirectionalAttentionFlowTest(AllenNlpTestCase):
                                'token_characters': TokenCharactersIndexer()}
 
         self.model = BidirectionalAttentionFlow.from_params(self.vocab, Params({}))
+
+        small_params = Params({
+                'text_field_embedder': {
+                        'tokens': {
+                                'type': 'embedding',
+                                'pretrained_file': constants.GLOVE_PATH,
+                                'trainable': False,
+                                'projection_dim': 4
+                                },
+                        'token_characters': {
+                                'type': 'character_encoding',
+                                'embedding': {
+                                        'embedding_dim': 8
+                                        },
+                                'encoder': {
+                                        'type': 'cnn',
+                                        'embedding_dim': 8,
+                                        'num_filters': 4,
+                                        'ngram_filter_sizes': [5]
+                                        }
+                                }
+                        },
+                'phrase_layer': {
+                        'type': 'lstm',
+                        'bidirectional': True,
+                        'input_size': 8,
+                        'hidden_size': 4,
+                        'num_layers': 1,
+                        },
+                'similarity_function': {
+                        'type': 'linear',
+                        'combination': 'x,y,x*y',
+                        'tensor_1_dim': 8,
+                        'tensor_2_dim': 8
+                        },
+                'modeling_layer': {
+                        'type': 'lstm',
+                        'bidirectional': True,
+                        'input_size': 32,
+                        'hidden_size': 4,
+                        'num_layers': 1,
+                        },
+                'span_end_encoder': {
+                        'type': 'lstm',
+                        'bidirectional': True,
+                        'input_size': 56,
+                        'hidden_size': 4,
+                        'num_layers': 1,
+                        },
+
+                })
+        self.small_model = BidirectionalAttentionFlow.from_params(self.vocab, small_params)
         initializer = InitializerApplicator()
         initializer(self.model)
 
     def test_forward_pass_runs_correctly(self):
         training_arrays = arrays_to_variables(self.dataset.as_arrays())
         _ = self.model.forward(**training_arrays)
+
+    def test_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.small_model, self.dataset)
 
     def test_predict_span_gives_reasonable_outputs(self):
         # TODO(mattg): "What", "is", "?" crashed, because the CNN encoder expected at least 5
@@ -75,6 +130,13 @@ class BidirectionalAttentionFlowTest(AllenNlpTestCase):
         # best previous span position.  We should not get (1, 1), because that's an empty span.
         span_begin_probs = torch.FloatTensor([0.4, 0.5, 0.1])
         span_end_probs = torch.FloatTensor([0.3, 0.6, 0.1])
+        begin_end_idxs = BidirectionalAttentionFlow._get_best_span(span_begin_probs, span_end_probs)
+        assert begin_end_idxs == (0, 1)
+
+        # Testing another edge case of the dynamic program here, where (0, 0) is the best solution
+        # without constraints.
+        span_begin_probs = torch.FloatTensor([0.8, 0.1, 0.1])
+        span_end_probs = torch.FloatTensor([0.8, 0.1, 0.1])
         begin_end_idxs = BidirectionalAttentionFlow._get_best_span(span_begin_probs, span_end_probs)
         assert begin_end_idxs == (0, 1)
 
