a4712685440e3ddd3fc7d18a6536e3ab00e8b99e
Mark Neumann <markn@allenai.org>
2017-08-04 14:15:43 -0700

    use incorrect american spelling of labeller

--- allennlp/data/dataset_readers/__init__.py
+++ allennlp/data/dataset_readers/__init__.py
@@ -3,4 +3,4 @@ from allennlp.data.dataset_readers.language_modeling import LanguageModelingRead
 from allennlp.data.dataset_readers.sequence_tagging import SequenceTaggingDatasetReader
 from allennlp.data.dataset_readers.snli import SnliReader
 from allennlp.data.dataset_readers.squad import SquadReader, SquadSentenceSelectionReader
-from allennlp.data.dataset_readers.semantic_role_labelling import SrlReader
+from allennlp.data.dataset_readers.semantic_role_labeling import SrlReader
