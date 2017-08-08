1df07c3d0d7e191d89964a6b2ecbaab98cf5ef53
Joel Grus <joelgrus@gmail.com>
2017-08-08 11:04:27 -0700

    pr comments

--- allennlp/service/servable/models/bidaf.py
+++ allennlp/service/servable/models/bidaf.py
@@ -11,36 +11,26 @@ class BidafServable(Servable):
     def __init__(self):
         constants.GLOVE_PATH = 'tests/fixtures/glove.6B.100d.sample.txt.gz'
 
-        reader_params = Params({
-                'token_indexers': {
-                        'tokens': {
-                                'type': 'single_id'
-                                },
-                        'token_characters': {
-                                'type': 'characters'
-                                }
-                        }
-                })
-        dataset = SquadReader.from_params(reader_params).read('tests/fixtures/squad_example.json')
+        self.token_indexers = {'tokens': SingleIdTokenIndexer(lowercase_tokens=True),
+                               'token_characters': TokenCharactersIndexer()}
+
+        self.tokenizer = WordTokenizer()
+
+        dataset = SquadReader(token_indexers=self.token_indexers).read('tests/fixtures/squad_example.json')
         vocab = Vocabulary.from_dataset(dataset)
         self.vocab = vocab
-        print("vocab size", vocab.get_vocab_size())
         dataset.index_instances(vocab)
         self.dataset = dataset
-        self.token_indexers = {'tokens': SingleIdTokenIndexer(),
-                               'token_characters': TokenCharactersIndexer()}
 
         self.model = BidirectionalAttentionFlow.from_params(self.vocab, Params({}))
 
     def predict_json(self, inputs: JSONDict) -> JSONDict:
-        tokenizer = WordTokenizer()
-
         question_text = inputs["question"]
         passage_text = inputs["passage"]
 
 
-        question = TextField(tokenizer.tokenize(question_text), token_indexers=self.token_indexers)
-        passage = TextField(tokenizer.tokenize(passage_text) + [SquadReader.STOP_TOKEN],
+        question = TextField(self.tokenizer.tokenize(question_text), token_indexers=self.token_indexers)
+        passage = TextField(self.tokenizer.tokenize(passage_text) + [SquadReader.STOP_TOKEN],
                             token_indexers=self.token_indexers)
 
         output_dict = self.model.predict_span(question, passage)
