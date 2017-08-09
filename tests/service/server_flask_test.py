56ae5f1d069a09729aa3cb76689c3f69380565f5
Joel Grus <joelgrus@gmail.com>
2017-08-08 17:14:31 -0700

    refactor tests

--- tests/service/server_flask_test.py
+++ tests/service/server_flask_test.py
@@ -5,26 +5,24 @@ from allennlp.service.server_flask import make_app
 from allennlp.service.servable import ServableCollection
 from allennlp.common.testing import AllenNlpTestCase
 
-# Add default models to app
-app = make_app()
-app.servables = ServableCollection.default()
-
 class TestApp(AllenNlpTestCase):
+
+    def setUp(self):
+        super().setUp()
+        app = make_app()
+        app.testing = True
+        app.servables = ServableCollection.default()
+        self.client = app.test_client()
 
     # TODO(joelgrus): this is a fragile test
     def test_list_models(self):
-        app.testing = True
-        client = app.test_client()
-
-        models = client.get("models")
+        models = self.client.get("models")
         data = json.loads(models.get_data().decode('utf-8'))
         assert "bidaf" in set(data["models"])
 
     def test_unknown_model(self):
-        app.testing = True
-        client = app.test_client()
-        prediction = client.post("predict/bogus_model",
-                                 data="""{"input": "broken"}""",
-                                 content_type='application/json')
+        prediction = self.client.post("predict/bogus_model",
+                                      data="""{"input": "broken"}""",
+                                      content_type='application/json')
         assert prediction.status_code == 400
         assert b"unknown model" in prediction.get_data()
--- tests/service/server_sanic_test.py
+++ tests/service/server_sanic_test.py
@@ -5,24 +5,23 @@ from allennlp.service.server_sanic import make_app
 from allennlp.service.servable import ServableCollection
 from allennlp.common.testing import AllenNlpTestCase
 
-app = make_app()
-# Add default models to app
-app.servables = ServableCollection.default()
 
 class TestApp(AllenNlpTestCase):
 
-    def test_list_models(self):
+    def setUp(self):
+        super().setUp()
+        app = make_app()
+        app.servables = ServableCollection.default()
         app.testing = True
-        client = app.test_client
+        self.client = app.test_client
 
-        _, response = client.get("/models")
+    def test_list_models(self):
+        _, response = self.client.get("/models")
         data = json.loads(response.text)
         assert "bidaf" in set(data["models"])
 
     def test_unknown_model(self):
-        app.testing = True
-        client = app.test_client
-        _, response = client.post("/predict/bogus_model",
-                                  json={"input": "broken"})
+        _, response = self.client.post("/predict/bogus_model",
+                                       json={"input": "broken"})
         assert response.status == 400
         assert "unknown model" in response.text and "bogus_model" in response.text
