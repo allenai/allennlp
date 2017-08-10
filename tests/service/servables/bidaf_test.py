050504f0f1ee8b96adb4fddce468bd309ac8b746
Joel Grus <joelgrus@gmail.com>
2017-08-10 10:07:06 -0700

    refactor `Servable` model wrappers to instantiate `from_params` instead of using default values (#112)
    * servables from files
    
    * instantiate servables from params
    
    * fix bidaf servable
    
    * fix up srl
    
    * decomposable attention
    
    * pylint fixes
    
    * use specific reader types to eliminate mypy ignores
    
    * put all servable models in eval() mode
    
    * remove unused import

--- tests/service/servables/bidaf_test.py
+++ tests/service/servables/bidaf_test.py
@@ -1,7 +1,9 @@
 # pylint: disable=no-self-use,invalid-name
-
+import json
 from unittest import TestCase
 
+from allennlp.common import Params
+from allennlp.common.params import replace_none
 from allennlp.service.servable.models.bidaf import BidafServable
 
 
@@ -12,7 +14,14 @@ class TestBidafServable(TestCase):
                 "passage": "One time I was writing a unit test, and it succeeded on the first attempt."
         }
 
-        model = BidafServable()
+        with open('experiment_config/bidaf.json') as f:
+            config = json.loads(f.read())
+            config['serialization_prefix'] = 'tests/fixtures/bidaf'
+            config['model']['text_field_embedder']['tokens']['pretrained_file'] = \
+                'tests/fixtures/glove.6B.100d.sample.txt.gz'
+            bidaf_config = Params(replace_none(config))
+
+        model = BidafServable.from_config(bidaf_config)
 
         result = model.predict_json(inputs)
 
