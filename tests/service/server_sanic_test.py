ba876349bdd807d1cda302c6454b5c3cac8b0d44
Joel Grus <joelgrus@gmail.com>
2017-08-11 14:22:11 -0700

    fix int64 serialization + add tests

--- tests/service/server_sanic_test.py
+++ tests/service/server_sanic_test.py
@@ -25,3 +25,26 @@ class TestApp(AllenNlpTestCase):
                                        json={"input": "broken"})
         assert response.status == 400
         assert "unknown model" in response.text and "bogus_model" in response.text
+
+    def test_bidaf(self):
+        _, response = self.client.post("/predict/bidaf",
+                                       json={"passage": "the super bowl was played in seattle",
+                                             "question": "where was the super bowl played?"})
+        assert response.status == 200
+        results = json.loads(response.text)
+        assert "best_span" in results
+
+    def test_snli(self):
+        _, response = self.client.post("/predict/snli",
+                                       json={"premise": "the super bowl was played in seattle",
+                                             "hypothesis": "the super bowl was played in ohio"})
+        assert response.status == 200
+        results = json.loads(response.text)
+        assert "label_probs" in results
+
+    def test_srl(self):
+        _, response = self.client.post("/predict/srl",
+                                       json={"sentence": "the super bowl was played in seattle"})
+        assert response.status == 200
+        results = json.loads(response.text)
+        assert "verbs" in results
