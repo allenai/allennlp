707266674849ec1acc58610e5e1ff447f6731dbc
Joel Grus <joelgrus@gmail.com>
2017-08-22 14:02:49 -0700

    add comments

--- allennlp/common/file_utils.py
+++ allennlp/common/file_utils.py
@@ -12,32 +12,42 @@ CACHE_ROOT = os.path.expanduser(os.path.join('~', '.allennlp'))
 DATASET_CACHE = os.path.join(CACHE_ROOT, "datasets")
 
 def url_to_filename(url: str) -> str:
+    """
+    Converts a url into a filename in a reversible way.
+    """
     url_bytes = url.encode('utf-8')
     b64_bytes = base64.b64encode(url_bytes)
     return b64_bytes.decode('utf-8')
 
 def filename_to_url(filename: str) -> str:
+    """
+    Recovers the the url from the encoded filename.
+    """
     filename_bytes = filename.encode('utf-8')
     url_bytes = base64.b64decode(filename_bytes)
     return url_bytes.decode('utf-8')
 
-def cached_path(maybe_uri: str, cache_dir: str = DATASET_CACHE) -> str:
+def cached_path(url_or_filename: str, cache_dir: str = DATASET_CACHE) -> str:
     """
-    Given something that might be a URI (or might be a local path),
-    determine which. If it's a URI, download the file and cache it, and
+    Given something that might be a URL (or might be a local path),
+    determine which. If it's a URL, download the file and cache it, and
     return the path to the cached file. If it's already a local path,
-    make sure it exists and then return it.
+    make sure the file exists and then return the path.
     """
-    parsed = urlparse(maybe_uri)
+    parsed = urlparse(url_or_filename)
 
     if parsed.scheme in ('http', 'https'):
-        return get_from_cache(maybe_uri, cache_dir)
-    elif parsed.scheme == '' and os.path.exists(maybe_uri):
-        return maybe_uri
+        # URL, so get it from the cache (downloading if necessary)
+        return get_from_cache(url_or_filename, cache_dir)
+    elif parsed.scheme == '' and os.path.exists(url_or_filename):
+        # File, and it exists.
+        return url_or_filename
     elif parsed.scheme == '':
-        raise FileNotFoundError("file {} not found".format(maybe_uri))
+        # File, but it doesn't exist.
+        raise FileNotFoundError("file {} not found".format(url_or_filename))
     else:
-        raise ValueError("unable to parse {} as a URL or as a local path".format(maybe_uri))
+        # Something unknown
+        raise ValueError("unable to parse {} as a URL or as a local path".format(url_or_filename))
 
 
 # TODO(joelgrus): do we want to do checksums or anything like that?
