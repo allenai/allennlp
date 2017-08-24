31561b5d6950330bf3b335ec9e45302c2cc8ae5c
Matt Gardner <mattg@allenai.org>
2017-08-24 22:26:08 +0000

    Make mypy happy

--- allennlp/data/tokenizers/word_splitter.py
+++ allennlp/data/tokenizers/word_splitter.py
@@ -136,4 +136,4 @@ class SpacyWordSplitter(WordSplitter):
 
     @overrides
     def split_words(self, sentence: str) -> List[str]:
-        return [str(token) for token in self.en_nlp.tokenizer(sentence)]
+        return [str(token) for token in self.en_nlp.tokenizer(sentence)]  # type: ignore
