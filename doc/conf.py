d26ed4e3ff6487ab9309bcd8b60f75f1c9717939
Mark Neumann <markn@allenai.org>
2017-08-30 15:13:29 -0700

    try adding logo to docs

--- doc/conf.py
+++ doc/conf.py
@@ -109,6 +109,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['static']
+html_logo = 'static/allennlp-logo-dark.svg'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
