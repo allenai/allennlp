ebc7c07a1807c97bedad4725960f1bd1a8170ef1
Joel Grus <joelgrus@gmail.com>
2017-09-01 10:53:28 -0700

    add caching to sanic app

--- allennlp/service/server_sanic.py
+++ allennlp/service/server_sanic.py
@@ -6,11 +6,19 @@ Usually you would use :mod:`~allennlp.commands.serve`
 rather than instantiating an ``app`` yourself.
 """
 from typing import Dict
+import asyncio
+import json
+import logging
+from functools import lru_cache
+
 from sanic import Sanic, response, request
 from sanic.exceptions import ServerError
 
+from allennlp.common.util import JsonDict
 from allennlp.models.archival import load_archive
 from allennlp.service.predictors import Predictor
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 def run(port: int, workers: int, config: Dict[str, str]) -> None:
     """Run the server programatically"""
@@ -29,6 +37,13 @@ def make_app() -> Sanic:
     app.static('/', './allennlp/service/static/index.html')
     app.predictors = {}
 
+    @lru_cache(maxsize=128)
+    def _caching_prediction(model: Predictor, data: str) -> JsonDict:
+        """
+        Just a wrapper around ``model.predict_json`` that allows us to use a cache decorator.
+        """
+        return model.predict_json(json.loads(data))
+
     @app.route('/predict/<model_name>', methods=['POST'])
     async def predict(req: request.Request, model_name: str) -> response.HTTPResponse:  # pylint: disable=unused-variable
         """make a prediction using the specified model and return the results"""
@@ -38,10 +53,22 @@ def make_app() -> Sanic:
 
         data = req.json
 
+        # See if we hit or not. In theory this could result in false positives.
+        pre_hits = _caching_prediction.cache_info().hits  # pylint: disable=no-value-for-parameter
+
         try:
-            prediction = model.predict_json(data)
+            # lru_cache insists that all function arguments be hashable,
+            # so unfortunately we have to stringify the data.
+            prediction = _caching_prediction(model, json.dumps(data))
         except KeyError as err:
             raise ServerError("Required JSON field not found: " + err.args[0], status_code=400)
+
+        post_hits = _caching_prediction.cache_info().hits  # pylint: disable=no-value-for-parameter
+
+        if post_hits > pre_hits:
+            # Cache hit, so insert an artifical pause
+            logger.info("cache hit: %s %s", model_name, json.dumps(data))
+            await asyncio.sleep(0.25)
 
         return response.json(prediction)
 
