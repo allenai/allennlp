1cc4da2b0bbf19608f0a34651b9e13d4617d31a7
Joel Grus <joelgrus@gmail.com>
2017-09-05 15:10:57 -0700

    fix line break

--- allennlp/modules/text_field_embedders/__init__.py
+++ allennlp/modules/text_field_embedders/__init__.py
@@ -1,7 +1,6 @@
 """
 A :class:`~allennlp.modules.text_field_embedders.text_field_embedder.TextFieldEmbedder`
-is a ``Module`` that takes as input the
-``dict`` of NumPy arrays
+is a ``Module`` that takes as input the ``dict`` of NumPy arrays
 produced by a :class:`~allennlp.data.fields.text_field.TextField` and
 returns as output an embedded representation of the tokens in that field.
 """
