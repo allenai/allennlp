a360d42ec83d2e382b868c83a911051c28a98888
Mark Neumann <markn@allenai.org>
2017-09-14 09:40:11 -0700

    refactor SRU so we can read it

--- allennlp/modules/sru.py
+++ allennlp/modules/sru.py
@@ -1,7 +1,7 @@
 
 import torch
 import torch.nn as nn
-from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence, PackedSequence
 from torch.autograd import Function, Variable
 from cupy.cuda import function
 from pynvrtc.compiler import Program
@@ -313,47 +313,47 @@ SRU_BiBWD_FUNC = SRU_MOD.get_function('sru_bi_bwd')
 Stream = namedtuple('Stream', ['ptr'])
 SRU_STREAM = Stream(ptr=torch.cuda.current_stream().cuda_stream)
 
-class SRU_Compute(Function):
 
-    def __init__(self, use_tanh, d_out, bidirectional=False):
-        super(SRU_Compute, self).__init__()
+class _SruFunction(Function):
+
+    def __init__(self, use_tanh, hidden_size, bidirectional=False):
+        super(_SruFunction, self).__init__()
         self.use_tanh = use_tanh
-        self.d_out = d_out
+        self.hidden_size = hidden_size
         self.bidirectional = bidirectional
 
     def forward(self, u, x, bias, init=None, mask_h=None):
-        bidir = 2 if self.bidirectional else 1
+        output_size = 2 * self.hidden_size if self.bidirectional else self.hidden_size
         length = x.size(0) if x.dim() == 3 else 1
         batch = x.size(-2)
-        d = self.d_out
-        k = u.size(-1) // d
-        k_ = k//2 if self.bidirectional else k
-        ncols = batch*d*bidir
-        thread_per_block = min(512, ncols)
-        num_block = (ncols-1)/thread_per_block+1
+        k = u.size(-1) // self.hidden_size
+        k_ = k // 2 if self.bidirectional else k
+        ncols = batch * output_size
+        threads_per_block = min(512, ncols)
+        num_blocks = (ncols - 1) / threads_per_block + 1
 
         init_ = x.new(ncols).zero_() if init is None else init
-        size = (length, batch, d*bidir) if x.dim() == 3 else (batch, d*bidir)
+        size = (length, batch, output_size) if x.dim() == 3 else (batch, output_size)
         c = x.new(*size)
         h = x.new(*size)
 
-        FUNC = SRU_FWD_FUNC if not self.bidirectional else SRU_BiFWD_FUNC
-        FUNC(args=[
-            u.contiguous().data_ptr(),
-            x.contiguous().data_ptr() if k_ == 3 else 0,
-            bias.data_ptr(),
-            init_.contiguous().data_ptr(),
-            mask_h.data_ptr() if mask_h is not None else 0,
-            length,
-            batch,
-            d,
-            k_,
-            h.data_ptr(),
-            c.data_ptr(),
-            self.use_tanh],
-            block = (thread_per_block,1,1), grid = (num_block,1,1),
-            stream=SRU_STREAM
-        )
+        kernel = SRU_FWD_FUNC if not self.bidirectional else SRU_BiFWD_FUNC
+        kernel_args = [u.contiguous().data_ptr(),
+                       x.contiguous().data_ptr() if k_ == 3 else 0,
+                       bias.data_ptr(),
+                       init_.contiguous().data_ptr(),
+                       mask_h.data_ptr() if mask_h is not None else 0,
+                       length,
+                       batch,
+                       self.hidden_size,
+                       k_,
+                       h.data_ptr(),
+                       c.data_ptr(),
+                       self.use_tanh]
+        kernel(kernel_args,
+               block=(threads_per_block, 1, 1),
+               grid=(num_blocks, 1, 1),
+               stream=SRU_STREAM)
 
         self.save_for_backward(u, x, bias, init, mask_h)
         self.intermediate = c
@@ -366,112 +366,107 @@ class SRU_Compute(Function):
         return h, last_hidden
 
     def backward(self, grad_h, grad_last):
-        bidir = 2 if self.bidirectional else 1
+        output_size = 2 * self.hidden_size if self.bidirectional else self.hidden_size
         u, x, bias, init, mask_h = self.saved_tensors
         c = self.intermediate
         length = x.size(0) if x.dim() == 3 else 1
         batch = x.size(-2)
-        d = self.d_out
-        k = u.size(-1) // d
-        k_ = k//2 if self.bidirectional else k
-        ncols = batch*d*bidir
+        k = u.size(-1) // self.hidden_size
+        k_ = k // 2 if self.bidirectional else k
+        ncols = batch * output_size
         thread_per_block = min(512, ncols)
-        num_block = (ncols-1)/thread_per_block+1
+        num_block = (ncols - 1) / thread_per_block + 1
 
         init_ = x.new(ncols).zero_() if init is None else init
         grad_u = u.new(*u.size())
-        grad_bias = x.new(2, batch, d*bidir)
-        grad_init = x.new(batch, d*bidir)
+        grad_bias = x.new(2, batch, output_size)
+        grad_init = x.new(batch, output_size)
 
         # Normal use
         grad_x = x.new(*x.size()) if k_ == 3 else None
+        backward_kernel = SRU_BWD_FUNC if not self.bidirectional else SRU_BiBWD_FUNC
 
-        FUNC = SRU_BWD_FUNC if not self.bidirectional else SRU_BiBWD_FUNC
-        FUNC(args=[
-            u.contiguous().data_ptr(),
-            x.contiguous().data_ptr() if k_ == 3 else 0,
-            bias.data_ptr(),
-            init_.contiguous().data_ptr(),
-            mask_h.data_ptr() if mask_h is not None else 0,
-            c.data_ptr(),
-            grad_h.contiguous().data_ptr(),
-            grad_last.contiguous().data_ptr(),
-            length,
-            batch,
-            d,
-            k_,
-            grad_u.data_ptr(),
-            grad_x.data_ptr() if k_ == 3 else 0,
-            grad_bias.data_ptr(),
-            grad_init.data_ptr(),
-            self.use_tanh],
-            block = (thread_per_block,1,1), grid = (num_block,1,1),
-            stream=SRU_STREAM
-        )
+        kernel_args = [u.contiguous().data_ptr(),
+                       x.contiguous().data_ptr() if k_ == 3 else 0,
+                       bias.data_ptr(),
+                       init_.contiguous().data_ptr(),
+                       mask_h.data_ptr() if mask_h is not None else 0,
+                       c.data_ptr(),
+                       grad_h.contiguous().data_ptr(),
+                       grad_last.contiguous().data_ptr(),
+                       length,
+                       batch,
+                       self.hidden_size,
+                       k_,
+                       grad_u.data_ptr(),
+                       grad_x.data_ptr() if k_ == 3 else 0,
+                       grad_bias.data_ptr(),
+                       grad_init.data_ptr(),
+                       self.use_tanh]
+
+        backward_kernel(kernel_args,
+                        block=(thread_per_block, 1, 1),
+                        grid=(num_block, 1, 1),
+                        stream=SRU_STREAM)
         return grad_u, grad_x, grad_bias.sum(1).view(-1), grad_init, None
 
 
 class SRUCell(nn.Module):
-    def __init__(self, n_in, n_out, dropout=0, rnn_dropout=0,
-                use_tanh=1, bidirectional=False):
+    def __init__(self,
+                 input_size: int,
+                 hidden_size: int,
+                 dropout: float = 0,
+                 recurrent_dropout_probability: float = 0,
+                 use_tanh=1,
+                 bidirectional: bool = False):
         super(SRUCell, self).__init__()
-        self.n_in = n_in
-        self.n_out = n_out
-        self.rnn_dropout = rnn_dropout
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.recurrent_dropout_probability = recurrent_dropout_probability
         self.dropout = dropout
         self.bidirectional = bidirectional
         self.use_tanh = use_tanh
 
-        out_size = n_out*2 if bidirectional else n_out
-        k = 4 if n_in != out_size else 3
-        self.size_per_dir = n_out*k
-        self.weight = nn.Parameter(torch.Tensor(
-            n_in,
-            self.size_per_dir*2 if bidirectional else self.size_per_dir
-        ))
-        self.bias = nn.Parameter(torch.Tensor(
-            n_out*4 if bidirectional else n_out*2
-        ))
-        self.init_weight()
+        output_size = hidden_size * 2 if bidirectional else hidden_size
+        k = 4 if input_size != output_size else 3
+        size_per_dir = hidden_size * k
+        self.weight = nn.Parameter(torch.Tensor(input_size, size_per_dir * 2 if bidirectional else size_per_dir))
+        self.bias = nn.Parameter(torch.Tensor(hidden_size * 4 if bidirectional else hidden_size * 2))
+        self.reset_parameters()
 
-    def init_weight(self):
-        val_range = (3.0/self.n_in)**0.5
+    def reset_parameters(self):
+        val_range = (3.0 / self.input_size) ** 0.5
         self.weight.data.uniform_(-val_range, val_range)
         self.bias.data.zero_()
 
-    def set_bias(self, bias_val=0):
-        n_out = self.n_out
         if self.bidirectional:
-            self.bias.data[n_out*2:].zero_().add_(bias_val)
+            self.bias.data[self.hidden_size * 2:].zero_().fill_(1.0)
         else:
-            self.bias.data[n_out:].zero_().add_(bias_val)
+            self.bias.data[self.hidden_size:].zero_().fill_(1.0)
 
-    def forward(self, input, c0=None):
-        assert input.dim() == 2 or input.dim() == 3
-        n_in, n_out = self.n_in, self.n_out
-        batch = input.size(-2)
-        if c0 is None:
-            c0 = Variable(input.data.new(
-                batch, n_out if not self.bidirectional else n_out*2
-            ).zero_())
+    def forward(self, inputs, initial_state=None):
+        assert inputs.dim() == 2 or inputs.dim() == 3
+        batch = inputs.size(-2)
+        state_size = self.hidden_size if not self.bidirectional else self.hidden_size * 2
+        if initial_state is None:
+            initial_state = Variable(inputs.data.new(batch, state_size).zero_())
 
-        if self.training and (self.rnn_dropout>0):
-            mask = self.get_dropout_mask_((batch, n_in), self.rnn_dropout)
-            x = input * mask.expand_as(input)
+        if self.training and (self.recurrent_dropout_probability > 0):
+            dropout_mask = self.get_dropout_mask_((batch, self.input_size), self.recurrent_dropout_probability)
+            inputs = inputs * dropout_mask.expand_as(inputs)
+
+        two_dim_inputs = inputs if inputs.dim() == 2 else inputs.contiguous().view(-1, self.input_size)
+        projected_inputs = two_dim_inputs.mm(self.weight)
+
+        if self.training and (self.dropout > 0):
+            mask_h = self.get_dropout_mask_((batch, state_size), self.dropout)
+            cell_function = _SruFunction(self.use_tanh, self.hidden_size, self.bidirectional)
+            outputs, state = cell_function(projected_inputs, inputs, self.bias, initial_state, mask_h)
         else:
-            x = input
+            cell_function = _SruFunction(self.use_tanh, self.hidden_size, self.bidirectional)
+            outputs, state = cell_function(projected_inputs, inputs, self.bias, initial_state)
 
-        x_2d = x if x.dim() == 2 else x.contiguous().view(-1, n_in)
-        u = x_2d.mm(self.weight)
-
-        if self.training and (self.dropout>0):
-            bidir = 2 if self.bidirectional else 1
-            mask_h = self.get_dropout_mask_((batch, n_out*bidir), self.dropout)
-            h, c = SRU_Compute(self.use_tanh, n_out, self.bidirectional)(u, input, self.bias, c0, mask_h)
-        else:
-            h, c = SRU_Compute(self.use_tanh, n_out, self.bidirectional)(u, input, self.bias, c0)
-
-        return h, c
+        return outputs, state
 
     def get_dropout_mask_(self, size, p):
         w = self.weight.data
@@ -479,56 +474,86 @@ class SRUCell(nn.Module):
 
 
 class SRU(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers=2, dropout=0, rnn_dropout=0,
-                use_tanh=1, bidirectional=False):
+    """
+    Parameters
+    ----------
+    input_size : int, required
+        The dimension of the inputs to the SRU.
+    hidden_size : int, required
+        The dimension of the outputs of the SRU.
+    num_layers : int, required
+        The number of stacked SRUs to use.
+    recurrent_dropout_probability: float, optional (default = 0.0)
+        The dropout probability to be used in a dropout scheme as stated in
+        `A Theoretically Grounded Application of Dropout in Recurrent Neural Networks
+        <https://arxiv.org/abs/1512.05287>`_ .
+    """
+    def __init__(self,
+                 input_size: int,
+                 hidden_size: int,
+                 num_layers: int = 2,
+                 dropout: float = 0.0,
+                 recurrent_dropout_probability: float = 0.0,
+                 use_tanh=1,
+                 bidirectional: bool = False):
         super(SRU, self).__init__()
-        self.n_in = input_size
-        self.n_out = hidden_size
-        self.depth = num_layers
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
         self.dropout = dropout
-        self.rnn_dropout = rnn_dropout
-        self.rnn_lst = nn.ModuleList()
+        self.recurrent_dropout_probability = recurrent_dropout_probability
+        self.sru_layers = nn.ModuleList()
         self.bidirectional = bidirectional
         self.out_size = hidden_size*2 if bidirectional else hidden_size
 
         for i in range(num_layers):
-            l = SRUCell(
-                n_in=self.n_in if i==0 else self.out_size,
-                n_out=self.n_out,
-                dropout = dropout if i+1 != num_layers else 0,
-                rnn_dropout = rnn_dropout,
-                use_tanh = use_tanh,
-                bidirectional = bidirectional
-            )
-            self.rnn_lst.append(l)
+            layer = SRUCell(input_size=self.input_size if i == 0 else self.out_size,
+                            hidden_size=self.hidden_size,
+                            dropout=dropout if i+1 != num_layers else 0,
+                            recurrent_dropout_probability=self.recurrent_dropout_probability,
+                            use_tanh=use_tanh,
+                            bidirectional=bidirectional)
+            self.sru_layers.append(layer)
 
-    def set_bias(self, bias_val=0):
-        for l in self.rnn_lst:
-            l.set_bias(bias_val)
+    def forward(self,
+                inputs: PackedSequence,
+                initial_state: torch.Tensor = None):
+        """
+        Parameters
+        ----------
+        inputs : ``PackedSequence``, required.
+            A batch first ``PackedSequence`` to run the stacked LSTM over.
+        initial_state : torch.Tensor, optional, (default = None)
+            A tensor representing the initial hidden state and memory
+            of the SRU, with shape (num_layers, batch_size, output_dimension).
 
-    def forward(self, input, c0=None):
+        Returns
+        -------
+        output_sequence : PackedSequence
+            The encoded sequence of shape (batch_size, sequence_length, hidden_size)
+        final_states: torch.Tensor
+            The per-layer final (state, memory) states of the LSTM, each with shape
+            (num_layers, batch_size, hidden_size).
+        """
+        inputs, lengths = pad_packed_sequence(inputs, batch_first=True)
 
-        input, lengths = pad_packed_sequence(input, batch_first=True)
-
-        input.transpose(0, 1)
-        assert input.dim() == 3 # (len, batch, n_in)
-        dir_ = 2 if self.bidirectional else 1
-        if c0 is None:
-            zeros = Variable(input.data.new(
-                input.size(1), self.n_out*dir_
-            ).zero_())
-            c0 = [zeros for _ in range(self.depth)]
+        # Kernel uses sequence_length as the first dimension, so we transpose here.
+        inputs.transpose(0, 1)
+        direction = 2 if self.bidirectional else 1
+        if initial_state is None:
+            zeros = Variable(inputs.data.new(inputs.size(1), self.hidden_size * direction).zero_())
+            initial_state = [zeros for _ in range(self.num_layers)]
         else:
-            assert c0.dim() == 3    # (depth, batch, n_out*dir_)
-            c0 = c0.chunk(self.depth, 0)
+            initial_state = initial_state.chunk(self.num_layers, 0)
 
-        prevx = input
-        lstc = []
-        for i, rnn in enumerate(self.rnn_lst):
-            h, c = rnn(prevx, c0[i])
-            prevx = h
-            lstc.append(c)
+        layer_output = inputs
+        final_states = []
+        for i, layer in enumerate(self.sru_layers):
+            output, state = layer(layer_output, initial_state[i])
+            layer_output = output
+            final_states.append(state)
 
-        prevx = pack_padded_sequence(prevx.transpose(0, 1), lengths)
-
-        return prevx, torch.stack(lstc)
+        # Returned output from kernel is sequence dimension first,
+        # so we transpose back to a batch first tensor.
+        layer_output = pack_padded_sequence(layer_output.transpose(0, 1), lengths)
+        return layer_output, torch.stack(final_states)
