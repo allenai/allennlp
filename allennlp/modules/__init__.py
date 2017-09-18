0b08910cd604d0fe88cc1e0ecc6e9ef6c2741b16
Matt Gardner <mattg@allenai.org>
2017-09-18 17:10:29 +0000

    Add MultiHeadedSimilarity
    This is a wrapper around any similarity function that projects and
    splits each tensor into multiple heads before computing the similarity.
    
    I also moved `similarity_function.py` to be inside the
    `similarity_functions` module.

--- allennlp/modules/__init__.py
+++ allennlp/modules/__init__.py
@@ -11,7 +11,7 @@ from allennlp.modules.highway import Highway
 from allennlp.modules.matrix_attention import MatrixAttention
 from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder
 from allennlp.modules.seq2vec_encoders import Seq2VecEncoder
-from allennlp.modules.similarity_function import SimilarityFunction
+from allennlp.modules.similarity_functions import SimilarityFunction
 from allennlp.modules.text_field_embedders import TextFieldEmbedder
 from allennlp.modules.time_distributed import TimeDistributed
 from allennlp.modules.token_embedders import TokenEmbedder
