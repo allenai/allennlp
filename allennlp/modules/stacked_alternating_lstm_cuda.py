87166a6f46c9e18a38250d2cea5e1ba8baad1cbd
Mark Neumann <markn@allenai.org>
2017-09-18 09:30:56 -0700

    better comments

--- allennlp/modules/stacked_alternating_lstm_cuda.py
+++ allennlp/modules/stacked_alternating_lstm_cuda.py
@@ -8,54 +8,62 @@ from allennlp.custom_extensions._ext import highway_lstm_layer
 
 
 class _HighwayLSTMFunction(NestedIOFunction):
-    def __init__(self, input_size: int,
-                 hidden_size: int,
-                 num_layers: int = 1,
-                 train: bool = True):
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int, train: bool):
         super(_HighwayLSTMFunction, self).__init__()
-
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.train = train
 
-    def forward_extended(self, input, weight, bias, hy, cy, dropout, lengths, gates):
-        sequence_length, batch_size, input_size = input.size()
-        tmp_i = input.new(batch_size, 6 * self.hidden_size)
-        tmp_h = input.new(batch_size, 5 * self.hidden_size)
-
+    def forward_extended(self,
+                         inputs: torch.Tensor,
+                         weight: torch.Tensor,
+                         bias: torch.Tensor,
+                         state_accumulator: torch.Tensor,
+                         memory_accumulator: torch.Tensor,
+                         dropout_mask: torch.Tensor,
+                         lengths: torch.Tensor,
+                         gates: torch.Tensor):
+        sequence_length, batch_size, input_size = inputs.size()
+        tmp_i = inputs.new(batch_size, 6 * self.hidden_size)
+        tmp_h = inputs.new(batch_size, 5 * self.hidden_size)
+        is_training = 1 if self.train else 0
         highway_lstm_layer.highway_lstm_forward_cuda(input_size,
                                                      self.hidden_size,
                                                      batch_size,
                                                      self.num_layers,
                                                      sequence_length,
-                                                     input,
+                                                     inputs,
                                                      lengths,
-                                                     hy,
-                                                     cy,
+                                                     state_accumulator,
+                                                     memory_accumulator,
                                                      tmp_i,
                                                      tmp_h,
                                                      weight,
                                                      bias,
-                                                     dropout,
+                                                     dropout_mask,
                                                      gates,
-                                                     1 if self.train else 0)
+                                                     is_training)
 
-        self.save_for_backward(input, lengths, weight, bias, hy, cy, dropout, gates)
+        self.save_for_backward(inputs, lengths, weight, bias, state_accumulator,
+                               memory_accumulator, dropout_mask, gates)
 
-        output = hy[-1, 1:]
-        return output, hy[:, 1:]
+        # The state_accumulator has shape: (num_layers, sequence_length + 1, batch_size, hidden_size)
+        # so for the output, we want the last layer and all but the first timestep, which was the
+        # initial state.
+        output = state_accumulator[-1, 1:, :, :]
+        return output, state_accumulator[:, 1:, :, :]
 
     def backward(self, grad_output, grad_hy):
-        input, lengths, weight, bias, hy, cy, dropout, gates = self.saved_tensors
-        input = input.contiguous()
-        sequence_length, batch_size, input_size = input.size()
+        inputs, lengths, weight, bias, hy, cy, dropout_mask, gates = self.saved_tensors
+        inputs = inputs.contiguous()
+        sequence_length, batch_size, input_size = inputs.size()
 
-        grad_input = input.new().resize_as_(input).zero_()
-        grad_hx = input.new().resize_as_(hy).zero_()
-        grad_cx = input.new().resize_as_(cy).zero_()
-        grad_weight = input.new()
-        grad_bias = input.new()
+        grad_input = inputs.new().resize_as_(inputs).zero_()
+        grad_hx = inputs.new().resize_as_(hy).zero_()
+        grad_cx = inputs.new().resize_as_(cy).zero_()
+        grad_weight = inputs.new()
+        grad_bias = inputs.new()
         grad_dropout = None
         grad_lengths = None
         grad_gates = None
@@ -64,15 +72,15 @@ class _HighwayLSTMFunction(NestedIOFunction):
             grad_weight.resize_as_(weight).zero_()
             grad_bias.resize_as_(bias).zero_()
 
-        tmp_i_gates_grad = input.new().resize_(batch_size, 6 * self.hidden_size).zero_()
-        tmp_h_gates_grad = input.new().resize_(batch_size, 5 * self.hidden_size).zero_()
+        tmp_i_gates_grad = inputs.new().resize_(batch_size, 6 * self.hidden_size).zero_()
+        tmp_h_gates_grad = inputs.new().resize_(batch_size, 5 * self.hidden_size).zero_()
 
         is_training = 1 if self.train else 0
         highway_lstm_layer.highway_lstm_backward_cuda(
             input_size, self.hidden_size, batch_size,
             self.num_layers, sequence_length, grad_output,
-            lengths, grad_hx, grad_cx, input, hy, cy, weight,
-            gates, dropout, tmp_h_gates_grad, tmp_i_gates_grad,
+            lengths, grad_hx, grad_cx, inputs, hy, cy, weight,
+            gates, dropout_mask, tmp_h_gates_grad, tmp_i_gates_grad,
             grad_hy, grad_input, grad_weight, grad_bias, is_training, 1 if self.needs_input_grad[1] else 0)
 
         return grad_input, grad_weight, grad_bias, grad_hx, grad_cx, grad_dropout, grad_lengths, grad_gates
@@ -118,23 +126,28 @@ class HighwayLSTM(torch.nn.Module):
         self.recurrent_dropout_prob = recurrent_dropout_prob
         self.training = True
 
-        self.ih_size = 6 * hidden_size
-        self.hh_size = 5 * hidden_size
-        self.bias_size = 5 * hidden_size
+        # Input dimensions consider the fact that we do
+        # all of the LSTM projections (and highway parts)
+        # in a single matrix multiplication.
+        input_projection_size = 6 * hidden_size
+        state_projection_size = 5 * hidden_size
+        bias_size = 5 * hidden_size
 
-        weight_size = 0
-        bias_size = 0
+        # Here we are creating a single weight and bias with the
+        # parameters for each layer unfolded into it.
+        total_weight_size = 0
+        total_bias_size = 0
         for layer in range(num_layers):
             layer_input_size = input_size if layer == 0 else hidden_size
 
-            ih_weights = self.ih_size * layer_input_size
-            hh_weights = self.hh_size * hidden_size
-            weight_size += ih_weights + hh_weights
+            input_weights = input_projection_size * layer_input_size
+            state_weights = state_projection_size * hidden_size
+            total_weight_size += input_weights + state_weights
 
-            bias_size += self.bias_size
+            total_bias_size += bias_size
 
-        self.weight = Parameter(torch.FloatTensor(weight_size))
-        self.bias = Parameter(torch.FloatTensor(bias_size))
+        self.weight = Parameter(torch.FloatTensor(total_weight_size))
+        self.bias = Parameter(torch.FloatTensor(total_bias_size))
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -173,9 +186,9 @@ class HighwayLSTM(torch.nn.Module):
 
         Returns
         -------
-        output_sequence : PackedSequence
+        output_sequence : ``PackedSequence``
             The encoded sequence of shape (batch_size, sequence_length, hidden_size)
-        final_states: torch.Tensor
+        final_states: ``torch.Tensor``
             The per-layer final (state, memory) states of the LSTM, each with shape
             (num_layers, batch_size, hidden_size).
         """
@@ -191,24 +204,21 @@ class HighwayLSTM(torch.nn.Module):
 
         dropout_weights = inputs.data.new().resize_(self.num_layers, batch_size, self.hidden_size).fill_(1.0)
         if self.training:
+            # Normalize by 1 - dropout_prob to preserve the output statistics of the layer.
             dropout_weights.bernoulli_(1 - self.recurrent_dropout_prob).div_((1 - self.recurrent_dropout_prob))
 
         dropout_weights = Variable(dropout_weights, requires_grad=False)
+        gates = Variable(inputs.data.new().resize_(self.num_layers, sequence_length, batch_size, 6 * self.hidden_size))
 
-        gates = Variable(inputs.data.new().resize_(self.num_layers,
-                                                   sequence_length,
-                                                   batch_size,
-                                                   6 * self.hidden_size))
-
-        lengths_var = Variable(torch.IntTensor(lengths))
+        lengths = Variable(torch.IntTensor(lengths))
         implementation = _HighwayLSTMFunction(self.input_size,
                                               self.hidden_size,
                                               num_layers=self.num_layers,
                                               train=self.training)
-        output, hidden = implementation(inputs, self.weight, self.bias, state_accumulator,
-                                        memory_accumulator, dropout_weights, lengths_var, gates)
+        output, final_states = implementation(inputs, self.weight, self.bias, state_accumulator,
+                                              memory_accumulator, dropout_weights, lengths, gates)
 
         output = output.transpose(0, 1)
         output = pack_padded_sequence(output, lengths, batch_first=True)
-        return output, hidden
+        return output, final_states
 
