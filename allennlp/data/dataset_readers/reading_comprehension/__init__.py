60ea1dc1ec968d87d19738488cc2c515ef3b9c00
Matt Gardner <mattg@allenai.org>
2017-09-22 18:04:59 +0000

    Moved SquadReader into a reading_comprehension/ module, removed old sentence selection code

---
+++ allennlp/data/dataset_readers/reading_comprehension/__init__.py
@@ -0,0 +1,6 @@
+"""
+Reading comprehension is loosely defined as follows: given a question and a passage of text that
+contains the answer, answer the question.
+"""
+
+from allennlp.data.dataset_readers.reading_comprehension.squad import SquadReader
