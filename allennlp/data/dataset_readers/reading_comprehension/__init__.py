ae3532b85b6a2bd5101e16c98039133520e713b1
Matt Gardner <mattg@allenai.org>
2017-09-22 18:12:21 +0000

    Moved BiDAF into a reading_comprehension/ module

--- allennlp/data/dataset_readers/reading_comprehension/__init__.py
+++ allennlp/data/dataset_readers/reading_comprehension/__init__.py
@@ -1,6 +1,8 @@
 """
 Reading comprehension is loosely defined as follows: given a question and a passage of text that
 contains the answer, answer the question.
+
+These submodules contain readers for things that are predominantly reading comprehension datasets.
 """
 
 from allennlp.data.dataset_readers.reading_comprehension.squad import SquadReader
--- allennlp/models/__init__.py
+++ allennlp/models/__init__.py
@@ -4,8 +4,8 @@ all of which are subclasses of :class:`~allennlp.models.model.Model`.
 """
 
 from allennlp.models.archival import archive_model, load_archive
-from allennlp.models.bidaf import BidirectionalAttentionFlow
 from allennlp.models.decomposable_attention import DecomposableAttention
 from allennlp.models.model import Model
-from allennlp.models.simple_tagger import SimpleTagger
+from allennlp.models.reading_comprehension.bidaf import BidirectionalAttentionFlow
 from allennlp.models.semantic_role_labeler import SemanticRoleLabeler
+from allennlp.models.simple_tagger import SimpleTagger
