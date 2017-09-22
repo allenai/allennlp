8535ba823c7a411017ab77136ce3a33fec711369
Joel Grus <joelgrus@gmail.com>
2017-09-22 12:32:29 -0700

    add test for regularization

--- tests/models/simple_tagger_test.py
+++ tests/models/simple_tagger_test.py
@@ -6,9 +6,13 @@ import numpy
 from allennlp.common.testing import ModelTestCase
 from allennlp.common.checks import ConfigurationError
 from allennlp.common.params import Params
+from allennlp.data.dataset_readers import DatasetReader
+from allennlp.data.iterators import DataIterator
 from allennlp.models import Model
 from allennlp.nn.util import arrays_to_variables
+from allennlp.training import Trainer
 
+import torch
 
 class SimpleTaggerTest(ModelTestCase):
     def setUp(self):
@@ -36,3 +40,42 @@ class SimpleTaggerTest(ModelTestCase):
         params["model"]["stacked_encoder"]["input_size"] = 10
         with pytest.raises(ConfigurationError):
             Model.from_params(self.vocab, params.pop("model"))
+
+class SimpleTaggerRegularizationTest(ModelTestCase):
+    def setUp(self):
+        super().setUp()
+        param_file = 'tests/fixtures/simple_tagger/experiment_with_regularization.json'
+        self.set_up_model(param_file,
+                          'tests/fixtures/data/sequence_tagging.tsv')
+        params = Params.from_file(param_file)
+        self.reader = DatasetReader.from_params(params['dataset_reader'])
+        self.iterator = DataIterator.from_params(params['iterator'])
+        self.trainer = Trainer.from_params(
+                self.model,
+                self.TEST_DIR,
+                self.iterator,
+                self.dataset,
+                None,
+                params.get('trainer')
+        )
+
+
+    def test_regularization(self):
+        penalty = self.model.get_regularization_penalty()
+        assert penalty is not None
+
+        penalty2 = 0
+
+        for name, parameter in self.model.named_parameters():
+            if name.endswith("weight"):
+                weight_penalty = 10 * torch.sum(torch.pow(parameter, 2))
+                penalty2 += weight_penalty
+            elif name.endswith("bias"):
+                bias_penalty = 5 * torch.sum(torch.abs(parameter))
+                penalty2 += bias_penalty
+
+        assert (penalty.data == penalty2.data).all()
+
+        # TODO(joelgrus): test that training applies regularization correctly,
+        # as soon as the trainer is refactored
+        self.trainer.train()
