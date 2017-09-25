bd072e65ac89cb1f87d576d9980a8f554e0bc1ca
Joel Grus <joelgrus@gmail.com>
2017-09-25 13:27:00 -0700

    address PR feedback

--- allennlp/training/trainer.py
+++ allennlp/training/trainer.py
@@ -11,7 +11,7 @@ import logging
 import os
 import shutil
 import time
-from typing import Dict, Optional, List
+from typing import Dict, Optional, List, Tuple
 
 import torch
 import torch.optim.lr_scheduler
@@ -148,13 +148,13 @@ class Trainer:
         if serialization_dir is not None:
             train_log = SummaryWriter(os.path.join(serialization_dir, "log", "train"))
             validation_log = SummaryWriter(os.path.join(serialization_dir, "log", "validation"))
-            self._writer = TensorboardWriter(train_log, validation_log)
+            self._tensorboard = TensorboardWriter(train_log, validation_log)
         else:
-            self._writer = TensorboardWriter()
+            self._tensorboard = TensorboardWriter()
 
     def _enable_gradient_clipping(self) -> None:
         if self._grad_clipping is not None:
-            # Pylint is unable to tell that we're in the case that _glad_clipping is not None...
+            # Pylint is unable to tell that we're in the case that _glar_clipping is not None...
             # pylint: disable=invalid-unary-operand-type
             clip_function = lambda grad: grad.clamp(-self._grad_clipping, self._grad_clipping)
             for parameter in self._model.parameters():
@@ -168,10 +168,9 @@ class Trainer:
         if self._grad_norm:
             clip_grad_norm(self._model.parameters(), self._grad_norm)
 
-    def _compute_loss(self, batch: torch.Tensor, for_training: bool) -> torch.Tensor:
+    def _batch_loss(self, batch: torch.Tensor, for_training: bool) -> torch.Tensor:
         """
         Does a forward pass on the given batch and returns the ``loss`` value in the result.
-        Also performs backpropagation if ``for_training`` is ``True``.
         """
         output_dict = self._forward(batch, for_training=for_training)
 
@@ -182,19 +181,15 @@ class Trainer:
             raise ConfigurationError("The model you are trying to optimize does not contain a"
                                      " 'loss' key in the output of model.forward(inputs).")
 
-        # Backpropagate the loss if we're training.
-        if for_training:
-            loss.backward()
-
         return loss
 
-    def _get_metrics_for_batch(self, total_loss: float, batch_num: int, reset: bool = False) -> dict:
+    def _get_metrics(self, total_loss: float, batch_num: int, reset: bool = False) -> dict:
         """
         Gets the metrics but sets ``"loss"`` to
         the total loss divided by the ``batch_num`` so that
         the ``"loss"`` metric is "average loss per batch".
         """
-        metrics = self._model.get_metrics(reset)
+        metrics = self._model.get_metrics(reset=reset)
         metrics["loss"] = float(total_loss / batch_num)
         return metrics
 
@@ -218,25 +213,22 @@ class Trainer:
 
         logger.info("Training")
         for batch in train_generator_tqdm:
-            # Increment batch and zero gradient
             batch_num += 1
             self._optimizer.zero_grad()
 
-            # Compute loss for batch and backpropagate
-            loss = self._compute_loss(batch, for_training=True)
+            loss = self._batch_loss(batch, for_training=True)
+            loss.backward()
 
             # Make sure Variable is on the cpu before converting to numpy.
             # .cpu() is a no-op if you aren't using GPUs.
             train_loss += loss.data.cpu().numpy()
 
-            # Rescale gradients
             self._rescale_gradients()
 
-            # Advance the optimizer
             self._optimizer.step()
 
             # Update the description with the latest metrics
-            metrics = self._get_metrics_for_batch(train_loss, batch_num)
+            metrics = self._get_metrics(train_loss, batch_num)
             description = self._description_from_metrics(metrics)
             train_generator_tqdm.set_description(description)
 
@@ -244,37 +236,85 @@ class Trainer:
             batch_num_total = num_training_batches * epoch + batch_num
             if batch_num_total % self._summary_interval == 0:
                 for name, param in self._model.named_parameters():
-                    self._writer.add_train_scalar("parameter_mean/" + name, param.data.mean(), batch_num_total)
-                    self._writer.add_train_scalar("parameter_std/" + name, param.data.std(), batch_num_total)
+                    self._tensorboard.add_train_scalar("parameter_mean/" + name,
+                                                       param.data.mean(),
+                                                       batch_num_total)
+                    self._tensorboard.add_train_scalar("parameter_std/" + name, param.data.std(), batch_num_total)
                     if param.grad is not None:
-                        self._writer.add_train_scalar("gradient_mean/" + name,
-                                                      param.grad.data.mean(),
-                                                      batch_num_total)
-                        self._writer.add_train_scalar("gradient_std/" + name,
-                                                      param.grad.data.std(),
-                                                      batch_num_total)
-                self._writer.add_train_scalar("loss/loss_train", metrics["loss"], batch_num_total)
+                        self._tensorboard.add_train_scalar("gradient_mean/" + name,
+                                                           param.grad.data.mean(),
+                                                           batch_num_total)
+                        self._tensorboard.add_train_scalar("gradient_std/" + name,
+                                                           param.grad.data.std(),
+                                                           batch_num_total)
+                self._tensorboard.add_train_scalar("loss/loss_train", metrics["loss"], batch_num_total)
 
             # Log progress in no-tqdm case
             if self._no_tqdm and time.time() - self._last_log > self._log_interval:
                 logger.info("Batch %d/%d: %s", batch_num, num_training_batches, description)
                 self._last_log = time.time()
 
-        # Return the metrics (and reset them)
-        return self._get_metrics_for_batch(train_loss, batch_num, reset=True)
+        return self._get_metrics(train_loss, batch_num, reset=True)
 
-    def _next_epoch_validation(self, epoch: int, metrics: dict,
-                               validation_metric_per_epoch: List[float]) -> bool:
+    def _should_stop_early(self, latest_metric: float, prev_metrics: List[float]) -> bool:
         """
-        Validate one epoch, return True if we should stop early, False otherwise.
+        uses patience and the validation metric to determine if training should stop early
+        """
+        if len(prev_metrics) > self._patience:
+            # Is the worst validation performance in past self._patience
+            # epochs is better than current value?
+            if self._validation_metric_decreases:
+                return max(prev_metrics[-self._patience:]) < latest_metric
+            else:
+                return min(prev_metrics[-self._patience:]) > latest_metric
+
+        return False
+
+    def _metrics_to_tensorboard(self,
+                                epoch: int,
+                                train_metrics: dict,
+                                val_metrics: dict = None) -> None:
+        if val_metrics:
+            message_template = "Training %s : %3f    Validation %s : %3f "
+        else:
+            message_template = "Training %s : %3f "
+
+        for name, value in train_metrics.items():
+            self._tensorboard.add_train_scalar(name, value, epoch)
+            if val_metrics:
+                logger.info(message_template, name, value, name, val_metrics[name])
+                self._tensorboard.add_validation_scalar(name, val_metrics[name], epoch)
+            else:
+                logger.info(message_template, name, value)
+
+    def _update_learning_rate(self, epoch: int, val_metric: float = None) -> None:
+        if not self._learning_rate_scheduler:
+            return
+
+        # Grim hack to determine whether the validation metric we are recording
+        # needs to be passed to the scheduler. This is required because the
+        # step() function of the different schedulers are (understandably)
+        # different to ReduceLROnPlateau.
+        reduce_on_plateau = isinstance(self._learning_rate_scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau)
+
+        if reduce_on_plateau and val_metric is None:
+            raise ConfigurationError("The reduce_on_plateau learning rate scheduler requires "
+                                     "a validation metric to compute the schedule and therefore "
+                                     "must be used with a validation dataset.")
+        elif reduce_on_plateau:
+            self._learning_rate_scheduler.step(val_metric, epoch)
+        else:
+            self._learning_rate_scheduler.step(epoch)
+
+    def _validation_loss(self) -> Tuple[float, int]:
+        """
+        Computes the validation loss. Returns it and the number of batches.
         """
         logger.info("Validating")
         num_validation_batches = self._iterator.get_num_batches(self._validation_dataset)
 
-        # Switch to evaluation mode.
         self._model.eval()
 
-        # Get tqdm for the validation batches
         val_generator = self._iterator(self._validation_dataset, num_epochs=1)
         val_generator_tqdm = tqdm.tqdm(val_generator,
                                        disable=self._no_tqdm,
@@ -284,12 +324,11 @@ class Trainer:
         for batch in val_generator_tqdm:
             batch_num += 1
 
-            # Compute loss but don't backpropagate
-            loss = self._compute_loss(batch, for_training=False)
+            loss = self._batch_loss(batch, for_training=False)
             val_loss += loss.data.cpu().numpy()
 
             # Update the description with the latest metrics
-            val_metrics = self._get_metrics_for_batch(val_loss, batch_num)
+            val_metrics = self._get_metrics(val_loss, batch_num)
             description = self._description_from_metrics(val_metrics)
             val_generator_tqdm.set_description(description)
 
@@ -298,106 +337,46 @@ class Trainer:
                 logger.info("Batch %d/%d: %s", batch_num, num_validation_batches, description)
                 self._last_log = time.time()
 
-        # Log parameter values to Tensorboard
-        val_metrics = self._get_metrics_for_batch(val_loss, batch_num, reset=True)
-        message_template = "Training %s : %3f    Validation %s : %3f "
-        for name, value in metrics.items():
-            logger.info(message_template, name, value, name, val_metrics[name])
-            self._writer.add_train_scalar(name, value, epoch)
-            self._writer.add_validation_scalar(name, val_metrics[name], epoch)
-
-        # Check validation metric for early stopping
-        this_epoch_val_metric = val_metrics[self._validation_metric]
-        if len(validation_metric_per_epoch) > self._patience:
-            # Is the worst validation performance in past self._patience
-            # epochs is better than current value?
-            if self._validation_metric_decreases:
-                should_stop = max(validation_metric_per_epoch[-self._patience:]) < this_epoch_val_metric
-            else:
-                should_stop = min(validation_metric_per_epoch[-self._patience:]) > this_epoch_val_metric
-            if should_stop:
-                logger.info("Ran out of patience.  Stopping training.")
-                return True  # should stop
-        validation_metric_per_epoch.append(this_epoch_val_metric)
-
-        # Check validation metric to see if it's the best so far
-        if self._validation_metric_decreases:
-            is_best_so_far = this_epoch_val_metric == min(validation_metric_per_epoch)
-        else:
-            is_best_so_far = this_epoch_val_metric == max(validation_metric_per_epoch)
-
-        # Save the checkpoint (and possibly update best weights)
-        self._save_checkpoint(epoch, is_best=is_best_so_far)
-
-        # Update the learning rate
-        if self._learning_rate_scheduler:
-            # Grim hack to determine whether the validation metric we are recording
-            # needs to be passed to the scheduler. This is required because the
-            # step() function of the different schedulers are (understandably)
-            # different to ReduceLROnPlateau.
-            if isinstance(self._learning_rate_scheduler,
-                          torch.optim.lr_scheduler.ReduceLROnPlateau):
-                self._learning_rate_scheduler.step(this_epoch_val_metric, epoch)
-            self._learning_rate_scheduler.step(epoch)
-
-        return False  # should not stop
-
-    def _next_epoch_no_validation(self, epoch: int, metrics: Dict) -> bool:
-        """
-        Handles end-of-epoch housekeeping in the case where we have no validation dataset.
-        """
-        # Log progress to Tensorboard
-        message_template = "Training %s : %3f "
-        for name, value in metrics.items():
-            logger.info(message_template, name, value)
-            self._writer.add_train_scalar(name, value, epoch)
-
-        # Save the checkpoint
-        self._save_checkpoint(epoch)
-
-        # Update the learning rate
-        if self._learning_rate_scheduler:
-            if isinstance(self._learning_rate_scheduler,
-                          torch.optim.lr_scheduler.ReduceLROnPlateau):
-                raise ConfigurationError("The reduce_on_plateau learning rate scheduler requires "
-                                         "a validation metric to compute the schedule and therefore "
-                                         "must be used with a validation dataset.")
-            self._learning_rate_scheduler.step(epoch)
-
-        return False  # should not stop
-
-    def _should_stop(self, epoch: int, metrics: dict,
-                     validation_metric_per_epoch: List[float]) -> bool:
-        """
-        Called at the end of each epoch.
-        Handles all end-of-epoch logic, and returns True if we should stop early.
-        Different code paths if there's a validation set or not.
-        """
-        if self._validation_dataset is not None:
-            return self._next_epoch_validation(epoch, metrics, validation_metric_per_epoch)
-        else:
-            return self._next_epoch_no_validation(epoch, metrics)
+        return val_loss, batch_num
 
     def train(self) -> None:
         """
         Trains the supplied model with the supplied parameters.
         """
-        # Restore from checkpoint, if appropriate.
         epoch_counter = self._restore_checkpoint()
-
-        # Enable gradient_clipping
         self._enable_gradient_clipping()
 
         logger.info("Beginning training.")
         validation_metric_per_epoch: List[float] = []
 
         for epoch in range(epoch_counter, self._num_epochs):
-            # Train one epoch and compute metrics.
-            metrics = self._train_epoch(epoch)
+            train_metrics = self._train_epoch(epoch)
 
-            # Validate and move on to the next epoch
-            if self._should_stop(epoch, metrics, validation_metric_per_epoch):
-                break
+            if self._validation_dataset is not None:
+                # We have a validation set, so compute all the metrics on it.
+                val_loss, num_batches = self._validation_loss()
+                val_metrics = self._get_metrics(val_loss, num_batches, reset=True)
+
+                # Check validation metric for early stopping
+                this_epoch_val_metric = val_metrics[self._validation_metric]
+                if self._should_stop_early(this_epoch_val_metric, validation_metric_per_epoch):
+                    logger.info("Ran out of patience.  Stopping training.")
+                    break
+                validation_metric_per_epoch.append(this_epoch_val_metric)
+
+                # Check validation metric to see if it's the best so far
+                if self._validation_metric_decreases:
+                    is_best_so_far = this_epoch_val_metric == min(validation_metric_per_epoch)
+                else:
+                    is_best_so_far = this_epoch_val_metric == max(validation_metric_per_epoch)
+            else:
+                # No validation set, so just assume it's the best so far.
+                is_best_so_far = True
+                val_metrics = this_epoch_val_metric = None
+
+            self._save_checkpoint(epoch, is_best=is_best_so_far)
+            self._metrics_to_tensorboard(epoch, train_metrics, val_metrics=val_metrics)
+            self._update_learning_rate(epoch, val_metric=this_epoch_val_metric)
 
     def _forward(self, batch: dict, for_training: bool) -> dict:
         tensor_batch = arrays_to_variables(batch, self._cuda_device, for_training=for_training)
