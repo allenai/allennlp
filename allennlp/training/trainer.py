0478454b58501d6322ea48121c689458f488d75d
Joel Grus <joelgrus@gmail.com>
2017-09-25 13:47:40 -0700

    add missing line

--- allennlp/training/trainer.py
+++ allennlp/training/trainer.py
@@ -248,6 +248,7 @@ class Trainer:
                                                            param.grad.data.std(),
                                                            batch_num_total)
                 self._tensorboard.add_train_scalar("loss/loss_train", metrics["loss"], batch_num_total)
+                self._metrics_to_tensorboard(epoch, metrics)
 
             # Log progress in no-tqdm case
             if self._no_tqdm and time.time() - self._last_log > self._log_interval:
