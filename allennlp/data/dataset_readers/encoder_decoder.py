7f51af6efc592261f2a67aaad5a71f0474ac2d1f
pdasigi <pdasigi@cs.cmu.edu>
2017-10-08 18:53:22 -0400

    Moved namespace selection to config file and adrressed other Matt's comments

--- allennlp/data/dataset_readers/encoder_decoder.py
+++ allennlp/data/dataset_readers/encoder_decoder.py
@@ -11,7 +11,7 @@ from allennlp.data.dataset import Dataset
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from allennlp.data.fields import TextField
 from allennlp.data.instance import Instance
-from allennlp.data.tokenizers import Tokenizer, WordTokenizer
+from allennlp.data.tokenizers import Token, Tokenizer, WordTokenizer
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
@@ -23,30 +23,33 @@ END_SYMBOL = "@@END@@"
 class EncoderDecoderDatasetReader(DatasetReader):
     """
     Read a tsv file containing paired sequences, and create a dataset for an ``EncoderDecoder`` model.
+    Expected format for each line: <source_sequence_string>\t<target_sequence_string>
 
     Parameters
     ----------
     source_tokenizer : ``Tokenizer``, optional (default=``WordTokenizer()``)
         Tokenizer to use to split the input sequences into words or other kinds of tokens.
-    target_tokenizer : ``Tokenizer``, optional (default=``WordTokenizer(start_tokens=[START_SYMBOL],
-                                                                        end_tokens=[END_SYMBOL])``)
-        Tokenizer to use to split the output sequences (during training) into words or other kinds of tokens.
+    target_tokenizer : ``Tokenizer``, optional (default = source_tokenizer)
+        Tokenizer to use to split the output sequences (during training) into words or other kinds
+        of tokens.
     source_token_indexers : ``Dict[str, TokenIndexer]``, optional
-                             (default=``{"tokens": SingleIdTokenizer("input_tokens")}``)
+                             (default=``{"tokens": SingleIdTokenizer()}``)
         Indexers used to define input (source side) token representations.
     target_token_indexers : ``Dict[str, TokenIndexer]``, optional
-                            (default=``{"tokens": SingleIdTokenizer("output_tokens")}``)
+                            (default=``{"tokens": SingleIdTokenizer()}``)
         Indexers used to define output (target side) token representations.
     """
-    def __init__(self, source_tokenizer: Tokenizer = None, target_tokenizer: Tokenizer = None,
+    def __init__(self,
+                 source_tokenizer: Tokenizer = None,
+                 target_tokenizer: Tokenizer = None,
                  source_token_indexers: Dict[str, TokenIndexer] = None,
                  target_token_indexers: Dict[str, TokenIndexer] = None) -> None:
         self._source_tokenizer = source_tokenizer or WordTokenizer()
-        self._target_tokenizer = target_tokenizer or WordTokenizer(start_tokens=[START_SYMBOL],
-                                                                   end_tokens=[END_SYMBOL])
-        self._source_token_indexers = source_token_indexers or {"tokens": SingleIdTokenIndexer("source_tokens")}
-        self._target_token_indexers = target_token_indexers or {"tokens": SingleIdTokenIndexer("target_tokens")}
-        self._token_indexers = {"source": self._source_token_indexers, "target": self._target_token_indexers}
+        self._target_tokenizer = target_tokenizer or self._source_tokenizer
+        self._source_token_indexers = source_token_indexers or {"tokens": SingleIdTokenIndexer()}
+        self._target_token_indexers = target_token_indexers or self._source_token_indexers
+        self._token_indexers = {"source": self._source_token_indexers,
+                                "target": self._target_token_indexers}
 
     @overrides
     def read(self, file_path):
@@ -63,29 +66,41 @@ class EncoderDecoderDatasetReader(DatasetReader):
                 if len(line_parts) != 2:
                     raise ConfigurationError("Invalid line format: %s (line number %d)" % (line, line_num + 1))
                 source_sequence, target_sequence = line_parts
-                source_field = TextField(self._source_tokenizer.tokenize(source_sequence),
-                                         self._source_token_indexers)
-                target_field = TextField(self._target_tokenizer.tokenize(target_sequence),
-                                         self._target_token_indexers)
-                instances.append(Instance({'source_tokens': source_field, 'target_tokens': target_field}))
+                instances.append(self.text_to_instance(source_sequence, target_sequence))
         if not instances:
             raise ConfigurationError("No instances read!")
         return Dataset(instances)
 
     @overrides
-    def text_to_instance(self, input_string: str) -> Instance:  # type: ignore
+    def text_to_instance(self, source_string: str, target_string: str = None) -> Instance:  # type: ignore
         # pylint: disable=arguments-differ
-        tokenized_string = self._source_tokenizer.tokenize(input_string)
-        input_field = TextField(tokenized_string, self._source_token_indexers)
-        return Instance({'source_tokens': input_field})
+        tokenized_source = self._source_tokenizer.tokenize(source_string)
+        source_field = TextField(tokenized_source, self._source_token_indexers)
+        if target_string is not None:
+            tokenized_target = self._target_tokenizer.tokenize(target_string)
+            tokenized_target.insert(0, Token(START_SYMBOL))
+            tokenized_target.append(Token(END_SYMBOL))
+            target_field = TextField(tokenized_target, self._target_token_indexers)
+            return Instance({"source_tokens": source_field, "target_tokens": target_field})
+        else:
+            return Instance({'source_tokens': source_field})
 
     @classmethod
     def from_params(cls, params: Params) -> 'EncoderDecoderDatasetReader':
-        source_tokenizer = Tokenizer.from_params(params.pop('source_tokenizer', {}))
-        target_tokenizer = Tokenizer.from_params(params.pop('target_tokenizer', {"start_tokens": [START_SYMBOL],
-                                                                                 "end_tokens": [END_SYMBOL]}))
-        source_token_indexers = TokenIndexer.dict_from_params(params.pop('source_token_indexers', {}))
-        target_token_indexers = TokenIndexer.dict_from_params(params.pop('target_token_indexers', {}))
+        source_tokenizer_type = params.pop('source_tokenizer', None)
+        source_tokenizer = None if source_tokenizer_type is None else Tokenizer.from_params(source_tokenizer_type)
+        target_tokenizer_type = params.pop('target_tokenizer', None)
+        target_tokenizer = None if target_tokenizer_type is None else Tokenizer.from_params(target_tokenizer_type)
+        source_indexers_type = params.pop('source_token_indexers', None)
+        if source_indexers_type is None:
+            source_token_indexers = None
+        else:
+            source_token_indexers = TokenIndexer.dict_from_params(source_indexers_type)
+        target_indexers_type = params.pop('target_token_indexers', None)
+        if target_indexers_type is None:
+            target_token_indexers = None
+        else:
+            target_token_indexers = TokenIndexer.dict_from_params(target_indexers_type)
         params.assert_empty(cls.__name__)
         return EncoderDecoderDatasetReader(source_tokenizer, target_tokenizer,
                                            source_token_indexers, target_token_indexers)
--- allennlp/models/encoder_decoders/encoder_decoder.py
+++ allennlp/models/encoder_decoders/encoder_decoder.py
@@ -10,10 +10,10 @@ from torch.nn.modules.linear import Linear
 import torch.nn.functional as F
 
 from allennlp.common import Params
-from allennlp.common.checks import ConfigurationError
 from allennlp.data.vocabulary import Vocabulary
 from allennlp.data.dataset_readers.encoder_decoder import START_SYMBOL, END_SYMBOL
 from allennlp.modules import Attention, TextFieldEmbedder, Seq2SeqEncoder
+from allennlp.modules.similarity_functions import SimilarityFunction
 from allennlp.modules.token_embedders import Embedding
 from allennlp.models.model import Model
 from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits, weighted_sum
@@ -34,43 +34,62 @@ class EncoderDecoder(Model):
     Parameters
     ----------
     vocab : ``Vocabulary``, required
-        Vocabulary containing ``tokens`` for input tokens and ``labels`` for output tokens.
+        Vocabulary containing source and target vocabularies. They may be under the same namespace (``tokens``)
+        or the target tokens can have a different namespace, in which case it needs to be specified as
+        ``target_namespace``.
     source_embedder : ``TextFieldEmbedder``, required
         Embedder for source side sequences
     encoder : ``Seq2SeqEncoder``, required
         The encoder of the ``EncoderDecoder`` model
     max_decoding_steps : int, required
         Length of decoded sequences
-    use_attention: bool
-        Should the decoder use attention to get a dynamic summary of the encoder outputs at each step of decoding?
+    target_namespace : str, optional (default = 'tokens')
+        If the target side vocabulary is different from the source side's, you need to specify the target's
+        namespace here. If not, we'll assume it is "tokens", which is also the default choice for the source side,
+        and this might cause them to share vocabularies.
+    target_embedding_dim : int, optional (default = source_embedding_dim)
+        You can specify an embedding dimensionality for the target side. If not, we'll use the same value as the
+        source embedder's.
+    attention_function: ``SimilarityFunction``, optional (default = None)
+        If you want to use attention to get a dynamic summary of the encoder outputs at each step of decoding, this
+        is the function used to compute similarity between the decoder hidden state and encoder outputs.
     scheduled_sampling_ratio: float, optional (default = 0.0)
         At each timestep during training, we sample a random number between 0 and 1, and if it is not less than
-        this value, we use the ground truth labels. Else, we use the predictions from the previous time step.
-        If this value is 0.0 (default), this corresponds to teacher forcing, and if it is 1.0, it corresponds
-        to not using target side ground truth labels. See the following paper for more information:
+        this value, we use the ground truth labels for the whole batch. Else, we use the predictions from the
+        previous time step for the whole batch. If this value is 0.0 (default), this corresponds to teacher
+        forcing, and if it is 1.0, it corresponds to not using target side ground truth labels.
+        See the following paper for more information:
         Scheduled Sampling for Sequence Prediction with Recurrent Neural Networks. Bengio et al., 2015.
     """
-    def __init__(self, vocab: Vocabulary, source_embedder: TextFieldEmbedder,
-                 encoder: Seq2SeqEncoder, max_decoding_steps: int, use_attention: bool = False,
+    def __init__(self,
+                 vocab: Vocabulary,
+                 source_embedder: TextFieldEmbedder,
+                 encoder: Seq2SeqEncoder,
+                 max_decoding_steps: int,
+                 target_namespace: str = "tokens",
+                 target_embedding_dim: int = None,
+                 attention_function: SimilarityFunction = None,
                  scheduled_sampling_ratio: float = 0.0) -> None:
         super(EncoderDecoder, self).__init__(vocab)
         self._source_embedder = source_embedder
         self._encoder = encoder
         self._max_decoding_steps = max_decoding_steps
-        self._use_attention = use_attention
+        self._target_namespace = target_namespace
+        self._attention_function = attention_function
         self._scheduled_sampling_ratio = scheduled_sampling_ratio
-        self._start_index = self.vocab.get_token_index(START_SYMBOL, "target_tokens")
-        self._end_index = self.vocab.get_token_index(END_SYMBOL, "target_tokens")
-        self._num_classes = self.vocab.get_vocab_size("target_tokens")
+        # We need the start symbol to provide as the input at the first timestep of decoding, and end symbol
+        # as a way to indicate the end of the decoded sequence.
+        self._start_index = self.vocab.get_token_index(START_SYMBOL, self._target_namespace)
+        self._end_index = self.vocab.get_token_index(END_SYMBOL, self._target_namespace)
+        self._num_classes = self.vocab.get_vocab_size(self._target_namespace)
         # Decoder output dim needs to be the same as the encoder output dim since we initialize the hidden
         # state of the decoder with that of the final hidden states of the encoder. Also, if we're using
         # attention with ``DotProductSimilarity``, this is needed.
         self._decoder_output_dim = self._encoder.get_output_dim()
-        # TODO (pradeep): target_embedding_dim need not be the same as the source embedding dim.
-        target_embedding_dim = self._source_embedder.get_output_dim()
+        target_embedding_dim = target_embedding_dim or self._source_embedder.get_output_dim()
         self._target_embedder = Embedding(self._num_classes, target_embedding_dim)
-        if self._use_attention:
-            self._decoder_attention = Attention()
+        if self._attention_function:
+            self._decoder_attention = Attention(self._attention_function)
             # The output of attention will be concatenated to the input vector of the decoder at
             # each time step.
             self._decoder_input_dim = self._encoder.get_output_dim() + target_embedding_dim
@@ -79,8 +98,6 @@ class EncoderDecoder(Model):
         # TODO (pradeep): Do not hardcode decoder cell type.
         self._decoder_cell = LSTMCell(self._decoder_input_dim, self._decoder_output_dim)
         self._output_projection_layer = Linear(self._decoder_output_dim, self._num_classes)
-        # We need the start symbol to provide as the input at the first timestep of decoding, and end symbol
-        # as a way of letting decoder choose to stop decoding.
 
     @overrides
     def forward(self,  # type: ignore
@@ -93,19 +110,23 @@ class EncoderDecoder(Model):
         Parameters
         ----------
         source_tokens : Dict[str, torch.LongTensor]
-           The output of ``TextField.as_array()`` applied on the input ``TextField``. This will be passed
+           The output of ``TextField.as_array()`` applied on the source ``TextField``. This will be passed
            through a ``TextFieldEmbedder`` and then through an encoder.
         target_tokens : Dict[str, torch.LongTensor], optional (default = None)
-            Dict with indices of target side tokens, of shape ``(batch_size, target_sequence_length)``
-            in ``tokens`` field.
+           Output of ``Textfield.as_array()`` applied on target ``TextField``. We assume that the arget tokens
+           are also represented as a ``TextField``.
         """
-        # (batch_size, input_seq_length, enc_output_dim)
-        encoder_outputs = self._get_encoder_outputs(source_tokens)
-        final_encoder_output = encoder_outputs[:, -1]  # (batch_size, enc_output_dim)
+        # (batch_size, input_sequence_length, encoder_output_dim)
+        embedded_input = self._source_embedder(source_tokens)
+        source_mask = get_text_field_mask(source_tokens)
+        encoder_outputs = self._encoder(embedded_input, source_mask)
+        final_encoder_output = encoder_outputs[:, -1]  # (batch_size, encoder_output_dim)
         if target_tokens:
             targets = target_tokens["tokens"]
             target_sequence_length = targets.size()[1]
-            num_decoding_steps = target_sequence_length
+            # The last input from the target is either padding or the end symbol. Either way, we don't have to
+            # process it.
+            num_decoding_steps = target_sequence_length - 1
         else:
             num_decoding_steps = self._max_decoding_steps
         # TODO (pradeep): Define a DecoderState class?
@@ -128,20 +149,21 @@ class EncoderDecoder(Model):
                                 final_encoder_output[:, 0]))
                 else:
                     input_choices = last_predictions
-            decoder_input = self._prepare_decode_step_input(input_choices, decoder_hidden, encoder_outputs)
+            decoder_input = self._prepare_decode_step_input(input_choices, decoder_hidden,
+                                                            encoder_outputs, source_mask)
             decoder_hidden, decoder_context = self._decoder_cell(decoder_input,
                                                                  (decoder_hidden, decoder_context))
             # (batch_size, num_classes)
             output_projections = self._output_projection_layer(decoder_hidden)
-            # (batch_size, 1, num_classes)
+            # list of (batch_size, 1, num_classes)
             step_logits.append(output_projections.unsqueeze(1))
             decoder_predictions = self._predict_step(output_projections)
             step_probabilities.append(decoder_predictions["class_probabilities"].unsqueeze(1))
             last_predictions = decoder_predictions["predicted_classes"]
             # (batch_size, 1)
-            step_predictions.append(last_predictions.view(-1, 1))
+            step_predictions.append(last_predictions.unsqueeze(1))
         # step_logits is a list containing tensors of shape (batch_size, 1, num_classes)
-        # This is (batch_size, num_decoding_steps, num_classes)
+        # This is (batch_size, num_decoding_steps-1, num_classes)
         logits = torch.cat(step_logits, 1)
         class_probabilities = torch.cat(step_probabilities, 1)
         all_predictions = torch.cat(step_predictions, 1)
@@ -151,34 +173,31 @@ class EncoderDecoder(Model):
         if target_tokens:
             target_mask = get_text_field_mask(target_tokens)
             # `targets` currently contains START + target_sequence + END, and logits correspond to the outputs
-            # from these timesteps. During training, we want the output from timestep i to be similar to the
-            # input token from timestep i + 1. That is, the comparison between logits and targets should be
-            # one-off.
+            # from these timesteps till the second from last.
+            # During training, we want the output from timestep i to be similar to the
+            # input token from timestep i + 1. That is, the targets should be shifted by one timestep for
+            # appropriate comparison.
             # Consider a single example where the target has 3 words, and padding is to 7 tokens. The complete
             #   sequence would correspond to <S> w1 w2 w3 <E> <P> <P>
             #   and the mask would be         1  1  1  1   1   0   0
-            #   and let the logits be         l1 l2 l3 l4  l5  l6  l7
+            #   and let the logits be         l1 l2 l3 l4  l5  l6
             # We actually need to compare:
             #   the sequence w1 w2 w3 <E> <P> <P>
             #   with masks   1  1  1   1   0   0
             #   against      l1 l2 l3  l4  l5  l6
-            relevant_logits = logits[:, :-1].contiguous()  # (batch_size, num_decoding_steps - 1, num_classes)
+            # TODO (pradeep): Write a test for this.
             relevant_targets = targets[:, 1:].contiguous()  # (batch_size, num_decoding_steps - 1)
             relevant_mask = target_mask[:, 1:].contiguous()  # (batch_size, num_decoding_steps - 1)
-            loss = sequence_cross_entropy_with_logits(relevant_logits, relevant_targets, relevant_mask)
+            loss = sequence_cross_entropy_with_logits(logits, relevant_targets, relevant_mask)
             output_dict["loss"] = loss
             # TODO: Define metrics
         return output_dict
 
-    def _get_encoder_outputs(self, source_tokens: Dict[str, torch.LongTensor]) -> torch.LongTensor:
-        embedded_input = self._source_embedder(source_tokens)
-        source_mask = get_text_field_mask(source_tokens)
-        encoded_text = self._encoder(embedded_input, source_mask)
-        return encoded_text
-
-    def _prepare_decode_step_input(self, input_indices: torch.LongTensor,
-                                   last_decoder_output: torch.LongTensor = None,
-                                   encoder_outputs: torch.LongTensor = None) -> torch.LongTensor:
+    def _prepare_decode_step_input(self,
+                                   input_indices: torch.LongTensor,
+                                   decoder_hidden_state: torch.LongTensor = None,
+                                   encoder_outputs: torch.LongTensor = None,
+                                   encoder_outputs_mask: torch.LongTensor = None) -> torch.LongTensor:
         """
         Given the input indices for the current timestep of the decoder, and all the encoder outputs,
         compute the input at the current timestep.
@@ -193,24 +212,25 @@ class EncoderDecoder(Model):
         ----------
         input_indices : torch.LongTensor
             Indices of either the gold inputs to the decoder or the predicted labels from the previous timestep.
-        last_decoder_output : torch.LongTensor, optional
+        decoder_hidden_state : torch.LongTensor, optional
             Output of from the decoder at the last time step. Needed only if using attention.
         encoder_outputs : torch.LongTensor
             Encoder outputs from all time steps. Needed only if using attention.
+        encoder_outputs_mask : torch.LongTensor
+            Masks on encoder outputs. Needed only if using attention.
         """
         # input_indices : (batch_size,)  since we are processing these one timestep at a time.
-        # (batch_size, enc_embedding_dim)
+        # (batch_size, target_embedding_dim)
         embedded_input = self._target_embedder(input_indices)
-        if self._use_attention:
-            if last_decoder_output is None or encoder_outputs is None:
-                raise ConfigurationError("Last decoder output and all encoder outputs are needed to compute "
-                                         "attention weights.")
-            # encoder_outputs : (batch_size, input_sequence_length, enc_output_dim)
+        if self._attention_function:
+            # encoder_outputs : (batch_size, input_sequence_length, encoder_output_dim)
+            # Ensuring mask is also a FloatTensor. Or else the multiplication within attention will complain.
+            encoder_outputs_mask = encoder_outputs_mask.type(torch.FloatTensor)
             # (batch_size, input_sequence_length)
-            input_weights = self._decoder_attention(last_decoder_output, encoder_outputs)
-            # (batch_size, enc_output_dim)
+            input_weights = self._decoder_attention(decoder_hidden_state, encoder_outputs, encoder_outputs_mask)
+            # (batch_size, encoder_output_dim)
             attended_input = weighted_sum(encoder_outputs, input_weights)
-            # (batch_size, enc_output_dim + enc_embedding_dim)
+            # (batch_size, encoder_output_dim + target_embedding_dim)
             return torch.cat((attended_input, embedded_input), -1)
         else:
             return embedded_input
@@ -270,6 +290,16 @@ class EncoderDecoder(Model):
         source_embedder = TextFieldEmbedder.from_params(vocab, source_embedder_params)
         encoder = Seq2SeqEncoder.from_params(params.pop("encoder"))
         max_decoding_steps = params.pop("max_decoding_steps")
-        use_attention = params.pop("use_attention", False)
+        target_namespace = params.pop("target_namespace", "tokens")
+        # If no attention function is specified, we should not use attention, not attention with default similarity
+        # function.
+        attention_function_type = params.pop("attention_function", None)
+        if attention_function_type is not None:
+            attention_function = SimilarityFunction.from_params(attention_function_type)
+        else:
+            attention_function = None
         scheduled_sampling_ratio = params.pop("scheduled_sampling_ratio", 0.0)
-        return cls(vocab, source_embedder, encoder, max_decoding_steps, use_attention, scheduled_sampling_ratio)
+        return cls(vocab, source_embedder=source_embedder,
+                   encoder=encoder, max_decoding_steps=max_decoding_steps,
+                   target_namespace=target_namespace, attention_function=attention_function,
+                   scheduled_sampling_ratio=scheduled_sampling_ratio)
