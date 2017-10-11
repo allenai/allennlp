317629b446fd6358d31c1f147ca46a4625587497
Mark Neumann <markn@allenai.org>
2017-10-11 14:41:28 -0700

    make bash script take ontonotes path as arg, add note about python 2.7

--- scripts/compile_coref_data.sh
+++ scripts/compile_coref_data.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# This script downloads and compiles the Ontonotes 2012 data in a helpful format
+# for co-reference resolution. It generates 3 files: {train, dev, test}.english.v4_gold_conll,
+# as well as a directory 'conll-2012' which contains the raw extracted data.
+# The script downloads and runs some python scripts which require python 2.X.
+
+ONTONOTES_PATH=$1
+
+if [ ! -n "$ONTONOTES_PATH" ] ; then
+  echo "USAGE: ./scripts/compile_coref_data.sh /path/to/ontonotes/data"
+  exit 1
+fi
+
 function download_and_extract() {
   wget $1/$2
   tar -xvzf $2
@@ -28,7 +40,7 @@ download_and_extract $conll_url conll-2012-scripts.v3.tar.gz
 download_and_extract http://conll.cemantix.org/download reference-coreference-scorers.v8.01.tar.gz
 mv reference-coreference-scorers conll-2012/scorer
 
-ontonotes_path=/projects/WebWare6/ontonotes-release-5.0
-bash conll-2012/v3/scripts/skeleton2conll.sh -D $ontonotes_path/data/files/data conll-2012
+# Convert the ontonotes data into the CONLL format.
+bash conll-2012/v3/scripts/skeleton2conll.sh -D $ONTONOTES_PATH/data/files/data conll-2012
 
 compile_language english
