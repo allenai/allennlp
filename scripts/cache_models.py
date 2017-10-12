0a3d339c253d7aee996e0015b66b83ae0a96d1a3
Michael Schmitz <michael@schmitztech.com>
2017-10-12 11:06:22 -0700

    Respond to PR feedback.

--- scripts/cache_models.py
+++ scripts/cache_models.py
@@ -7,12 +7,12 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.join(__file__, os.par
 from allennlp.commands import DEFAULT_MODELS
 from allennlp.common.file_utils import cached_path
 
-value = os.environ.get('CACHE_MODELS')
-if isinstance(value, str) and value.lower() == "true":
-    urls = DEFAULT_MODELS.values()
-    print("CACHE_MODELS is '%s'.  Downloading %i models." % (value, len(urls)))
-    for i, url in enumerate(urls):
-        print("Downloading model %i of %i from: %s" % (i, len(urls), url))
-        print(cached_path(url))
+value = os.environ.get('CACHE_MODELS', 'false')
+if value.lower() == "true":
+    models = DEFAULT_MODELS.items()
+    print("CACHE_MODELS is '%s'.  Downloading %i models." % (value, len(models)))
+    for i, (model, url) in enumerate(models):
+        print("Downloading '%s' model from %s" % (model, url))
+        print("Saved at %s" % cached_path(url))
 else:
     print("CACHE_MODELS is '%s'.  Not caching models." % value)
