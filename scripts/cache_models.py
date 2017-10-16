18c55f0b580faf02aa7c164dbb7cd8d8e56773ec
Michael Schmitz <michael@schmitztech.com>
2017-10-16 08:55:14 -0700

    Move cache_models.sh to a Python script. (#400)

---
+++ scripts/cache_models.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
+from allennlp.commands import DEFAULT_MODELS
+from allennlp.common.file_utils import cached_path
+
+value = os.environ.get('CACHE_MODELS', 'false')
+if value.lower() == "true":
+    models = DEFAULT_MODELS.items()
+    print("CACHE_MODELS is '%s'.  Downloading %i models." % (value, len(models)))
+    for i, (model, url) in enumerate(models):
+        print("Downloading '%s' model from %s" % (model, url))
+        print("Saved at %s" % cached_path(url))
+else:
+    print("CACHE_MODELS is '%s'.  Not caching models." % value)
--- scripts/cache_models.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$CACHE_MODELS" == "true" ]]; then
-    python -c "from allennlp.commands import DEFAULT_MODELS
-from allennlp.common.file_utils import cached_path
-for url in DEFAULT_MODELS.values():
-    print(cached_path(url))"
-fi
