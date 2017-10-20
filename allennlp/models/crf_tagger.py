fix masking issue + add better tests

allennlp/models/crf_tagger.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Dict, Optional
 
 from overrides import overrides
 import torch
@@ -12,9 +12,6 @@ from allennlp.models.model import Model
 from allennlp.nn import InitializerApplicator, RegularizerApplicator
 import allennlp.nn.util as util
 from allennlp.training.metrics import SpanBasedF1Measure
-
-START_TAG = "@@START@@"
-END_TAG = "@@END@@"
 
 @Model.register("crf_tagger")
 class CrfTagger(Model):
@@ -46,18 +43,14 @@ class CrfTagger(Model):
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
         super().__init__(vocab, regularizer)
+
         self.label_namespace = label_namespace
         self.text_field_embedder = text_field_embedder
-
-        # Make sure we have START and END tags
-        self.start_tag_index = vocab.add_token_to_namespace(START_TAG, label_namespace)
-        self.end_tag_index = vocab.add_token_to_namespace(END_TAG, label_namespace)
         self.num_tags = self.vocab.get_vocab_size(label_namespace)
-
         self.encoder = encoder
         self.tag_projection_layer = TimeDistributed(Linear(self.encoder.get_output_dim(),
                                                            self.num_tags))
-        self.crf = ConditionalRandomField(self.num_tags, self.start_tag_index, self.end_tag_index)
+        self.crf = ConditionalRandomField(self.num_tags)
 
         self.span_metric = SpanBasedF1Measure(vocab, tag_namespace=label_namespace)
 
@@ -107,7 +100,8 @@ class CrfTagger(Model):
         encoded_text = self.encoder(embedded_text_input, mask)
 
         logits = self.tag_projection_layer(encoded_text)
-        predicted_tags = self._viterbi_tags(logits, mask)
+        predicted_tags = [[self.vocab.get_token_from_index(index, self.label_namespace) for index in row]
+                          for row in self.crf.viterbi_tags(logits, mask)]
 
         output = {"logits": logits, "mask": mask, "tags": predicted_tags}
 
@@ -127,41 +121,6 @@ class CrfTagger(Model):
             self.span_metric(class_probabilities, tags, mask)
 
         return output
-
-    def _viterbi_tags(self, logits, mask) -> List[List[str]]:
-        """
-        Uses viterbi algorithm to find most likely tags
-        """
-        _, max_seq_length, num_tags = logits.size()
-
-        # Get the tensors out of the variables
-        transitions = self.crf.transitions.data
-        logits, mask = logits.data, mask.data
-
-        all_tags = []
-        # Pad the max sequence length by 2 to account for start_tag + end_tag.
-        tag_sequence = torch.Tensor(max_seq_length + 2, num_tags)
-
-        for prediction, prediction_mask in zip(logits, mask):
-            sequence_length = torch.sum(prediction_mask)
-
-            # Start with everything totally unlikely
-            tag_sequence.fill_(-10000.)
-            # At timestep 0 we must have the START_TAG
-            tag_sequence[0, self.start_tag_index] = 0.
-            # At steps 1, ..., sequence_length we just use the incoming prediction
-            tag_sequence[1:(sequence_length + 1)] = prediction[:sequence_length]
-            # And at the last timestep we must have the END_TAG
-            tag_sequence[sequence_length + 1, self.end_tag_index] = 0.
-
-            # We pass the tags and the transitions to ``viterbi_decode``.
-            viterbi_path, _ = util.viterbi_decode(tag_sequence[:(sequence_length + 2)], transitions)
-            # Get rid of START and END sentinels and replace the indices with the actual tags.
-            tags = [self.vocab.get_token_from_index(index, self.label_namespace)
-                    for index in viterbi_path[1:-1]]
-            all_tags.append(tags)
-
-        return all_tags
 
     @overrides
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:

allennlp/modules/conditional_random_field.py
@@ -1,9 +1,12 @@
 """
 Conditional random field
 """
-import torch
+from typing import List
 
-from allennlp.nn.util import logsumexp
+import torch
+from torch.autograd import Variable
+
+import allennlp.nn.util as util
 
 class ConditionalRandomField(torch.nn.Module):
     """
@@ -16,27 +19,18 @@ class ConditionalRandomField(torch.nn.Module):
     ----------
     num_tags : int, required
         The number of tags.
-    start_tag_id : int, required
-        The id of the sentinel <START> tag.
-    stop_tag_id : int, required
-        The id of the sentinel <STOP> tag.
     """
     def __init__(self,
-                 num_tags: int,
-                 start_tag_id: int,
-                 stop_tag_id: int) -> None:
+                 num_tags: int) -> None:
         super().__init__()
-
         self.num_tags = num_tags
-        self.start_tag_id = start_tag_id
-        self.stop_tag_id = stop_tag_id
 
-        # transitions[i, j] is the logit for transitioning from state i to state j
+        # transitions[i, j] is the logit for transitioning from state i to state j.
         self.transitions = torch.nn.Parameter(torch.randn(num_tags, num_tags))
 
-        # We never transition to the start tag and we never transition from the stop tag.
-        self.transitions.data[:, start_tag_id] = -10000
-        self.transitions.data[stop_tag_id, :] = -10000
+        # Also need logits for transitioning from "start" state and to "end" state.
+        self.start_transitions = torch.nn.Parameter(torch.randn(num_tags))
+        self.end_transitions = torch.nn.Parameter(torch.randn(num_tags))
 
     def _input_likelihood(self, logits: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         """
@@ -51,30 +45,32 @@ class ConditionalRandomField(torch.nn.Module):
 
         # Initial alpha is the (batch_size, num_tags) tensor of likelihoods combining the
         # transitions to the initial states and the logits for the first timestep.
-        alpha = self.transitions[self.start_tag_id].view(1, num_tags) + logits[0]
+        alpha = self.start_transitions.view(1, num_tags) + logits[0]
 
         # For each i we compute logits for the transitions from timestep i-1 to timestep i.
         # We do so in a (batch_size, num_tags, num_tags) tensor where the axes are
-        # (instance, prev_tag, next_tag)
+        # (instance, current_tag, next_tag)
         for i in range(1, sequence_length):
-            # The emit scores are for time i ("next_tag") so we broadcast along the prev_tag axis.
+            # The emit scores are for time i ("next_tag") so we broadcast along the current_tag axis.
             emit_scores = logits[i].view(batch_size, 1, num_tags)
-            # Transition scores are (prev_tag, next_tag) so we broadcast along the instance axis.
+            # Transition scores are (current_tag, next_tag) so we broadcast along the instance axis.
             transition_scores = self.transitions.view(1, num_tags, num_tags)
-            # Alpha is for the prev_tag, so we broadcast along the next_tag axis.
-            alpha = alpha.view(batch_size, num_tags, 1)
+            # Alpha is for the current_tag, so we broadcast along the next_tag axis.
+            broadcast_alpha = alpha.view(batch_size, num_tags, 1)
 
-            # We add the emit + transition scores only if timestep i - 1 was included.
-            inner = alpha + (emit_scores + transition_scores) * mask[i - 1].view(batch_size, 1, 1)
+            # Add all the scores together and logexp over the prev_tag axis
+            inner = broadcast_alpha + emit_scores + transition_scores
 
-            # Now we logsumexp over the "prev_tag" axis.
-            alpha = logsumexp(inner, 1)
+            # In valid positions (mask == 1) we want to take the logsumexp over the current_tag dimension
+            # of ``inner``. Otherwise (mask == 0) we want to retain the previous alpha.
+            alpha = (util.logsumexp(inner, 1) * mask[i].view(batch_size, 1) +
+                     alpha * (1 - mask[i]).view(batch_size, 1))
 
         # Every sequence needs to end with a transition to the stop_tag.
-        stops = alpha + self.transitions[:, self.stop_tag_id].contiguous().view(1, num_tags)
+        stops = alpha + self.end_transitions.view(1, num_tags)
 
         # Finally we log_sum_exp along the num_tags dim, result is (batch_size,)
-        return logsumexp(stops)
+        return util.logsumexp(stops)
 
     def _joint_likelihood(self,
                           logits: torch.Tensor,
@@ -91,7 +87,7 @@ class ConditionalRandomField(torch.nn.Module):
         tags = tags.transpose(0, 1).contiguous()
 
         # Start with the transition scores from start_tag to the first tag in each input
-        score = self.transitions[self.start_tag_id].index_select(0, tags[0])
+        score = self.start_transitions.index_select(0, tags[0])
 
         # Broadcast the transition scores to one per batch element
         broadcast_transitions = self.transitions.view(1, num_tags, num_tags).expand(batch_size, num_tags, num_tags)
@@ -130,7 +126,7 @@ class ConditionalRandomField(torch.nn.Module):
         last_tags = last_tags[0]
 
         # Compute score of transitioning to `stop_tag` from each "last tag".
-        last_transition_score = self.transitions.index_select(0, last_tags)[:, self.stop_tag_id]
+        last_transition_score = self.end_transitions.index_select(0, last_tags)
 
         # Add the last input if it's not masked.
         last_inputs = logits[-1]                                         # (batch_size, num_tags)
@@ -156,3 +152,44 @@ class ConditionalRandomField(torch.nn.Module):
         log_numerator = self._joint_likelihood(inputs, tags, mask)
 
         return torch.sum(log_numerator - log_denominator)
+
+    def viterbi_tags(self, logits: Variable, mask: Variable) -> List[List[int]]:
+        """
+        Uses viterbi algorithm to find most likely tags for the given inputs.
+        """
+        _, max_seq_length, num_tags = logits.size()
+
+        # Get the tensors out of the variables
+        logits, mask = logits.data, mask.data
+
+        # Augment transitions matrix with start and end transitions
+        start_tag = num_tags
+        end_tag = num_tags + 1
+        transitions = torch.Tensor(num_tags + 2, num_tags + 2).fill_(-10000.)
+
+        transitions[:num_tags, :num_tags] = self.transitions.data
+        transitions[start_tag, :num_tags] = self.start_transitions.data
+        transitions[:num_tags, end_tag] = self.end_transitions.data
+
+        all_tags = []
+        # Pad the max sequence length by 2 to account for start_tag + end_tag.
+        tag_sequence = torch.Tensor(max_seq_length + 2, num_tags + 2)
+
+        for prediction, prediction_mask in zip(logits, mask):
+            sequence_length = torch.sum(prediction_mask)
+
+            # Start with everything totally unlikely
+            tag_sequence.fill_(-10000.)
+            # At timestep 0 we must have the START_TAG
+            tag_sequence[0, start_tag] = 0.
+            # At steps 1, ..., sequence_length we just use the incoming prediction
+            tag_sequence[1:(sequence_length + 1), :num_tags] = prediction[:sequence_length]
+            # And at the last timestep we must have the END_TAG
+            tag_sequence[sequence_length + 1, end_tag] = 0.
+
+            # We pass the tags and the transitions to ``viterbi_decode``.
+            viterbi_path, _ = util.viterbi_decode(tag_sequence[:(sequence_length + 2)], transitions)
+            # Get rid of START and END sentinels and append.
+            all_tags.append(viterbi_path[1:-1])
+
+        return all_tags

tests/modules/conditional_random_field_test.py
@@ -3,7 +3,6 @@ import itertools
 import math
 
 from pytest import approx
-from flaky import flaky
 import torch
 from torch.autograd import Variable
 
@@ -12,21 +11,18 @@ from allennlp.common.testing import AllenNlpTestCase
 
 
 class TestConditionalRandomField(AllenNlpTestCase):
-    @flaky
-    def test_forward_works_without_mask(self):
-        START_TAG = 0
-        END_TAG = 1
-
-        logits = Variable(torch.Tensor([
+    def setUp(self):
+        super().setUp()
+        self.logits = Variable(torch.Tensor([
                 [[0, 0, .5, .5, .2], [0, 0, .3, .3, .1], [0, 0, .9, 10, 1]],
                 [[0, 0, .2, .5, .2], [0, 0, 3, .3, .1], [0, 0, .9, 1, 1]],
         ]))
-        tags = Variable(torch.LongTensor([
+        self.tags = Variable(torch.LongTensor([
                 [2, 3, 4],
                 [3, 2, 2]
         ]))
 
-        transitions = torch.Tensor([
+        self.transitions = torch.Tensor([
                 [0.1, 0.2, 0.3, 0.4, 0.5],
                 [0.8, 0.3, 0.1, 0.7, 0.9],
                 [-0.3, 2.1, -5.6, 3.4, 4.0],
@@ -34,36 +30,43 @@ class TestConditionalRandomField(AllenNlpTestCase):
                 [1.0, 1.0, 1.0, 1.0, 1.0]
         ])
 
+        self.transitions_from_start = torch.Tensor([0.1, 0.2, 0.3, 0.4, 0.6])
+        self.transitions_to_end = torch.Tensor([-0.1, -0.2, 0.3, -0.4, -0.4])
+
         # Use the CRF Module with fixed transitions to compute the log_likelihood
-        crf = ConditionalRandomField(5, START_TAG, END_TAG)
-        crf.transitions = torch.nn.Parameter(transitions)
-        log_likelihood = crf.forward(logits, tags).data[0]
+        self.crf = ConditionalRandomField(5)
+        self.crf.transitions = torch.nn.Parameter(self.transitions)
+        self.crf.start_transitions = torch.nn.Parameter(self.transitions_from_start)
+        self.crf.end_transitions = torch.nn.Parameter(self.transitions_to_end)
+
+    def score(self, logits, tags):
+        """
+        Computes the likelihood score for the given sequence of tags,
+        given the provided logits (and the transition weights in the CRF model)
+        """
+        # Start with transitions from START and to END
+        total = self.transitions_from_start[tags[0]] + self.transitions_to_end[tags[-1]]
+        # Add in all the intermediate transitions
+        for tag, next_tag in zip(tags, tags[1:]):
+            total += self.transitions[tag, next_tag]
+        # Add in the logits for the observed tags
+        for logit, tag in zip(logits, tags):
+            total += logit[tag]
+        return total
+
+    def test_forward_works_without_mask(self):
+        log_likelihood = self.crf.forward(self.logits, self.tags).data[0]
 
         # Now compute the log-likelihood manually
         manual_log_likelihood = 0.0
-
-        def score(logits, tags):
-            """
-            Computes the likelihood score for the given sequence of tags,
-            given the provided logits (and the transition weights in the CRF model)
-            """
-            # Start with transitions from START and to END
-            total = transitions[START_TAG, tags[0]] + transitions[tags[-1], END_TAG]
-            # Add in all the intermediate transitions
-            for tag, next_tag in zip(tags, tags[1:]):
-                total += transitions[tag, next_tag]
-            # Add in the logits for the observed tags
-            for logit, tag in zip(logits, tags):
-                total += logit[tag]
-            return total
 
         # For each instance, manually compute the numerator
         # (which is just the score for the logits and actual tags)
         # and the denominator
         # (which is the log-sum-exp of the scores for the logits across all possible tags)
-        for logits_i, tags_i in zip(logits, tags):
-            numerator = score(logits_i.data, tags_i.data)
-            all_scores = [score(logits_i.data, tags_j) for tags_j in itertools.product(range(5), repeat=3)]
+        for logits_i, tags_i in zip(self.logits, self.tags):
+            numerator = self.score(logits_i.data, tags_i.data)
+            all_scores = [self.score(logits_i.data, tags_j) for tags_j in itertools.product(range(5), repeat=3)]
             denominator = math.log(sum(math.exp(score) for score in all_scores))
             # And include them in the manual calculation.
             manual_log_likelihood += numerator - denominator
@@ -73,17 +76,65 @@ class TestConditionalRandomField(AllenNlpTestCase):
 
 
     def test_forward_works_with_mask(self):
-        logits = Variable(torch.Tensor([
-                [[0, 0, .5, .5, .2], [0, 0, .3, .3, .1], [0, 0, .9, 10, 1]],
-                [[0, 0, .2, .5, .2], [0, 0, 3, .3, .1], [0, 0, .9, 1, 1]],
-        ]))
-        labels = Variable(torch.LongTensor([
-                [2, 3, 4],
-                [3, 2, 2]
-        ]))
-        mask = Variable(torch.ByteTensor([
+        # Use a non-trivial mask
+        mask = Variable(torch.LongTensor([
                 [1, 1, 1],
                 [1, 1, 0]
         ]))
 
-        ConditionalRandomField(5, 0, 1).forward(logits, labels, mask)
+        log_likelihood = self.crf.forward(self.logits, self.tags, mask).data[0]
+
+        # Now compute the log-likelihood manually
+        manual_log_likelihood = 0.0
+
+        # For each instance, manually compute the numerator
+        #   (which is just the score for the logits and actual tags)
+        # and the denominator
+        #   (which is the log-sum-exp of the scores for the logits across all possible tags)
+        for logits_i, tags_i, mask_i in zip(self.logits, self.tags, mask):
+            # Find the sequence length for this input and only look at that much of each sequence.
+            sequence_length = torch.sum(mask_i.data)
+            logits_i = logits_i.data[:sequence_length]
+            tags_i = tags_i.data[:sequence_length]
+
+            numerator = self.score(logits_i, tags_i)
+            all_scores = [self.score(logits_i, tags_j)
+                          for tags_j in itertools.product(range(5), repeat=sequence_length)]
+            denominator = math.log(sum(math.exp(score) for score in all_scores))
+            # And include them in the manual calculation.
+            manual_log_likelihood += numerator - denominator
+
+        # The manually computed log likelihood should equal the result of crf.forward.
+        assert manual_log_likelihood == approx(log_likelihood)
+
+
+    def test_viterbi_tags(self):
+        mask = Variable(torch.LongTensor([
+                [1, 1, 1],
+                [1, 1, 0]
+        ]))
+
+        viterbi_tags = self.crf.viterbi_tags(self.logits, mask)
+
+        # Check that the viterbi tags are what I think they should be.
+        assert viterbi_tags == [
+                [2, 4, 3],
+                [4, 2]
+        ]
+
+        # We can also iterate over all possible tag sequences and use self.score
+        # to check the likelihood of each. The most likely sequence should be the
+        # same as what we get from viterbi_tags.
+        most_likely_tags = []
+
+        for logit, mas in zip(self.logits, mask):
+            sequence_length = torch.sum(mas.data)
+            most_likely, most_likelihood = None, -float('inf')
+            for tags in itertools.product(range(5), repeat=sequence_length):
+                score = self.score(logit.data, tags)
+                if score > most_likelihood:
+                    most_likely, most_likelihood = tags, score
+            # Convert tuple to list; otherwise == complains.
+            most_likely_tags.append(list(most_likely))
+
+        assert viterbi_tags == most_likely_tags