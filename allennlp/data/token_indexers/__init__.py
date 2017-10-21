0678dbb0e38e867b9f97cc2a5b8d29cc8352d587
Matt Gardner <mattg@allenai.org>
2017-10-21 16:21:01 +0000

    Add NER tag token indexer

--- allennlp/data/token_indexers/__init__.py
+++ allennlp/data/token_indexers/__init__.py
@@ -2,8 +2,9 @@
 A ``TokenIndexer`` determines how string tokens get represented as arrays of indices in a model.
 """
 
-from allennlp.data.token_indexers.token_indexer import TokenIndexer
-from allennlp.data.token_indexers.token_characters_indexer import TokenCharactersIndexer
-from allennlp.data.token_indexers.single_id_token_indexer import SingleIdTokenIndexer
-from allennlp.data.token_indexers.pos_tag_indexer import PosTagIndexer
 from allennlp.data.token_indexers.dep_label_indexer import DepLabelIndexer
+from allennlp.data.token_indexers.ner_tag_indexer import NerTagIndexer
+from allennlp.data.token_indexers.pos_tag_indexer import PosTagIndexer
+from allennlp.data.token_indexers.single_id_token_indexer import SingleIdTokenIndexer
+from allennlp.data.token_indexers.token_characters_indexer import TokenCharactersIndexer
+from allennlp.data.token_indexers.token_indexer import TokenIndexer
