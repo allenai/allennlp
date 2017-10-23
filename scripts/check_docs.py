90bb5758d61fd641a6960c2c4c208141a6ff2cc3
Joel Grus <joelgrus@gmail.com>
2017-10-23 13:48:15 -0700

    add whitelists to check_docs

---
+++ scripts/check_docs.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+# pylint: disable=invalid-name,redefined-outer-name
+from glob import glob
+import os
+import re
+import sys
+from typing import Set
+
+DOCS_DIR = 'doc/api'
+MODULE_REGEX = r"\ballennlp\.[a-z0-9_.]+\b"
+MODULE_GLOB = 'allennlp/**/*.py'
+
+MODULES_THAT_NEED_NO_DOCS: Set[str] = {
+        'allennlp',  # no docs at top level
+}
+
+DOCS_THAT_NEED_NO_MODULES: Set[str] = {
+        'allennlp.commands.main',  # is in __init__.py
+}
+
+
+def documented_modules(docs_dir: str = DOCS_DIR, module_regex: str = MODULE_REGEX) -> Set[str]:
+    modules: Set[str] = set()
+
+    for path in glob(os.path.join(docs_dir, '**/*.rst'), recursive=True):
+        with open(path) as f:
+            text = f.read()
+            for module in re.findall(module_regex, text):
+                modules.add(module)
+
+    return modules
+
+
+def existing_modules(module_glob: str = MODULE_GLOB) -> Set[str]:
+    modules: Set[str] = set()
+
+    for path in glob(module_glob, recursive=True):
+        path = re.sub(".py$", "", path)
+        path = re.sub("/__init__", "", path)
+        path = path.replace("/", ".")
+        modules.add(path)
+
+    return modules
+
+if __name__ == "__main__":
+    success = True
+    existing = existing_modules()
+    documented = documented_modules()
+    for module in existing:
+        if module not in documented and module not in MODULES_THAT_NEED_NO_DOCS:
+            print("undocumented module:", module)
+            success = False
+    for module in documented:
+        if module not in existing and module not in DOCS_THAT_NEED_NO_MODULES:
+            print("documented but nonexistent:", module)
+            success = False
+
+    if not success:
+        sys.exit(1)
