32df2c11f71cbad4e4ecb5a3a8e35839a4e16e3a
Joel Grus <joelgrus@gmail.com>
2017-10-23 13:30:14 -0700

    add sphinx coverage plugin

--- doc/conf.py
+++ doc/conf.py
@@ -36,6 +36,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.coverage',
               'sphinx.ext.doctest',
               'sphinx.ext.linkcode',
               'sphinx.ext.mathjax',
