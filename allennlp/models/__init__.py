c18306b5ec5175b096e5009dab3661613b552394
Matt Gardner <mattg@allenai.org>
2017-11-01 16:28:45 -0700

    Initial DecodingAlgorithm API (#403)
    * Initial DecodingAlgorithm API
    
    * Making progress on maximum marginal likelihood...
    
    * Getting closer to nailing down the API
    
    * API is nearly settled; just need to get the parser to use it correclty
    
    * Cherry-picking the logsumexp commit - remove this on rebase
    
    * The WikiTablesSemanticParser now passes mypy with the new API
    
    It won't actually train yet, because some of the tensor operations are
    surely wrong, but it's getting close.
    
    * Fix some minor bugs
    
    * Make `ListField` handle nesting
    
    * Added test fixtures for max marginal likelihood, updated experiment json
    
    * Got wikitables parser to pass tests with max marginal likelihood and toy data
    
    * Fixes from PR comments, and from running this

--- allennlp/models/__init__.py
+++ allennlp/models/__init__.py
@@ -7,6 +7,7 @@ from allennlp.models.archival import archive_model, load_archive, Archive
 from allennlp.models.crf_tagger import CrfTagger
 from allennlp.models.decomposable_attention import DecomposableAttention
 from allennlp.models.encoder_decoders.simple_seq2seq import SimpleSeq2Seq
+from allennlp.models.encoder_decoders.wikitables_semantic_parser import WikiTablesSemanticParser
 from allennlp.models.model import Model
 from allennlp.models.reading_comprehension.bidaf import BidirectionalAttentionFlow
 from allennlp.models.semantic_role_labeler import SemanticRoleLabeler
