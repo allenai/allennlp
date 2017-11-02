0b5381cf31cdfa845296c0b501b503c6f3430504
Pradeep Dasigi <pradeep.dasigi@gmail.com>
2017-11-02 11:59:46 -0400

    Wikitables type declaration and action sequence generation (#434)
    * Initial helper classes for reading wikitables dataset
    
    * Dealt with reverse properly, addressed Matt's comments, added another test
    
    * Close to full coverage of a types in DPD output
    
    * Dealt with lambda expressions and cleaned up type declaration
    
    * Dealth with part, dates and added a large scale action sequence test
    
    * Made a KnowledgeGraph super class and (hopefully) fixed some mypy errors
    
    * Forgot to add one file to the previous commit
    
    * Added type declaration tests
    
    * Addressed PR comments

---
+++ allennlp/data/dataset_readers/wikitables/__init__.py
@@ -0,0 +1,5 @@
+from allennlp.data.dataset_readers.wikitables.type_declaration import COLUMN_TYPE, CELL_TYPE, PART_TYPE
+from allennlp.data.dataset_readers.wikitables.type_declaration import COMMON_NAME_MAPPING, COMMON_TYPE_SIGNATURE
+from allennlp.data.dataset_readers.wikitables.type_declaration import DynamicTypeLogicParser
+from allennlp.data.dataset_readers.wikitables.table import TableKnowledgeGraph
+from allennlp.data.dataset_readers.wikitables.world import World
