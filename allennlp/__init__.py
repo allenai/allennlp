d3acb737af20c5d7c402ca8914cad9db997c39f8
Joel Grus <joelgrus@gmail.com>
2017-11-06 18:08:25 -0800

    pin sanic-cors

--- allennlp/__init__.py
+++ allennlp/__init__.py
@@ -1,1 +1,3 @@
-import spacy
+# On some systems this prevents the dreaded
+# ImportError: dlopen: cannot load any more object with static TLS
+import spacy, torch, numpy  # pylint: disable=multiple-imports
