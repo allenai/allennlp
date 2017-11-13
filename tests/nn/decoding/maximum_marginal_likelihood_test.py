9398bcdb89701f822a700641cbda6d11274d709e
Matt Gardner <mattg@allenai.org>
2017-11-13 13:07:49 -0800

    Implemented batched decoding (#481)
    * Implemented batched decoding
    
    * Fixed beam search, addressed PR comments
    
    * Fix pylint and sphinx

--- tests/nn/decoding/maximum_marginal_likelihood_test.py
+++ tests/nn/decoding/maximum_marginal_likelihood_test.py
@@ -11,10 +11,21 @@ class TestMaximumMarginalLikelihood(AllenNlpTestCase):
         # `1` is our "start symbol" here - the first index is always assumed to be the start symbol
         # and is ignored.  This is due to how the data processing for seq2seq data works.  If that
         # logic changes, we can revisit this.
-        targets = torch.autograd.Variable(torch.Tensor([[1, 2, 3], [1, 4, 5], [1, 2, 7]]))
-        target_mask = torch.autograd.Variable(torch.Tensor([[1, 1, 1], [1, 1, 1], [1, 1, 0]]))
+        targets = torch.autograd.Variable(torch.Tensor([[[1, 2, 3], [1, 4, 5], [1, 2, 7]],
+                                                        [[1, 9, 10], [1, 3, 5], [1, 3, 3]]]))
+        target_mask = torch.autograd.Variable(torch.Tensor([[[1, 1, 1], [1, 1, 1], [1, 1, 0]],
+                                                            [[0, 0, 0], [1, 1, 1], [1, 1, 1]]]))
         result = MaximumMarginalLikelihood._create_allowed_transitions(targets, target_mask)
-        assert len(result) == 3
-        assert result[()] == set([2, 4])
-        assert result[(2,)] == set([3])  # note that 7 is not in here; it was masked
-        assert result[(4,)] == set([5])
+        # There were two instances in this batch.
+        assert len(result) == 2
+
+        # The first instance had three valid action sequence prefixes.
+        assert len(result[0]) == 3
+        assert result[0][()] == set([2, 4])
+        assert result[0][(2,)] == set([3])  # note that 7 is not in here; it was masked
+        assert result[0][(4,)] == set([5])
+
+        # The second instance had two valid action sequence prefixes.
+        assert len(result[1]) == 2
+        assert result[1][()] == set([3])  # note that 9 is not in here; it was masked
+        assert result[1][(3,)] == set([3, 5])
