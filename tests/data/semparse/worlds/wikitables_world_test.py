2a184c2694caf8cd8f6d0eca762d1412e50f0021
Pradeep Dasigi <pradeep.dasigi@gmail.com>
2017-11-30 13:58:09 -0500

    NLVR Type Declaration (#541)
    * Separated general type declaration functionality into a separate module
    
    * Pulled general world functionality into a newly created class World and cleaned type declarations
    
    * Action sequences contain original names now
    
    * Minor modifications to NLVR language to make type checking work
    
    * Updated docs
    
    * Non terminals can be lower cased now
    
    * All wikitables specific terminals start with fb
    
    * Addressed PR comments

--- tests/data/semparse/worlds/wikitables_world_test.py
+++ tests/data/semparse/worlds/wikitables_world_test.py
@@ -12,34 +12,35 @@ class TestWikiTablesWorldRepresentation(AllenNlpTestCase):
 
     def test_world_processes_sempre_forms_correctly(self):
         sempre_form = "((reverse fb:row.row.year) (fb:row.row.league fb:cell.usl_a_league))"
-        expression = self.world.process_sempre_forms([sempre_form])[0]
+        expression = self.world.parse_logical_form(sempre_form)
         assert str(expression) == "R(C0,C1(cell:usl_a_league))"
 
     def test_world_returns_correct_actions_with_reverse(self):
         sempre_form = "((reverse fb:row.row.year) (fb:row.row.league fb:cell.usl_a_league))"
-        expression = self.world.process_sempre_forms([sempre_form])[0]
+        expression = self.world.parse_logical_form(sempre_form)
         actions = self.world.get_action_sequence(expression)
         target_action_sequence = ['e', 'e -> [<r,e>, r]', '<r,e> -> [<<#1,#2>,<#2,#1>>, <e,r>]',
-                                  '<<#1,#2>,<#2,#1>> -> R', '<e,r> -> C0', 'r -> [<e,r>, e]',
-                                  '<e,r> -> C1', 'e -> cell:usl_a_league']
+                                  '<<#1,#2>,<#2,#1>> -> reverse', '<e,r> -> fb:row.row.year',
+                                  'r -> [<e,r>, e]', '<e,r> -> fb:row.row.league', 'e -> fb:cell.usl_a_league']
         assert actions == target_action_sequence
 
     def test_world_returns_correct_actions_with_two_reverses(self):
         sempre_form = ("(max ((reverse fb:cell.cell.date) ((reverse fb:row.row.year) "
                        "(fb:row.row.league fb:cell.usl_a_league))))")
-        expression = self.world.process_sempre_forms([sempre_form])[0]
+        expression = self.world.parse_logical_form(sempre_form)
         actions = self.world.get_action_sequence(expression)
-        target_action_sequence = ['d', 'd -> [<d,d>, d]', '<d,d> -> M0', 'd -> [<e,d>, e]',
-                                  '<e,d> -> [<<#1,#2>,<#2,#1>>, <d,e>]', '<<#1,#2>,<#2,#1>> -> R',
-                                  '<d,e> -> D1', 'e -> [<r,e>, r]', '<r,e> -> [<<#1,#2>,<#2,#1>>, <e,r>]',
-                                  '<<#1,#2>,<#2,#1>> -> R', '<e,r> -> C0', 'r -> [<e,r>, e]',
-                                  '<e,r> -> C1', 'e -> cell:usl_a_league']
+        target_action_sequence = ['d', 'd -> [<d,d>, d]', '<d,d> -> max', 'd -> [<e,d>, e]',
+                                  '<e,d> -> [<<#1,#2>,<#2,#1>>, <d,e>]', '<<#1,#2>,<#2,#1>> -> reverse',
+                                  '<d,e> -> fb:cell.cell.date', 'e -> [<r,e>, r]',
+                                  '<r,e> -> [<<#1,#2>,<#2,#1>>, <e,r>]', '<<#1,#2>,<#2,#1>> -> reverse',
+                                  '<e,r> -> fb:row.row.year', 'r -> [<e,r>, e]',
+                                  '<e,r> -> fb:row.row.league', 'e -> fb:cell.usl_a_league']
         assert actions == target_action_sequence
 
     def test_large_scale_processing(self):
         # A sample of 500 logical forms taken randomly from DPD outputs of all questions in training data.
         forms = [x.strip() for x in open("tests/fixtures/data/wikitables/logical_forms_large_sample.txt")]
-        expressions = self.world.process_sempre_forms(forms)
+        expressions = [self.world.parse_logical_form(form) for form in forms]
         for form, expression in zip(forms, expressions):
             action_sequence = self.world.get_action_sequence(expression)
             for action in action_sequence:
