52b3bcc7e4f1f655e3a32d2bda9b6c6e0ab21be5
Mark Neumann <markng@allenai.org>
2017-12-04 11:34:42 -0800

    more work on reading all conll annotations

--- allennlp/data/dataset_readers/dataset_utils/ontonotes.py
+++ allennlp/data/dataset_readers/dataset_utils/ontonotes.py
@@ -4,7 +4,9 @@ import os
 import logging
 
 from overrides import overrides
+from nltk import Tree
 import tqdm
+
 
 from allennlp.common import Params
 from allennlp.common.checks import ConfigurationError
@@ -13,8 +15,9 @@ from allennlp.data.dataset import Dataset
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from allennlp.data.fields import Field, TextField, SequenceLabelField
 from allennlp.data.instance import Instance
-from allennlp.data.token_indexers import , TokenIndexer
+from allennlp.data.token_indexers import TokenIndexer
 from allennlp.data.tokenizers import Token
+
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
@@ -23,20 +26,32 @@ TypedSpan = Tuple[str, Tuple[int, int]] # pylint: disable=invalid-name
 class OntonotesSentence:
 
     def __init__(self,
-                 words: List[str]):
+                 document_id: str,
+                 sentence_id: str,
+                 words: List[str],
+                 pos_tags: List[str],
+                 parse_tree: Tree,
+                 predicate_lemmas: List[str],
+                 predicate_framenet_ids: List[str],
+                 word_senses: List[int],
+                 speakers: List[str],
+                 named_entities: List[str],
+                 srl_frames: List[List[str]]):
+
+        self.document_id = document_id
+        self.sentence_id = sentence_id
         self.words = words
+        self.pos_tags = pos_tags
+        self.parse_tree = parse_tree
+        self.predicate_lemmas = predicate_lemmas
+        self.predicate_framenet_ids = predicate_framenet_ids
+        self.word_senses = word_senses
+        self.speakers = speakers
+        self.named_entities = named_entities
+        self.srl_frames = srl_frames
 
-class BioOntonotesSentence(OntonotesSentence):
 
-
-class SpanOntonotesSentence(OntonotesSentence):
-
-
-
-
-
-
-class Ontonotes(DatasetReader):
+class Ontonotes:
     """
     This DatasetReader is designed to read in the English OntoNotes v5.0 data
     in the format used by the CoNLL 2011/2012 shared tasks. In order to use this
@@ -140,57 +155,14 @@ class Ontonotes(DatasetReader):
 
     """
     def __init__(self, tagging_type: str = "bio") -> None:
-
         self.tagging_type = tagging_type
 
-        # Admin for keeping track of the CONLL sentences.
-        # The string id of the document. E.g "test/test/03/test_003"
-        self._document_id: str = None
-        # The numeric id of the sentence within this document.
-        self._sentence_id: int = None
+    def datset_iterator(self, file_path) -> Iterator[OntonotesSentence]:
 
-        # This first block of attributes are simple.
-        # They are represented with a single item per word.
+        for conll_file in self.dataset_path_iterator(file_path):
+            yield from self.sentence_iterator(conll_file)
 
-        # The words in the sentence.
-        self._sentence: List[str] = []
-        # The pos tags of the words in the sentence.
-        self._pos_tags: List[str] = []
-        # The lemmatised form of the words in the sentence which
-        # have SRL or word sense information.
-        self._predicate_lemmas: List[str] = []
-        # The FrameNet ID of the predicate.
-        self._predicate_framenet_id: List[str] = []
-        # The sense of the word, if available.
-        self._word_senses: List[int] = []
-        # The current speaker, if available.
-        self._speakers: List[str] = []
-
-        # The fine grained NER tags for the sentence.
-        self._named_entities: List[str] = []
-        # the pieces of the parse tree.
-        self._parse_pieces: List[str] = []
-
-        self._verbal_predicates: List[int] = []
-        self._predicate_argument_labels: List[List[str]] = []
-
-        self._coreference = List[str] = []
-
-    def reset(self):
-        self._document_id: str = None
-        self._sentence_id: int = None
-        self._sentence: List[str] = []
-        self._pos_tags: List[str] = []
-        self._parse_pieces: List[str] = []
-        self._predicate_lemmas: List[str] = []
-        self._predicate_framenet_id: List[str] = []
-        self._word_senses: List[int] = []
-        self._speakers: List[str] = []
-        self._named_entities: List[str] = []
-        self._verbal_predicates: List[int] = []
-        self._predicate_argument_labels: List[List[str]] = []
-
-    def dataset_iterator(self, file_path: str) -> Iterator[str]:
+    def dataset_path_iterator(self, file_path: str) -> Iterator[str]:
         """
         An iterator containing file_paths in a directory
         containing CONLL-formatted files.
@@ -212,7 +184,6 @@ class Ontonotes(DatasetReader):
         An iterator over the sentences in an individual CONLL formatted file.
         """
         with codecs.open(file_path, 'r', encoding='utf8') as open_file:
-
             conll_rows = []
             for line in open_file:
                 line = line.strip()
@@ -220,47 +191,75 @@ class Ontonotes(DatasetReader):
                     conll_rows.append(line)
                 else:
                     if not conll_rows:
-                        self.reset()
                         continue
                     else:
                         yield self.conll_rows_to_sentence(conll_rows)
-                        self.reset()
 
     def conll_rows_to_sentence(self, conll_rows: List[str]) -> OntonotesSentence:
+
+        # The words in the sentence.
         sentence: List[str] = []
+        # The pos tags of the words in the sentence.
         pos_tags: List[str] = []
+        # the pieces of the parse tree.
         parse_pieces: List[str] = []
+        # The lemmatised form of the words in the sentence which
+        # have SRL or word sense information.
         predicate_lemmas: List[str] = []
+        # The FrameNet ID of the predicate.
         predicate_framenet_ids: List[str] = []
+        # The sense of the word, if available.
         word_senses: List[int] = []
+        # The current speaker, if available.
         speakers: List[str] = []
-        named_entities: List[str] = []
 
         coreference = List[str] = []
-        verbal_predicates: List[int] = []
-        predicate_argument_labels: List[List[str]] = []
+        verbal_predicates: List[str] = []
+        span_labels: List[List[str]] = []
+        current_span_labels: List[str] = []
 
         for row in conll_rows:
             conll_components = row.split()
 
+            document_id = conll_components[0]
+            sentence_id = conll_components[1]
             word = conll_components[3]
             pos_tag = conll_components[4]
             parse_piece = conll_components[5]
 
-            parse_piece.replace("*", f" {word}")
+            # Replace brackets in text with a different
+            # token for parse trees.
+            if word == "(":
+                parse_word = "-LRB-"
+            elif word == ")":
+                parse_word = "-RRB-"
+            else:
+                parse_word = word
+            parse_piece.replace("*", f" {parse_word}")
             lemmatised_word = conll_components[6]
             framenet_id = conll_components[7]
             word_sense = conll_components[8]
             speaker = conll_components[9]
-            ner_bit = conll_components[10]
 
-            if predicate_argument_labels is None:
-                predicate_argument_labels = [[] for _ in conll_components[11:-1]]
+            if not span_labels:
+                # Create empty lists to collect the NER and SRL BIO labels.
+                span_labels = [[] for _ in conll_components[10:-1]]
+                # Create variables representing the current label for each label
+                # sequence we are collecting.
+                current_span_labels = [None for _ in conll_components[10:-1]]
 
-            for i, srl_component in enumerate(conll_components[10:-1]):
+            current_span_labels, span_labels = self.process_span_annotations_for_word(conll_components[10:-1],
+                                                       span_labels,
+                                                       current_span_labels)
 
-                component_with_word = srl_component.replace("*", f" {word}")
-                predicate_argument_labels[i].append(component_with_word)
+            # If any annotation contains this word as a verb predicate,
+            # we need to record its index. This also has the side effect
+            # of ordering the verbal predicates by their location in the
+            # sentence, automatically aligning them with the annotations.
+            word_is_verbal_predicate = any(["(V" in x for x in
+                                           [labels[-1] for labels in span_labels[1:]]])
+            if word_is_verbal_predicate:
+                verbal_predicates.append(word)
 
             coreference.append(conll_components[-1])
 
@@ -271,125 +270,56 @@ class Ontonotes(DatasetReader):
             predicate_framenet_ids.append(framenet_id)
             word_senses.append(word_sense)
             speakers.append(speaker)
-            named_entities.append(ner_bit)
 
+        named_entities = span_labels[0]
+        srl_frames = span_labels[1:]
+        parse_tree = Tree.fromstring(" ".join(parse_pieces))
 
-    def process_span_annotation(self):
+        return OntonotesSentence(document_id,
+                                 sentence_id,
+                                 sentence,
+                                 pos_tags,
+                                 parse_tree,
+                                 predicate_lemmas,
+                                 predicate_framenet_ids,
+                                 word_senses,
+                                 speakers,
+                                 named_entities,
+                                 srl_frames)
 
+    @staticmethod
+    def process_span_annotations_for_word(annotations: List[str],
+                                          predicate_argument_labels: List[List[str]],
+                                          current_span_labels: List[Optional[str]]):
 
-    @overrides
-    def read(self, file_path: str):
-        # if `file_path` is a URL, redirect to the cache
-        file_path = cached_path(file_path)
+        for annotation_index in range(len(annotations)):
+            annotation = annotations[annotation_index]
+            label = annotation.strip("()*")
 
-        instances = []
+            if "(" in annotation:
+                # Entering into a span for a particular semantic role label.
+                # We append the label and set the current span for this annotation.
+                bio_label = "B-" + label
+                predicate_argument_labels[annotation_index].append(bio_label)
+                current_span_labels[annotation_index] = label
 
-        sentence: List[str] = []
-        verbal_predicates: List[int] = []
-        predicate_argument_labels: List[List[str]] = []
-        current_span_label: List[Optional[str]] = []
+            elif current_span_labels[annotation_index] is not None:
+                # If there's no '(' token, but the current_span_label is not None,
+                # then we are inside a span.
+                bio_label = "I-" + current_span_labels[annotation_index]
+                predicate_argument_labels[annotation_index].append(bio_label)
+            else:
+                # We're outside a span.
+                predicate_argument_labels[annotation_index].append("O")
 
-        for data_file_path in self.dataset_iterator(file_path):
-            with codecs.open(data_file_path, 'r', encoding='utf8') as open_file:
+            # Exiting a span, so we reset the current span label for this annotation.
+            if ")" in annotation:
+                current_span_labels[annotation_index] = None
 
-                conll_lines = []
-                for line in open_file:
-                    line = line.strip()
-
-                    conll_lines.append(line)
-                    if line == '' or line.startswith("#"):
-
-                        # Conll format data begins and ends with lines containing a hash,
-                        # which may or may not occur after an empty line. To deal with this
-                        # we check if the sentence is empty or not and if it is, we just skip
-                        # adding instances, because there aren't any to add.
-                        if not sentence:
-                            continue
-                        instances.extend(self._process_sentence(sentence,
-                                                                verbal_predicates,
-                                                                predicate_argument_labels))
-                        # Reset everything for the next sentence.
-                        sentence = []
-                        verbal_predicates = []
-                        predicate_argument_labels = []
-                        current_span_label = []
-                        continue
-
-                    conll_components = line.split()
-                    word = conll_components[3]
-
-                    sentence.append(word)
-                    word_index = len(sentence) - 1
-                    if word_index == 0:
-                        # We're starting a new sentence. Here we set up a list of lists
-                        # for the BIO labels for the annotation for each verb and create
-                        # a temporary 'current_span_label' list for each annotation which
-                        # we will use to keep track of whether we are beginning, inside of,
-                        # or outside a particular span.
-                        predicate_argument_labels = [[] for _ in conll_components[11:-1]]
-                        current_span_label = [None for _ in conll_components[11:-1]]
-
-                    num_annotations = len(predicate_argument_labels)
-                    is_verbal_predicate = False
-                    # Iterate over all verb annotations for the current sentence.
-                    for annotation_index in range(num_annotations):
-                        annotation = conll_components[11 + annotation_index]
-                        label = annotation.strip("()*")
-
-                        if "(" in annotation:
-                            # Entering into a span for a particular semantic role label.
-                            # We append the label and set the current span for this annotation.
-                            bio_label = "B-" + label
-                            predicate_argument_labels[annotation_index].append(bio_label)
-                            current_span_label[annotation_index] = label
-
-                        elif current_span_label[annotation_index] is not None:
-                            # If there's no '(' token, but the current_span_label is not None,
-                            # then we are inside a span.
-                            bio_label = "I-" + current_span_label[annotation_index]
-                            predicate_argument_labels[annotation_index].append(bio_label)
-                        else:
-                            # We're outside a span.
-                            predicate_argument_labels[annotation_index].append("O")
-
-                        # Exiting a span, so we reset the current span label for this annotation.
-                        if ")" in annotation:
-                            current_span_label[annotation_index] = None
-                        # If any annotation contains this word as a verb predicate,
-                        # we need to record its index. This also has the side effect
-                        # of ordering the verbal predicates by their location in the
-                        # sentence, automatically aligning them with the annotations.
-                        if "(V" in annotation:
-                            is_verbal_predicate = True
-
-                    if is_verbal_predicate:
-                        verbal_predicates.append(word_index)
-
-        if not instances:
-            raise ConfigurationError("No instances were read from the given filepath {}. "
-                                     "Is the path correct?".format(file_path))
-        return Dataset(instances)
-
-    def text_to_instance(self,  # type: ignore
-                         tokens: List[Token],
-                         verb_label: List[int],
-                         tags: List[str] = None) -> Instance:
-        """
-        We take `pre-tokenized` input here, along with a verb label.  The verb label should be a
-        one-hot binary vector, the same length as the tokens, indicating the position of the verb
-        to find arguments for.
-        """
-        # pylint: disable=arguments-differ
-        fields: Dict[str, Field] = {}
-        text_field = TextField(tokens, token_indexers=self._token_indexers)
-        fields['tokens'] = text_field
-        fields['verb_indicator'] = SequenceLabelField(verb_label, text_field)
-        if tags:
-            fields['tags'] = SequenceLabelField(tags, text_field)
-        return Instance(fields)
+        return current_span_labels, predicate_argument_labels
 
     @classmethod
     def from_params(cls, params: Params) -> 'Ontonotes':
-        token_indexers = TokenIndexer.dict_from_params(params.pop('token_indexers', {}))
+        tagging_type = params.pop("tagging_type", "bio")
         params.assert_empty(cls.__name__)
-        return Ontonotes(token_indexers=token_indexers)
+        return Ontonotes(tagging_type=tagging_type)
