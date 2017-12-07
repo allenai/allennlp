ee7d42af306f25d216431bbb1e4c39d1d40c6c96
Michael Schmitz <michael@schmitztech.com>
2017-12-07 09:37:22 -0800

    Move to PyTorch 0.3 (#578)
    * Change URLs to PyTorch 0.3
    
    * Add dim args to softmaxes to stop warnings

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -37,7 +37,7 @@ source activate testenv
 
 # Install requirements via pip and download data inside our conda environment.
 INSTALL_TEST_REQUIREMENTS="true" bash scripts/install_requirements.sh
-pip install --no-cache-dir -q http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp36-cp36m-manylinux1_x86_64.whl
+pip install --no-cache-dir -q http://download.pytorch.org/whl/cu80/torch-0.3.0.post4-cp36-cp36m-linux_x86_64.whl
 
 # List the packages to get their versions for debugging
 pip list
