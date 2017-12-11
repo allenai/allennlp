67575ba6dfab5015f865183c35d7a1e700a7208e
Matt Gardner <mattg@allenai.org>
2017-12-11 11:06:36 -0800

    ProductionRuleField (#573)
    * Adding a ProductionRuleField
    
    * Added tests for ProductionRuleField, got almost all tests passing
    
    * Updates to use new method for creating arrays, fix tests
    
    * Fix and add docs
    
    * Remove print
    
    * Improve docstring

--- tests/models/encoder_decoders/wikitables_semantic_parser_test.py
+++ tests/models/encoder_decoders/wikitables_semantic_parser_test.py
@@ -8,5 +8,5 @@ class WikiTablesSemanticParserTest(ModelTestCase):
         self.set_up_model("tests/fixtures/encoder_decoder/wikitables_semantic_parser/experiment.json",
                           "tests/fixtures/data/wikitables/sample_data.examples")
 
-    def test_encoder_decoder_can_train_save_and_load(self):
+    def test_model_can_train_save_and_load(self):
         self.ensure_model_can_train_save_and_load(self.param_file)
