1bce5a7eb39f194da77b1478afc45dde6cbf4203
Mark Neumann <markng@allenai.org>
2017-12-11 11:45:43 -0800

    add positional encoding and stacked self attention

---
+++ allennlp/modules/seq2seq_encoders/stacked_self_attention.py
@@ -0,0 +1,156 @@
+from overrides import overrides
+import torch
+from torch.nn.modules import Dropout
+
+from allennlp.common import Params
+from allennlp.nn.util import add_positional_features
+from allennlp.modules.similarity_functions import MultiHeadedSimilarity, SimilarityFunction, DotProductSimilarity
+from allennlp.modules import FeedForward, LayerNorm
+from allennlp.modules.seq2seq_encoders.seq2seq_encoder import Seq2SeqEncoder
+from allennlp.modules.seq2seq_encoders.intra_sentence_attention import IntraSentenceAttentionEncoder
+
+
+@Seq2SeqEncoder.register("stacked_self_attention")
+class StackedSelfAttentionEncoder(Seq2SeqEncoder):
+
+    """
+    Implements a stacked self-attention encoder similar to the Transformer
+    architecture in `Attention is all you Need
+    <https://www.semanticscholar.org/paper/Attention-Is-All-You-Need-Vaswani-Shazeer/0737da0767d77606169cbf4187b83e1ab62f6077>`_ .
+
+    This encoder combines 3 layers in a 'block':
+    - A 2 layer FeedForward network.
+    - Multi-headed self attention, which uses 2 learnt linear projections
+      to perform a dot-product similarity between every pair of elements
+      scaled by the square root of the sequence length.
+    - Layer Normalisation.
+
+    These are then stacked into ``num_layers`` layers.
+
+    Parameters
+    ----------
+    input_dim : ``int``, required.
+        The input dimension of the encoder.
+    projection_dim : ``int``, required.
+        The dimension of the linear projections for the self-attention layers.
+    non_linear_hidden_dim : ``int``, required.
+        The middle dimension of the FeedForward network. The input and output
+        dimensions are fixed to ensure sizes match up for the self attention layers.
+    num_layers : ``int``, required.
+        The number of stacked self attention -> feedfoward -> layer normalisation blocks.
+    num_attention_heads : ``int``, required.
+        The number of attention heads to use per layer.
+    internal_similarity : ``SimilarityFunction``, optional (default = DotProductSimilarity(scale_output=True)).
+        The internal similarity function to use for creating the attention distributions per
+        attention head.
+    use_positional_encoding: ``bool``, optional, (default = True)
+        Whether to add sinusoidal frequencies to the input tensor. This is strongly recommended,
+        as without this feature, the self attention layers have no idea of absolute or relative
+        position (as they are just computing pairwise similarity between vectors of elements),
+        which can be important features for many tasks.
+    """
+
+    def __init__(self,
+                 input_dim: int,
+                 projection_dim: int,
+                 non_linear_hidden_dim: int,
+                 num_layers: int,
+                 num_attention_heads: int,
+                 internal_similarity: SimilarityFunction = DotProductSimilarity(scale_output=True),
+                 use_positional_encoding: bool = True,
+                 dropout_prob: float = 0.2) -> None:
+        super(StackedSelfAttentionEncoder, self).__init__()
+
+        self._use_positional_encoding = use_positional_encoding
+        self._attention_layers = []
+        self._feedfoward_layers = []
+        self._layer_norm_layers = []
+
+        for i in range(num_layers):
+            # Project output of attention encoder through a feedforward network
+            # and back to the input size for the next layer.
+            feedfoward = FeedForward(input_dim,
+                                     activations=[torch.nn.ReLU(), lambda x: x],
+                                     hidden_dims=[non_linear_hidden_dim, input_dim],
+                                     num_layers=2)
+
+            self.add_module(f"feedforward_{i}", feedfoward)
+            self._feedfoward_layers.append(feedfoward)
+
+            similarity_function = MultiHeadedSimilarity(num_attention_heads,
+                                                        tensor_1_dim=input_dim,
+                                                        internal_similarity=internal_similarity)
+            self_attention = IntraSentenceAttentionEncoder(input_dim,
+                                                           projection_dim,
+                                                           similarity_function,
+                                                           num_attention_heads,
+                                                           combination='1,2',
+                                                           output_projection_dim=input_dim)
+
+            self.add_module(f"self_attention_{i}", self_attention)
+            self._attention_layers.append(self_attention)
+
+            layer_norm = LayerNorm(self_attention.get_output_dim())
+            self.add_module(f"layer_norm_{i}", feedfoward)
+            self._layer_norm_layers.append(layer_norm)
+
+        self._input_dim = input_dim
+        # This is equal to projection_dim + hidden_dim, because the output of the self
+        # attention layer is the concatenation of the projected attention
+        # representation and the representation of the input token from the feedfoward
+        # network.
+        self._output_dim = self._attention_layers[-1].get_output_dim()
+        self._dropout = Dropout(p=dropout_prob)
+
+    @overrides
+    def get_input_dim(self) -> int:
+        return self._input_dim
+
+    @overrides
+    def get_output_dim(self) -> int:
+        return self._output_dim
+
+    def forward(self, inputs: torch.Tensor, mask: torch.Tensor):
+        if self._use_positional_encoding:
+            output = add_positional_features(inputs)
+        else:
+            output = inputs
+        for attention, feedforward, layer_norm in zip(self._attention_layers,
+                                                      self._feedfoward_layers,
+                                                      self._layer_norm_layers):
+            cached_input = output
+            # highway layers for all steps.
+
+            # shape (batch_size, timesteps, input_size)
+            non_linear_output = feedforward(output) + cached_input
+            # shape (batch_size, )
+            attention_output = attention(non_linear_output, mask) + non_linear_output
+            output = layer_norm(attention_output)
+        return output
+
+    @classmethod
+    def from_params(cls, params: Params):
+        input_dim = params.pop('input_dim')
+        projection_dim = params.pop('projection_dim', None)
+        non_linear_hidden_dim = params.pop("non_linear_hidden_dim")
+        num_layers = params.pop("num_layers", 2)
+        num_attention_heads = params.pop('num_attention_heads', 3)
+        internal_similarity = SimilarityFunction.from_params(params.pop('internal_similarity', {}))
+        dropout_prob = params.pop("dropout_prob", 0.2)
+
+        return cls(input_dim=input_dim,
+                   non_linear_hidden_dim=non_linear_hidden_dim,
+                   projection_dim=projection_dim,
+                   num_layers=num_layers,
+                   num_attention_heads=num_attention_heads,
+                   internal_similarity=internal_similarity,
+                   dropout_prob=dropout_prob)
+
+
+
+
+
+
+
+
+
--- allennlp/nn/util.py
+++ allennlp/nn/util.py
@@ -835,3 +835,56 @@ def remove_sentence_boundaries(tensor: torch.Tensor,
             new_mask[i, :(j - 2)] = 1
 
     return tensor_without_boundary_tokens, new_mask
+
+
+def add_positional_features(tensor: torch.Tensor,
+                            min_timescale: float = 1.0,
+                            max_timescale: float = 1.0e4):
+    """
+    Implements the frequency based positional encoding described
+    in `Attention is all you Need
+    <https://www.semanticscholar.org/paper/Attention-Is-All-You-Need-Vaswani-Shazeer/0737da0767d77606169cbf4187b83e1ab62f6077>`_ .
+
+    Adds sinusoids of different frequencies to a ``Tensor``. Each dimension of
+    the input ``Tensor`` is added to a sinusoid of a different frequency and phase.
+    This allows the attention heads to use absolute and relative positions.
+
+    The number of timescales is equal to hidden_dim / 2 within the range
+    (min_timescale, max_timescale). For each timescale, the two sinusoidal
+    signals  sin(timestep / timescale) and cos(timestep / timescale) are
+    generated and concatenated along the hidden_dim dimension.
+
+    Parameters
+    ----------
+    tensor : ``torch.Tensor``
+        a Tensor with shape (batch_size, timesteps, hidden_dim).
+    min_timescale : ``float``, optional (default = 1.0)
+        The smallest timescale to use.
+    max_timescale : ``float``, optional (default = 1.0e4)
+        The largest timescale to use.
+
+    Returns
+    -------
+    The input tensor augmented with the sinusoidal frequencies.
+    """
+    _, timesteps, hidden_dim = tensor.size()
+
+    timestep_range = torch.arange(0, timesteps)
+    # We're generating both cos and sin frequencies,
+    # so half for each.
+    num_timescales = hidden_dim // 2
+
+    log_timescale_increments = math.log(float(max_timescale) / float(min_timescale)) / (num_timescales - 1)
+    inverse_timescales = min_timescale * torch.exp(torch.arange(0, num_timescales)) * -log_timescale_increments
+
+    # Broadcasted multiplication - shape (timesteps, num_timescales)
+    scaled_time = timestep_range.unsqueeze(1) * inverse_timescales.unsqueeze(0)
+    # shape (timesteps, 2 * num_timescales)
+    sinusoids = Variable(torch.cat([torch.sin(scaled_time), torch.cos(scaled_time)], 1))
+    if hidden_dim % 2 != 0:
+        # if the number of dimensions is odd, the cos and sin
+        # timescales had size (hidden_dim - 1) / 2, so we need
+        # to add a row of zeros to make up the difference.
+        sinusoids = torch.cat([sinusoids, Variable(sinusoids.data.new(timesteps, 1).fill_(0))], 1)
+
+    return tensor + sinusoids.unsqueeze(0)
