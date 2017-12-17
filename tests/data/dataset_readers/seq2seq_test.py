9787b4f37abe6ea4b8dc7224dadae3d6bc2d7d99
Max Del <max.del.edu@gmail.com>
2017-12-17 20:11:01 +0200

    Make pylint happy

--- tests/data/dataset_readers/seq2seq_test.py
+++ tests/data/dataset_readers/seq2seq_test.py
@@ -10,12 +10,12 @@ class TestSeq2SeqDatasetReader(AllenNlpTestCase):
 
         assert len(dataset.instances) == 3
         fields = dataset.instances[0].fields
-        assert [t.text for t in fields["source_tokens"].tokens] == ["@@START@@", "this", "is", 
+        assert [t.text for t in fields["source_tokens"].tokens] == ["@@START@@", "this", "is",
                                                                     "a", "sentence", "@@END@@"]
         assert [t.text for t in fields["target_tokens"].tokens] == ["@@START@@", "this", "is",
                                                                     "a", "sentence", "@@END@@"]
         fields = dataset.instances[1].fields
-        assert [t.text for t in fields["source_tokens"].tokens] == ["@@START@@", "this", "is", 
+        assert [t.text for t in fields["source_tokens"].tokens] == ["@@START@@", "this", "is",
                                                                     "another", "@@END@@"]
         assert [t.text for t in fields["target_tokens"].tokens] == ["@@START@@", "this", "is",
                                                                     "another", "@@END@@"]
@@ -28,7 +28,7 @@ class TestSeq2SeqDatasetReader(AllenNlpTestCase):
     def test_source_add_start_token(self):
         reader = Seq2SeqDatasetReader(source_add_start_token=False)
         dataset = reader.read('tests/fixtures/data/seq2seq_copy.tsv')
-        
+
         assert len(dataset.instances) == 3
         fields = dataset.instances[0].fields
         assert [t.text for t in fields["source_tokens"].tokens] == ["this", "is", "a", "sentence", "@@END@@"]
