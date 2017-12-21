49704a4b419110c5491a3d06684745e5e3499fad
Mark Neumann <markng@allenai.org>
2017-12-21 17:40:25 +0000

    big refactor of SRL predictor to use support batch processing

--- allennlp/service/predictors/semantic_role_labeler.py
+++ allennlp/service/predictors/semantic_role_labeler.py
@@ -2,7 +2,7 @@ from typing import List, Tuple
 
 from overrides import overrides
 
-from allennlp.common.util import JsonDict, sanitize
+from allennlp.common.util import JsonDict, sanitize, group_by_count
 from allennlp.data import DatasetReader, Instance
 from allennlp.data.tokenizers.word_splitter import SpacyWordSplitter
 from allennlp.models import Model
@@ -41,15 +41,57 @@ class SemanticRoleLabelerPredictor(Predictor):
 
         return " ".join(frame)
 
-    def _json_to_instance(self, json_dict: JsonDict) -> Tuple[Instance, JsonDict]:
-        # We're overriding `predict_json` directly, so we don't need this.  But I'd rather have a
-        # useless stub here then make the base class throw a RuntimeError instead of a
-        # NotImplementedError - the checking on the base class is worth it.
-        raise RuntimeError("this should never be called")
+    def _sentence_to_srl_instances(self, json_dict: JsonDict) -> Tuple[List[Instance], JsonDict]:
+
+        sentence = json_dict["sentence"]
+        tokens = self._tokenizer.split_words(sentence)
+        words = [token.text for token in tokens]
+        result_dict: JsonDict = {"words": words, "verbs": []}
+        instances: List[Instance] = []
+        for i, word in enumerate(tokens):
+            if word.pos_ == "VERB":
+                verb = word.text
+                result_dict["verbs"].append(verb)
+                verb_labels = [0 for _ in words]
+                verb_labels[i] = 1
+                instance = self._dataset_reader.text_to_instance(tokens, verb_labels)
+                instances.append(instance)
+        return instances, result_dict
 
     @overrides
-    def _batch_json_to_instances(self, json_dicts: List[JsonDict]) -> List[Instance]:
-        raise NotImplementedError("The SRL Predictor does not currently support batch prediction.")
+    def predict_batch_json(self, inputs: List[JsonDict], cuda_device: int = -1) -> List[JsonDict]:
+
+        batch_size = len(inputs)
+        instances_per_sentence, return_dicts = zip(*[self._sentence_to_srl_instances(json)
+                                                     for json in inputs])
+
+        flattened_instances = sum(instances_per_sentence, [])
+        batched_instances = group_by_count(flattened_instances, batch_size, None)
+        batched_instances[-1] = [instance for instance in batched_instances
+                                 if instance is not None]
+        outputs = []
+        for batch in batched_instances:
+            outputs.extend(self._model.forward_on_instances(batch, cuda_device))
+
+        sentence_index = 0
+        for results in return_dicts:
+            verbs_for_sentence: List[str] = results["verbs"]
+            results["verbs"] = []
+            for verb in verbs_for_sentence:
+
+                output = outputs[sentence_index]
+                tags = output['tags']
+                description = SemanticRoleLabelerPredictor.make_srl_string(results["words"], tags)
+                results["verbs"].append({
+                        "verb": verb,
+                        "description": description,
+                        "tags": tags,
+                })
+                sentence_index += 1
+
+            results["tokens"] = results["words"]
+
+        return return_dicts
 
     @overrides
     def predict_json(self, inputs: JsonDict, cuda_device: int = -1) -> JsonDict:
@@ -66,28 +108,20 @@ class SemanticRoleLabelerPredictor(Predictor):
                 {"verb": "...", "description": "...", "tags": [...]},
             ]}
         """
-        sentence = inputs["sentence"]
+        instances, results = self._sentence_to_srl_instances(inputs)
+        verbs_for_instances: List[str] = results["verbs"]
+        results["verbs"] = []
 
-        tokens = self._tokenizer.split_words(sentence)
-        words = [token.text for token in tokens]
-        results: JsonDict = {"words": words, "verbs": []}
-        for i, word in enumerate(tokens):
-            if word.pos_ == "VERB":
-                verb = word.text
-                verb_labels = [0 for _ in words]
-                verb_labels[i] = 1
-                instance = self._dataset_reader.text_to_instance(tokens, verb_labels)
-                output = self._model.forward_on_instance(instance, cuda_device)
+        outputs = self._model.forward_on_instances(instances, cuda_device)
+
+        for output, verb in zip(outputs, verbs_for_instances):
                 tags = output['tags']
-
-                description = SemanticRoleLabelerPredictor.make_srl_string(words, tags)
-
+                description = SemanticRoleLabelerPredictor.make_srl_string(results["words"], tags)
                 results["verbs"].append({
                         "verb": verb,
                         "description": description,
                         "tags": tags,
                 })
 
-        results["tokens"] = words
-
+        results["tokens"] = results["words"]
         return sanitize(results)
