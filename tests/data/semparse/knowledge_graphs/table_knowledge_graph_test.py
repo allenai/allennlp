d44c400d0c7280a079f524f6fad2034508cacdc6
Matt Gardner <mattg@allenai.org>
2018-01-12 15:35:36 -0800

    Better KnowledgeGraphField (#663)
    * Got the cell text encoding working in KnowledgeGraphField
    
    * Finished the entity linking features
    
    * Stub out the link embedding and linking score components

--- tests/data/semparse/knowledge_graphs/table_knowledge_graph_test.py
+++ tests/data/semparse/knowledge_graphs/table_knowledge_graph_test.py
@@ -10,11 +10,20 @@ class TestTableKnowledgeGraph(AllenNlpTestCase):
                 'cells': [['Paradeniz', 'Mersin'],
                           ['Lake Gala', 'Edirne']]
                 }
-        knowledge_graph = TableKnowledgeGraph.read_from_json(json)
-        neighbors = set(knowledge_graph.get_cell_neighbors('fb:cell.mersin'))
+        graph = TableKnowledgeGraph.read_from_json(json)
+        neighbors = set(graph.neighbors['fb:cell.mersin'])
+        assert graph.entities == ['fb:cell.edirne', 'fb:cell.lake_gala', 'fb:cell.mersin',
+                                  'fb:cell.paradeniz', 'fb:row.row.location',
+                                  'fb:row.row.name_in_english']
         assert neighbors == {'fb:row.row.location'}
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row.name_in_english'))
+        neighbors = set(graph.neighbors['fb:row.row.name_in_english'])
         assert neighbors == {'fb:cell.paradeniz', 'fb:cell.lake_gala'}
+        assert graph.entity_text['fb:cell.edirne'] == 'Edirne'
+        assert graph.entity_text['fb:cell.lake_gala'] == 'Lake Gala'
+        assert graph.entity_text['fb:cell.mersin'] == 'Mersin'
+        assert graph.entity_text['fb:cell.paradeniz'] == 'Paradeniz'
+        assert graph.entity_text['fb:row.row.location'] == 'Location'
+        assert graph.entity_text['fb:row.row.name_in_english'] == 'Name in English'
 
     def test_read_from_json_handles_diacritics(self):
         json = {
@@ -22,8 +31,8 @@ class TestTableKnowledgeGraph(AllenNlpTestCase):
                 'cells': [['Lake Van', 'Van Gölü', 'Mersin'],
                           ['Lake Gala', 'Gala Gölü', 'Edirne']]
                 }
-        knowledge_graph = TableKnowledgeGraph.read_from_json(json)
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row.name_in_turkish'))
+        graph = TableKnowledgeGraph.read_from_json(json)
+        neighbors = set(graph.neighbors['fb:row.row.name_in_turkish'])
         assert neighbors == {'fb:cell.van_golu', 'fb:cell.gala_golu'}
 
     def test_read_from_json_handles_newlines_in_columns(self):
@@ -34,20 +43,20 @@ class TestTableKnowledgeGraph(AllenNlpTestCase):
                 'cells': [['1', '2'],
                           ['3', '4']]
                 }
-        knowledge_graph = TableKnowledgeGraph.read_from_json(json)
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row.peak_aus'))
+        graph = TableKnowledgeGraph.read_from_json(json)
+        neighbors = set(graph.neighbors['fb:row.row.peak_aus'])
         assert neighbors == {'fb:cell.1', 'fb:cell.3'}
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row.peak_nz'))
+        neighbors = set(graph.neighbors['fb:row.row.peak_nz'])
         assert neighbors == {'fb:cell.2', 'fb:cell.4'}
-        neighbors = set(knowledge_graph.get_cell_neighbors('fb:cell.1'))
+        neighbors = set(graph.neighbors['fb:cell.1'])
         assert neighbors == {'fb:row.row.peak_aus'}
 
         json = {
                 'columns': ['Title'],
                 'cells': [['Dance of the\\nSeven Veils']]
                 }
-        knowledge_graph = TableKnowledgeGraph.read_from_json(json)
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row.title'))
+        graph = TableKnowledgeGraph.read_from_json(json)
+        neighbors = set(graph.neighbors['fb:row.row.title'])
         assert neighbors == {'fb:cell.dance_of_the_seven_veils'}
 
     def test_read_from_json_handles_diacritics_and_newlines(self):
@@ -55,8 +64,8 @@ class TestTableKnowledgeGraph(AllenNlpTestCase):
                 'columns': ['Notes'],
                 'cells': [['8 districts\nFormed from Orūzgān Province in 2004']]
                 }
-        knowledge_graph = TableKnowledgeGraph.read_from_json(json)
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row.notes'))
+        graph = TableKnowledgeGraph.read_from_json(json)
+        neighbors = set(graph.neighbors['fb:row.row.notes'])
         assert neighbors == {'fb:cell.8_districts_formed_from_oruzgan_province_in_2004'}
 
     def test_read_from_json_handles_crazy_unicode(self):
@@ -65,8 +74,8 @@ class TestTableKnowledgeGraph(AllenNlpTestCase):
                 'cells': [['Viðareiði'],
                           ['Funningsfjørður']]
                 }
-        knowledge_graph = TableKnowledgeGraph.read_from_json(json)
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row.town'))
+        graph = TableKnowledgeGraph.read_from_json(json)
+        neighbors = set(graph.neighbors['fb:row.row.town'])
         assert neighbors == {'fb:cell.funningsfj_r_ur',
                              'fb:cell.vi_arei_i',
                              }
@@ -78,8 +87,8 @@ class TestTableKnowledgeGraph(AllenNlpTestCase):
                           ['South Korea (KOR)'],
                           ['Area (km²)']]
                 }
-        knowledge_graph = TableKnowledgeGraph.read_from_json(json)
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row.urban_settlements'))
+        graph = TableKnowledgeGraph.read_from_json(json)
+        neighbors = set(graph.neighbors['fb:row.row.urban_settlements'])
         assert neighbors == {'fb:cell.dzhebariki_khaya',
                              'fb:cell.south_korea_kor',
                              'fb:cell.area_km'}
@@ -90,10 +99,10 @@ class TestTableKnowledgeGraph(AllenNlpTestCase):
                 'cells': [['1', '2'],
                           ['3', '4']]
                 }
-        knowledge_graph = TableKnowledgeGraph.read_from_json(json)
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row._of_votes'))
+        graph = TableKnowledgeGraph.read_from_json(json)
+        neighbors = set(graph.neighbors['fb:row.row._of_votes'])
         assert neighbors == {'fb:cell.1', 'fb:cell.3'}
-        neighbors = set(knowledge_graph.get_column_neighbors('fb:row.row._of_votes_2'))
+        neighbors = set(graph.neighbors['fb:row.row._of_votes_2'])
         assert neighbors == {'fb:cell.2', 'fb:cell.4'}
-        neighbors = set(knowledge_graph.get_cell_neighbors('fb:cell.1'))
+        neighbors = set(graph.neighbors['fb:cell.1'])
         assert neighbors == {'fb:row.row._of_votes'}
