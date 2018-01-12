46bdc6ef2a7df8b5c8e1fc76bfdb7cd3e59ffa8d
Matt Gardner <mattg@allenai.org>
2018-01-12 19:01:34 +0000

    Added a batch_tokenize method to the tokenizers

--- allennlp/data/tokenizers/word_splitter.py
+++ allennlp/data/tokenizers/word_splitter.py
@@ -17,6 +17,15 @@ class WordSplitter(Registrable):
     """
     default_implementation = 'spacy'
 
+    def batch_split_words(self, sentences: List[str]) -> List[List[Token]]:
+        """
+        Spacy needs to do batch processing, or it can be really slow.  This method lets you take
+        advantage of that if you want.  Default implementation is to just iterate of the sentences
+        and call ``split_words``, but the ``SpacyWordSplitter`` will actually do batched
+        processing.
+        """
+        return [self.split_words(sentence) for sentence in senteces]
+
     def split_words(self, sentence: str) -> List[Token]:
         """
         Splits ``sentence`` into a list of :class:`Token` objects.
@@ -169,6 +178,10 @@ class SpacyWordSplitter(WordSplitter):
                  parse: bool = False,
                  ner: bool = False) -> None:
         self.spacy = get_spacy_model(language, pos_tags, parse, ner)
+
+    @overrides
+    def batch_split_words(self, sentences: List[str]) -> List[List[Token]]:
+        return self.spacy.pipe(sentences)
 
     @overrides
     def split_words(self, sentence: str) -> List[Token]:
