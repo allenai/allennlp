976c9f033da815965b3362882c5e1364936ac99b
Matt Gardner <mattg@allenai.org>
2018-01-12 14:12:22 -0800

    Train parser on actual wikitables data (#639)
    * Trying to get this to run with real data, lots of data loading fixes
    
    * Trying to figure out a few more things...
    
    * Got the parser actually training on most of the data
    
    This was a lot of work figuring out why some logical forms wouldn't
    parse, and fixing the problems, adding test cases as I went.  I also
    found some bugs in the logic for computing new states, so I wrote some
    more test cases for the decoder, and fixed the bugs.  This now
    successfully reads in the subsampled data and starts training, though
    there is still a problematic instance that causes the training to crash
    partway through.  Looking into that next...
    
    * Make it so that parsing logical forms doesn't change world state
    
    * Fixed a bug with batched input
    
    * Add a parse accuracy metric, with code provided by Oyvind
    
    * Pylint
    
    * Fix dataset reader test
    
    * Fixed tests
    
    * Fix typo
    
    * Removed unused import
    
    * Fix mypy
    
    * Remove now-unnecessary large sample file
    
    * Address PR feedback
    
    * Add a TODO about sorting the LFs

--- allennlp/data/semparse/worlds/wikitables_world.py
+++ allennlp/data/semparse/worlds/wikitables_world.py
@@ -10,7 +10,7 @@ from nltk.sem.logic import Type
 from overrides import overrides
 
 from allennlp.data.tokenizers import Token
-from allennlp.data.semparse.worlds.world import World
+from allennlp.data.semparse.worlds.world import ParsingError, World
 from allennlp.data.semparse.type_declarations import wikitables_type_declaration as types
 from allennlp.data.semparse.knowledge_graphs import TableKnowledgeGraph
 
@@ -36,18 +36,21 @@ class WikiTablesWorld(World):
                                               global_name_mapping=types.COMMON_NAME_MAPPING,
                                               num_nested_lambdas=1)
         self.table_graph = table_graph
+        self.question_tokens = [token.text for token in question_tokens]
 
         # For every new Sempre column name seen, we update this counter to map it to a new NLTK name.
         self._column_counter = 0
 
-        # This adds all of the cell and column names to our local name mapping, so we can get them
-        # as valid actions in the parser.
-        for entity in table_graph.get_all_entities():
-            self._map_name(entity)
+        # This adds all of the cell and column names to our local name mapping, including null
+        # cells and columns, so we can get them as valid actions in the parser.  The null cell and
+        # column are used to check against empty sets, e.g., for questions like "Is there a team
+        # that won three times in a row?".
+        for entity in table_graph.get_all_entities() + ['fb:cell.null', 'fb:row.row.null']:
+            self._map_name(entity, keep_mapping=True)
 
-        numbers = self._get_numbers_from_tokens(question_tokens) + list(str(i) for i in range(10))
+        numbers = self._get_numbers_from_tokens(question_tokens) + list(str(i) for i in range(-1, 10))
         for number in numbers:
-            self._add_name_mapping(number, number, types.CELL_TYPE)
+            self._map_name(number, keep_mapping=True)
 
     def _get_numbers_from_tokens(self, tokens: List[Token]) -> List[str]:
         """
@@ -56,15 +59,30 @@ class WikiTablesWorld(World):
         Eventually, we'd want this to detect things like ordinals ("first", "third") and cardinals
         ("one", "two"), but for now we just look for literal digits, and make up for missing
         ordinals and cardinals by adding all single-digit numbers as possible numbers to output.
+
+        We also handle year ranges expressed as decade or centuries ("1800s" or "1950s"), adding
+        the endpoints of the range as possible numbers to generate.
         """
         # pylint: disable=no-self-use
         numbers = []
         for token in tokens:
             # We'll use a check for float(text) to find numbers, because text.isdigit() doesn't
             # catch things like "-3" or "0.07".
+            text = token.text.replace(',', '')
+            is_range = False
+            if len(text) > 1 and text[-1] == 's' and text[-2] == '0':
+                is_range = True
+                text = text[:-1]
             try:
-                float(token.text)
-                numbers.append(token.text)
+                number = float(text)
+                if '.' in text:
+                    text = '%.3f' % number
+                numbers.append(text)
+                if is_range:
+                    num_zeros = 1
+                    while text[-(num_zeros + 1)] == '0':
+                        num_zeros += 1
+                    numbers.append(str(int(number + 10 ** num_zeros)))
             except ValueError:
                 pass
         return numbers
@@ -74,20 +92,10 @@ class WikiTablesWorld(World):
         return types.BASIC_TYPES
 
     @overrides
-    def _map_name(self, name: str) -> str:
-        """
-        Takes the name of a predicate or a constant as used by Sempre, maps it to a unique string such that
-        NLTK processes it appropriately. This is needed because NLTK has a naming convention for variables:
-            Function variables: Single upper case letter optionally followed by digits
-            Individual variables: Single lower case letter (except e for events) optionally followed by digits
-            Constants: Everything else
-
-        Parameters
-        ----------
-        name : str
-            Token from Sempre's logical form.
-        """
+    def _map_name(self, name: str, keep_mapping: bool = False) -> str:
         if name not in types.COMMON_NAME_MAPPING and name not in self.local_name_mapping:
+            if not keep_mapping:
+                raise ParsingError(f"Encountered un-mapped name: {name}")
             if name.startswith("fb:row.row"):
                 # Column name
                 translated_name = "C%d" % self._column_counter
@@ -109,7 +117,9 @@ class WikiTablesWorld(World):
                     # The string is a negative number. This makes NLTK interpret this as a negated expression
                     # and force its type to be TRUTH_VALUE (t).
                     translated_name = translated_name.replace("-", "~")
-                self._add_name_mapping(name, translated_name)
+                    # TODO(mattg): bare numbers are treated as cells by the type system.  This
+                    # might not actually be correct...
+                self._add_name_mapping(name, translated_name, types.CELL_TYPE)
         else:
             if name in types.COMMON_NAME_MAPPING:
                 translated_name = types.COMMON_NAME_MAPPING[name]
