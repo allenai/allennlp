3aa33139216ff3f597fe2630d44aa69afe4d97a7
Mark Neumann <markn@allenai.org>
2018-01-17 16:29:50 -0800

    Coref data fix (#691)
    * add iterator for multiple docs per file, change coref reader accordingly
    
    * add back script for compiling coref data
    
    * add tests and fixtures
    
    * add tqdm
    
    * add newlines

--- tests/models/coreference_resolution/coref_test.py
+++ tests/models/coreference_resolution/coref_test.py
@@ -9,7 +9,7 @@ class CorefTest(ModelTestCase):
     def setUp(self):
         super(CorefTest, self).setUp()
         self.set_up_model('tests/fixtures/coref/experiment.json',
-                          'tests/fixtures/conll_2012/')
+                          'tests/fixtures/coref/coref.gold_conll')
 
     def test_coref_model_can_train_save_and_load(self):
         self.ensure_model_can_train_save_and_load(self.param_file)
