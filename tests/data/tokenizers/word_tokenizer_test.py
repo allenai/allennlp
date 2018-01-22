6de331b6e4eff01ef9e86f991d7c1c820f2fe902
Matt Gardner <mattg@allenai.org>
2018-01-22 14:06:38 -0800

    Allow tokenizers to do batch tokenization (#680)
    * Added a batch_tokenize method to the tokenizers
    
    * Let spacy figure out how many threads to use
    
    * Fix bug
    
    * Fix mypy

--- tests/data/tokenizers/word_tokenizer_test.py
+++ tests/data/tokenizers/word_tokenizer_test.py
@@ -13,6 +13,20 @@ class TestWordTokenizer(AllenNlpTestCase):
                            "punctuation", "\"", ".", "^^"]
         assert tokens == expected_tokens
 
+    def test_batch_tokenization(self):
+        tokenizer = WordTokenizer()
+        sentences = ["This is a sentence",
+                     "This isn't a sentence.",
+                     "This is the 3rd sentence."
+                     "Here's the 'fourth' sentence."]
+        batch_tokenized = tokenizer.batch_tokenize(sentences)
+        separately_tokenized = [tokenizer.tokenize(sentence) for sentence in sentences]
+        assert len(batch_tokenized) == len(separately_tokenized)
+        for batch_sentence, separate_sentence in zip(batch_tokenized, separately_tokenized):
+            assert len(batch_sentence) == len(separate_sentence)
+            for batch_word, separate_word in zip(batch_sentence, separate_sentence):
+                assert batch_word.text == separate_word.text
+
     def test_stems_and_filters_correctly(self):
         tokenizer = WordTokenizer.from_params(Params({'word_stemmer': {'type': 'porter'},
                                                       'word_filter': {'type': 'stopwords'}}))
