a17f8a3bd97da25f6d189cdad36f751a4929b3b5
Mark Neumann <markn@allenai.org>
2018-01-23 12:31:49 -0800

    Span field (#707)
    * span field
    
    * docs
    
    * PR feedback
    
    * remove unused imports

--- allennlp/data/fields/__init__.py
+++ allennlp/data/fields/__init__.py
@@ -11,4 +11,5 @@ from allennlp.data.fields.list_field import ListField
 from allennlp.data.fields.metadata_field import MetadataField
 from allennlp.data.fields.sequence_field import SequenceField
 from allennlp.data.fields.sequence_label_field import SequenceLabelField
+from allennlp.data.fields.span_field import SpanField
 from allennlp.data.fields.text_field import TextField
