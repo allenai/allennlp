2636869bb6b9b11f3e783cbf08c24171b739b561
Matt Gardner <mattg@allenai.org>
2018-01-24 11:47:43 -0800

    Speed up WikiTablesDatasetReader (#688)
    * Sped up the WikiTablesDatasetReader by a large factor
    
    * Removing pyparsing from our code, as it is really slow
    
    * Removed pyparsing from NLVR code
    
    * Fixed tests, pylint and mypy (I think)
    
    * Added docs for semparse util

---
+++ allennlp/data/semparse/util.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def lisp_to_nested_expression(lisp_string: str) -> List:
+    """
+    Takes a logical form as a lisp string and returns a nested list representation of the lisp.
+    For example, "(count (division first))" would get mapped to ['count', ['division', 'first']].
+    """
+    stack: List = []
+    current_expression: List = []
+    tokens = lisp_string.split()
+    for token in tokens:
+        while token[0] == '(':
+            nested_expression: List = []
+            current_expression.append(nested_expression)
+            stack.append(current_expression)
+            current_expression = nested_expression
+            token = token[1:]
+        current_expression.append(token.replace(')', ''))
+        while token[-1] == ')':
+            current_expression = stack.pop()
+            token = token[:-1]
+    return current_expression
--- tests/data/semparse/type_declarations/grammar_state_test.py
+++ tests/data/semparse/type_declarations/grammar_state_test.py
@@ -14,23 +14,23 @@ class TestGrammarState(AllenNlpTestCase):
 
     def test_get_valid_actions_uses_top_of_stack(self):
         state = GrammarState(['s'], {}, {'s': [1, 2], 't': [3, 4]}, {})
-        assert state.get_valid_actions() == [1,2]
+        assert state.get_valid_actions() == [1, 2]
         state = GrammarState(['t'], {}, {'s': [1, 2], 't': [3, 4]}, {})
-        assert state.get_valid_actions() == [3,4]
+        assert state.get_valid_actions() == [3, 4]
         state = GrammarState(['e'], {}, {'s': [1, 2], 't': [3, 4], 'e': []}, {})
         assert state.get_valid_actions() == []
 
     def test_get_valid_actions_adds_lambda_productions(self):
         state = GrammarState(['s'], {('s', 'x'): ['s']}, {'s': [1, 2]}, {'s -> x': 5})
-        assert state.get_valid_actions() == [1,2,5]
+        assert state.get_valid_actions() == [1, 2, 5]
         # We're doing this assert twice to make sure we haven't accidentally modified the state.
-        assert state.get_valid_actions() == [1,2,5]
+        assert state.get_valid_actions() == [1, 2, 5]
 
     def test_get_valid_actions_adds_lambda_productions_only_for_correct_type(self):
         state = GrammarState(['t'], {('s', 'x'): ['t']}, {'s': [1, 2], 't': [3, 4]}, {'s -> x': 5})
-        assert state.get_valid_actions() == [3,4]
+        assert state.get_valid_actions() == [3, 4]
         # We're doing this assert twice to make sure we haven't accidentally modified the state.
-        assert state.get_valid_actions() == [3,4]
+        assert state.get_valid_actions() == [3, 4]
 
     def test_take_action_gives_correct_next_states_with_non_lambda_productions(self):
         # state.take_action() doesn't read or change these objects, it just passes them through, so
