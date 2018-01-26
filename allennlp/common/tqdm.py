ac3513a741cc4f69f6bebe81dc17bd4e557c3934
Michael Schmitz <michael@schmitztech.com>
2018-01-26 14:11:11 -0800

    mypy

--- allennlp/common/tqdm.py
+++ allennlp/common/tqdm.py
@@ -4,10 +4,10 @@ This wraps tqdm so we can add configurable global defaults for certain tqdm para
 from tqdm import tqdm as _tqdm
 
 class Tqdm:
-    default_mininterval: int = 0.1
+    default_mininterval: float = 0.1
 
     @staticmethod
-    def set_default_mininterval(value: int) -> None:
+    def set_default_mininterval(value: float) -> None:
         Tqdm.default_mininterval = value
 
     @staticmethod
