c6bbd97ab1fe39dc16fed43dd5a219f63083c059
Michael Schmitz <michael@schmitztech.com>
2018-01-30 13:31:26 -0800

    Squelch exception raised by failed subcommands.

--- scripts/verify.py
+++ scripts/verify.py
@@ -6,32 +6,38 @@
 import argparse
 
 from subprocess import run
+from subprocess import CalledProcessError
 import os
 import shutil
 
 def main(checks):
-    print("Verifying with " + str(checks))
-    if "pytest" in checks:
-        print("Tests (pytest):", flush=True)
-        run("pytest -v --color=yes", shell=True, check=True)
+    try:
+        print("Verifying with " + str(checks))
+        if "pytest" in checks:
+            print("Tests (pytest):", flush=True)
+            run("pytest -v --color=yes", shell=True, check=True)
 
-    if "pylint" in checks:
-        print("Linter (pylint):", flush=True)
-        run("pylint -d locally-disabled,locally-enabled -f colorized allennlp tests", shell=True, check=True)
-        print("pylint checks passed")
+        if "pylint" in checks:
+            print("Linter (pylint):", flush=True)
+            run("pylint -d locally-disabled,locally-enabled -f colorized allennlp tests", shell=True, check=True)
+            print("pylint checks passed")
 
-    if "mypy" in checks:
-        print("Typechecker (mypy):", flush=True)
-        run("mypy allennlp --ignore-missing-imports", shell=True, check=True)
-        print("mypy checks passed")
+        if "mypy" in checks:
+            print("Typechecker (mypy):", flush=True)
+            run("mypy allennlp --ignore-missing-imports", shell=True, check=True)
+            print("mypy checks passed")
 
-    if "build-docs" in checks:
-        print("Documentation (build):", flush=True)
-        run("cd docs; make html-strict", shell=True, check=True)
+        if "build-docs" in checks:
+            print("Documentation (build):", flush=True)
+            run("cd docs; make html-strict", shell=True, check=True)
 
-    if "checks-docs" in checks:
-        print("Documentation (check):", flush=True)
-        run("./script/check_docs.py", shell=True, check=True)
+        if "checks-docs" in checks:
+            print("Documentation (check):", flush=True)
+            run("./script/check_docs.py", shell=True, check=True)
+
+    except CalledProcessError:
+        # squelch
+        pass
 
 if __name__ == "__main__":
     checks = ['pytest', 'pylint', 'mypy', 'build-docs', 'check-docs']
