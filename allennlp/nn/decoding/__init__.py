afef2711f1f5d5bcb805cd2098e6a792d836e16e
Pradeep Dasigi <pradeep.dasigi@gmail.com>
2018-01-30 16:04:40 -0800

    Initial NLVR semantic parser (#607)
    * nlvr semantic parser and dataset reader
    
    * Experiment config
    
    * NLVR semantic parser
    
    * Better docstrings
    
    * use production rule field
    
    * Fix filename in test
    
    * Fixed mypy issues, added tests, made agenda use ProductionRuleField
    
    * Yes another mypy issue fix
    
    * Monor changes
    
    * Agenda now contains indices to actions
    
    * Use agenda's mask while scoring checklists
    
    * Fixed most of PR comments. Parser still runs.
    
    * Removed dependence on wikitables parser, decoder state and decoder step
    
    * Fixed mypy issue
    
    * mypy and docstring fixes
    
    * One more doc fix
    
    * Changed checklist score, fixed beam pruning in trainer, removed unnecessary decoder beam search
    
    * Sort finished states

--- allennlp/nn/decoding/__init__.py
+++ allennlp/nn/decoding/__init__.py
@@ -3,3 +3,4 @@ from allennlp.nn.decoding.decoder_step import DecoderStep
 from allennlp.nn.decoding.decoder_state import DecoderState
 from allennlp.nn.decoding.decoder_trainer import DecoderTrainer
 from allennlp.nn.decoding.maximum_marginal_likelihood import MaximumMarginalLikelihood
+from allennlp.nn.decoding.beam_search_trainer import BeamSearchTrainer
