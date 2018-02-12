1e71030f011cb7ae3fa46a82365bf5285ae3985a
rajas <rajas@cs.washington.edu>
2018-02-12 15:24:40 -0800

    Logical form is run against sempre, and answer displayed in demo.

--- allennlp/service/predictors/wikitables_parser.py
+++ allennlp/service/predictors/wikitables_parser.py
@@ -1,10 +1,17 @@
 from typing import Tuple
 from overrides import overrides
+import os
+from subprocess import run
 
-from allennlp.common.util import JsonDict
+from allennlp.common.file_utils import cached_path
+from allennlp.common.util import JsonDict, sanitize
 from allennlp.data import Instance
 from allennlp.service.predictors.predictor import Predictor
 
+
+DEFAULT_EXECUTOR_JAR = "https://s3-us-west-2.amazonaws.com/allennlp/misc/wikitables-executor-0.1.0.jar"
+ABBREVIATIONS_FILE = "https://s3-us-west-2.amazonaws.com/allennlp/misc/wikitables-abbreviations.tsv"
+GROW_FILE = "https://s3-us-west-2.amazonaws.com/allennlp/misc/wikitables-grow.grammar"
 
 @Predictor.register('wikitables-parser')
 class WikiTablesParserPredictor(Predictor):
@@ -36,3 +43,48 @@ class WikiTablesParserPredictor(Predictor):
                                                          tokenized_question=tokenized_question)
         extra_info = {'question_tokens': tokenized_question}
         return instance, extra_info
+
+    @overrides
+    def predict_json(self, inputs: JsonDict, cuda_device: int = -1) -> JsonDict:
+        instance, return_dict = self._json_to_instance(inputs)
+        outputs = self._model.forward_on_instance(instance, cuda_device)
+
+        os.makedirs('data', exist_ok=True)
+        if not os.path.exists('data/abbreviations.tsv'):
+            run(f'wget {ABBREVIATIONS_FILE}', shell=True)
+            run('mv wikitables-abbreviations.tsv data/abbreviations.tsv', shell=True)
+        if not os.path.exists('data/grow.grammar'):
+            run(f'wget {GROW_FILE}', shell=True)
+            run('mv wikitables-grow.grammar data/grow.grammar', shell=True)
+
+        logical_form_filename = os.path.join('data', 'logical_forms.txt')
+        with open(logical_form_filename, 'w') as f:
+            f.write(outputs['logical_form'] + '\n')
+
+        table_dir = 'data/csv/'
+        os.makedirs(table_dir, exist_ok=True)
+        table_filename = 'context.csv'
+        with open(table_dir + table_filename, 'w', encoding='utf-8') as f:
+            f.write(inputs["table"])
+
+        test_record = ('(example (id %s) (utterance %s) (context (graph tables.TableKnowledgeGraph %s)) (targetValue (list (description "6"))))' % ('nt-0', inputs['question'], table_filename))
+        test_data_filename = 'data/data.examples'
+        with open(test_data_filename, 'w') as f:
+            f.write(test_record)
+
+        command = ' '.join(['java',
+                            '-jar',
+                            cached_path(DEFAULT_EXECUTOR_JAR),
+                            test_data_filename,
+                            logical_form_filename,
+                            table_dir,
+                            ])
+        run(command, shell=True)
+
+        denotations_file = 'data/logical_forms_denotations.tsv'
+        with open(denotations_file) as f:
+            line = f.readline().split('\t')
+            outputs['answer'] = line[1] if len(line) > 1 else line[0]
+
+        return_dict.update(outputs)
+        return sanitize(return_dict)
