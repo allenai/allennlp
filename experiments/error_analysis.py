18f61ca2f3463a722c50722dd817c44ca69e9e3c
rlebras <ronanlb@allenai.org>
2018-02-13 10:46:48 -0800

    error_analysis

--- experiments/error_analysis.py
+++ experiments/error_analysis.py
@@ -20,14 +20,14 @@ def main(argv):
     else:
         device = -1
             
-    chunker = NeuralChunker('encoder.final.pt','decoder.final.pt', output_lang, max_length)
+    chunker, pairs, pairs_dev = initializeChunker('encoder.bootstrap.pt','decoder.bootstrap.pt','data/mturk.005.train.txt','data/mturk.005.dev.txt',max_input_length = 120)
 
     encoder1 = chunker.encoder
     attn_decoder1 = chunker.decoder
     
-    print("*** starting evaluation ***")
+    print("*** starting training ***")
     if use_cuda:
-        print("*** using cuda to eval ***")
+        print("*** using cuda to train ***")
         encoder1 = encoder1.cuda()
         attn_decoder1 = attn_decoder1.cuda()
 
@@ -36,10 +36,8 @@ def main(argv):
     #    trainItersElmo(encoder1, attn_decoder1, output_lang, 750, 200, pairs, pairs_dev, max_length, print_every=1, save_every=10)
     print("*** done training ***")
     print(validate(encoder1, attn_decoder1, output_lang, pairs_dev, max_length, 460))
-#print(validate(torch.load('encoder.final.pt'), torch.load('decoder.final.pt'), output_lang, pairs_dev, max_length, 100))
-#    torch.save(encoder1, 'encoder.final.pt')
+#torch.save(encoder1, 'encoder.final.pt')
 #    torch.save(attn_decoder1, 'decoder.final.pt')
-
 
 if __name__ == "__main__":
     main(sys.argv)
