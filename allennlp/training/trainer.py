ca6e72e3b7cd0b119b0eb36c37d4a45da06b81d1
Matthew Peters <matt-peters@users.noreply.github.com>
2018-02-15 15:48:00 -0800

    Fix clamping in LSTMCellWithProjection (#862)
    * More logging options in trainer
    
    * Don't clamp in place
    
    * cleanup

--- allennlp/training/trainer.py
+++ allennlp/training/trainer.py
@@ -149,6 +149,7 @@ class Trainer:
                  grad_norm: Optional[float] = None,
                  grad_clipping: Optional[float] = None,
                  learning_rate_scheduler: Optional[PytorchLRScheduler] = None,
+                 summary_interval: int = 100,
                  histogram_interval: int = None) -> None:
         """
         Parameters
@@ -205,6 +206,8 @@ class Trainer:
             provided to determine if learning has plateaued.  To support updating the learning
             rate on every batch, this can optionally implement ``step_batch(batch_num)`` which
             updates the learning rate given the batch number.
+        summary_interval: ``int``, optional, (default = 100)
+            Number of batches between logging scalars to tensorboard
         histogram_interval : ``int``, optional, (default = ``None``)
             If not None, then log histograms to tensorboard every ``histogram_interval`` batches.
             When this parameter is specified, the following additional logging is enabled:
@@ -268,7 +271,7 @@ class Trainer:
             self._model = self._model.cuda(self._cuda_devices[0])
 
         self._log_interval = 10  # seconds
-        self._summary_interval = 100  # num batches between logging to tensorboard
+        self._summary_interval = summary_interval
         self._histogram_interval = histogram_interval
         self._log_histograms_this_batch = False
         self._batch_num_total = 0
@@ -870,6 +873,13 @@ class Trainer:
         else:
             scheduler = None
 
+        num_serialized_models_to_keep = params.pop_int("num_serialized_models_to_keep", None)
+        keep_serialized_model_every_num_seconds = params.pop_int(
+                "keep_serialized_model_every_num_seconds", None)
+        model_save_interval = params.pop_float("model_save_interval", None)
+        summary_interval = params.pop_int("summary_interval", 100)
+        histogram_interval = params.pop_int("histogram_interval", None)
+
         params.assert_empty(cls.__name__)
         return Trainer(model, optimizer, iterator,
                        train_data, validation_data,
@@ -880,4 +890,9 @@ class Trainer:
                        cuda_device=cuda_device,
                        grad_norm=grad_norm,
                        grad_clipping=grad_clipping,
-                       learning_rate_scheduler=scheduler)
+                       learning_rate_scheduler=scheduler,
+                       num_serialized_models_to_keep=num_serialized_models_to_keep,
+                       keep_serialized_model_every_num_seconds=keep_serialized_model_every_num_seconds,
+                       model_save_interval=model_save_interval,
+                       summary_interval=summary_interval,
+                       histogram_interval=histogram_interval)
