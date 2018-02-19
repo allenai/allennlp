cd901bdc46be359d8f191d53e87811378381118e
Mark Neumann <markng@allenai.org>
2018-02-19 10:08:21 -0800

    switch demo model to use elmo

--- allennlp/commands/serve.py
+++ allennlp/commands/serve.py
@@ -39,7 +39,7 @@ DEFAULT_MODELS = {
                 'semantic-role-labeling'
         ),
         'textual-entailment': DemoModel(
-                'https://s3-us-west-2.amazonaws.com/allennlp/models/decomposable-attention-2017.09.04.tar.gz',  # pylint: disable=line-too-long
+                'https://s3-us-west-2.amazonaws.com/allennlp/models/decomposable-attention-elmo-2018.02.19.tar.gz',  # pylint: disable=line-too-long
                 'textual-entailment'
         ),
         'coreference-resolution': DemoModel(
