c52166fac0c519402f7d83863ec09bdadb32bd18
Mark Neumann <markng@allenai.org>
2018-02-19 15:35:32 -0800

    PR feedback

--- allennlp/training/metrics/evalb_bracketing_scorer.py
+++ allennlp/training/metrics/evalb_bracketing_scorer.py
@@ -20,13 +20,17 @@ class EvalbBracketingScorer(Metric):
     on parse trees. Here, we use it to compute the Precision, Recall and F1 metrics.
     You can download the source for EVALB from here: <http://nlp.cs.nyu.edu/evalb/>.
 
-    Note that this software is 20 years' old. In order to compile it on modern hardware,
+    Note that this software is 20 years old. In order to compile it on modern hardware,
     you may need to remove an ``include <malloc.h>`` statement in ``evalb.c`` before it
     will compile.
 
     AllenNLP contains the EVALB software, but you will need to compile it yourself
     before using it because the binary it generates is system depenedent. To build it,
     run ``make`` inside the ``scripts/EVALB`` directory.
+
+    Note that this metric reads and writes from disk quite a bit. You probably don't
+    want to include it in your training loop; instead, you should calculate this on
+    a validation set only.
 
     Parameters
     ----------
@@ -35,12 +39,13 @@ class EvalbBracketingScorer(Metric):
     evalb_param_filename: ``str``, optional (default = "COLLINS.prm")
         The relative name of the EVALB configuration file used when scoring the trees.
         By default, this uses the COLLINS.prm configuration file which comes with EVALB.
+        This configuration ignores POS tags and some punctuation labels.
     """
     def __init__(self, evalb_directory_path: str, evalb_param_filename: str = "COLLINS.prm") -> None:
         self._evalb_program_path = os.path.join(evalb_directory_path, "evalb")
         self._evalb_param_path = os.path.join(evalb_directory_path, evalb_param_filename)
 
-        if not os.path.exists(self._evalb_param_path):
+        if not os.path.exists(self._evalb_program_path):
             raise ConfigurationError("You must compile the EVALB scorer before using it."
                                      " Run 'make' in the 'scripts/EVALB' directory.")
 
