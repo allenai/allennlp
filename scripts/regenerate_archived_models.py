d13621a96cfa91869c040ccacc117d094a14d0aa
Mark Neumann <markng@allenai.org>
2018-02-22 12:58:15 -0800

    regenerate test fixtures, address some regressions

--- scripts/regenerate_archived_models.py
+++ scripts/regenerate_archived_models.py
@@ -6,7 +6,7 @@ import sys
 import logging
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
-from allennlp.models.archival import _CONFIG_NAME, _WEIGHTS_NAME
+from allennlp.models.archival import CONFIG_NAME, _WEIGHTS_NAME
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
@@ -27,7 +27,7 @@ def generate_archive(config_file: str,
     logger.info("creating new archive file %s", archive_file)
 
     with tarfile.open(archive_file, 'w:gz') as archive:
-        archive.add(config_file, arcname=_CONFIG_NAME)
+        archive.add(config_file, arcname=CONFIG_NAME)
         archive.add(os.path.join(serialization_dir, weights_file), arcname=_WEIGHTS_NAME)
         archive.add(os.path.join(serialization_dir, "vocabulary"), arcname="vocabulary")
 
