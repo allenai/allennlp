e00641a34eb61bc1576fd8fb9e6487feb0da4f95
rajas <rajasagashe@users.noreply.github.com>
2018-02-24 22:27:13 -0800

    added test for mixture feedforward configuration

--- tests/models/encoder_decoders/wikitables_semantic_parser_test.py
+++ tests/models/encoder_decoders/wikitables_semantic_parser_test.py
@@ -311,6 +311,15 @@ class WikiTablesSemanticParserTest(ModelTestCase):
                 }
 
 
+class WikiTablesSemanticParserTestWithMixtureFeedforward(ModelTestCase):
+    def setUp(self):
+        super(WikiTablesSemanticParserTestWithMixtureFeedforward, self).setUp()
+        self.set_up_model("tests/fixtures/encoder_decoder/wikitables_semantic_parser_with_mixture_feedforward/experiment.json",
+                          "tests/fixtures/data/wikitables/sample_data.examples")
+
+    def test_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
+
 class WikiTablesDecoderStepTest(AllenNlpTestCase):
     def setUp(self):
         super().setUp()
