c1d77154f04218bc0651b577d434e73a41c653d2
Michael Schmitz <michael@schmitztech.com>
2018-02-27 14:54:17 -0800

    Move spacy installation into install_requirements.txt

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -4,7 +4,6 @@ pip install -r requirements.txt
 # Temporary fix to the build whilst NLTK sort stuff out. TODO(Mark): revert this.
 python -m nltk.downloader -u https://pastebin.com/raw/D3TBY4Mj punkt
 # python -m nltk.downloader punkt
-spacy download en_core_web_sm
 
 # only install test requirements if explicitly specified
 if [[ "$INSTALL_TEST_REQUIREMENTS" == "true" ]]; then
