4fa7b4e00841a907a403bf9bf1248acb2c17b161
Mark Neumann <markng@allenai.org>
2018-03-07 15:20:52 -0800

    initial winobias work

---
+++ allennlp/data/dataset_readers/coreference_resolution/winobias.py
@@ -0,0 +1,173 @@
+import logging
+import collections
+from typing import Any, Dict, List, Optional, Tuple, DefaultDict, Set
+
+from overrides import overrides
+
+from allennlp.common import Params
+from allennlp.common.file_utils import cached_path
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.fields import Field, ListField, TextField, SpanField, MetadataField, SequenceLabelField
+from allennlp.data.instance import Instance
+from allennlp.data.tokenizers import Token
+from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
+from allennlp.data.dataset_readers.dataset_utils import Ontonotes, enumerate_spans
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+@DatasetReader.register("winobias")
+class WinobiasReader(DatasetReader):
+    """
+    Returns a ``Dataset`` where the ``Instances`` have four fields: ``text``, a ``TextField``
+    containing the full sentence text, ``spans``, a ``ListField[SpanField]`` of inclusive start and
+    end indices for span candidates, and ``metadata``, a ``MetadataField`` that stores the instance's
+    original text. For data with gold cluster labels, we also include the original ``clusters``
+    (a list of list of index pairs) and a ``SequenceLabelField`` of cluster ids for every span
+    candidate.
+
+    Parameters
+    ----------
+    max_span_width: ``int``, required.
+        The maximum width of candidate spans to consider.
+    token_indexers : ``Dict[str, TokenIndexer]``, optional
+        This is used to index the words in the document.  See :class:`TokenIndexer`.
+        Default is ``{"tokens": SingleIdTokenIndexer()}``.
+    """
+    def __init__(self,
+                 max_span_width: int,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 lazy: bool = False) -> None:
+        super().__init__(lazy)
+        self._max_span_width = max_span_width
+        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+
+    @overrides
+    def _read(self, file_path: str):
+        # if `file_path` is a URL, redirect to the cache
+        file_path = cached_path(file_path)
+
+        for sentence in open(file_path, "r"):
+
+            tokens = sentence.strip().split(" ")
+
+            clusters: DefaultDict[int, List[Tuple[int, int]]] = collections.defaultdict(list)
+            words = []
+            inside_open_span = False
+            for index, token in enumerate(tokens):
+                if "[" in token and "]" in token:
+                    clusters[0].append((index, index))
+                elif "[" in token:
+                    clusters[0].append((index, index))
+                    inside_open_span = True
+                elif inside_open_span and "]" in token:
+                    old_span = clusters[0][-1]
+                    clusters[0][-1] = (old_span[0], old_span[1] + 1)
+
+                
+
+
+
+            print(tokens)
+            square_bracket_endpoints = [index for index, x in enumerate(tokens) if "[" in x or "]" in x]
+            print(square_bracket_endpoints)
+            round_bracket_endpoints = [index for index, x in enumerate(tokens) if "(" in x or ")" in x]
+            print(round_bracket_endpoints)
+
+
+
+
+
+
+
+
+            total_tokens = 0
+            yield self.text_to_instance(tokens, clusters)
+
+    @overrides
+    def text_to_instance(self,  # type: ignore
+                         sentences: List[str],
+                         gold_clusters: Optional[List[List[Tuple[int, int]]]] = None) -> Instance:
+        # pylint: disable=arguments-differ
+        """
+        Parameters
+        ----------
+        sentences : ``List[List[str]]``, required.
+            A list of lists representing the tokenised words and sentences in the document.
+        gold_clusters : ``Optional[List[List[Tuple[int, int]]]]``, optional (default = None)
+            A list of all clusters in the document, represented as word spans. Each cluster
+            contains some number of spans, which can be nested and overlap, but will never
+            exactly match between clusters.
+
+        Returns
+        -------
+        An ``Instance`` containing the following ``Fields``:
+            text : ``TextField``
+                The text of the full document.
+            spans : ``ListField[SpanField]``
+                A ListField containing the spans represented as ``SpanFields``
+                with respect to the document text.
+            span_labels : ``SequenceLabelField``, optional
+                The id of the cluster which each possible span belongs to, or -1 if it does
+                 not belong to a cluster. As these labels have variable length (it depends on
+                 how many spans we are considering), we represent this a as a ``SequenceLabelField``
+                 with respect to the ``spans ``ListField``.
+        """
+        flattened_sentences = [self._normalize_word(word)
+                               for sentence in sentences
+                               for word in sentence]
+
+        metadata: Dict[str, Any] = {"original_text": flattened_sentences}
+        if gold_clusters is not None:
+            metadata["clusters"] = gold_clusters
+
+        text_field = TextField([Token(word) for word in flattened_sentences], self._token_indexers)
+
+        cluster_dict = {}
+        if gold_clusters is not None:
+            for cluster_id, cluster in enumerate(gold_clusters):
+                for mention in cluster:
+                    cluster_dict[tuple(mention)] = cluster_id
+
+        spans: List[Field] = []
+        span_labels: Optional[List[int]] = [] if gold_clusters is not None else None
+
+        sentence_offset = 0
+        for sentence in sentences:
+            for start, end in enumerate_spans(sentence,
+                                              offset=sentence_offset,
+                                              max_span_width=self._max_span_width):
+                if span_labels is not None:
+                    if (start, end) in cluster_dict:
+                        span_labels.append(cluster_dict[(start, end)])
+                    else:
+                        span_labels.append(-1)
+
+                spans.append(SpanField(start, end, text_field))
+            sentence_offset += len(sentence)
+
+        span_field = ListField(spans)
+        metadata_field = MetadataField(metadata)
+
+        fields: Dict[str, Field] = {"text": text_field,
+                                    "spans": span_field,
+                                    "metadata": metadata_field}
+        if span_labels is not None:
+            fields["span_labels"] = SequenceLabelField(span_labels, span_field)
+
+        return Instance(fields)
+
+    @classmethod
+    def from_params(cls, params: Params) -> "WinobiasReader":
+        token_indexers = TokenIndexer.dict_from_params(params.pop("token_indexers", {}))
+        max_span_width = params.pop_int("max_span_width")
+        lazy = params.pop('lazy', False)
+        params.assert_empty(cls.__name__)
+        return cls(token_indexers=token_indexers, max_span_width=max_span_width, lazy=lazy)
+
+    @staticmethod
+    def _normalize_word(word):
+        if word == "/." or word == "/?":
+            return word[1:]
+        else:
+            return word
