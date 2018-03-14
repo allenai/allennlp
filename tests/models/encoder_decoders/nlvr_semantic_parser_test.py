401e8ad0692cc941f534191490c8ec4f1916d1bf
Pradeep Dasigi <pradeep.dasigi@gmail.com>
2018-03-13 17:29:40 -0700

    Dynamically decrease checklist cost weight between epochs (#972)
    * decrease checklist cost weight dynamically
    
    * fixes for mypy
    
    * addressed pr comments, added tests
    
    * added missing doc
    
    * Adding missing doc, take 2

--- tests/models/encoder_decoders/nlvr_semantic_parser_test.py
+++ tests/models/encoder_decoders/nlvr_semantic_parser_test.py
@@ -3,7 +3,11 @@ from numpy.testing import assert_almost_equal
 import torch
 from torch.autograd import Variable
 
+from allennlp.common import Params
 from allennlp.common.testing import ModelTestCase
+from allennlp.data.iterators import EpochTrackingBucketIterator
+from allennlp.models import Model
+
 
 class NlvrSemanticParserTest(ModelTestCase):
     def setUp(self):
@@ -29,3 +33,18 @@ class NlvrSemanticParserTest(ModelTestCase):
         assert_almost_equal(target_checklist.data.numpy(), [[1], [0], [1]])
         assert_almost_equal(terminal_actions.data.numpy(), [[0], [2], [4]])
         assert_almost_equal(checklist_mask.data.numpy(), [[1], [1], [1]])
+
+    def test_forward_with_epoch_num_changes_cost_weight(self):
+        # Redefining model. We do not want this to change the state of ``self.model``.
+        params = Params.from_file(self.param_file)
+        model = Model.from_params(self.vocab, params['model'])
+        # Initial cost weight, before forward is called.
+        assert model._checklist_cost_weight == 0.8
+        iterator = EpochTrackingBucketIterator(sorting_keys=[['sentence', 'num_tokens']])
+        cost_weights = []
+        for epoch_data in iterator(self.dataset, num_epochs=4):
+            model.forward(**epoch_data)
+            cost_weights.append(model._checklist_cost_weight)
+        # The config file has ``wait_num_epochs`` set to 0, so the model starts decreasing the const
+        # weight at epoch 0 itself.
+        assert_almost_equal(cost_weights, [0.72, 0.648, 0.5832, 0.52488])
