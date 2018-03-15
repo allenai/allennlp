d2bcc64367e9a5f74d2a8870ec888cdbb22babb9
Mark Neumann <markng@allenai.org>
2018-03-15 11:08:34 -0700

    mention elmo

--- demo/src/components/ConstituencyParserComponent.js
+++ demo/src/components/ConstituencyParserComponent.js
@@ -26,7 +26,7 @@ const description = (
     </span>
     <a href="https://www.semanticscholar.org/paper/A-Minimal-Span-Based-Neural-Constituency-Parser-Stern-Andreas/593e4e749bd2dbcaf8dc25298d830b41d435e435" target="_blank" rel="noopener noreferrer">{' '} a Minimal Span Based Constituency Parser (Stern et al, 2017)</a>
     <span>
-      . This model achieved state of the art single model performance on the Penn Tree Bank in 2017.
+      . This model uses <a href="https://arxiv.org/abs/1802.05365">ELMo embeddings</a>, which are completely character based and improve single model performance from 91.77 F1 to 94.2 F1 on the Penn Treebank.
     </span> 
   </span>
 );
