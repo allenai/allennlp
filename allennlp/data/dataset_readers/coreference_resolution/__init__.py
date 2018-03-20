760853c5f9a3b3d470bbdcc65526f2fac012514a
Mark Neumann <markn@allenai.org>
2018-03-20 13:28:18 -0700

    Winobias reader (#968)
    * initial winobias work
    
    * add winobias reader
    
    * add reader to docs
    
    * refer to document, not sentence
    
    * review comments

--- allennlp/data/dataset_readers/coreference_resolution/__init__.py
+++ allennlp/data/dataset_readers/coreference_resolution/__init__.py
@@ -3,3 +3,4 @@ Coreference resolution is defined as follows: given a document, find and cluster
 """
 
 from allennlp.data.dataset_readers.coreference_resolution.conll import ConllCorefReader
+from allennlp.data.dataset_readers.coreference_resolution.winobias import WinobiasReader
