7bb32a9560661399d62cae639c5e2c65a52d16f6
Matt Gardner <mattg@allenai.org>
2018-03-23 21:30:25 +0000

    Fix circular import errors in semantic parsing code

--- allennlp/semparse/__init__.py
+++ allennlp/semparse/__init__.py
@@ -3,6 +3,12 @@ This module contains classes related to handling data for semantic parsing. That
 declarations, and world representations for all the domains for which we want to build semantic parsers.
 """
 
+# We have some circular dependencies with the data code.  Importing this loads all of the data code
+# before we try to import stuff under semparse.  This means that we can't do
+# `from allennlp.semparse import [whatever]` in the `data` module, but it resolves the other
+# dependency issues.  If you want to import semparse stuff from the data code, just use a more
+# complete path, like `from allennlp.semparse.worlds import WikiTablesWorld`.
+from allennlp.data.tokenizers import Token as _
 from allennlp.semparse.knowledge_graphs.knowledge_graph import KnowledgeGraph
 from allennlp.semparse.worlds.world import ParsingError, World
 from allennlp.semparse.action_space_walker import ActionSpaceWalker
