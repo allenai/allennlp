c19dd3ae52a3783c4b1d0ac17ca9224a9b781fa0
Nelson Liu <nfliu@cs.washington.edu>
2018-03-24 22:02:10 -0700

    Add test for SST data reader

---
+++ tests/data/dataset_readers/stanford_sentiment_tree_bank_test.py
@@ -0,0 +1,95 @@
+# pylint: disable=no-self-use,invalid-name
+import pytest
+
+from allennlp.data.dataset_readers import (
+        StanfordSentimentTreeBankTokensDatasetReader)
+from allennlp.common.util import ensure_list
+
+class TestStanfordSentimentTreebankReader():
+    @pytest.mark.parametrize("lazy", (True, False))
+    def test_read_from_file(self, lazy):
+        reader = StanfordSentimentTreeBankTokensDatasetReader(lazy=lazy)
+        instances = reader.read('tests/fixtures/data/sst.txt')
+        instances = ensure_list(instances)
+
+        instance1 = {"tokens": ["The", "actors", "are", "fantastic", "."],
+                     "label": "4"}
+        instance2 = {"tokens": ["It", "was", "terrible", "."],
+                     "label": "0"}
+        instance3 = {"tokens": ["Chomp", "chomp", "!"],
+                     "label": "2"}
+
+        assert len(instances) == 3
+        fields = instances[0].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance1["tokens"]
+        assert fields["label"].label == instance1["label"]
+        fields = instances[1].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance2["tokens"]
+        assert fields["label"].label == instance2["label"]
+        fields = instances[2].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance3["tokens"]
+        assert fields["label"].label == instance3["label"]
+
+    def test_use_subtrees(self):
+        reader = StanfordSentimentTreeBankTokensDatasetReader(use_subtrees=True)
+        instances = reader.read('tests/fixtures/data/sst.txt')
+        instances = ensure_list(instances)
+
+        instance1 = {"tokens": ["The", "actors", "are", "fantastic", "."],
+                     "label": "4"}
+        instance2 = {"tokens": ["The", "actors"],
+                     "label": "2"}
+        instance3 = {"tokens": ["The"],
+                     "label": "2"}
+
+        assert len(instances) == 21
+        fields = instances[0].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance1["tokens"]
+        assert fields["label"].label == instance1["label"]
+        fields = instances[1].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance2["tokens"]
+        assert fields["label"].label == instance2["label"]
+        fields = instances[2].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance3["tokens"]
+        assert fields["label"].label == instance3["label"]
+
+    def test_3_class(self):
+        reader = StanfordSentimentTreeBankTokensDatasetReader(granularity="3-class")
+        instances = reader.read('tests/fixtures/data/sst.txt')
+        instances = ensure_list(instances)
+
+        instance1 = {"tokens": ["The", "actors", "are", "fantastic", "."],
+                     "label": "2"}
+        instance2 = {"tokens": ["It", "was", "terrible", "."],
+                     "label": "0"}
+        instance3 = {"tokens": ["Chomp", "chomp", "!"],
+                     "label": "1"}
+
+        assert len(instances) == 3
+        fields = instances[0].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance1["tokens"]
+        assert fields["label"].label == instance1["label"]
+        fields = instances[1].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance2["tokens"]
+        assert fields["label"].label == instance2["label"]
+        fields = instances[2].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance3["tokens"]
+        assert fields["label"].label == instance3["label"]
+
+    def test_2_class(self):
+        reader = StanfordSentimentTreeBankTokensDatasetReader(granularity="2-class")
+        instances = reader.read('tests/fixtures/data/sst.txt')
+        instances = ensure_list(instances)
+
+        instance1 = {"tokens": ["The", "actors", "are", "fantastic", "."],
+                     "label": "1"}
+        instance2 = {"tokens": ["It", "was", "terrible", "."],
+                     "label": "0"}
+
+        assert len(instances) == 2
+        fields = instances[0].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance1["tokens"]
+        assert fields["label"].label == instance1["label"]
+        fields = instances[1].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance2["tokens"]
+        assert fields["label"].label == instance2["label"]
