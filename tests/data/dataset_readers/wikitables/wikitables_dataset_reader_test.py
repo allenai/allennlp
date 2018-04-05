dea3c5a87471d64a69114222f8626091e723f756
Matt Gardner <mattg@allenai.org>
2018-04-05 17:11:21 +0000

    Fixed logical form sorting

--- tests/data/dataset_readers/wikitables/wikitables_dataset_reader_test.py
+++ tests/data/dataset_readers/wikitables/wikitables_dataset_reader_test.py
@@ -102,26 +102,17 @@ def assert_dataset_correct(dataset):
     assert num_action_sequences == 10
 
     # We should have sorted the logical forms by length.  This is the action sequence
-    # corresponding to the shortest logical form in the examples, which is _not_ the first one
-    # in the file.
+    # corresponding to the shortest logical form in the examples _by tree size_, which is _not_ the
+    # first one in the file, or the shortest logical form by _string length_.  It's also a totally
+    # made up logical form, just to demonstrate that we're sorting things correctly.
     action_sequence = instance.fields["target_action_sequences"].field_list[0]
     action_indices = [l.sequence_index for l in action_sequence.field_list]
     actions = [actions[i] for i in action_indices]
     assert actions == [
-            '@START@ -> d',
-            'd -> [<nd,nd>, d]',
-            '<nd,nd> -> max',
-            'd -> [<c,d>, c]',
-            '<c,d> -> [<<#1,#2>,<#2,#1>>, <d,c>]',
-            '<<#1,#2>,<#2,#1>> -> reverse',
-            '<d,c> -> fb:cell.cell.date',
-            'c -> [<r,c>, r]',
-            '<r,c> -> [<<#1,#2>,<#2,#1>>, <c,r>]',
-            '<<#1,#2>,<#2,#1>> -> reverse',
-            '<c,r> -> fb:row.row.year',
+            '@START@ -> r',
             'r -> [<c,r>, c]',
             '<c,r> -> fb:row.row.league',
-            'c -> fb:cell.usl_a_league'
+            'c -> fb:cell.3rd_usl_3rd'
             ]
 
 
