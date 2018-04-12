60c41a37058c1b8c199a1d642ddd622264fd21bf
Matt Gardner <mattg@allenai.org>
2018-04-12 13:09:12 -0700

    Minor fixes of rebase issues (#1079)
    * Minor fixes of rebase issues
    
    * Pylint

--- allennlp/nn/util.py
+++ allennlp/nn/util.py
@@ -1,6 +1,7 @@
 """
 Assorted utilities for working with neural networks in AllenNLP.
 """
+# pylint: disable=too-many-lines
 from collections import defaultdict
 from typing import Dict, List, Optional, Any, Tuple, Callable
 import logging
--- tests/models/encoder_decoders/wikitables/wikitables_semantic_parser_test.py
+++ tests/models/encoder_decoders/wikitables/wikitables_semantic_parser_test.py
@@ -1,7 +1,6 @@
 # pylint: disable=invalid-name,no-self-use,protected-access
 from collections import namedtuple
 
-from flaky import flaky
 from numpy.testing import assert_almost_equal
 import torch
 from torch.autograd import Variable
@@ -17,7 +16,6 @@ class WikiTablesSemanticParserTest(ModelTestCase):
         self.set_up_model(f"{self.fixture_dir}/experiment.json",
                           "tests/fixtures/data/wikitables/sample_data.examples")
 
-    @flaky
     def test_model_can_train_save_and_load(self):
         self.ensure_model_can_train_save_and_load(self.param_file)
 
