0d51121089330671c9dede3947d64aa80a91507f
Michael Schmitz <michael@schmitztech.com>
2018-04-12 15:47:07 -0700

    Revert "Top menu -> Left menu (#1074)"
    This reverts commit a92bf48a346ba3ce99d1f6a87ed181f908c42db7.

--- demo/src/components/Header.js
+++ demo/src/components/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './Menu.css';
-
 /*******************************************************************************
   <Header /> Component
 *******************************************************************************/
 
-class Menu extends React.Component {
+class Header extends React.Component {
     render() {
       const { selectedModel, clearData } = this.props;
 
@@ -24,16 +22,8 @@ class Menu extends React.Component {
       }
 
       return (
-        <div className="menu">
-          <div className="menu__content">
-            <h1 className="menu__content__logo">
-              <a href="http://www.allennlp.org/" target="_blank" rel="noopener noreferrer">
-                <svg>
-                  <use xlinkHref="#icon__allennlp-logo"></use>
-                </svg>
-                <span className="u-hidden">AllenNLP</span>
-              </a>
-            </h1>
+        <header>
+          <div className="header__content">
             <nav>
               <ul>
                 {buildLink("machine-comprehension", "Machine Comprehension")}
@@ -44,10 +34,18 @@ class Menu extends React.Component {
                 {buildLink("constituency-parsing", "Constituency Parsing")}
               </ul>
             </nav>
+            <h1 className="header__content__logo">
+              <a href="http://www.allennlp.org/" target="_blank" rel="noopener noreferrer">
+                <svg>
+                  <use xlinkHref="#icon__allennlp-logo"></use>
+                </svg>
+                <span className="u-hidden">AllenNLP</span>
+              </a>
+            </h1>
           </div>
-        </div>
+        </header>
       );
     }
   }
 
-export default Menu;
+export default Header;
