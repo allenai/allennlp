6829edbe932f2f48d6c5f8dc15d16070bc1666c7
Michael Schmitz <michael@schmitztech.com>
2018-04-13 07:36:52 -0700

    Move the top nav bar to the left pane. (#1082)

--- demo/src/components/Menu.js
+++ demo/src/components/Menu.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
   <Header /> Component
 *******************************************************************************/
 
-class Header extends React.Component {
+class Menu extends React.Component {
     render() {
       const { selectedModel, clearData } = this.props;
 
@@ -22,8 +22,16 @@ class Header extends React.Component {
       }
 
       return (
-        <header>
-          <div className="header__content">
+        <div className="menu">
+          <div className="menu__content">
+            <h1 className="menu__content__logo">
+              <a href="http://www.allennlp.org/" target="_blank" rel="noopener noreferrer">
+                <svg>
+                  <use xlinkHref="#icon__allennlp-logo"></use>
+                </svg>
+                <span className="u-hidden">AllenNLP</span>
+              </a>
+            </h1>
             <nav>
               <ul>
                 {buildLink("machine-comprehension", "Machine Comprehension")}
@@ -34,18 +42,10 @@ class Header extends React.Component {
                 {buildLink("constituency-parsing", "Constituency Parsing")}
               </ul>
             </nav>
-            <h1 className="header__content__logo">
-              <a href="http://www.allennlp.org/" target="_blank" rel="noopener noreferrer">
-                <svg>
-                  <use xlinkHref="#icon__allennlp-logo"></use>
-                </svg>
-                <span className="u-hidden">AllenNLP</span>
-              </a>
-            </h1>
           </div>
-        </header>
+        </div>
       );
     }
   }
 
-export default Header;
+export default Menu;
