b006aea902c5c7b1853faf0a86ef2d3251aa02a2
Nelson Liu <nfliu@cs.washington.edu>
2018-05-17 19:18:36 -0700

    Fix sequence_tagging_test

--- allennlp/tests/data/dataset_readers/sequence_tagging_test.py
+++ allennlp/tests/data/dataset_readers/sequence_tagging_test.py
@@ -3,12 +3,13 @@ import pytest
 
 from allennlp.data.dataset_readers import SequenceTaggingDatasetReader
 from allennlp.common.util import ensure_list
+from allennlp.common.testing import AllenNlpTestCase
 
 class TestSequenceTaggingDatasetReader():
     @pytest.mark.parametrize("lazy", (True, False))
     def test_default_format(self, lazy):
         reader = SequenceTaggingDatasetReader(lazy=lazy)
-        instances = reader.read('tests/fixtures/data/sequence_tagging.tsv')
+        instances = reader.read(AllenNlpTestCase.FIXTURES_ROOT / 'data' / 'sequence_tagging.tsv')
         instances = ensure_list(instances)
 
         assert len(instances) == 4
@@ -27,7 +28,7 @@ class TestSequenceTaggingDatasetReader():
 
     def test_brown_corpus_format(self):
         reader = SequenceTaggingDatasetReader(word_tag_delimiter='/')
-        instances = reader.read('tests/fixtures/data/brown_corpus.txt')
+        instances = reader.read(AllenNlpTestCase.FIXTURES_ROOT / 'data' / 'brown_corpus.txt')
         instances = ensure_list(instances)
 
         assert len(instances) == 4
