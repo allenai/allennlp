1e580319cea9fe9bdf700fb1397f878212997e32
Nelson Liu <nfliu@cs.washington.edu>
2018-05-17 20:18:01 -0700

    Fix BCN

--- allennlp/tests/models/biattentive_classification_network_test.py
+++ allennlp/tests/models/biattentive_classification_network_test.py
@@ -11,15 +11,15 @@ from allennlp.models import Model
 class BiattentiveClassificationNetworkMaxoutTest(ModelTestCase):
     def setUp(self):
         super(BiattentiveClassificationNetworkMaxoutTest, self).setUp()
-        self.set_up_model('tests/fixtures/biattentive_classification_network/experiment.json',
-                          'tests/fixtures/data/sst.txt')
+        self.set_up_model(self.FIXTURES_ROOT / 'biattentive_classification_network' / 'experiment.json',
+                          self.FIXTURES_ROOT / 'data' / 'sst.txt')
 
     def test_maxout_bcn_can_train_save_and_load(self):
         self.ensure_model_can_train_save_and_load(self.param_file)
 
     def test_feedforward_bcn_can_train_save_and_load(self):
         # pylint: disable=line-too-long
-        self.ensure_model_can_train_save_and_load('tests/fixtures/biattentive_classification_network/feedforward_experiment.json')
+        self.ensure_model_can_train_save_and_load(self.FIXTURES_ROOT / 'biattentive_classification_network' / 'feedforward_experiment.json')
 
     def test_batch_predictions_are_consistent(self):
         self.ensure_batch_predictions_are_consistent()
