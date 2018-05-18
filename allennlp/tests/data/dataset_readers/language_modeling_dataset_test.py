a75c6741d1dcee8ede3626d57e43b99858c1a72a
Nelson Liu <nfliu@cs.washington.edu>
2018-05-17 18:55:13 -0700

    Fix language_modeling_dataset

--- allennlp/tests/data/dataset_readers/language_modeling_dataset_test.py
+++ allennlp/tests/data/dataset_readers/language_modeling_dataset_test.py
@@ -3,13 +3,14 @@ import pytest
 
 from allennlp.data.dataset_readers import LanguageModelingReader
 from allennlp.common.util import ensure_list
+from allennlp.common.testing import AllenNlpTestCase
 
 class TestLanguageModelingDatasetReader:
     @pytest.mark.parametrize("lazy", (True, False))
     def test_read_from_file(self, lazy):
         reader = LanguageModelingReader(tokens_per_instance=3, lazy=lazy)
 
-        instances = ensure_list(reader.read('tests/fixtures/data/language_modeling.txt'))
+        instances = ensure_list(reader.read(AllenNlpTestCase.FIXTURES_ROOT / 'data' / 'language_modeling.txt'))
         # The last potential instance is left out, which is ok, because we don't have an end token
         # in here, anyway.
         assert len(instances) == 5
