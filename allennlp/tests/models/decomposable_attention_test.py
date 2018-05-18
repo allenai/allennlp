ac0ff2286c41e08373fac30197035fabe53e593b
Nelson Liu <nfliu@cs.washington.edu>
2018-05-17 21:14:19 -0700

    Fix decomposable_attention_test

--- allennlp/tests/models/decomposable_attention_test.py
+++ allennlp/tests/models/decomposable_attention_test.py
@@ -3,6 +3,7 @@ from flaky import flaky
 import pytest
 import numpy
 from numpy.testing import assert_almost_equal
+import os
 
 from allennlp.common import Params
 from allennlp.common.checks import ConfigurationError
@@ -13,8 +14,8 @@ from allennlp.models import DecomposableAttention, Model
 class TestDecomposableAttention(ModelTestCase):
     def setUp(self):
         super(TestDecomposableAttention, self).setUp()
-        self.set_up_model('tests/fixtures/decomposable_attention/experiment.json',
-                          'tests/fixtures/data/snli.jsonl')
+        self.set_up_model(self.FIXTURES_ROOT / 'decomposable_attention' / 'experiment.json',
+                          self.FIXTURES_ROOT / 'data' / 'snli.jsonl')
 
     def test_forward_pass_runs_correctly(self):
         training_tensors = self.dataset.as_tensor_dict()
@@ -30,12 +31,17 @@ class TestDecomposableAttention(ModelTestCase):
         self.ensure_batch_predictions_are_consistent()
 
     def test_model_load(self):
-        params = Params.from_file('tests/fixtures/decomposable_attention/experiment.json')
-        model = Model.load(params, serialization_dir='tests/fixtures/decomposable_attention/serialization')
+        params = Params.from_file(self.FIXTURES_ROOT / 'decomposable_attention' / 'experiment.json')
+        model = Model.load(params, serialization_dir=self.FIXTURES_ROOT /
+                           'decomposable_attention' / 'serialization')
 
         assert isinstance(model, DecomposableAttention)
 
     def test_mismatched_dimensions_raise_configuration_errors(self):
+        initial_working_dir = os.getcwd()
+        # Change directory to module root.
+        os.chdir(self.MODULE_ROOT)
+
         params = Params.from_file(self.param_file)
         # Make the input_dim to the first feedforward_layer wrong - it should be 2.
         params["model"]["attend_feedforward"]["input_dim"] = 10
@@ -48,3 +54,5 @@ class TestDecomposableAttention(ModelTestCase):
         params["model"]["aggregate_feedforward"]["output_dim"] = 10
         with pytest.raises(ConfigurationError):
             Model.from_params(self.vocab, params.pop("model"))
+        # Change directory back to what it was initially
+        os.chdir(initial_working_dir)
