36d4df7ab91daaea8b85ceaa91563f0a9765ef42
Nelson Liu <nfliu@cs.washington.edu>
2018-05-17 22:14:20 -0700

    Fix predictors.coref_test

--- allennlp/tests/service/predictors/coref_test.py
+++ allennlp/tests/service/predictors/coref_test.py
@@ -1,15 +1,15 @@
 # pylint: disable=no-self-use,invalid-name
-from unittest import TestCase
+from allennlp.common.testing import AllenNlpTestCase
 
 from allennlp.models.archival import load_archive
 from allennlp.service.predictors import Predictor
 
 
-class TestCorefPredictor(TestCase):
+class TestCorefPredictor(AllenNlpTestCase):
     def test_uses_named_inputs(self):
         inputs = {"document": "This is a single string document about a test. Sometimes it "
                               "contains coreferent parts."}
-        archive = load_archive('tests/fixtures/coref/serialization/model.tar.gz')
+        archive = load_archive(self.FIXTURES_ROOT / 'coref' / 'serialization' / 'model.tar.gz')
         predictor = Predictor.from_archive(archive, 'coreference-resolution')
 
         result = predictor.predict_json(inputs)
