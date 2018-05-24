3785684979b8df01e212f993862ce9b5733513ef
paul.murphy <paul.murphy@teckro.com>
2018-05-24 14:34:23 +0100

    adding linear and dot product attention.

--- tests/modules/attention/linear_attention_test.py
+++ tests/modules/attention/linear_attention_test.py
@@ -14,11 +14,11 @@ class LinearAttentionTests(AllenNlpTestCase):
         linear = LinearAttention(3, 3)
         linear._weight_vector = Parameter(torch.FloatTensor([-.3, .5, 2.0, -1.0, 1, 1]))
         linear._bias = Parameter(torch.FloatTensor([.1]))
-        output = linear(Variable(torch.FloatTensor([[[0, 0, 0]], [[-7, -8, -9]]])),
-                        Variable(torch.FloatTensor([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])))
+        output = linear(Variable(torch.FloatTensor([[-7, -8, -9]])),
+                        Variable(torch.FloatTensor([[[1, 2, 3], [4, 5, 6]]])))
 
-        assert_almost_equal(output.data.numpy(), np.array([[[4.1000], [17.4000]],
-                                                           [[-9.8000], [36.6000]]]), decimal=2)
+
+        assert_almost_equal(output.data.numpy(), np.array([[0.0474, 0.9526]]), decimal=2)
 
 
 
