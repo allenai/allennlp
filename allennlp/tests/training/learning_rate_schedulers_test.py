c26a51387fc758a62bd08c1e0f6e077f04f73d0c
Mark Neumann <markn@allenai.org>
2018-05-25 16:05:11 -0700

    Noam lr schedule (#1258)

--- allennlp/tests/training/learning_rate_schedulers_test.py
+++ allennlp/tests/training/learning_rate_schedulers_test.py
@@ -33,3 +33,11 @@ class LearningRateSchedulersTest(AllenNlpTestCase):
                                                                       Params({"type": "adam"})),
                                                 Params({"type": "step", "step_size": 1}))
         lrs.step(None, None)
+
+    def test_noam_learning_rate_schedule_does_not_crash(self):
+        model = torch.nn.Sequential(torch.nn.Linear(10, 10))
+        lrs = LearningRateScheduler.from_params(Optimizer.from_params(model.named_parameters(),
+                                                                      Params({"type": "adam"})),
+                                                Params({"type": "noam", "model_size": 10, "warmup_steps": 2000}))
+        lrs.step(None)
+        lrs.step_batch(None)
--- allennlp/training/learning_rate_schedulers.py
+++ allennlp/training/learning_rate_schedulers.py
@@ -79,6 +79,51 @@ class LearningRateWithMetricsWrapper(LearningRateScheduler):
         self.lr_scheduler.step(metric, epoch)
 
 
+class NoamLR(torch.optim.lr_scheduler._LRScheduler): # pylint: disable=protected-access
+    """
+    Implements the Noam Learning rate schedule. This corresponds to increasing the learning rate
+    linearly for the first ``warmup_steps`` training steps, and decreasing it thereafter proportionally
+    to the inverse square root of the step number, scaled by the inverse square root of the
+    dimensionality of the model. Time will tell if this is just madness or it's actually important.
+
+    Parameters
+    ----------
+    model_size : ``int``, required.
+        The hidden size parameter which dominates the number of parameters in your model.
+    warmup_steps: ``int``, required.
+        The number of steps to linearly increase the learning rate.
+    factor : ``float``, optional (default = 1.0).
+        The overall scale factor for the learning rate decay.
+    """
+    def __init__(self,
+                 optimizer: torch.optim.Optimizer,
+                 model_size: int,
+                 warmup_steps: int,
+                 factor: float = 1.0,
+                 last_epoch: int = -1) -> None:
+        self.warmup_steps = warmup_steps
+        self.factor = factor
+        self.model_size = model_size
+        super().__init__(optimizer, last_epoch=last_epoch)
+
+    def step(self, epoch=None):
+        pass
+
+    def step_batch(self, epoch=None):
+        if epoch is None:
+            epoch = self.last_epoch + 1
+        self.last_epoch = epoch
+        for param_group, learning_rate in zip(self.optimizer.param_groups, self.get_lr()):
+            param_group['lr'] = learning_rate
+
+
+    def get_lr(self):
+        step = max(self.last_epoch, 1)
+        scale = self.factor *  (self.model_size ** (-0.5) *
+                                min(step ** (-0.5), step * self.warmup_steps ** (-1.5)))
+
+        return [scale for _ in range(len(self.base_lrs))]
+
 # We just use the Pytorch LRSchedulers, so here we force them into
 # Registry._registry so we can build them from params.
 Registrable._registry[LearningRateScheduler] = {   # pylint: disable=protected-access
@@ -87,4 +132,5 @@ Registrable._registry[LearningRateScheduler] = {   # pylint: disable=protected-a
         "exponential": torch.optim.lr_scheduler.ExponentialLR,
         "reduce_on_plateau": torch.optim.lr_scheduler.ReduceLROnPlateau,
         "cosine": torch.optim.lr_scheduler.CosineAnnealingLR,
+        "noam": NoamLR,
 }
