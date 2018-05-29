06254cb3908ec5234ec5e2eaf526440f1a93989a
Mark Neumann <markng@allenai.org>
2018-05-29 11:36:57 -0700

    newlines, add predictors to index of docs

--- allennlp/service/predictors/nlvr_parser.py
+++ allennlp/service/predictors/nlvr_parser.py
@@ -4,4 +4,3 @@ import warnings
 from allennlp.predictors.nlvr_parser import NlvrParserPredictor
 warnings.warn("allennlp.service.predictors.* has been deprecated."
               " Please use allennlp.predictors.*", FutureWarning)
-
