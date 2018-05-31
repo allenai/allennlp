cebf719c4645bd6660e14a499c48e23fbe272e1c
Mark Neumann <markn@allenai.org>
2018-05-31 10:28:46 -0700

    SRL elmo 5b (#1310)
    New SRL model using ELMo - this moves our F1 from 78.9(boo) to 84.97 (84.93 on test).
    
    I used a bit of a hacky repackaging method here so that we can distribute it - it basically involves loading in the GPU specific model, copying the config and weights into a new model which doesn't have the GPU only LSTM implementation.
    
    Full results below for posterity:
    
    ```
                  corr.  excess  missed    prec.    rec.      F1
    ------------------------------------------------------------
       Overall    70191   12778   12062    84.60   85.34   84.97
    ----------
          ARG0    16309    1586    1327    91.14   92.48   91.80
          ARG1    25106    3566    3123    87.56   88.94   88.24
          ARG2     7735    1642    1795    82.49   81.16   81.82
          ARG3      402     186     274    68.37   59.47   63.61
          ARG4      446     121     137    78.66   76.50   77.57
          ARG5        7       4       9    63.64   43.75   51.85
          ARGA        0       0       4     0.00    0.00    0.00
      ARGM-ADJ      162     131      92    55.29   63.78   59.23
      ARGM-ADV     2085    1135    1083    64.75   65.81   65.28
      ARGM-CAU      401     145     151    73.44   72.64   73.04
      ARGM-COM       19      27      15    41.30   55.88   47.50
      ARGM-DIR      316     217     233    59.29   57.56   58.41
      ARGM-DIS     2445     580     555    80.83   81.50   81.16
      ARGM-DSP        0       0       2     0.00    0.00    0.00
      ARGM-EXT      126     120     147    51.22   46.15   48.55
      ARGM-GOL       16      49      81    24.62   16.49   19.75
      ARGM-LOC     1572     718     605    68.65   72.21   70.38
      ARGM-LVB       47      10       8    82.46   85.45   83.93
      ARGM-MNR     1389     608     604    69.55   69.69   69.62
      ARGM-MOD     2776      72      48    97.47   98.30   97.88
      ARGM-NEG     1501      83      60    94.76   96.16   95.45
      ARGM-PNC       23      79      90    22.55   20.35   21.40
      ARGM-PRD       72     176     344    29.03   17.31   21.69
      ARGM-PRP      336     244     192    57.93   63.64   60.65
      ARGM-REC        4       2       9    66.67   30.77   42.11
      ARGM-TMP     5186    1011     814    83.69   86.43   85.04
        R-ARG0      917      80      85    91.98   91.52   91.75
        R-ARG1      632      99      82    86.46   88.52   87.47
        R-ARG2       34      16      23    68.00   59.65   63.55
        R-ARG3        0       0       7     0.00    0.00    0.00
        R-ARG4        1       0       1   100.00   50.00   66.67
    R-ARGM-ADV        0       1       3     0.00    0.00    0.00
    R-ARGM-CAU        1       3       1    25.00   50.00   33.33
    R-ARGM-COM        0       1       0     0.00    0.00    0.00
    R-ARGM-DIR        0       0       1     0.00    0.00    0.00
    R-ARGM-EXT        0       0       2     0.00    0.00    0.00
    R-ARGM-LOC       68      42      21    61.82   76.40   68.34
    R-ARGM-MNR        9       3       5    75.00   64.29   69.23
    R-ARGM-PRP        0       0       1     0.00    0.00    0.00
    R-ARGM-TMP       48      21      28    69.57   63.16   66.21
    ------------------------------------------------------------
             V    34783       0       0   100.00  100.00  100.00
    ------------------------------------------------------------
    ```

--- scripts/write_srl_predictions_to_conll_format.py
+++ scripts/write_srl_predictions_to_conll_format.py
@@ -1,73 +1,102 @@
 import os
 import sys
 
-import torch
-
 sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
 import argparse
-from allennlp.common import Params
+
+import torch
+
 from allennlp.common.tqdm import Tqdm
+from allennlp.common import Params
+from allennlp.models.archival import load_archive
 from allennlp.data.iterators import BasicIterator
 from allennlp.data import DatasetReader
 from allennlp.models import Model
 from allennlp.models.semantic_role_labeler import write_to_conll_eval_file
+from allennlp.modules.elmo import Elmo
 
-
-def main(serialization_directory, device):
+def main(serialization_directory: int,
+         device: int,
+         data: str,
+         prefix: str,
+         domain: str = None):
     """
     serialization_directory : str, required.
         The directory containing the serialized weights.
     device: int, default = -1
         The device to run the evaluation on.
+    data: str, default = None
+        The data to evaluate on. By default, we use the validation data from
+        the original experiment.
+    prefix: str, default=""
+        The prefix to prepend to the generated gold and prediction files, to distinguish
+        different models/data.
+    domain: str, optional (default = None)
+        If passed, filters the ontonotes evaluation/test dataset to only contain the
+        specified domain. This overwrites the domain in the config file from the model,
+        to allow evaluation on domains other than the one the model was trained on.
     """
-    torch.set_grad_enabled(False)
-
     config = Params.from_file(os.path.join(serialization_directory, "config.json"))
+
+    if domain is not None:
+        # Hack to allow evaluation on different domains than the
+        # model was trained on.
+        config["dataset_reader"]["domain_identifier"] = domain
+        prefix = f"{domain}_{prefix}"
+    else:
+        config["dataset_reader"].pop("domain_identifier", None)
+
     dataset_reader = DatasetReader.from_params(config['dataset_reader'])
-    evaluation_data_path = config['validation_data_path']
+    evaluation_data_path = data if data else config['validation_data_path']
 
-    model = Model.load(config, serialization_dir=serialization_directory, cuda_device=device)
+    archive = load_archive(os.path.join(serialization_directory, "model.tar.gz"), cuda_device=device)
+    model = archive.model
+    model.eval()
 
-    prediction_file_path = os.path.join(serialization_directory, "predictions.txt")
-    gold_file_path = os.path.join(serialization_directory, "gold.txt")
+    prediction_file_path = os.path.join(serialization_directory, prefix + "_predictions.txt")
+    gold_file_path = os.path.join(serialization_directory, prefix + "_gold.txt")
     prediction_file = open(prediction_file_path, "w+")
     gold_file = open(gold_file_path, "w+")
 
     # Load the evaluation data and index it.
-    print("Reading evaluation data from {}".format(evaluation_data_path))
+    print("reading evaluation data from {}".format(evaluation_data_path))
     instances = dataset_reader.read(evaluation_data_path)
-    iterator = BasicIterator(batch_size=32)
-    iterator.index_with(model.vocab)
 
-    model_predictions = []
-    batches = iterator(instances, num_epochs=1, shuffle=False, cuda_device=device)
-    for batch in Tqdm.tqdm(batches):
-        result = model(**batch)
-        predictions = model.decode(result)
-        model_predictions.extend(predictions["tags"])
+    with torch.autograd.no_grad():
+        iterator = BasicIterator(batch_size=32)
+        iterator.index_with(model.vocab)
 
-    for instance, prediction in zip(instances, model_predictions):
-        fields = instance.fields
-        try:
-            # Most sentences have a verbal predicate, but not all.
-            verb_index = fields["verb_indicator"].labels.index(1)
-        except ValueError:
-            verb_index = None
+        model_predictions = []
+        batches = iterator(instances, num_epochs=1, shuffle=False, cuda_device=device)
+        for batch in Tqdm.tqdm(batches):
+            result = model(**batch)
+            predictions = model.decode(result)
+            model_predictions.extend(predictions["tags"])
 
-        gold_tags = fields["tags"].labels
-        sentence = fields["tokens"].tokens
+        for instance, prediction in zip(instances, model_predictions):
+            fields = instance.fields
+            try:
+                # Most sentences have a verbal predicate, but not all.
+                verb_index = fields["verb_indicator"].labels.index(1)
+            except ValueError:
+                verb_index = None
 
-        write_to_conll_eval_file(prediction_file, gold_file,
-                                 verb_index, sentence, prediction, gold_tags)
-    prediction_file.close()
-    gold_file.close()
+            gold_tags = fields["tags"].labels
+            sentence = [x.text for x in fields["tokens"].tokens]
+
+            write_to_conll_eval_file(prediction_file, gold_file,
+                                     verb_index, sentence, prediction, gold_tags)
+        prediction_file.close()
+        gold_file.close()
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Write CONLL format SRL predictions"
+    parser = argparse.ArgumentParser(description="write conll format srl predictions"
                                                  " to file from a pretrained model.")
-    parser.add_argument('--path', type=str, help='The serialization directory.')
-    parser.add_argument('--device', type=int, default=-1, help='The device to load the model onto.')
-
+    parser.add_argument('--path', type=str, help='the serialization directory.')
+    parser.add_argument('--device', type=int, default=-1, help='the device to load the model onto.')
+    parser.add_argument('--data', type=str, default=None, help='A directory containing a dataset to evaluate on.')
+    parser.add_argument('--prefix', type=str, default="", help='A prefix to distinguish model outputs.')
+    parser.add_argument('--domain', type=str, default=None, help='An optional domain to filter by for producing results.')
     args = parser.parse_args()
-    main(args.path, args.device)
+    main(args.path, args.device, args.data, args.prefix, args.domain)
