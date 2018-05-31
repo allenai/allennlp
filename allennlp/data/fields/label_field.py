cbf68c5aa68d69690b3d9e1ecbe73c848e4deab9
Joel Grus <joelgrus@gmail.com>
2018-05-31 14:58:23 -0700

    add ignore for spurious pylint warning

--- allennlp/data/fields/label_field.py
+++ allennlp/data/fields/label_field.py
@@ -90,7 +90,7 @@ class LabelField(Field[torch.Tensor]):
     def as_tensor(self,
                   padding_lengths: Dict[str, int],
                   cuda_device: int = -1) -> torch.Tensor:
-        # pylint: disable=unused-argument
+        # pylint: disable=unused-argument,not-callable
         tensor = torch.tensor(self._label_id, dtype=torch.long)
         return tensor if cuda_device == -1 else tensor.cuda(cuda_device)
 
