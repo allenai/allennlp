55c5693ebea21bc2352b6e7d946c9ee1feb4c6e3
rlebras <ronanlb@allenai.org>
2018-06-03 22:36:58 -0700

    Semantic parser for rate calculus

---
+++ allennlp/semparse/worlds/ratecalculus_world.py
@@ -0,0 +1,96 @@
+"""
+We store all the information related to a world (i.e. the context in which logical forms will be
+executed) here.
+"""
+from typing import Dict, List, Set
+import re
+
+from nltk.sem.logic import Type
+from overrides import overrides
+
+from allennlp.semparse.worlds.world import ParsingError, World
+from allennlp.semparse.type_declarations import ratecalculus_type_declaration as types
+from allennlp.semparse.contexts import QuestionKnowledgeGraph
+
+
+class RateCalculusWorld(World):
+    """
+    World representation for the Rate Calculus domain.
+
+    Parameters
+    ----------
+    knowledge_graph : ``QuestionKnowledgeGraph``
+        Context associated with this world.
+    """
+    # When we're converting from logical forms to action sequences, this set tells us which
+    # functions in the logical form are curried functions, and how many arguments the function
+    # actually takes.  This is necessary because NLTK curries all multi-argument functions to a
+    # series of one-argument function applications.  See `world._get_transitions` for more info.
+    curried_functions = {
+            types.CONJUNCTION_TYPE: 2,
+            types.BINARY_NUM_OP_TYPE: 2,
+            types.BINARY_BOOL_OP_TYPE: 2,
+            types.BINARY_NUM_TO_BOOL_OP_TYPE: 2,
+            types.RATE_TYPE: 3,
+            types.VALUE_TYPE: 2,
+            types.UNION_TYPE: 2
+            }
+
+    def __init__(self, question_knowledge_graph: QuestionKnowledgeGraph) -> None:
+        super(RateCalculusWorld, self).__init__(constant_type_prefixes={"num": types.NUMBER_TYPE},
+                                                global_type_signatures=types.COMMON_TYPE_SIGNATURE,
+                                                global_name_mapping=types.COMMON_NAME_MAPPING,
+                                                num_nested_lambdas=0)
+        self.question_knowledge_graph = question_knowledge_graph
+
+        for entity in question_knowledge_graph.entities + ['1']:
+            self._map_name(entity, keep_mapping=True)
+
+        self._entity_set = set(question_knowledge_graph.entities)
+
+    def _get_curried_functions(self) -> Dict[Type, int]:
+        return RateCalculusWorld.curried_functions
+
+    def is_question_entity(self, entity_name: str) -> bool:
+        """
+        Returns ``True`` if the given entity is one of the entities in the question.
+        """
+        return entity_name in self._entity_set
+
+    @overrides
+    def get_basic_types(self) -> Set[Type]:
+        return types.BASIC_TYPES
+
+    @overrides
+    def get_valid_actions(self) -> Dict[str, List[str]]:
+        valid_actions = super().get_valid_actions()
+
+        return valid_actions
+
+
+    @overrides
+    def get_valid_starting_types(self) -> Set[Type]:
+        return {types.BOOLEAN_TYPE}
+
+    @overrides
+    def _map_name(self, name: str, keep_mapping: bool = False) -> str:
+        if name not in types.COMMON_NAME_MAPPING and name not in self.local_name_mapping:
+            if not keep_mapping:
+                raise ParsingError(f"Encountered un-mapped name: {name}")
+
+            # The only other unmapped names we should see are numbers.
+            # NLTK throws an error if it sees a "." in constants, which will most likely happen
+            # within numbers as a decimal point. We're changing those to underscores.
+            translated_name = name.replace(".", "_")
+            if re.match("-[0-9_]+", translated_name):
+                # The string is a negative number. This makes NLTK interpret this as a negated
+                # expression and force its type to be TRUTH_VALUE (t).
+                translated_name = translated_name.replace("-", "~")
+            translated_name = f"num:{translated_name}"
+            self._add_name_mapping(name, translated_name, types.NUMBER_TYPE)
+        else:
+            if name in types.COMMON_NAME_MAPPING:
+                translated_name = types.COMMON_NAME_MAPPING[name]
+            else:
+                translated_name = self.local_name_mapping[name]
+        return translated_name
---
+++ allennlp/tests/data/dataset_readers/ratecalculus_test.py
@@ -0,0 +1,144 @@
+# pylint: disable=invalid-name,no-self-use,protected-access
+from allennlp.common.testing import AllenNlpTestCase
+from allennlp.data.dataset_readers import RateCalculusDatasetReader
+from allennlp.semparse.worlds import RateCalculusWorld
+
+expected_sample_actions = {
+    0: ['@start@ -> b',
+        'b -> [<n,<n,b>>, n, n]',
+        '<n,<n,b>> -> Equals',
+        'n -> [<o,<d,n>>, o, d]',
+        '<o,<d,n>> -> Value',
+        'o -> fb:s',
+        'd -> Dollar',
+        'n -> 20'],
+    1: ['@start@ -> b',
+        'b -> [<b,<b,b>>, b, b]',
+        '<b,<b,b>> -> And',
+        'b -> [<n,<n,b>>, n, n]',
+        '<n,<n,b>> -> Equals',
+        'n -> [<o,<d,n>>, o, d]',
+        '<o,<d,n>> -> Value',
+        'o -> fb:s',
+        'd -> Unit',
+        'n -> 5',
+        'b -> [<b,<b,b>>, b, b]',
+        '<b,<b,b>> -> And',
+        'b -> [<n,<n,b>>, n, n]',
+        '<n,<n,b>> -> Equals',
+        'n -> [<o,<d,<d,n>>>, o, d, d]',
+        '<o,<d,<d,n>>> -> Rate',
+        'o -> fb:t',
+        'd -> Dollar',
+        'd -> Unit',
+        'n -> 10',
+        'b -> [<b,<b,b>>, b, b]',
+        '<b,<b,b>> -> And',
+        'b -> [<n,<n,b>>, n, n]',
+        '<n,<n,b>> -> Equals',
+        'n -> [<o,<d,n>>, o, d]',
+        '<o,<d,n>> -> Value',
+        'o -> fb:t',
+        'd -> Unit',
+        'n -> 3',
+        'b -> [<n,<n,b>>, n, n]',
+        '<n,<n,b>> -> Equals',
+        'n -> [<o,<d,n>>, o, d]',
+        '<o,<d,n>> -> Value',
+        'o -> [<o,<o,o>>, o, o]',
+        '<o,<o,o>> -> Join',
+        'o -> fb:s',
+        'o -> fb:t',
+        'd -> Dollar',
+        'n -> fb:p']
+}
+
+expected_alg514_actions = {
+    0: ['@start@ -> b',
+        'b -> [<n,<n,b>>, n, n]',
+        '<n,<n,b>> -> Equals',
+        'n -> [<o,<d,n>>, o, d]',
+        '<o,<d,n>> -> Value',
+        'o -> fb:s',
+        'd -> Dollar',
+        'n -> 20']
+}
+
+def assert_sample_instances_correct(instance, correct_actions):
+    # The content of this will be tested indirectly by checking the actions; we'll just make
+    # sure we get a RateCalculusWorld object in here.
+    assert isinstance(instance.fields['world'].as_tensor({}), RateCalculusWorld)
+
+    action_fields = instance.fields['actions'].field_list
+    actions = [action_field.rule for action_field in action_fields]
+
+    # We should have been able to read all of the logical forms in the file.  If one of them can't
+    # be parsed, or the action sequences can't be mapped correctly, the DatasetReader will skip the
+    # logical form, log an error, and keep going (i.e., it won't crash).  This is good, because
+    # sometimes DPD does silly things that we don't want to reproduce.  But it also means if we
+    # break something, we might not notice in the test unless we check this explicitly.
+    #num_action_sequences = len(instance.fields["target_action_sequences"].field_list)
+    # assert num_action_sequences == 10
+
+    # We should have sorted the logical forms by length.  This is the action sequence
+    # corresponding to the shortest logical form in the examples, which is _not_ the first one
+    # in the file.
+    action_sequence = instance.fields["target_action_sequences"].field_list[0]
+    action_indices = [l.sequence_index for l in action_sequence.field_list]
+    actions = [actions[i] for i in action_indices]
+
+    assert actions == correct_actions
+
+
+def assert_alg514_instances_correct(instance, correct_actions):
+    # The content of this will be tested indirectly by checking the actions; we'll just make
+    # sure we get a RateCalculusWorld object in here.
+    assert isinstance(instance.fields['world'].as_tensor({}), RateCalculusWorld)
+
+    action_fields = instance.fields['actions'].field_list
+    actions = [action_field.rule for action_field in action_fields]
+
+    # We should have been able to read all of the logical forms in the file.  If one of them can't
+    # be parsed, or the action sequences can't be mapped correctly, the DatasetReader will skip the
+    # logical form, log an error, and keep going (i.e., it won't crash).  This is good, because
+    # sometimes DPD does silly things that we don't want to reproduce.  But it also means if we
+    # break something, we might not notice in the test unless we check this explicitly.
+    #num_action_sequences = len(instance.fields["target_action_sequences"].field_list)
+    # assert num_action_sequences == 10
+
+    # We should have sorted the logical forms by length.  This is the action sequence
+    # corresponding to the shortest logical form in the examples, which is _not_ the first one
+    # in the file.
+    action_sequence = instance.fields["target_action_sequences"].field_list[0]
+    action_indices = [l.sequence_index for l in action_sequence.field_list]
+    actions = [actions[i] for i in action_indices]
+
+    assert actions == correct_actions
+
+def assert_sample_dataset_correct(dataset):
+    instances = list(dataset)
+    assert len(instances) == 2
+
+    for i, instance in enumerate(instances):
+        assert_sample_instances_correct(instance, expected_sample_actions[i])
+
+def assert_alg514_dataset_correct(dataset):
+    instances = list(dataset)
+    assert len(instances) == 512
+
+    #for i, instance in enumerate(instances):
+    #    assert_alg514_instances_correct(instance, expected_alg514_actions[i])
+
+
+class RateCalculusDatasetReaderTest(AllenNlpTestCase):
+    def test_reader_reads_sample_data(self):
+        reader = RateCalculusDatasetReader(lazy=False)
+        dataset = reader.read(str(self.FIXTURES_ROOT / "data" / "ratecalculus" / "sample_data.json"))
+        assert_sample_dataset_correct(dataset)
+
+    def test_reader_reads_alg514(self):
+        reader = RateCalculusDatasetReader(lazy=False)
+        dataset = reader.read(str(self.FIXTURES_ROOT / "data" / "ratecalculus" / "alg514_binaryAnd_unitDims_simplifiedVars.json"))
+        assert_alg514_dataset_correct(dataset)
+
+
