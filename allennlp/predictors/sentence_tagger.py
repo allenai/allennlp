90ab920a6b19faf804a472deda766e0a7312eddd
tahirahmad2030 <tahirahmad2030@gmail.com>
2018-06-05 16:39:35 +0530

    Add JustSpacesWordSplitter

--- allennlp/predictors/sentence_tagger.py
+++ allennlp/predictors/sentence_tagger.py
@@ -3,7 +3,7 @@ from overrides import overrides
 
 from allennlp.common.util import JsonDict
 from allennlp.data import DatasetReader, Instance
-from allennlp.data.tokenizers.word_splitter import SpacyWordSplitter
+from allennlp.data.tokenizers.word_splitter import JustSpacesWordSplitter#SpacyWordSplitter
 from allennlp.models import Model
 from allennlp.predictors.predictor import Predictor
 
@@ -19,7 +19,7 @@ class SentenceTaggerPredictor(Predictor):
     """
     def __init__(self, model: Model, dataset_reader: DatasetReader) -> None:
         super().__init__(model, dataset_reader)
-        self._tokenizer = SpacyWordSplitter(language='en_core_web_sm', pos_tags=True)
+        self._tokenizer = JustSpacesWordSplitter()#SpacyWordSplitter(language='en_core_web_sm', pos_tags=True)
 
     def predict(self, sentence: str) -> JsonDict:
         return self.predict_json({"sentence" : sentence})
