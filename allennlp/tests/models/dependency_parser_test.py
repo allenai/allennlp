9b83f33085ff75f8c7b7a34d4a27c2a703c94673
Mark Neumann <markng@allenai.org>
2018-06-17 15:14:32 -0700

    add test fixture for dependency parser

---
+++ allennlp/tests/models/dependency_parser_test.py
@@ -0,0 +1,18 @@
+# pylint: disable=no-self-use,invalid-name,no-value-for-parameter
+import torch
+
+from allennlp.common.testing.model_test_case import ModelTestCase
+
+class DependencyParserTest(ModelTestCase):
+
+    def setUp(self):
+        super(DependencyParserTest, self).setUp()
+        self.set_up_model(self.FIXTURES_ROOT / "dependency_parser" / "experiment.json",
+                          self.FIXTURES_ROOT / "data" / "dependencies.conllu")
+
+    def test_span_parser_can_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
+
+    def test_batch_predictions_are_consistent(self):
+        self.ensure_batch_predictions_are_consistent()
+
