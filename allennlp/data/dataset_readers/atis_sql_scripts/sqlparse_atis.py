2f57da3b2db5cc02fbd1e698cee70701ca9aa78f
Kevin Lin <kevinl@kevinl.local>
2018-06-21 16:18:33 -0700

    sqlparse_atis

--- allennlp/data/dataset_readers/atis_sql_scripts/sqlparse_atis.py
+++ allennlp/data/dataset_readers/atis_sql_scripts/sqlparse_atis.py
@@ -1,54 +1,66 @@
 import sqlparse
-from sqlparse.sql import Where, TokenList, Token, Parenthesis
-from sqlparse.tokens import Whitespace, Punctuation
+from sqlparse.sql import Where, TokenList, Token, Parenthesis, Comparison
+from sqlparse.tokens import Whitespace, Punctuation, Keyword
 
-parsed = sqlparse.parse("""( SELECT DISTINCT flight.flight_id FROM flight WHERE flight.departure_time = 1200 OR ( flight.departure_time = 1800 AND flight.depature_time = 1900 ) );""")
+parsed = sqlparse.parse("""( SELECT DISTINCT flight.flight_id FROM flight WHERE flight.departure_time = 1200 AND ( flight.arrival_time = 1234 OR flight.dept_time = 4321 ) ) ;""")
 
-CONJ = ["AND", "OR"]
+CONJ = ["AND", "OR", "="]
 
 
-def adjust_where(token_list):
-    print("adjust where", token_list.tokens)
-    if not any([token.value in CONJ for token in token_list.tokens]):
+def prefix_condition(token_list, start=None, end=None):
+    print('updated')
+    print("prefix condition", token_list.tokens)
+    '''
+    if not any([token.value in CONJ for token in token_list.flatten()]):
         return token_list
+    '''
 
     stack = []
     prefix_token_list = []
     token_list.tokens = [token for token in token_list.tokens if token.ttype != Whitespace] 
 
-    for token in token_list.tokens:
+    for token in token_list.tokens[start:end]:
         if token.value not in CONJ:
             if isinstance(token, Parenthesis):
-                token = adjust_where(token)
+                token = prefix_condition(token, 1, -1)
+
+            if isinstance(token, Comparison) and isinstance(token, TokenList):
+                 token = prefix_condition(token) 
+
             prefix_token_list.append(token)
 
         elif token.value in CONJ:
             if len(stack) == 0:
                 stack.append(token)
             else:
-                # prefix_token_list.insert(0, Token(Punctuation, ")"))
-                # prefix_token_list.append(Token(Punctuation, "("))
                 prefix_token_list.append(stack.pop())
+                prefix_token_list.insert(0, Token(Punctuation, ")"))
+                prefix_token_list.append(Token(Punctuation, "("))
                 stack.append(token)
+        print('stack:', stack)
+        print('prefix_token_list', prefix_token_list)
+
         
     while len(stack) > 0: 
-        print(prefix_token_list)
-        #prefix_token_list.insert(0, Token(Punctuation, ")"))
-        # prefix_token_list.append(Token(Punctuation, "("))
         prefix_token_list.append(stack.pop())
+        prefix_token_list.insert(0, Token(Punctuation, ")"))
+        prefix_token_list.append(Token(Punctuation, "("))
     
-    prefix_token_list.append(token_list.tokens[0])
     prefix_token_list.reverse()
+
+    token_list.tokens = prefix_token_list
     token_list.value = " ".join([token.value for token in prefix_token_list])
+
     return token_list
     
-
 
 def to_prefix(token_list):
     new_token_list = [] 
     for token in token_list.tokens:
         if isinstance(token, Where):
-            token = adjust_where(token)
+            token = prefix_condition(token, 1, None)
+            token.tokens.insert(0, Token(Keyword, "WHERE"))
+            token.value = " ".join([where_token.value for where_token in token.tokens])
         elif isinstance(token, TokenList):
             token = to_prefix(token)
         new_token_list.append(token)
