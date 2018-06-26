42f565132949441a3903143b274a8302700caf02
Nelson Liu <nfliu@cs.washington.edu>
2018-06-25 17:51:31 -0700

    Add positive_label argument to CategoricalAccuracy

--- allennlp/training/metrics/label_categorical_accuracy.py
+++ allennlp/training/metrics/label_categorical_accuracy.py
@@ -5,30 +5,21 @@ import torch
 
 from allennlp.common.checks import ConfigurationError
 from allennlp.training.metrics.metric import Metric
+from allennlp.training.metrics.categorical_accuracy import CategoricalAccuracy
 
 
-@Metric.register("categorical_accuracy")
-class CategoricalAccuracy(Metric):
+@Metric.register("label_categorical_accuracy")
+class LabelCategoricalAccuracy(CategoricalAccuracy):
     """
-    Categorical Top-K accuracy. Assumes integer labels, with
-    each item to be classified having a single correct class.
-
-    Parameters
-    ----------
-    top_k : int, optional (default=``1``)
-        Counts a prediction as correct if the value of the gold label is
-        within the top_k of all predictions.
-    positive_label : int, optional (default=``None``)
-        Only track the accuracy of a specific gold label.
+    Categorical Top-K accuracy for a specific label. Assumes
+    integer labels, with each item to be classified having a
+    single correct class.
     """
-    def __init__(self,  # type: ignore
-                 top_k: int = 1,
-                 positive_label: Optional[int] = None) -> None:
-        self._top_k = top_k
+    def __init__(self, positive_label: int, top_k: int = 1) -> None:
+        super(LabelCategoricalAccuracy, self).__init__(top_k)
         self._positive_label = positive_label
-        self.correct_count = 0.
-        self.total_count = 0.
 
+    @overrides
     def __call__(self,
                  predictions: torch.Tensor,
                  gold_labels: torch.Tensor,
@@ -65,37 +56,13 @@ class CategoricalAccuracy(Metric):
         # This is of shape (batch_size, ..., top_k).
         correct = top_k.eq(gold_labels.long().unsqueeze(-1)).float()
 
-        # If positive_label is provided, we must have some sort of mask.
-        if self._positive_label is not None:
-            if mask is not None:
-                # Update the mask to account for positive_label
-                label_mask = gold_labels.eq(self._positive_label)
-                # elementwise-AND between label_mask and mask to get a mask
-                # over elements that are both 1
-                mask = label_mask.long() & mask.long()
-            else:
-                # positive_label itself defines the mask
-                mask = gold_labels.eq(self._positive_label)
+        if mask is None:
+            mask = torch.ones_like(gold_labels)
+        label_mask = gold_labels.eq(self._positive_label)
+        # elementwise-AND between label_mask and mask to get a mask
+        # over elements that are both 1
+        mask = label_mask.long() & mask.long()
 
-        if mask is not None:
-            correct *= mask.float().unsqueeze(-1)
-            self.total_count += mask.sum()
-        else:
-            self.total_count += gold_labels.numel()
+        correct *= mask.float().unsqueeze(-1)
+        self.total_count += mask.sum()
         self.correct_count += correct.sum()
-
-    def get_metric(self, reset: bool = False):
-        """
-        Returns
-        -------
-        The accumulated accuracy.
-        """
-        accuracy = float(self.correct_count) / float(self.total_count)
-        if reset:
-            self.reset()
-        return accuracy
-
-    @overrides
-    def reset(self):
-        self.correct_count = 0.0
-        self.total_count = 0.0
