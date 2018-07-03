faeb10e6658109a1a530c271684573914439516a
Joel Grus <joelgrus@gmail.com>
2018-07-03 11:14:47 -0700

    add comment about special from_params logic

--- allennlp/data/vocabulary.py
+++ allennlp/data/vocabulary.py
@@ -368,6 +368,11 @@ class Vocabulary(Registrable):
         -------
         A ``Vocabulary``.
         """
+        # Vocabulary is ``Registrable`` so that you can configure a custom subclass,
+        # but (unlike most of our registrables) almost everyone will want to use the
+        # base implementation. So instead of having an abstract ``VocabularyBase`` or
+        # such, we just add the logic for instantiating a registered subclass here,
+        # so that most users can continue doing what they were doing.
         vocab_type = params.pop("type", None)
         if vocab_type is not None:
             return cls.by_name(vocab_type).from_params(params=params, instances=instances)
