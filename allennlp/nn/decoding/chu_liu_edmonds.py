f11e04d9300b6741b2181c290f93a711219d701e
Mark Neumann <markng@allenai.org>
2018-07-06 13:44:10 -0700

    refactor

--- allennlp/nn/decoding/chu_liu_edmonds.py
+++ allennlp/nn/decoding/chu_liu_edmonds.py
@@ -85,49 +85,6 @@ def decode_mst(energy: numpy.ndarray,
 
     return heads, head_type
 
-def _find_cycle(parents: List[int],
-                length: int,
-                current_nodes: numpy.ndarray) -> Tuple[bool, List[int]]:
-    added = [False for _ in range(length)]
-    added[0] = True
-    cycle = set()
-    has_cycle = False
-    for i in range(1, length):
-        if has_cycle:
-            break
-        # don't redo nodes we've already
-        # visited or aren't considering.
-        if added[i] or not current_nodes[i]:
-            continue
-        # Initialize a new possible cycle.
-        this_cycle = set()
-        this_cycle.add(i)
-        added[i] = True
-        has_cycle = True
-        next_node = i
-
-        while parents[next_node] not in this_cycle:
-            next_node = parents[next_node]
-            # If we see a node we've already processed,
-            # we can stop, because the node we are
-            # processing would have been in that cycle.
-            if added[next_node]:
-                has_cycle = False
-                break
-            added[next_node] = True
-            this_cycle.add(next_node)
-
-        if has_cycle:
-            original = next_node
-            cycle.add(original)
-            next_node = parents[original]
-            while next_node != original:
-                cycle.add(next_node)
-                next_node = parents[next_node]
-            break
-
-    return has_cycle, list(cycle)
-
 def chu_liu_edmonds(length, score_matrix, current_nodes, final_edges, old_input, old_output, reps):
     # Set the initial graph to be the greedy best one.
     parents = [-1]
@@ -235,3 +192,47 @@ def chu_liu_edmonds(length, score_matrix, current_nodes, final_edges, old_input,
         parent = old_input[parents[l], l]
         final_edges[child] = parent
         l = parents[l]
+
+
+def _find_cycle(parents: List[int],
+                length: int,
+                current_nodes: numpy.ndarray) -> Tuple[bool, List[int]]:
+    added = [False for _ in range(length)]
+    added[0] = True
+    cycle = set()
+    has_cycle = False
+    for i in range(1, length):
+        if has_cycle:
+            break
+        # don't redo nodes we've already
+        # visited or aren't considering.
+        if added[i] or not current_nodes[i]:
+            continue
+        # Initialize a new possible cycle.
+        this_cycle = set()
+        this_cycle.add(i)
+        added[i] = True
+        has_cycle = True
+        next_node = i
+
+        while parents[next_node] not in this_cycle:
+            next_node = parents[next_node]
+            # If we see a node we've already processed,
+            # we can stop, because the node we are
+            # processing would have been in that cycle.
+            if added[next_node]:
+                has_cycle = False
+                break
+            added[next_node] = True
+            this_cycle.add(next_node)
+
+        if has_cycle:
+            original = next_node
+            cycle.add(original)
+            next_node = parents[original]
+            while next_node != original:
+                cycle.add(next_node)
+                next_node = parents[next_node]
+            break
+
+    return has_cycle, list(cycle)
