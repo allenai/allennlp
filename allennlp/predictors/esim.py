4f6d37fc729a12d7a94351329ac6821605b072ef
Matthew Peters <matthewp@allenai.org>
2018-07-09 14:52:37 -0700

    ESIM predictor

--- allennlp/predictors/esim.py
+++ allennlp/predictors/esim.py
@@ -1,9 +1,8 @@
-from typing import Tuple
 from overrides import overrides
 
 from allennlp.common.util import JsonDict
 from allennlp.data import Instance
-from allennlp.service.predictors.predictor import Predictor
+from allennlp.predictors.predictor import Predictor
 
 
 @Predictor.register('esim')
