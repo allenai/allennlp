8b3d8fb0403dc1d370eed9ca943a1db4b126a6ee
Mark Neumann <markng@allenai.org>
2018-07-12 14:40:00 -0700

    metrics tweaks

--- allennlp/training/metrics/attachment_scores.py
+++ allennlp/training/metrics/attachment_scores.py
@@ -16,10 +16,10 @@ class AttachmentScores(Metric):
     itself.
     """
     def __init__(self) -> None:
-        self._labeled_count = 0.
-        self._unlabeled_count = 0.
-        self._exact_labeled_count = 0.
-        self._exact_unlabeled_count = 0.
+        self._labeled_correct = 0.
+        self._unlabeled_correct = 0.
+        self._exact_labeled_correct = 0.
+        self._exact_unlabeled_correct = 0.
         self._total_words = 0.
         self._total_sentences = 0.
 
@@ -59,10 +59,10 @@ class AttachmentScores(Metric):
         correct_labels_and_indices = correct_indices * correct_labels
         labeled_exact_match = (correct_labels_and_indices + (1 - mask)).prod(dim=-1)
 
-        self._unlabeled_count += correct_indices.sum()
-        self._exact_unlabeled_count += unlabeled_exact_match.sum()
-        self._labeled_count += correct_labels_and_indices.sum()
-        self._exact_labeled_count += labeled_exact_match.sum()
+        self._unlabeled_correct += correct_indices.sum()
+        self._exact_unlabeled_correct += unlabeled_exact_match.sum()
+        self._labeled_correct += correct_labels_and_indices.sum()
+        self._exact_labeled_correct += labeled_exact_match.sum()
         self._total_sentences += correct_indices.size(0)
         self._total_words += correct_indices.numel() - (1 - mask).sum()
 
@@ -72,10 +72,16 @@ class AttachmentScores(Metric):
         -------
         The accumulated metrics as a dictionary.
         """
-        unlabeled_attachment_score = float(self._unlabeled_count) / float(self._total_words)
-        labeled_attachment_score = float(self._labeled_count) / float(self._total_words)
-        unlabeled_exact_match = float(self._exact_unlabeled_count) / float(self._total_sentences)
-        labeled_exact_match = float(self._exact_labeled_count) / float(self._total_sentences)
+        unlabeled_attachment_score = 0.0
+        labeled_attachment_score = 0.0
+        unlabeled_exact_match = 0.0
+        labeled_exact_match = 0.0
+        if self._total_words > 0.0:
+            unlabeled_attachment_score = float(self._unlabeled_correct) / float(self._total_words)
+            labeled_attachment_score = float(self._labeled_correct) / float(self._total_words)
+        if self._total_sentences > 0:
+            unlabeled_exact_match = float(self._exact_unlabeled_correct) / float(self._total_sentences)
+            labeled_exact_match = float(self._exact_labeled_correct) / float(self._total_sentences)
         if reset:
             self.reset()
         return {
@@ -87,9 +93,9 @@ class AttachmentScores(Metric):
 
     @overrides
     def reset(self):
-        self._labeled_count = 0.
-        self._unlabeled_count = 0.
-        self._exact_labeled_count = 0.
-        self._exact_unlabeled_count = 0.
+        self._labeled_correct = 0.
+        self._unlabeled_correct = 0.
+        self._exact_labeled_correct = 0.
+        self._exact_unlabeled_correct = 0.
         self._total_words = 0.
         self._total_sentences = 0.
