ea8ed8993ef7b5b1e14b460bb64aed50d6390fb9
harshtrivedi <harshjtrivedi94@gmail.com>
2018-07-13 00:50:41 -0400

    Update and add tests.

--- allennlp/tests/commands/make_vocab_test.py
+++ allennlp/tests/commands/make_vocab_test.py
@@ -1,12 +1,10 @@
 # pylint: disable=invalid-name,no-self-use
 import os
 
-import pytest
-
 from allennlp.common import Params
-from allennlp.common.checks import ConfigurationError
 from allennlp.common.testing import AllenNlpTestCase
 from allennlp.commands.make_vocab import make_vocab_from_params
+from allennlp.data import Vocabulary
 
 class TestMakeVocab(AllenNlpTestCase):
     def setUp(self):
@@ -38,17 +36,14 @@ class TestMakeVocab(AllenNlpTestCase):
                 }
         })
 
-    def test_make_vocab_fails_without_vocabulary_key(self):
-        with pytest.raises(ConfigurationError):
-            make_vocab_from_params(self.params)
+    def test_make_vocab_succeeds_without_vocabulary_key(self):
+        vocab_path = self.TEST_DIR / 'vocabulary'
+        make_vocab_from_params(self.params, vocab_path)
 
     def test_make_vocab_makes_vocab(self):
         vocab_path = self.TEST_DIR / 'vocabulary'
 
-        self.params['vocabulary'] = {}
-        self.params['vocabulary']['directory_path'] = vocab_path
-
-        make_vocab_from_params(self.params)
+        make_vocab_from_params(self.params, vocab_path)
 
         vocab_files = os.listdir(vocab_path)
         assert set(vocab_files) == {'labels.txt', 'non_padded_namespaces.txt', 'tokens.txt'}
@@ -69,10 +64,9 @@ class TestMakeVocab(AllenNlpTestCase):
         vocab_path = self.TEST_DIR / 'vocabulary'
 
         self.params['vocabulary'] = {}
-        self.params['vocabulary']['directory_path'] = vocab_path
         self.params['vocabulary']['min_count'] = {"tokens" : 3}
 
-        make_vocab_from_params(self.params)
+        make_vocab_from_params(self.params, vocab_path)
 
         vocab_files = os.listdir(vocab_path)
         assert set(vocab_files) == {'labels.txt', 'non_padded_namespaces.txt', 'tokens.txt'}
@@ -84,6 +78,42 @@ class TestMakeVocab(AllenNlpTestCase):
         assert tokens == ['.', '@@UNKNOWN@@', 'animals', 'are']
 
         with open(vocab_path / 'labels.txt') as f:
+            labels = [line.strip() for line in f]
+
+        labels.sort()
+        assert labels == ['N', 'V']
+
+    def test_make_vocab_with_extension(self):
+        existing_vocab_path = self.TEST_DIR / 'vocabulary_existing'
+        extended_vocab_path = self.TEST_DIR / 'vocabulary_extended'
+
+        vocab = Vocabulary()
+        vocab.add_token_to_namespace('some_weird_token_1', namespace='tokens')
+        vocab.add_token_to_namespace('some_weird_token_2', namespace='tokens')
+        os.makedirs(existing_vocab_path, exist_ok=True)
+        vocab.save_to_files(existing_vocab_path)
+
+        self.params['vocabulary'] = {}
+        self.params['vocabulary']['directory_path'] = existing_vocab_path
+        self.params['vocabulary']['extend'] = True
+        self.params['vocabulary']['min_count'] = {"tokens" : 3}
+        make_vocab_from_params(self.params, extended_vocab_path)
+
+        vocab_files = os.listdir(extended_vocab_path)
+        assert set(vocab_files) == {'labels.txt', 'non_padded_namespaces.txt', 'tokens.txt'}
+
+        with open(extended_vocab_path / 'tokens.txt') as f:
+            tokens = [line.strip() for line in f]
+
+        assert tokens[0] == '@@UNKNOWN@@'
+        assert tokens[1] == 'some_weird_token_1'
+        assert tokens[2] == 'some_weird_token_2'
+
+        tokens.sort()
+        assert tokens == ['.', '@@UNKNOWN@@', 'animals', 'are',
+                          'some_weird_token_1', 'some_weird_token_2']
+
+        with open(extended_vocab_path / 'labels.txt') as f:
             labels = [line.strip() for line in f]
 
         labels.sort()
