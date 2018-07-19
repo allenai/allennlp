dccabca17192d8234ba9187af5a1241bea7f9480
harshtrivedi <harshjtrivedi94@gmail.com>
2018-07-19 15:29:43 -0400

    Add model loading and parameter inspecting in dry-run.

--- allennlp/commands/dry_run.py
+++ allennlp/commands/dry_run.py
@@ -29,14 +29,16 @@ a model.
 import argparse
 import logging
 import os
+import re
 
 from allennlp.commands.train import datasets_from_params
 from allennlp.commands.subcommand import Subcommand
 from allennlp.common.checks import ConfigurationError
 from allennlp.common.params import Params
-from allennlp.common.util import prepare_environment
+from allennlp.common.util import prepare_environment, get_frozen_and_tunable_parameter_names
 from allennlp.data import Vocabulary
 from allennlp.data.dataset import Batch
+from allennlp.models import Model
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
@@ -112,3 +114,19 @@ def dry_run_from_params(params: Params, serialization_dir: str) -> None:
 
     logger.info(f"writing the vocabulary to {vocab_dir}.")
     vocab.save_to_files(vocab_dir)
+
+    model = Model.from_params(vocab=vocab, params=params.pop('model'))
+    trainer_params = params.pop("trainer")
+    no_grad_regexes = trainer_params.pop("no_grad", ())
+    for name, parameter in model.named_parameters():
+        if any(re.search(regex, name) for regex in no_grad_regexes):
+            parameter.requires_grad_(False)
+
+    frozen_parameter_names, tunable_parameter_names = \
+                   get_frozen_and_tunable_parameter_names(model)
+    logger.info("Following parameters are Frozen  (without gradient):")
+    for name in frozen_parameter_names:
+        logger.info(name)
+    logger.info("Following parameters are Tunable (with gradient):")
+    for name in tunable_parameter_names:
+        logger.info(name)
