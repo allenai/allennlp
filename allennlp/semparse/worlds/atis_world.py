5dfff5ff87a498c1d7e19e74d890bf97f19bd3ee
Kevin Lin <kevinl@kevinl.local>
2018-07-20 14:12:32 -0700

    dataset reader and semant parse tests

--- allennlp/semparse/worlds/atis_world.py
+++ allennlp/semparse/worlds/atis_world.py
@@ -1,9 +1,9 @@
-from typing import List
+from typing import List, Dict
 import re
 
 from parsimonious.grammar import Grammar
-from parsimonious.nodes import NodeVisitor, RegexNode
-from parsimonious.expressions import Literal 
+from parsimonious.nodes import Node, NodeVisitor
+from parsimonious.expressions import Literal
 
 from allennlp.semparse.contexts import atis_tables
 
@@ -16,11 +16,19 @@ class AtisWorld():
     """
     def __init__(self, conversation_context, utterance=None) -> None:
         self.conversation_context = conversation_context
-        self.utterance = utterance
-        self.valid_actions = self.get_all_valid_actions()
-        self.grammar_str = self.get_grammar_str()
+        self.utterance: str = utterance
+        self.valid_actions: Dict[str, List[str]] = self.init_all_valid_actions()
+        self.grammar_str: str = self.get_grammar_str()
 
-    def get_all_valid_actions(self):
+    def get_valid_actions(self) -> Dict[str, List[str]]:
+        return self.valid_actions
+
+    def init_all_valid_actions(self) -> Dict[str, List[str]]:
+        """
+        We initialize the world's valid actions with that of the context. This means that the strings
+        and numbers that were valid earlier in the interaction are also valid. We then add new valid strings
+        and numbers from the current utterance.
+        """
         valid_actions = self.conversation_context.valid_actions
         for local_str in self.get_local_strs():
             if local_str not in valid_actions['string']:
@@ -33,8 +41,12 @@ class AtisWorld():
         return valid_actions
 
     def get_grammar_str(self) -> str:
+        """
+        Generate a string that can be used to instantiate a ``Grammar`` object. The string is a sequence of
+        rules that define the grammar.
+        """
+
         grammar_str_with_context = self.conversation_context.base_sql_def
-        
 
         grammar_str_with_context += "\n    col_ref \t\t = " +  \
                                     " / ".join(sorted(self.valid_actions['col_ref'], reverse=True)) + \
@@ -54,15 +66,16 @@ class AtisWorld():
 
 
     def get_local_strs(self) -> List[str]:
-        local_strs: List[str] = [] 
-
+        """
+        Based on the current utterance, return a list of valid strings and numbers that should be added.
+        """
+        local_strs: List[str] = []
         trigger_lists = [atis_tables.CITIES, atis_tables.AIRPORT_CODES,
                          atis_tables.STATES, atis_tables.STATE_CODES,
                          atis_tables.FARE_BASIS_CODE, atis_tables.CLASS,
                          atis_tables.AIRLINE_CODE_LIST, atis_tables.DAY_OF_WEEK,
                          atis_tables.CITY_CODE_LIST, atis_tables.MEALS,
                          atis_tables.RESTRICT_CODES]
-
 
         for trigger_list in trigger_lists:
             for trigger in trigger_list:
@@ -93,8 +106,12 @@ class AtisWorld():
         return []
 
     def all_possible_actions(self) -> List[str]:
+        """
+        Return a list of strings representing all possible actions
+        of the form lhs -> [rhs]
+        """
         all_actions = set()
-        for non_term, rhs_list in self.get_all_valid_actions().items():
+        for non_term, rhs_list in self.valid_actions.items():
             for rhs in rhs_list:
                 if non_term == 'string':
                     all_actions.add('{} -> ["\'{}\'"]'.format(non_term, rhs))
@@ -106,34 +123,49 @@ class AtisWorld():
                     ws_str = rhs.lstrip("(").rstrip(")")
                     curr_child_strs = [tok for tok in re.split(" ws |ws | ws", ws_str) if tok]
                     all_actions.add("{} -> [{}]".format(non_term, ", ".join(curr_child_strs)))
+
         return sorted(all_actions)
 
 
 class SQLVisitor(NodeVisitor):
+    """
+    ``SQLVisitor`` performs a depths-first traversal of the the AST. It takes the parse tree
+    and gives us a action sequence that resulted in that parse.
 
-    def __init__(self, grammar):
-        self.prod_acc = []
-        self.grammar = grammar
+    """
+    def __init__(self, grammar: str) -> None:
+        """
+        Parameters
+        __________
+        grammar : ``str``
+            A string that descrbies the PEG (parsing expression grammar) in the form of:
+                nonterminal = ...
+        """
+        self.prod_acc: List[str] = []
+        self.grammar: str = grammar
 
         for nonterm in self.grammar.keys():
             if nonterm != 'stmt':
                 self.__setattr__('visit_' + nonterm, self.add_prod_rule)
 
-    def generic_visit(self, node, visited_children):
+    def generic_visit(self, node: Node, visited_children) -> None:
         self.add_prod_rule(node)
 
-    def add_prod_rule(self, node, children=None):
+    def add_prod_rule(self, node: Node, children=None) -> None:
+        """
+        For each node, we accumulate the rules that generated its children in a list
+        """
         if node.expr.name and node.expr.name != 'ws':
             lhs = '{} -> '.format(node.expr.name)
 
             if isinstance(node.expr, Literal):
                 rhs = '["{}"]'.format(node.text)
-                
+
             else:
                 child_strs = []
                 for child in node.__iter__():
                     if child.expr.name == 'ws':
-                        continue 
+                        continue
                     if child.expr.name != '':
                         child_strs.append(child.expr.name)
                     else:
@@ -145,6 +177,9 @@ class SQLVisitor(NodeVisitor):
             rule = lhs + rhs
             self.prod_acc = [rule] + self.prod_acc
 
-    def visit_stmt(self, node, children):
+    def visit_stmt(self, node: Node, children=None) -> List[str]:
+        """
+        The top level is a statement, so return the productions that we have accumulated
+        """
         self.add_prod_rule(node)
         return self.prod_acc
