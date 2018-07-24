244eeaf783436026c6276b5d091342fcedb82c4b
harshtrivedi <harshjtrivedi94@gmail.com>
2018-07-24 14:04:57 -0400

    Add typing in MatchTuple

--- scripts/check_links.py
+++ scripts/check_links.py
@@ -6,11 +6,13 @@ import re
 import sys
 import pathlib
 from multiprocessing.dummy import Pool
-from collections import namedtuple
-from typing import Tuple
+from typing import Tuple, NamedTuple
 import requests
 
-MatchTuple = namedtuple('MatchTuple', 'source name link')
+class MatchTuple(NamedTuple):
+    source: str
+    name: str
+    link: str
 
 def url_ok(match_tuple: MatchTuple) -> Tuple[MatchTuple, bool]:
     try:
@@ -31,7 +33,7 @@ if __name__ == "__main__":
                 matches = url_regex.findall(line)
                 for name, link in matches:
                     if 'localhost' not in link:
-                        all_matches.add(MatchTuple(source=markdown_file, name=name, link=link))
+                        all_matches.add(MatchTuple(source=str(markdown_file), name=name, link=link))
 
     with Pool(processes=10) as pool:
         results = map(url_ok, [match for match in list(all_matches)])
@@ -41,7 +43,7 @@ if __name__ == "__main__":
         print("UnReachable Links:")
         for index, result in enumerate(unreachable_results):
             print("\n{}.".format(index))
-            print("Source: " + str(result[0].source))
+            print("Source: " + result[0].source)
             print("Name: " + result[0].name)
             print("Link: " + result[0].link)
         sys.exit(1)
