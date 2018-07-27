52b689f2967ac4e102deb6f8f13cf6a57857a5f1
Kevin Lin <kevinl@kevinl.local>
2018-07-27 14:09:48 -0700

    Restructure SQL into own module

--- allennlp/semparse/worlds/atis_world.py
+++ allennlp/semparse/worlds/atis_world.py
@@ -1,25 +1,47 @@
+from copy import deepcopy
 from typing import List, Dict
-import re
-from overrides import overrides
+from collections import defaultdict
 
 from parsimonious.grammar import Grammar
-from parsimonious.nodes import Node, NodeVisitor
-from parsimonious.expressions import Literal
 
-from allennlp.semparse.contexts import atis_tables
+from allennlp.semparse.contexts.atis_tables import * # pylint: disable=wildcard-import,unused-wildcard-import
+from allennlp.semparse.contexts.sql_table_context import SqlTableContext, SqlVisitor, generate_one_of_str
 
-def generate_one_of_str(nonterminal: str, literals: List[str]) -> str:
-    return  f"\n{nonterminal} \t\t = " + " / ".join([f'"{literal}"' for literal in literals])
+from allennlp.data.tokenizers import WordTokenizer
+
+def get_trigger_dict(trigger_lists: List[List[str]],
+                     trigger_dicts: List[Dict[str, List[str]]]) -> Dict[str, List[str]]:
+    merged_trigger_dict: Dict[str, List[str]] = defaultdict(list)
+    for trigger_list in trigger_lists:
+        for trigger in trigger_list:
+            merged_trigger_dict[trigger.lower()].append(trigger)
+
+    for trigger_dict in trigger_dicts:
+        for key, value in trigger_dict.items():
+            merged_trigger_dict[key.lower()].extend(value)
+
+    return merged_trigger_dict
 
 class AtisWorld():
     """
-    World representation for the Atis SQL domain.
+    World representation for the Atis SQL domain. This class has a ``SqlTableContext`` which holds the base
+    grammars, it then augments this grammar with the entities that are detected from utterances.
+
+    Parameters
+    __________
+    utterances: ``List[str]``
+        A list of utterances in the interaction, the last element in this list is the
+        current utterance that we are interested in.
     """
-    def __init__(self, conversation_context, utterance=None) -> None:
-        self.conversation_context = conversation_context
-        self.utterance: str = utterance
+    sql_table_context = SqlTableContext(TABLES)
+
+    def __init__(self, utterances: List[str], tokenizer=None) -> None:
+        self.utterances: List[str] = utterances
+        self.tokenizer = tokenizer if tokenizer else WordTokenizer()
+        self.tokenized_utterances = [self.tokenizer.tokenize(utterance) for utterance in self.utterances]
         self.valid_actions: Dict[str, List[str]] = self.init_all_valid_actions()
         self.grammar_str: str = self.get_grammar_str()
+        self.grammar_with_context = Grammar(self.grammar_str)
 
     def get_valid_actions(self) -> Dict[str, List[str]]:
         return self.valid_actions
@@ -30,16 +52,17 @@ class AtisWorld():
         and numbers that were valid earlier in the interaction are also valid. We then add new valid strings
         and numbers from the current utterance.
         """
-        valid_actions = self.conversation_context.valid_actions
+        valid_actions = deepcopy(self.sql_table_context.valid_actions)
         for local_str in self.get_local_strs():
             if local_str not in valid_actions['string']:
                 valid_actions['string'].append(local_str)
 
         numbers = ['0', '1']
-        numbers.extend(atis_tables.get_numbers_from_utterance(self.utterance))
-        for number in numbers:
-            if number not in valid_actions['number']:
-                valid_actions['number'].append(number)
+        for utterance in self.utterances:
+            numbers.extend(get_numbers_from_utterance(utterance))
+            for number in numbers:
+                if number not in valid_actions['number']:
+                    valid_actions['number'].append(number)
 
         return valid_actions
 
@@ -49,14 +72,8 @@ class AtisWorld():
         rules that define the grammar.
         """
 
-        grammar_str_with_context = self.conversation_context.base_sql_def
+        grammar_str_with_context = self.sql_table_context.grammar_str
 
-        grammar_str_with_context += "\n    col_ref \t\t = " +  \
-                                    " / ".join(sorted(self.valid_actions['col_ref'], reverse=True))
-
-        grammar_str_with_context += generate_one_of_str("table_name",
-                                                        sorted(self.valid_actions["table_name"],
-                                                               reverse=True))
         grammar_str_with_context += generate_one_of_str("number",
                                                         sorted(self.valid_actions['number'],
                                                                reverse=True))
@@ -69,38 +86,37 @@ class AtisWorld():
 
     def get_local_strs(self) -> List[str]:
         """
-        Based on the current utterance, return a list of valid strings and numbers that should be added.
+        Based on the current utterance, return a list of valid strings that should be added.
         """
         local_strs: List[str] = []
-        trigger_lists = [atis_tables.CITIES, atis_tables.AIRPORT_CODES,
-                         atis_tables.STATES, atis_tables.STATE_CODES,
-                         atis_tables.FARE_BASIS_CODE, atis_tables.CLASS,
-                         atis_tables.AIRLINE_CODE_LIST, atis_tables.DAY_OF_WEEK,
-                         atis_tables.CITY_CODE_LIST, atis_tables.MEALS,
-                         atis_tables.RESTRICT_CODES]
+        trigger_lists = [CITIES, AIRPORT_CODES,
+                         STATES, STATE_CODES,
+                         FARE_BASIS_CODE, CLASS,
+                         AIRLINE_CODE_LIST, DAY_OF_WEEK,
+                         CITY_CODE_LIST, MEALS,
+                         RESTRICT_CODES]
+        trigger_dicts = [CITY_AIRPORT_CODES,
+                         AIRLINE_CODES,
+                         CITY_CODES,
+                         GROUND_SERVICE,
+                         YES_NO,
+                         MISC_STR]
 
-        for trigger_list in trigger_lists:
-            for trigger in trigger_list:
-                if trigger.lower() in self.utterance.lower():
-                    local_strs.append(trigger)
+        trigger_dict = get_trigger_dict(trigger_lists, trigger_dicts)
 
-        trigger_dict_list = [atis_tables.AIRLINE_CODES,
-                             atis_tables.CITY_CODES,
-                             atis_tables.GROUND_SERVICE,
-                             atis_tables.YES_NO,
-                             atis_tables.MISC_STR]
+        for tokenized_utterance in self.tokenized_utterances:
+            if tokenized_utterance:
+                for first_token, second_token in zip(tokenized_utterance, tokenized_utterance[1:]):
+                    local_strs.extend(trigger_dict.get(first_token.text.lower(), []))
+                    bigram = f"{first_token.text} {second_token.text}"
+                    local_strs.extend(trigger_dict.get(bigram.lower(), []))
+                local_strs.extend(trigger_dict.get(tokenized_utterance[-1].text.lower(), []))
 
-        for trigger_dict in trigger_dict_list:
-            for trigger in trigger_dict:
-                if trigger.lower() in self.utterance.lower():
-                    local_strs.append(trigger_dict[trigger])
-
-        local_strs.extend(atis_tables.DAY_OF_WEEK)
+        local_strs.extend(DAY_OF_WEEK)
         return local_strs
 
     def get_action_sequence(self, query: str) -> List[str]:
-        grammar_with_context = Grammar(self.get_grammar_str())
-        sql_visitor = SqlVisitor(grammar_with_context)
+        sql_visitor = SqlVisitor(self.grammar_with_context)
         query = query.split("\n")[0]
         if query:
             action_sequence = sql_visitor.parse(query)
@@ -127,54 +143,3 @@ class AtisWorld():
                     all_actions.add(f"{nonterminal} -> [{', '.join(child_strings)}]")
 
         return sorted(all_actions)
-
-
-class SqlVisitor(NodeVisitor):
-    """
-    ``SqlVisitor`` performs a depths-first traversal of the the AST. It takes the parse tree
-    and gives us a action sequence that resulted in that parse.
-
-    """
-    def __init__(self, grammar: str) -> None:
-        """
-        Parameters
-        __________
-        grammar : ``str``
-            A string that descrbies the PEG (parsing expression grammar) in the form of:
-                nonterminal = ...
-        """
-        self.action_sequence: List[str] = []
-        self.grammar: Grammar = grammar
-
-    @overrides
-    def generic_visit(self, node: Node, visited_children: List[None]) -> List[str]: # pylint
-        self.add_action(node)
-        if node.expr.name == 'statement':
-            return self.action_sequence
-        return []
-
-    def add_action(self, node: Node) -> None:
-        """
-        For each node, we accumulate the rules that generated its children in a list
-        """
-        if node.expr.name and node.expr.name != 'ws':
-            nonterminal = f'{node.expr.name} -> '
-
-            if isinstance(node.expr, Literal):
-                right_hand_side = f'["{node.text}"]'
-
-            else:
-                child_strings = []
-                for child in node.__iter__():
-                    if child.expr.name == 'ws':
-                        continue
-                    if child.expr.name != '':
-                        child_strings.append(child.expr.name)
-                    else:
-                        child_right_side_string = child.expr._as_rhs().lstrip("(").rstrip(")") # pylint: disable=protected-access
-                        child_right_side_list = [tok for tok in re.split(" ws |ws | ws", child_right_side_string) if tok]
-                        child_strings.extend(child_right_side_list)
-                right_hand_side = "[" + ", ".join(child_strings) + "]"
-
-            rule = nonterminal + right_hand_side
-            self.action_sequence = [rule] + self.action_sequence
