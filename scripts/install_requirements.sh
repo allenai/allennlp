10ac9ede83df2bb6747b42731e0a2fbabb680d61
Michael Schmitz <michael@schmitztech.com>
2018-07-30 10:04:37 -0700

    Update install_requirements.sh

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip install -r requirements.txt
-python -m nltk.downloader punkt
-spacy download en_core_web_sm # Used by allennlp.data.tokenizers.word_splitter.NltkWordSplitter
+python -m nltk.downloader punkt # Used by allennlp.data.tokenizers.word_splitter.NltkWordSplitter
+spacy download en_core_web_sm
