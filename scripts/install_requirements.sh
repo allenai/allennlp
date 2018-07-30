d49dca6ef2b50e016881a32159e9dab4a61ed735
Michael Schmitz <michael@schmitztech.com>
2018-07-30 09:33:35 -0700

    Document where punkt model is used.

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -2,4 +2,4 @@
 
 pip install -r requirements.txt
 python -m nltk.downloader punkt
-spacy download en_core_web_sm
+spacy download en_core_web_sm # Used by allennlp.data.tokenizers.word_splitter.NltkWordSplitter
