1c2a0deb2c32bb7294db0ae3a406a0d07f38e150
Michael Schmitz <michael@schmitztech.com>
2018-07-30 10:03:25 -0700

    Remove requirements_test.txt (merge into requirements.txt) (#1541)

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -2,9 +2,4 @@
 
 pip install -r requirements.txt
 python -m nltk.downloader punkt
-spacy download en_core_web_sm
-
-# only install test requirements if explicitly specified
-if [[ "$INSTALL_TEST_REQUIREMENTS" == "true" ]]; then
-    pip install -r requirements_test.txt
-fi
+spacy download en_core_web_sm # Used by allennlp.data.tokenizers.word_splitter.NltkWordSplitter
