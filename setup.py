54b871dcff0cbf63c9c2665e2197f4ace6df5241
Joel Grus <joelgrus@gmail.com>
2018-07-30 16:22:05 -0700

    loosen bounds on pytorch version

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(name='allennlp',
       packages=find_packages(exclude=["*.tests", "*.tests.*",
                                       "tests.*", "tests"]),
       install_requires=[
-          'torch==0.4.1',
+          'torch>=0.4.0,<0.5.0',
           'jsonnet==0.10.0',
           'typing',
           'overrides',
