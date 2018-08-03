d2c02742ea0f147366d51a73ecb1c209e61425c6
Michael Schmitz <michael@schmitztech.com>
2018-08-03 07:38:43 -0700

    Don't use a hard-coded temp directory. (#1564)

--- allennlp/common/testing/test_case.py
+++ allennlp/common/testing/test_case.py
@@ -3,10 +3,12 @@ import logging
 import os
 import pathlib
 import shutil
+import tempfile
 from unittest import TestCase
 
 from allennlp.common.checks import log_pytorch_version_info
 
+TEST_DIR = tempfile.mkdtemp(prefix="allennlp_tests")
 
 class AllenNlpTestCase(TestCase):  # pylint: disable=too-many-public-methods
     """
@@ -31,7 +33,7 @@ class AllenNlpTestCase(TestCase):  # pylint: disable=too-many-public-methods
         logging.getLogger('urllib3.connectionpool').disabled = True
         log_pytorch_version_info()
 
-        self.TEST_DIR = pathlib.Path("/tmp/allennlp_tests/")
+        self.TEST_DIR = pathlib.Path(TEST_DIR)
 
         os.makedirs(self.TEST_DIR, exist_ok=True)
 
