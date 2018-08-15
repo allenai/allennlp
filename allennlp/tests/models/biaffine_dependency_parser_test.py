954012557ab2488bebda4101d81357ae9a6d02ce
Mark Neumann <markn@allenai.org>
2018-08-14 19:00:12 -0700

    Tree decoding fix (#1606)
    * initial fix
    
    * correct approach
    
    * fix and test
    
    * fix predictor test
    
    * fix pylint
    
    * use unique edge weights

--- allennlp/tests/models/biaffine_dependency_parser_test.py
+++ allennlp/tests/models/biaffine_dependency_parser_test.py
@@ -1,5 +1,7 @@
 # pylint: disable=no-self-use,invalid-name,no-value-for-parameter
 
+import torch
+
 from allennlp.common.testing.model_test_case import ModelTestCase
 
 class BiaffineDependencyParserTest(ModelTestCase):
@@ -31,3 +33,30 @@ class BiaffineDependencyParserTest(ModelTestCase):
                                                       'tag_loss', 'loss', 'mask',
                                                       'predicted_dependencies', 'predicted_heads',
                                                       'words', 'pos'])
+
+    def test_mst_respects_no_outgoing_root_edges_constraint(self):
+        # This energy tensor expresses the following relation:
+        # energy[i,j] = "Score that j is the head of i". In this
+        # case, we have heads pointing to their children.
+
+        # We want to construct a case that has 2 children for the ROOT node,
+        # because in a typical dependency parse there should only be one
+        # word which has the ROOT as it's head.
+
+        energy = torch.Tensor([[0, 2, 1],
+                               [10, 0, 0.5],
+                               [9, 0.2, 0]]).view(1, 1, 3, 3)
+        # In this case, the maximum weight tree looks like this,
+        # with both edges having weight 10.
+        #               A(index 0)
+        #               /        \
+        #              \/        \/
+        #          B(index 1)   C(index 2)
+        length = torch.LongTensor([3])
+        heads, _ = self.model._run_mst_decoding(energy, length) # pylint: disable=protected-access
+        # This is the correct MST, but not desirable for dependency parsing.
+        assert heads.tolist()[0] == [-1, 0, 0]
+
+        energy[:, :, 0, :] = 0
+        heads, _ = self.model._run_mst_decoding(energy, length) # pylint: disable=protected-access
+        assert heads.tolist()[0] == [-1, 0, 1]
