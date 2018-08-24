640fed4175c82d6f80cc6c92cf3febcfcc0409cb
Eunsol Choi <eunsol@Eunsols-MacBook-Pro.local>
2018-08-24 16:19:19 -0700

    ;

--- allennlp/tests/models/reading_comprehension/dialog_qa_test.py
+++ allennlp/tests/models/reading_comprehension/dialog_qa_test.py
@@ -1,18 +1,7 @@
 # pylint: disable=no-self-use,invalid-name
+
 from allennlp.common.testing import ModelTestCase
 from allennlp.data.dataset import Batch
-
-import pytest
-import numpy
-from numpy.testing import assert_almost_equal
-import torch
-
-from allennlp.common import Params
-from allennlp.common.checks import ConfigurationError
-from allennlp.common.testing import ModelTestCase
-from allennlp.data import DatasetReader, Vocabulary
-from allennlp.data.dataset import Batch
-from allennlp.models import BidirectionalAttentionFlow, Model
 
 
 class DialogQATest(ModelTestCase):
@@ -34,15 +23,4 @@ class DialogQATest(ModelTestCase):
 
     def test_batch_predictions_are_consistent(self):
         # Save some state.
-        saved_model = self.model
-        saved_instances = self.instances
-
-        # Modify the state, run the test with modified state.
-        params = Params.from_file(self.param_file)
-        reader = DatasetReader.from_params(params['dataset_reader'])
-        self.instances = reader.read(self.FIXTURES_ROOT / 'data' / 'quac_sample.json')
-        vocab = Vocabulary.from_instances(self.instances)
-        for instance in self.instances:
-            instance.index_fields(vocab)
-        self.model = Model.from_params(vocab=vocab, params=params['model'])
         self.ensure_batch_predictions_are_consistent()
