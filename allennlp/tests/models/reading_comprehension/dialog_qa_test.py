348e3d61739489a9ba659148e4d1fc3ecc312f08
eunsol <high07@gmail.com>
2018-08-24 16:01:06 -0700

    made test smaller

--- allennlp/tests/models/reading_comprehension/dialog_qa_test.py
+++ allennlp/tests/models/reading_comprehension/dialog_qa_test.py
@@ -2,19 +2,6 @@
 from allennlp.common.testing import ModelTestCase
 from allennlp.data.dataset import Batch
 
-import pytest
-import numpy
-from numpy.testing import assert_almost_equal
-import torch
-
-from allennlp.common import Params
-from allennlp.common.checks import ConfigurationError
-from allennlp.common.testing import ModelTestCase
-from allennlp.data import DatasetReader, Vocabulary
-from allennlp.data.dataset import Batch
-from allennlp.models import BidirectionalAttentionFlow, Model
-
-
 class DialogQATest(ModelTestCase):
     def setUp(self):
         super(DialogQATest, self).setUp()
@@ -22,6 +9,7 @@ class DialogQATest(ModelTestCase):
                           self.FIXTURES_ROOT / 'data' / 'quac_sample.json')
         self.batch = Batch(self.instances)
         self.batch.index_instances(self.vocab)
+        print(self.instances)
 
     def test_forward_pass_runs_correctly(self):
         training_tensors = self.batch.as_tensor_dict()
@@ -33,16 +21,4 @@ class DialogQATest(ModelTestCase):
         self.ensure_model_can_train_save_and_load(self.param_file, tolerance=1e-4)
 
     def test_batch_predictions_are_consistent(self):
-        # Save some state.
-        saved_model = self.model
-        saved_instances = self.instances
-
-        # Modify the state, run the test with modified state.
-        params = Params.from_file(self.param_file)
-        reader = DatasetReader.from_params(params['dataset_reader'])
-        self.instances = reader.read(self.FIXTURES_ROOT / 'data' / 'quac_sample.json')
-        vocab = Vocabulary.from_instances(self.instances)
-        for instance in self.instances:
-            instance.index_fields(vocab)
-        self.model = Model.from_params(vocab=vocab, params=params['model'])
         self.ensure_batch_predictions_are_consistent()
