6c7c807220be45e106bde12c0e6d128a70ca32c8
Handsome Zebra <aaahchi@gmail.com>
2018-08-27 09:32:00 -0700

    Adding decoder to bimpm and improve demo server. (#1665)
    * Adding decoder to bimpm and add model weights and overrides to flask server.
    
    * Refine comments for pylint.

--- allennlp/tests/models/bimpm_test.py
+++ allennlp/tests/models/bimpm_test.py
@@ -19,3 +19,9 @@ class TestBiMPM(ModelTestCase):
 
     def test_batch_predictions_are_consistent(self):
         self.ensure_batch_predictions_are_consistent()
+
+    def test_decode_runs_correctly(self):
+        training_tensors = self.dataset.as_tensor_dict()
+        output_dict = self.model(**training_tensors)
+        decode_output_dict = self.model.decode(output_dict)
+        assert "label" in decode_output_dict
