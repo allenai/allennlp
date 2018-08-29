f817f9deabc6dbd039ac5b2bf0c413b015c0e71d
Nelson Liu <nfliu@cs.washington.edu>
2018-08-29 00:50:08 -0700

    Add more docs

--- allennlp/training/metrics/covariance.py
+++ allennlp/training/metrics/covariance.py
@@ -10,6 +10,20 @@ from allennlp.training.metrics.metric import Metric
 class Covariance(Metric):
     """
     This ``Metric`` calculates the unbiased sample covariance between two tensors.
+    Each element in the two tensors is assumed to be a different observation of the
+    variable (i.e., the input tensors are implicitly flattened into vectors and the
+    covariance is calculated between the vectors).
+
+    This implementation is mostly modeled after the streaming_covariance function in Tensorflow.
+    See https://github.com/tensorflow/tensorflow/blob/v1.10.1/tensorflow/contrib/metrics/python/ops/metric_ops.py#L3127-L3264 .
+
+    The following is copied from the Tensorflow documentation:
+
+    The algorithm used for this online computation is described in
+    https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Online .
+    Specifically, the formula used to combine two sample comoments is
+    `C_AB = C_A + C_B + (E[x_A] - E[x_B]) * (E[y_A] - E[y_B]) * n_A * n_B / n_AB`
+    The comoment for a single batch of data is simply `sum((x - E[x]) * (y - E[y]))`, optionally masked.
     """
     def __init__(self) -> None:
         self._total_prediction_mean = 0.0
--- allennlp/training/metrics/pearson_correlation.py
+++ allennlp/training/metrics/pearson_correlation.py
@@ -12,7 +12,23 @@ from allennlp.training.metrics.metric import Metric
 class PearsonCorrelation(Metric):
     """
     This ``Metric`` calculates the sample Pearson correlation coefficient (r)
-    between two tensors.
+    between two tensors. Each element in the two tensors is assumed to be
+    a different observation of the variable (i.e., the input tensors are
+    implicitly flattened into vectors and the correlation is calculated
+    between the vectors).
+
+    This implementation is mostly modeled after the streaming_pearson_correlation function in Tensorflow.
+    See https://github.com/tensorflow/tensorflow/blob/v1.10.1/tensorflow/contrib/metrics/python/ops/metric_ops.py#L3267-L3351
+
+    This metric delegates to the Covariance metric the tracking of three [co]variances:
+
+    - ``covariance(predictions, labels)``, i.e. covariance
+    - ``covariance(predictions, predictions)``, i.e. variance of ``predictions``
+    - ``covariance(labels, labels)``, i.e. variance of ``labels``
+
+    If we have these values, the sample Pearson correlation coefficient is simply:
+
+    r = covariance * (sqrt(predictions_variance) * sqrt(labels_variance))
     """
     def __init__(self) -> None:
         self._predictions_labels_covariance = Covariance()
