df387012f956598dab3dc6de43e9d2e6b802bc79
epwalsh <epwalsh10@gmail.com>
2018-08-31 09:39:29 -0700

    updates

--- scripts/check_requirements_and_setup.py
+++ scripts/check_requirements_and_setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
 """
-Checks that the requirements as specifed in requirements.txt and in setup.py are identical.
+This script checks the requirements in requirements.txt against those in setup.py.
+
+In particular, it makes sure that there are no duplicates within either setup.py or
+requirements.txt and that each package that is listed in both setup.py
+and requirements.txt having matching versions. It also ensures that any requirement
+listed as "ESSENTIAL" in requirements.txt appears under the "install_requires"
+field in setup.py.
 """
 
 import re
@@ -54,90 +60,97 @@ def parse_requirements() -> Tuple[PackagesType, PackagesType, Set[str]]:
     return essential_packages, other_packages, duplicates
 
 
-def parse_setup() -> Tuple[PackagesType, PackagesType, Set[str]]:
+def parse_setup() -> Tuple[PackagesType, PackagesType, Set[str], Set[str]]:
     """Parse all dependencies out of the setup.py script."""
     essential_packages: PackagesType = {}
     test_packages: PackagesType = {}
-    duplicates: Set[str] = set()
+    essential_duplicates: Set[str] = set()
+    test_duplicates: Set[str] = set()
+
     with open('setup.py') as setup_file:
         contents = setup_file.read()
 
     # Parse out essential packages.
-    package_string = re.search(r"""install_requires=\[[\s\n]*['"](.*?)['"][\s\n]*\]""",
+    package_string = re.search(r"""install_requires=\[[\s\n]*['"](.*?)['"],?[\s\n]*\]""",
                                contents, re.DOTALL).groups()[0].strip()
     for package in re.split(r"""['"],[\s\n]+['"]""", package_string):
         module, version = parse_package(package)
         if module in essential_packages:
-            duplicates.add(module)
+            essential_duplicates.add(module)
         else:
             essential_packages[module] = version
 
     # Parse packages only needed for testing.
-    package_string = re.search(r"""tests_require=\[[\s\n]*['"](.*?)['"][\s\n]*\]""",
+    package_string = re.search(r"""tests_require=\[[\s\n]*['"](.*?)['"],?[\s\n]*\]""",
                                contents, re.DOTALL).groups()[0].strip()
     for package in re.split(r"""['"],[\s\n]+['"]""", package_string):
         module, version = parse_package(package)
-        if module in essential_packages or module in test_packages:
-            duplicates.add(module)
+        if module in test_packages:
+            test_duplicates.add(module)
         else:
             test_packages[module] = version
 
-    return essential_packages, test_packages, duplicates
+    return essential_packages, test_packages, essential_duplicates, test_duplicates
 
 
 def main() -> int:
     exit_code = 0
 
-    requirements_essential, requirements_other, duplicates = parse_requirements()
-    if duplicates:
+    requirements_essential, requirements_other, requirements_duplicate = parse_requirements()
+    requirements_all = dict(requirements_essential, **requirements_other)
+    setup_essential, setup_test, essential_duplicates, test_duplicates = parse_setup()
+
+    if requirements_duplicate:
         exit_code = 1
-        for module in duplicates:
+        for module in requirements_duplicate:
             print(f"  ✗ '{module}' appears more than once in requirements.txt")
 
-    setup_essential, setup_test, duplicates = parse_setup()
-    if duplicates:
+    if essential_duplicates:
         exit_code = 1
-        for module in duplicates:
-            print(f"  ✗ '{module}' appears more than once in setup.py")
+        for module in essential_duplicates:
+            print(f"  ✗ '{module}' appears more than once in 'install_requires' "
+                  f"section of setup.py")
+
+    if test_duplicates:
+        exit_code = 1
+        for module in test_duplicates:
+            print(f"  ✗ '{module}' appears more than once in 'tests_require' "
+                  f"section of setup.py")
 
     # Find all packages listed as essential in requirements.txt that differ
     # in or are absent from setup.py.
     for module, version in requirements_essential.items():
-        if module not in setup_essential and module not in setup_test:
+        if module not in setup_essential:
             exit_code = 1
             print(f"  ✗ '{module}' listed as essential in requirements.txt "
-                  f"but is missing from setup.py")
-        elif module in setup_test:
-            exit_code = 1
-            print(f"  ✗ '{module}' listed as essential in requirements.txt "
-                  f"but is only listed a test requirement in setup.py")
+                  f"but is missing from 'install_requires' field of setup.py")
         elif setup_essential[module] != version:
             exit_code = 1
-            print(f"  ✗ '{module}' has version '{version}' in requirements.txt "
-                  f"but '{setup_essential[module]}' in setup.py")
+            print(f"  ✗ '{module}' has version '{version}' in requirements.txt but "
+                  f"'{setup_essential[module]}' in 'install_requires' field of setup.py")
 
-    # Find all packages listed as essential setup.py that are not listed as essential
-    # in requirements.txt.
+    # Find all packages listed under 'install_requires' in setup.py that are not
+    # listed as essential in requirements.txt.
     for module, version in setup_essential.items():
         if module in requirements_other:
             exit_code = 1
-            print(f"  ✗ '{module}' appears in setup.py but is listed as "
-                  f"non-essential in requirements.txt")
+            print(f"  ✗ '{module}' appears under 'install_requires' in setup.py but "
+                  f"is listed as non-essential in requirements.txt")
         elif module not in requirements_essential:
             exit_code = 1
             print(f"  ✗ '{module}' appears in setup.py but not in requirements.txt")
 
-    # Find all packages listed under `tests_require` in setup.py that are missing
-    # from requirements.txt.
+    # Find all packages listed under 'tests_require' in setup.py that are missing
+    # from requirements.txt, or have a version discrepancy.
     for module, version in setup_test.items():
-        if module not in requirements_other:
+        if module not in requirements_all:
             exit_code = 1
-            print(f"  ✗ '{module}' appears in as a test requirement in setup.py "
+            print(f"  ✗ '{module}' appears under 'tests_require' in setup.py "
                   f"but is missing from requirements.txt")
-        elif requirements_other[module] != version:
+        elif requirements_all[module] != version:
             exit_code = 1
-            print(f"  ✗ '{module}' has version '{version}' in setup.py "
-                  f"but '{requirements_other[module]}' in requirements.txt")
+            print(f"  ✗ '{module}' has version '{version}' in 'tests_require' field of "
+                  f"setup.py but '{requirements_all[module]}' in requirements.txt")
 
     return exit_code
 
