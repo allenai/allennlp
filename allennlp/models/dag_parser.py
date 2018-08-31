ce11bd18a7652a5b8db72f21387b65f4764a5bf6
Mark Neumann <markng@allenai.org>
2018-08-31 10:19:45 -0700

    add intitial work on dag parser

---
+++ allennlp/models/dag_parser.py
@@ -0,0 +1,344 @@
+from typing import Dict, Optional, Tuple, Any, List
+import logging
+import copy
+
+from overrides import overrides
+import torch
+from torch.nn.modules import Dropout
+import numpy
+
+from allennlp.common.checks import check_dimensions_match, ConfigurationError
+from allennlp.data import Vocabulary
+from allennlp.modules import Seq2SeqEncoder, TextFieldEmbedder, Embedding, InputVariationalDropout
+from allennlp.modules.matrix_attention.bilinear_matrix_attention import BilinearMatrixAttention
+from allennlp.modules import FeedForward
+from allennlp.models.model import Model
+from allennlp.nn import InitializerApplicator, RegularizerApplicator, Activation
+from allennlp.nn.util import get_text_field_mask
+from allennlp.nn.util import get_lengths_from_binary_sequence_mask
+from allennlp.training.metrics import AttachmentScores
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+@Model.register("dag_parser")
+class DagParser(Model):
+    """
+    A Parser for arbitrary DAG stuctures.
+
+    Parameters
+    ----------
+    vocab : ``Vocabulary``, required
+        A Vocabulary, required in order to compute sizes for input/output projections.
+    text_field_embedder : ``TextFieldEmbedder``, required
+        Used to embed the ``tokens`` ``TextField`` we get as input to the model.
+    encoder : ``Seq2SeqEncoder``
+        The encoder (with its own internal stacking) that we will use to generate representations
+        of tokens.
+    tag_representation_dim : ``int``, required.
+        The dimension of the MLPs used for dependency tag prediction.
+    arc_representation_dim : ``int``, required.
+        The dimension of the MLPs used for head arc prediction.
+    tag_feedforward : ``FeedForward``, optional, (default = None).
+        The feedforward network used to produce tag representations.
+        By default, a 1 layer feedforward network with an elu activation is used.
+    arc_feedforward : ``FeedForward``, optional, (default = None).
+        The feedforward network used to produce arc representations.
+        By default, a 1 layer feedforward network with an elu activation is used.
+    pos_tag_embedding : ``Embedding``, optional.
+        Used to embed the ``pos_tags`` ``SequenceLabelField`` we get as input to the model.
+    dropout : ``float``, optional, (default = 0.0)
+        The variational dropout applied to the output of the encoder and MLP layers.
+    input_dropout : ``float``, optional, (default = 0.0)
+        The dropout applied to the embedded text input.
+    initializer : ``InitializerApplicator``, optional (default=``InitializerApplicator()``)
+        Used to initialize the model parameters.
+    regularizer : ``RegularizerApplicator``, optional (default=``None``)
+        If provided, will be used to calculate the regularization penalty during training.
+    """
+    def __init__(self,
+                 vocab: Vocabulary,
+                 text_field_embedder: TextFieldEmbedder,
+                 encoder: Seq2SeqEncoder,
+                 tag_representation_dim: int,
+                 arc_representation_dim: int,
+                 tag_feedforward: FeedForward = None,
+                 arc_feedforward: FeedForward = None,
+                 pos_tag_embedding: Embedding = None,
+                 dropout: float = 0.0,
+                 input_dropout: float = 0.0,
+                 initializer: InitializerApplicator = InitializerApplicator(),
+                 regularizer: Optional[RegularizerApplicator] = None) -> None:
+        super(DagParser, self).__init__(vocab, regularizer)
+
+        self.text_field_embedder = text_field_embedder
+        self.encoder = encoder
+
+        encoder_dim = encoder.get_output_dim()
+
+        self.head_arc_feedforward = arc_feedforward or \
+                                        FeedForward(encoder_dim, 1,
+                                                    arc_representation_dim,
+                                                    Activation.by_name("elu")())
+        self.child_arc_feedforward = copy.deepcopy(self.head_arc_feedforward)
+
+        self.arc_attention = BilinearMatrixAttention(arc_representation_dim,
+                                                     arc_representation_dim,
+                                                     use_input_biases=True)
+
+        num_labels = self.vocab.get_vocab_size("head_tags")
+
+        self.head_tag_feedforward = tag_feedforward or \
+                                        FeedForward(encoder_dim, 1,
+                                                    tag_representation_dim,
+                                                    Activation.by_name("elu")())
+        self.child_tag_feedforward = copy.deepcopy(self.head_tag_feedforward)
+
+        self.tag_bilinear = BilinearMatrixAttention(tag_representation_dim,
+                                                    tag_representation_dim,
+                                                    num_labels)
+
+        self._pos_tag_embedding = pos_tag_embedding or None
+        self._dropout = InputVariationalDropout(dropout)
+        self._input_dropout = Dropout(input_dropout)
+        self._head_sentinel = torch.nn.Parameter(torch.randn([1, 1, encoder.get_output_dim()]))
+
+        representation_dim = text_field_embedder.get_output_dim()
+        if pos_tag_embedding is not None:
+            representation_dim += pos_tag_embedding.get_output_dim()
+
+        check_dimensions_match(representation_dim, encoder.get_input_dim(),
+                               "text field embedding dim", "encoder input dim")
+        check_dimensions_match(tag_representation_dim, self.head_tag_feedforward.get_output_dim(),
+                               "tag representation dim", "tag feedforward output dim")
+        check_dimensions_match(arc_representation_dim, self.head_arc_feedforward.get_output_dim(),
+                               "arc representation dim", "arc feedforward output dim")
+
+        self._attachment_scores = AttachmentScores()
+        self._arc_loss = torch.nn.BCEWithLogitsLoss(reduce=False)
+        self._tag_loss = torch.nn.NLLLoss(reduce=False)
+        initializer(self)
+
+    @overrides
+    def forward(self,  # type: ignore
+                words: Dict[str, torch.LongTensor],
+                pos_tags: torch.LongTensor,
+                metadata: List[Dict[str, Any]],
+                head_tags: torch.LongTensor = None,
+                head_indices: torch.LongTensor = None) -> Dict[str, torch.Tensor]:
+        # pylint: disable=arguments-differ
+        """
+        Parameters
+        ----------
+        words : Dict[str, torch.LongTensor], required
+            The output of ``TextField.as_array()``.
+        pos_tags : ``torch.LongTensor``, required.
+            The output of a ``SequenceLabelField`` containing POS tags.
+            POS tags are required regardless of whether they are used in the model,
+            because they are used to filter the evaluation metric to only consider
+            heads of words which are not punctuation.
+        head_tags : torch.LongTensor, optional (default = None)
+            A torch tensor representing the sequence of integer gold class labels for the arcs
+            in the dependency parse. Has shape ``(batch_size, sequence_length)``.
+        head_indices : torch.LongTensor, optional (default = None)
+            A torch tensor representing the sequence of integer indices denoting the parent of every
+            word in the dependency parse. Has shape ``(batch_size, sequence_length)``.
+
+        Returns
+        -------
+        An output dictionary.
+        """
+        embedded_text_input = self.text_field_embedder(words)
+        if pos_tags is not None and self._pos_tag_embedding is not None:
+            embedded_pos_tags = self._pos_tag_embedding(pos_tags)
+            embedded_text_input = torch.cat([embedded_text_input, embedded_pos_tags], -1)
+        elif self._pos_tag_embedding is not None:
+            raise ConfigurationError("Model uses a POS embedding, but no POS tags were passed.")
+
+        mask = get_text_field_mask(words)
+        embedded_text_input = self._input_dropout(embedded_text_input)
+        encoded_text = self.encoder(embedded_text_input, mask)
+        batch_size, _, encoding_dim = encoded_text.size()
+
+        # TODO(Mark) See if we need this. Maybe we don't now we don't have a single head?
+        head_sentinel = self._head_sentinel.expand(batch_size, 1, encoding_dim)
+        # Concatenate the head sentinel onto the sentence representation.
+        encoded_text = torch.cat([head_sentinel, encoded_text], 1)
+        mask = torch.cat([mask.new_ones(batch_size, 1), mask], 1)
+        if head_indices is not None:
+            head_indices = torch.cat([head_indices.new_zeros(batch_size, 1), head_indices], 1)
+        if head_tags is not None:
+            head_tags = torch.cat([head_tags.new_zeros(batch_size, 1), head_tags], 1)
+        float_mask = mask.float()
+        encoded_text = self._dropout(encoded_text)
+
+        # shape (batch_size, sequence_length, arc_representation_dim)
+        head_arc_representation = self._dropout(self.head_arc_feedforward(encoded_text))
+        child_arc_representation = self._dropout(self.child_arc_feedforward(encoded_text))
+
+        # shape (batch_size, sequence_length, tag_representation_dim)
+        head_tag_representation = self._dropout(self.head_tag_feedforward(encoded_text))
+        child_tag_representation = self._dropout(self.child_tag_feedforward(encoded_text))
+        # shape (batch_size, sequence_length, sequence_length)
+        attended_arcs = self.arc_attention(head_arc_representation,
+                                           child_arc_representation)
+        head_tag_logits = self.tag_bilinear(head_tag_representation,
+                                            child_tag_representation)
+
+        minus_inf = -1e8
+        minus_mask = (1 - float_mask) * minus_inf
+        attended_arcs = attended_arcs + minus_mask.unsqueeze(2) + minus_mask.unsqueeze(1)
+
+        predicted_heads, predicted_head_tags = self._greedy_decode(attended_arcs,
+                                                                   head_tag_logits,
+                                                                   mask)
+        output_dict = {
+                "heads": predicted_heads,
+                "head_tags": predicted_head_tags,
+                "mask": mask,
+                "words": [meta["words"] for meta in metadata],
+                "pos": [meta["pos"] for meta in metadata]
+                }
+
+        if head_indices is not None and head_tags is not None:
+            arc_nll, tag_nll = self._construct_loss(attended_arcs=attended_arcs,
+                                                    head_tag_logits=head_tag_logits,
+                                                    head_indices=head_indices,
+                                                    head_tags=head_tags,
+                                                    mask=mask)
+            output_dict["loss"] = arc_nll + tag_nll
+            output_dict["arc_loss"] = arc_nll
+            output_dict["tag_loss"] = tag_nll
+
+            # We calculate attatchment scores for the whole sentence
+            # but excluding the symbolic ROOT token at the start,
+            # which is why we start from the second element in the sequence.
+
+            # TODO(Mark): Refactor to make it work with matrices, just reshape
+            self._attachment_scores(predicted_heads[:, 1:],
+                                    predicted_head_tags[:, 1:],
+                                    head_indices[:, 1:],
+                                    head_tags[:, 1:],
+                                    mask[:, 1:])
+        return output_dict
+
+    @overrides
+    def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+
+        head_tags = output_dict.pop("head_tags").cpu().detach().numpy()
+        heads = output_dict.pop("heads").cpu().detach().numpy()
+        mask = output_dict.pop("mask")
+        lengths = get_lengths_from_binary_sequence_mask(mask)
+        head_tag_labels = []
+        head_indices = []
+        for instance_heads, instance_tags, length in zip(heads, head_tags, lengths):
+            instance_heads = list(instance_heads[1:length])
+            instance_tags = instance_tags[1:length]
+            labels = [self.vocab.get_token_from_index(label, "head_tags")
+                      for label in instance_tags]
+            head_tag_labels.append(labels)
+            head_indices.append(instance_heads)
+
+        output_dict["predicted_dependencies"] = head_tag_labels
+        output_dict["predicted_heads"] = head_indices
+        return output_dict
+
+    def _construct_loss(self,
+                        attended_arcs: torch.Tensor,
+                        head_tag_logits: torch.Tensor,
+                        head_indices: torch.Tensor,
+                        head_tags: torch.Tensor,
+                        mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Computes the arc and tag loss for a sequence given gold head indices and tags.
+
+        Parameters
+        ----------
+        attended_arcs : ``torch.Tensor``, required.
+            A tensor of shape (batch_size, sequence_length, sequence_length) used to generate
+            a distribution over attachements of a given word to all other words.
+        head_indices : ``torch.Tensor``, required.
+            A tensor of shape (batch_size, sequence_length).
+            The indices of the heads for every word.
+        head_tags : ``torch.Tensor``, required.
+            A tensor of shape (batch_size, sequence_length).
+            The dependency labels of the heads for every word.
+        mask : ``torch.Tensor``, required.
+            A mask of shape (batch_size, sequence_length), denoting unpadded
+            elements in the sequence.
+
+        Returns
+        -------
+        arc_nll : ``torch.Tensor``, required.
+            The negative log likelihood from the arc loss.
+        tag_nll : ``torch.Tensor``, required.
+            The negative log likelihood from the arc tag loss.
+        """
+        arc_nll = self._arc_loss(attended_arcs, head_indices) * mask.unsqueeze(1) * mask.unsqeeze(2)
+        # We want the mask for the tags to only include the unmasked words
+        # and we only care about the loss with respect to the gold arcs.
+        tag_mask = mask.unsqeeze(1) * mask.unsqeeze(2) * head_indices
+        tag_nll = self._tag_loss(head_tag_logits, head_tags) * tag_mask
+
+        # We don't care about predictions for the symbolic ROOT token's head,
+        # so we remove it from the loss.
+        arc_nll = arc_nll[:, 1:]
+        tag_nll = tag_nll[:, 1:]
+
+        # The number of valid positions is equal to the number of unmasked elements minus
+        # 1 per sequence in the batch, to account for the symbolic HEAD token.
+        valid_positions = mask.sum() - mask.size(0)
+
+        arc_nll = arc_nll.sum() / valid_positions.float()
+        tag_nll = tag_nll.sum() / valid_positions.float()
+        return arc_nll, tag_nll
+
+    @staticmethod
+    def _greedy_decode(attended_arcs: torch.Tensor,
+                       head_tag_logits: torch.Tensor,
+                       mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Decodes the head and head tag predictions by decoding the unlabeled arcs
+        independently for each word and then again, predicting the head tags of
+        these greedily chosen arcs indpendently. Note that this method of decoding
+        is not guaranteed to produce trees (i.e. there maybe be multiple roots,
+        or cycles when children are attached to their parents).
+
+        Parameters
+        ----------
+        attended_arcs : ``torch.Tensor``, required.
+            A tensor of shape (batch_size, sequence_length, sequence_length) used to generate
+            a distribution over attachements of a given word to all other words.
+        head_tag_logits : ``torch.Tensor``, required.
+            A tensor of shape (batch_size, sequence_length, sequence_length, num_tags) used to
+            generate a distribution over tags for each arc.
+
+        Returns
+        -------
+        heads : ``torch.Tensor``
+            A tensor of shape (batch_size, sequence_length) representing the
+            greedily decoded heads of each word.
+        head_tags : ``torch.Tensor``
+            A tensor of shape (batch_size, sequence_length) representing the
+            dependency tags of the greedily decoded heads of each word.
+        """
+        # Mask the diagonal, because the head of a word can't be itself.
+        inf_diagonal_mask = torch.diag(attended_arcs.new(mask.size(1)).fill_(-numpy.inf))
+        attended_arcs = attended_arcs + inf_diagonal_mask
+        # shape (batch_size, sequence_length, sequence_length, num_tags)
+        head_tag_logits = head_tag_logits + inf_diagonal_mask.unsqueeze(-1).unsqueeze(-1)
+        # Mask padded tokens, because we only want to consider actual words as heads.
+        if mask is not None:
+            minus_mask = (1 - mask).byte().unsqueeze(2)
+            attended_arcs.masked_fill_(minus_mask, -numpy.inf)
+        # TODO(Mark) Think about masking the head tags here too, because
+        # Compute the heads greedily.
+        # shape (batch_size, sequence_length, sequence_length)
+        arc_probs = attended_arcs.sigmoid()
+        # shape (batch_size, sequence_length, sequence_length, num_tags)
+        head_tag_probs = torch.nn.functional.softmax(head_tag_logits)
+        return arc_probs, head_tag_probs
+
+    @overrides
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return self._attachment_scores.get_metric(reset)
