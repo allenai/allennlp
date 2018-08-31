bc7a02cfefe059c07360f2664b10352ddf36f9dd
rajasagashe <rajas@cs.washington.edu>
2018-08-31 08:34:15 -0700

    dataset reader test

--- allennlp/data/dataset_readers/__init__.py
+++ allennlp/data/dataset_readers/__init__.py
@@ -26,4 +26,5 @@ from allennlp.data.dataset_readers.universal_dependencies import UniversalDepend
 from allennlp.data.dataset_readers.stanford_sentiment_tree_bank import (
         StanfordSentimentTreeBankDatasetReader)
 from allennlp.data.dataset_readers.wikitables import WikiTablesDatasetReader
+from allennlp.data.dataset_readers.java import JavaDatasetReader
 from allennlp.data.dataset_readers.quora_paraphrase import QuoraParaphraseDatasetReader
