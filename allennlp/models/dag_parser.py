e5133b638a263218bde2361dab0264b5fab472f9
Mark Neumann <markng@allenai.org>
2018-09-04 12:12:37 -0700

    more cleanup, refactor decode method

--- allennlp/models/dag_parser.py
+++ allennlp/models/dag_parser.py
@@ -114,7 +114,7 @@ class DagParser(Model):
 
         self._attachment_scores = AttachmentScores()
         self._arc_loss = torch.nn.BCEWithLogitsLoss(reduce=False)
-        self._tag_loss = torch.nn.NLLLoss(reduce=False)
+        self._tag_loss = torch.nn.CrossEntropyLoss(reduce=False)
         initializer(self)
 
     @overrides
@@ -134,12 +134,9 @@ class DagParser(Model):
             POS tags are required regardless of whether they are used in the model,
             because they are used to filter the evaluation metric to only consider
             heads of words which are not punctuation.
-        head_tags : torch.LongTensor, optional (default = None)
-            A torch tensor representing the sequence of integer gold class labels for the arcs
-            in the dependency parse. Has shape ``(batch_size, sequence_length)``.
-        head_indices : torch.LongTensor, optional (default = None)
+        arc_labels : torch.LongTensor, optional (default = None)
             A torch tensor representing the sequence of integer indices denoting the parent of every
-            word in the dependency parse. Has shape ``(batch_size, sequence_length)``.
+            word in the dependency parse. Has shape ``(batch_size, sequence_length, sequence_length)``.
 
         Returns
         -------
@@ -169,27 +166,29 @@ class DagParser(Model):
         # shape (batch_size, sequence_length, sequence_length)
         attended_arcs = self.arc_attention(head_arc_representation,
                                            child_arc_representation)
-        head_tag_logits = self.tag_bilinear(head_tag_representation,
-                                            child_tag_representation)
-        head_tag_logits = head_tag_logits.permute(0, 2, 3, 1).contiguous()
+        # shape (batch_size, num_tags, sequence_length, sequence_length)
+        arc_tag_logits = self.tag_bilinear(head_tag_representation,
+                                           child_tag_representation)
+        # Switch to (batch_size, sequence_length, sequence_length, num_tags)
+        arc_tag_logits = arc_tag_logits.permute(0, 2, 3, 1).contiguous()
 
         minus_inf = -1e8
         minus_mask = (1 - float_mask) * minus_inf
         attended_arcs = attended_arcs + minus_mask.unsqueeze(2) + minus_mask.unsqueeze(1)
 
-        predicted_heads, predicted_head_tags = self._greedy_decode(attended_arcs,
-                                                                   head_tag_logits,
-                                                                   mask)
+        arc_probs, arc_tag_probs = self._greedy_decode(attended_arcs,
+                                                       arc_tag_logits,
+                                                       mask)
         output_dict = {
-                "heads": predicted_heads,
-                "head_tags": predicted_head_tags,
+                "arc_probs": arc_probs,
+                "arc_tag_probs": arc_tag_probs,
                 "mask": mask,
                 }
 
         if arc_labels is not None:
             arc_nll, tag_nll = self._construct_loss(attended_arcs=attended_arcs,
-                                                    head_tag_logits=head_tag_logits,
-                                                    head_tags=arc_labels,
+                                                    arc_tag_logits=arc_tag_logits,
+                                                    arc_tags=arc_labels,
                                                     mask=mask)
             output_dict["loss"] = arc_nll + tag_nll
             output_dict["arc_loss"] = arc_nll
@@ -200,38 +199,47 @@ class DagParser(Model):
     @overrides
     def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
 
-        head_tags = output_dict.pop("head_tags").cpu().detach().numpy()
-        heads = output_dict.pop("heads").cpu().detach().numpy()
+        arc_tag_probs = output_dict.pop("arc_tag_probs").cpu().detach().numpy()
+        arc_probs = output_dict.pop("arc_probs").cpu().detach().numpy()
         mask = output_dict.pop("mask")
         lengths = get_lengths_from_binary_sequence_mask(mask)
-        head_tag_labels = []
-        head_indices = []
-        for instance_heads, instance_tags, length in zip(heads, head_tags, lengths):
-            instance_heads = list(instance_heads[1:length])
-            instance_tags = instance_tags[1:length]
-            labels = [self.vocab.get_token_from_index(label, "head_tags")
-                      for label in instance_tags]
-            head_tag_labels.append(labels)
-            head_indices.append(instance_heads)
+        arcs = []
+        arc_tags = []
+        for instance_arc_probs, instance_arc_tag_probs, length in zip(arc_probs, arc_tag_probs, lengths):
 
-        output_dict["predicted_dependencies"] = head_tag_labels
-        output_dict["predicted_heads"] = head_indices
+            arc_matrix = instance_arc_probs > 0.5
+            edges = []
+            edge_tags = []
+            for i in range(length):
+                for j in range(length):
+                    if arc_matrix[i, j] == 1:
+                        edges.append((i, j))
+                        tag = instance_arc_tag_probs[i, j].argmax(-1)
+                        edge_tags.append(self.vocab.get_token_from_index(tag, "labels"))
+            arcs.append(edges)
+            arc_tags.append(edge_tags)
+
+        output_dict["arcs"] = arcs
+        output_dict["arc_tags"] = arc_tags
         return output_dict
 
     def _construct_loss(self,
                         attended_arcs: torch.Tensor,
-                        head_tag_logits: torch.Tensor,
-                        head_tags: torch.Tensor,
+                        arc_tag_logits: torch.Tensor,
+                        arc_tags: torch.Tensor,
                         mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """
-        Computes the arc and tag loss for a sequence given gold head indices and tags.
+        Computes the arc and tag loss for an adjacency matrix.
 
         Parameters
         ----------
         attended_arcs : ``torch.Tensor``, required.
-            A tensor of shape (batch_size, sequence_length, sequence_length) used to generate
-            a distribution over attachements of a given word to all other words.
-        head_tags : ``torch.Tensor``, required.
+            A tensor of shape (batch_size, sequence_length, sequence_length) used to generate a
+            binary classification decision for whether an edge is present between two words.
+        arc_tag_logits : ``torch.Tensor``, required.
+            A tensor of shape (batch_size, sequence_length, sequence_length, num_tags) used to generate
+            a distribution over edge tags for a given edge.
+        arc_tags : ``torch.Tensor``, required.
             A tensor of shape (batch_size, sequence_length, sequence_length).
             The dependency labels of the heads for every word.
         mask : ``torch.Tensor``, required.
@@ -246,18 +254,18 @@ class DagParser(Model):
             The negative log likelihood from the arc tag loss.
         """
         float_mask = mask.float()
-        head_indices = (head_tags != -1).float()
+        head_indices = (arc_tags != -1).float()
         # Make the head tags not have negative values anywhere.
-        head_tags = head_tags * head_indices
+        arc_tags = arc_tags * head_indices
         arc_nll = self._arc_loss(attended_arcs, head_indices) * float_mask.unsqueeze(1) * float_mask.unsqueeze(2)
         # We want the mask for the tags to only include the unmasked words
         # and we only care about the loss with respect to the gold arcs.
         tag_mask = float_mask.unsqueeze(1) * float_mask.unsqueeze(2) * head_indices
 
-        batch_size, sequence_length, _, num_tags = head_tag_logits.size()
+        batch_size, sequence_length, _, num_tags = arc_tag_logits.size()
         original_shape = [batch_size, sequence_length, sequence_length]
-        reshaped_logits = head_tag_logits.view(-1, num_tags)
-        reshaped_tags = head_tags.view(-1)
+        reshaped_logits = arc_tag_logits.view(-1, num_tags)
+        reshaped_tags = arc_tags.view(-1)
         tag_nll = self._tag_loss(reshaped_logits, reshaped_tags.long()).view(original_shape) * tag_mask
 
         # The number of valid positions is equal to the number of unmasked elements minus
@@ -270,7 +278,7 @@ class DagParser(Model):
 
     @staticmethod
     def _greedy_decode(attended_arcs: torch.Tensor,
-                       head_tag_logits: torch.Tensor,
+                       arc_tag_logits: torch.Tensor,
                        mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Decodes the head and head tag predictions by decoding the unlabeled arcs
@@ -284,34 +292,34 @@ class DagParser(Model):
         attended_arcs : ``torch.Tensor``, required.
             A tensor of shape (batch_size, sequence_length, sequence_length) used to generate
             a distribution over attachements of a given word to all other words.
-        head_tag_logits : ``torch.Tensor``, required.
+        arc_tag_logits : ``torch.Tensor``, required.
             A tensor of shape (batch_size, sequence_length, sequence_length, num_tags) used to
             generate a distribution over tags for each arc.
 
         Returns
         -------
-        heads : ``torch.Tensor``
-            A tensor of shape (batch_size, sequence_length) representing the
-            greedily decoded heads of each word.
-        head_tags : ``torch.Tensor``
-            A tensor of shape (batch_size, sequence_length) representing the
-            dependency tags of the greedily decoded heads of each word.
+        arc_probs : ``torch.Tensor``
+            A tensor of shape (batch_size, sequence_length, sequence_length) representing the
+            probability of an arc being present for this edge.
+        arc_tag_probs : ``torch.Tensor``
+            A tensor of shape (batch_size, sequence_length, sequence_length, sequence_length)
+            representing the distribution over edge tags for a given edge.
         """
         # Mask the diagonal, because the head of a word can't be itself.
         inf_diagonal_mask = torch.diag(attended_arcs.new(mask.size(1)).fill_(-numpy.inf))
         attended_arcs = attended_arcs + inf_diagonal_mask
         # shape (batch_size, sequence_length, sequence_length, num_tags)
-        head_tag_logits = head_tag_logits + inf_diagonal_mask.unsqueeze(0).unsqueeze(-1)
+        arc_tag_logits = arc_tag_logits + inf_diagonal_mask.unsqueeze(0).unsqueeze(-1)
         # Mask padded tokens, because we only want to consider actual words as heads.
         if mask is not None:
             minus_mask = (1 - mask).byte().unsqueeze(2)
             attended_arcs.masked_fill_(minus_mask, -numpy.inf)
-            head_tag_logits.masked_fill_(minus_mask.unsqueeze(-1), -numpy.inf)
+            arc_tag_logits.masked_fill_(minus_mask.unsqueeze(-1), -numpy.inf)
         # shape (batch_size, sequence_length, sequence_length)
         arc_probs = attended_arcs.sigmoid()
         # shape (batch_size, sequence_length, sequence_length, num_tags)
-        head_tag_probs = torch.nn.functional.softmax(head_tag_logits)
-        return arc_probs, head_tag_probs
+        arc_tag_probs = torch.nn.functional.softmax(arc_tag_logits)
+        return arc_probs, arc_tag_probs
 
     @overrides
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
