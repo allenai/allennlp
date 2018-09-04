40ea81c0686ec5b0a45d53a01357bf12928de0d8
Kevin Lin <kevinl@kevinl.local>
2018-09-04 15:11:58 -0700

    add entities to atis world

--- allennlp/semparse/worlds/atis_world.py
+++ allennlp/semparse/worlds/atis_world.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
 from typing import List, Dict, Tuple
 import numpy
+from pprint import pprint
+import sqlite3
 
 from parsimonious.grammar import Grammar
 
@@ -9,6 +11,24 @@ from allennlp.semparse.contexts.sql_table_context import \
         SqlTableContext, SqlVisitor, generate_one_of_string, format_action
 
 from allennlp.data.tokenizers import Token, Tokenizer, WordTokenizer
+
+def get_strings_from_utterance(tokenized_utterance: List[Token]) -> Dict[str, List[int]]:
+    """
+    Based on the current utterance, return a dictionary where the keys are the strings in the utterance
+    that map to lists of the token indices that they are linked to.
+    """
+    string_linking_scores: Dict[str, List[int]] = defaultdict(list)
+    for index, (first_token, second_token) in enumerate(zip(tokenized_utterance, tokenized_utterance[1:])):
+        for string in ATIS_TRIGGER_DICT.get(first_token.text.lower(), []):
+            string_linking_scores[string].append(index)
+        bigram = f"{first_token.text} {second_token.text}".lower()
+        for string in ATIS_TRIGGER_DICT.get(bigram, []):
+            string_linking_scores[string].extend([index, index + 1])
+    if tokenized_utterance[-1].text.lower() in ATIS_TRIGGER_DICT:
+        for string in ATIS_TRIGGER_DICT[tokenized_utterance[-1].text.lower()]:
+            string_linking_scores[string].append(len(tokenized_utterance)-1)
+    return string_linking_scores
+
 
 class AtisWorld():
     """
@@ -31,20 +51,35 @@ class AtisWorld():
                  utterances: List[str],
                  tokenizer: Tokenizer = None,
                  database_directory: str = None) -> None:
+
+        self.all_tables = ALL_TABLES
+        self.tables_with_strings = TABLES_WITH_STRINGS 
+        if database_directory:
+            self.database_directory = database_directory
+            # database_directory = "/Users/kevinl/Documents/semant_parse/allennlp/atis/atis.db"
+            self.connection = sqlite3.connect(database_directory)
+            self.cursor = self.connection.cursor()
+
+
         self.sql_table_context = SqlTableContext(ALL_TABLES,
                                                  TABLES_WITH_STRINGS,
-                                                 database_directory) if database_directory else None
+                                                 database_directory,
+                                                 utterances=utterances) if database_directory else None
 
         self.utterances: List[str] = utterances
         self.tokenizer = tokenizer if tokenizer else WordTokenizer()
         self.tokenized_utterances = [self.tokenizer.tokenize(utterance) for utterance in self.utterances]
         valid_actions, linking_scores = self.init_all_valid_actions()
         self.valid_actions: Dict[str, List[str]] = valid_actions
+        self.entities = self.get_entities()
+
 
         # This has shape (num_entities, num_utterance_tokens).
         self.linking_scores: numpy.ndarray = linking_scores
         self.grammar_str: str = self.get_grammar_str()
         self.grammar_with_context: Grammar = Grammar(self.grammar_str)
+        all_possible_actions = self.all_possible_actions()
+        
 
     def get_valid_actions(self) -> Dict[str, List[str]]:
         return self.valid_actions
@@ -54,7 +89,6 @@ class AtisWorld():
         We initialize the valid actions with the global actions. We then iterate through the
         utterances up to and including the current utterance and add the valid strings.
         """
-
         valid_actions = deepcopy(self.sql_table_context.valid_actions)
         linking_scores = []
 
@@ -83,6 +117,31 @@ class AtisWorld():
         for number in list(numbers_list):
             action = format_action('number', number)
             valid_actions['number'].append(action)
+        
+        # add string linking dict
+        for tokenized_utterance in self.tokenized_utterances:
+            string_linking_dict = get_strings_from_utterance(tokenized_utterance)
+            # strings.update(string_linking_dict.keys())
+        # strings_list: List[str] = sorted(strings, reverse=True)
+        strings_list = []
+
+        if self.tables_with_strings:
+            for table, columns in self.tables_with_strings.items():
+                for column in columns:
+                    self.cursor.execute(f'SELECT DISTINCT {table} . {column} FROM {table}')
+                    strings_list.extend([(format_action(f"{table}_{column}_string", row[0]), row[0])
+                                                for row in self.cursor.fetchall()])
+        strings_list = sorted(strings_list, key=lambda string_tuple: string_tuple[0]) 
+        # We construct the linking scores for strings from the ``string_linking_dict`` here.
+        string_linking_scores = []
+        for string in strings_list:
+            entity_linking = [0 for token in current_tokenized_utterance]
+            # string_linking_dict has the strings and linking scores from the last utterance.
+            # If the string is not in the last utterance, then the linking scores will be all 0.
+            for token_index in string_linking_dict.get(string[1], []):
+                entity_linking[token_index] = 1
+            string_linking_scores.append(entity_linking)
+        linking_scores.extend(string_linking_scores)
 
         return valid_actions, numpy.array(linking_scores)
 
@@ -98,14 +157,15 @@ class AtisWorld():
         for tokenized_utterance in self.tokenized_utterances:
             dates.extend(get_date_from_utterance(tokenized_utterance))
         if dates:
+            grammar_str_lines[-1] += f' / ("date_day" ws "." ws "year" ws binaryop ws "{dates[0].year}")'
             for date in dates:
-                grammar_str_lines[-1] += f' / ("date_day" ws "." ws "year" ws binaryop ws "{date.year}")' \
-        f' / ("date_day" ws "." ws "month_number" ws binaryop ws "{date.month}")' \
-        f' / ("date_day" ws "." ws "day_number" ws binaryop ws "{date.day}")'
-                self.valid_actions['biexpr'].extend([f'biexpr -> ["date_day", ".", "year", binaryop, "{date.year}"]',
-                                                    f'biexpr -> ["date_day", ".", "month_number", binaryop, "{date.month}"]',
-                                                    f'biexpr -> ["date_day", ".", "day_number", binaryop, "{date.day}"]'])
-
+                grammar_str_lines[-1] += f' / ("date_day" ws "." ws "month_number" ws binaryop ws "{date.month}")' \
+                                         f' / ("date_day" ws "." ws "day_number" ws binaryop ws "{date.day}")'
+                for biexpr_rule in [f'biexpr -> ["date_day", ".", "year", binaryop, "{date.year}"]',
+                                    f'biexpr -> ["date_day", ".", "month_number", binaryop, "{date.month}"]',
+                                    f'biexpr -> ["date_day", ".", "day_number", binaryop, "{date.day}"]']:
+                    if biexpr_rule not in self.valid_actions:
+                        self.valid_actions['biexpr'].append(biexpr_rule)
         
         grammar_str_with_context = "\n".join(grammar_str_lines)
 
@@ -135,6 +195,16 @@ class AtisWorld():
                 all_actions.add(action)
         return sorted(all_actions)
 
+    def get_entities(self) -> List[str]:
+        numbers = self.valid_actions['number']
+        strings = []
+        for _, action_list in self.valid_actions.items():
+            for action in action_list:
+                lhs, _ = action.split(' ->')
+                if lhs.endswith('string'):
+                    strings.append(action)
+        return sorted(numbers, reverse=True) + sorted(strings)        
+
     def __eq__(self, other):
         if isinstance(self, other.__class__):
             return all([self.valid_actions == other.valid_actions,
@@ -142,3 +212,6 @@ class AtisWorld():
                         self.utterances == other.utterances,
                         self.grammar_str == other.grammar_str])
         return False
+
+
+
