7bf930f48ff8a72611db61301268b058e5cbaf48
Gabriel Stanovsky <gabriel.satanovsky@gmail.com>
2018-09-07 17:53:51 -0700

    Add Open Information Extraction (#1726)
    * adding Open IE
    
    * adding Open IE
    
    * adding Open IE
    
    * fixing typo
    
    * Using SRL model instead of a duplicated OIE model, adding an `ignore_span_metric` parameter to the SRL model to accomodate for Open IE
    
    * adding conversion script from open ie extractions to conll format
    
    * minor fixes
    
    * style fixes
    
    * fixing naming convention
    
    * fixing naming convention
    
    * returning empty dictionary when in get_metrics when ignoring span loss
    
    * minor
    
    * minor
    
    * adding Open IE predictor to docs
    
    * fixing comments for docs

---
+++ allennlp/tests/predictors/open_information_extraction_test.py
@@ -0,0 +1,51 @@
+# pylint: disable=no-self-use,invalid-name
+from allennlp.common.testing import AllenNlpTestCase
+from allennlp.models.archival import load_archive
+from allennlp.predictors import Predictor
+
+class TestOpenIePredictor(AllenNlpTestCase):
+    def test_uses_named_inputs(self):
+        """
+        Tests whether the model outputs conform to the expected format.
+        """
+        inputs = {
+                "sentence": "Angela Merkel met and spoke to her EU counterparts during the climate summit."
+        }
+
+        archive = load_archive(self.FIXTURES_ROOT / \
+                               'open_information_extraction' / 'serialization' / 'model.tar.gz')
+        predictor = Predictor.from_archive(archive, 'open-information-extraction')
+
+        result = predictor.predict_json(inputs)
+
+        words = result.get("words")
+        assert words == ["Angela", "Merkel", "met", "and", "spoke", "to", "her", "EU", "counterparts",
+                         "during", "the", "climate", "summit", "."]
+        num_words = len(words)
+
+        verbs = result.get("verbs")
+        assert verbs is not None
+        assert isinstance(verbs, list)
+
+        predicates = [verb["verb"] for verb in verbs]
+        assert predicates == ["met", "spoke"]
+
+        for verb in verbs:
+            tags = verb.get("tags")
+            assert tags is not None
+            assert isinstance(tags, list)
+            assert all(isinstance(tag, str) for tag in tags)
+            assert len(tags) == num_words
+
+
+    def test_prediction_with_no_verbs(self):
+        """
+        Tests whether the model copes with sentences without verbs.
+        """
+        input1 = {"sentence": "Blah no verb sentence."}
+        archive = load_archive(self.FIXTURES_ROOT / \
+                               'open_information_extraction' / 'serialization' / 'model.tar.gz')
+        predictor = Predictor.from_archive(archive, 'open-information-extraction')
+
+        result = predictor.predict_json(input1)
+        assert result == {'words': ['Blah', 'no', 'verb', 'sentence', '.'], 'verbs': []}
