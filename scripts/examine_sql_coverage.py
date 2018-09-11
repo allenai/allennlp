4a3896ff706546541ed8f6deb7ccc2daa3ac4e5a
Mark Neumann <markng@allenai.org>
2018-09-11 12:58:14 -0700

    tidy up

--- scripts/examine_sql_coverage.py
+++ scripts/examine_sql_coverage.py
@@ -1,7 +1,7 @@
 import json
 import os
 import sys
-from typing import Dict, Any, Iterable, Tuple
+from typing import Any, Iterable, Tuple
 import argparse
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
@@ -23,97 +23,104 @@ from parsimonious.grammar import Grammar
 
 # still TODO: 
 # GROUP BY, ORDER BY
-# sort out difference between variable and string matching
+# think about the  difference between variable and string matching
 # tablename in (tablename1, tablename2)
-# 
 # JOIN, seems hard.
 
 # TODO:
 # string set isn't an expr, it should only be in in_expr
 # not all functions can take * as an argument.    
-# orderby_clause   = ORDER BY order_clause
-#order_clause     = ordering_term (ws "," ordering_term)*
-#    ordering_term    = ws expr (ASC / DESC)?
-#    limit            = LIMIT ws number
 
 SQL_GRAMMAR2 = Grammar(
-    r"""
-    stmt             = (query ws ";") / (query ws)
-    query            = (ws select_core groupby_clause orderby_clause ws limit) / (ws select_core groupby_clause ws limit) / (ws select_core orderby_clause ws limit) / (ws select_core groupby_clause) / (ws select_core orderby_clause) / (ws select_core)
-    select_core      = (select_with_distinct select_results from_clause where_clause) / (select_with_distinct select_results from_clause) / (select_with_distinct select_results where_clause) / (select_with_distinct select_results)
-    select_with_distinct = (SELECT DISTINCT) / SELECT
-    select_results   = (ws select_result ws "," ws select_results) / (ws select_result)
-    select_result    = sel_res_all_star / sel_res_tab_star / sel_res_val / sel_res_col
+        r"""
+        stmt             = (query ws ";") / (query ws)
+        query            = (ws select_core groupby_clause orderby_clause ws limit) /
+                           (ws select_core groupby_clause ws limit) /
+                           (ws select_core orderby_clause ws limit) /
+                           (ws select_core groupby_clause) /
+                           (ws select_core orderby_clause) /
+                           (ws select_core)
 
-    sel_res_tab_star = name ".*"
-    sel_res_all_star = "*"
-    sel_res_val      = (expr AS wsp name) / expr
-    sel_res_col      = col_ref (AS wsp name)
+        select_core      = (select_with_distinct select_results from_clause where_clause) /
+                           (select_with_distinct select_results from_clause) /
+                           (select_with_distinct select_results where_clause) /
+                           (select_with_distinct select_results)
 
-    from_clause      = FROM source
-    source           = (ws single_source ws "," ws source) / (ws single_source)
-    single_source    = source_table / source_subq
-    source_table     = table_name (AS wsp name)
-    source_subq      = ("(" ws query ws ")" AS ws name) / ("(" ws query ws ")")
-    where_clause     = (WHERE wsp expr where_conj) / (WHERE wsp expr)
-    where_conj       = (AND wsp expr where_conj) / (AND wsp expr) 
-    
-    groupby_clause   = (GROUP BY group_clause having_clause) / (GROUP BY group_clause)
-    group_clause     = (ws expr ws "," group_clause) / (ws expr)
-    having_clause    = HAVING ws expr
+        select_with_distinct = (SELECT DISTINCT) / SELECT
+        select_results   = (ws select_result ws "," ws select_results) / (ws select_result)
+        select_result    = sel_res_all_star / sel_res_tab_star / sel_res_val / sel_res_col
 
-    orderby_clause   = ORDER BY order_clause
-    order_clause     = (ordering_term ws "," order_clause) / ordering_term
-    ordering_term    = (ws expr ordering) / (ws expr)
-    ordering         = ASC / DESC
-    limit            = LIMIT ws number
+        sel_res_tab_star = name ".*"
+        sel_res_all_star = "*"
+        sel_res_val      = (expr AS wsp name) / expr
+        sel_res_col      = col_ref (AS wsp name)
 
-    col_ref          = (table_name "." column_name) / column_name
-    table_name       = name
-    column_name      = name
-    ws               = ~"\s*"i
-    wsp              = ~"\s+"i
-    name             = ~"[a-zA-Z]\w*"i
+        from_clause      = FROM source
+        source           = (ws single_source ws "," ws source) / (ws single_source)
+        single_source    = source_table / source_subq
+        source_table     = table_name (AS wsp name)
+        source_subq      = ("(" ws query ws ")" AS ws name) / ("(" ws query ws ")")
+        where_clause     = (WHERE wsp expr where_conj) / (WHERE wsp expr)
+        where_conj       = (AND wsp expr where_conj) / (AND wsp expr)
 
-    expr             = in_expr / like_expr / between_expr / binary_expr / unary_expr / source_subq / value / string_set
-    like_expr        = value wsp LIKE ws string
-    in_expr          = (value wsp NOT IN wsp expr) / (value wsp IN wsp expr)
-    between_expr     = value BETWEEN wsp value AND wsp value
-    binary_expr      = value ws binaryop ws expr
-    unary_expr       = unaryop expr
-    value            = parenval / number / boolean / function / col_ref / string
-    parenval         = "(" ws expr ws ")"
-    function         = (fname ws "(" ws DISTINCT ws arg_list_or_star ws ")") / (fname ws "(" ws arg_list_or_star ws ")")
-    arg_list_or_star = arg_list / "*"
-    arg_list         = (expr ws "," ws arg_list) / expr
-    number           = ~"\d*\.?\d+"i
-    string_set       = ws "(" ws string_set_vals ws ")"
-    string_set_vals  = (string ws "," ws string_set_vals) / string
-    string           = ~"\'.*?\'"i
-    fname            = "COUNT" / "SUM" / "MAX" / "MIN" / "AVG"
-    boolean          = "true" / "false"
-    binaryop         = "+" / "-" / "*" / "/" / "=" / "<>" / ">=" / "<=" / ">" / "<" / ">" / "and" / "AND" / "or" / "OR"
-    binaryop_no_andor = "+" / "-" / "*" / "/" / "=" / "<>" / "<=" / ">" / "<" / ">"
-    unaryop          = "+" / "-" / "not" / "NOT"
+        groupby_clause   = (GROUP BY group_clause having_clause) / (GROUP BY group_clause)
+        group_clause     = (ws expr ws "," group_clause) / (ws expr)
+        having_clause    = HAVING ws expr
 
-    SELECT   = ws "SELECT"
-    FROM     = ws "FROM"
-    WHERE    = ws "WHERE"
-    AS       = ws "AS"
-    AND      = ws "AND"
-    DISTINCT = ws "DISTINCT"
-    GROUP    = ws "GROUP"
-    ORDER    = ws "ORDER"
-    BY       = ws "BY"
-    ASC      = ws "ASC"
-    DESC     = ws "DESC"
-    BETWEEN  = ws "BETWEEN"
-    IN       = ws "IN"
-    NOT      = ws "NOT"
-    HAVING   = ws "HAVING"
-    LIMIT    = ws "LIMIT"
-    LIKE     = ws "LIKE"
-    """
+        orderby_clause   = ORDER BY order_clause
+        order_clause     = (ordering_term ws "," order_clause) / ordering_term
+        ordering_term    = (ws expr ordering) / (ws expr)
+        ordering         = ASC / DESC
+        limit            = LIMIT ws number
+
+        col_ref          = (table_name "." column_name) / column_name
+        table_name       = name
+        column_name      = name
+        ws               = ~"\s*"i
+        wsp              = ~"\s+"i
+        name             = ~"[a-zA-Z]\w*"i
+
+        expr             = in_expr / like_expr / between_expr / binary_expr / unary_expr / source_subq / value / string_set
+        like_expr        = value wsp LIKE ws string
+        in_expr          = (value wsp NOT IN wsp expr) / (value wsp IN wsp expr)
+        between_expr     = value BETWEEN wsp value AND wsp value
+        binary_expr      = value ws binaryop ws expr
+        unary_expr       = unaryop expr
+        value            = parenval / number / boolean / function / col_ref / string
+        parenval         = "(" ws expr ws ")"
+        function         = (fname ws "(" ws DISTINCT ws arg_list_or_star ws ")") /
+                           (fname ws "(" ws arg_list_or_star ws ")")
+
+        arg_list_or_star = arg_list / "*"
+        arg_list         = (expr ws "," ws arg_list) / expr
+        number           = ~"\d*\.?\d+"i
+        string_set       = ws "(" ws string_set_vals ws ")"
+        string_set_vals  = (string ws "," ws string_set_vals) / string
+        string           = ~"\'.*?\'"i
+        fname            = "COUNT" / "SUM" / "MAX" / "MIN" / "AVG"
+        boolean          = "true" / "false"
+        binaryop         = "+" / "-" / "*" / "/" / "=" / "<>" / ">=" / "<=" / ">" / "<" / ">" / "and" / "AND" / "or" / "OR"
+        binaryop_no_andor = "+" / "-" / "*" / "/" / "=" / "<>" / "<=" / ">" / "<" / ">"
+        unaryop          = "+" / "-" / "not" / "NOT"
+
+        SELECT   = ws "SELECT"
+        FROM     = ws "FROM"
+        WHERE    = ws "WHERE"
+        AS       = ws "AS"
+        AND      = ws "AND"
+        DISTINCT = ws "DISTINCT"
+        GROUP    = ws "GROUP"
+        ORDER    = ws "ORDER"
+        BY       = ws "BY"
+        ASC      = ws "ASC"
+        DESC     = ws "DESC"
+        BETWEEN  = ws "BETWEEN"
+        IN       = ws "IN"
+        NOT      = ws "NOT"
+        HAVING   = ws "HAVING"
+        LIMIT    = ws "LIMIT"
+        LIKE     = ws "LIKE"
+        """
 )
 
 def parse_dataset(filename: str, filter_by: str = None, verbose: bool = False):
@@ -188,7 +195,7 @@ def main(data_directory: int, dataset: str = None, filter_by: str = None, verbos
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="process text2sql data into a more readable format.")
+    parser = argparse.ArgumentParser(description="Check the coverage of a SQL Grammar on the text2sql datasets.")
     parser.add_argument('--data', type=str, help='The path to the text2sql data directory.')
     parser.add_argument('--dataset', type=str, default=None,
                         help='The dataset to check coverage for. Defaults to all datasets.')
