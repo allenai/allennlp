5990b0cee0fc87d1c5f4dfa1b5f52a3c59bd57ad
Gabi Stanovsky <gabis@julianm.local>
2018-09-11 15:57:49 -0700

    merging overlapping predicates

--- allennlp/predictors/open_information_extraction.py
+++ allennlp/predictors/open_information_extraction.py
@@ -10,6 +10,8 @@ from allennlp.service.predictors.predictor import Predictor
 from allennlp.data.tokenizers.word_splitter import SpacyWordSplitter
 from allennlp.data.tokenizers import Token
 
+import pdb
+
 def join_mwp(tags: List[str]) -> List[str]:
     """
     Join multi-word predicates to a single
@@ -59,6 +61,42 @@ def make_oie_string(tokens: List[Token], tags: List[str]) -> str:
         frame.append("[" + " ".join(chunk) + "]")
 
     return " ".join(frame)
+
+def get_predicate_indices(tags: List[str]) -> List[int]:
+    """
+    Return the word indices of a predicate in BIO tags.
+    """
+    return [ind for (ind, tag)
+            in enumerate(tags) if 'V' in tag]
+
+def get_predicate_text(sent_tokens: List[Token], tags: List[str]) -> str:
+    """
+    Get the predicate in this prediction.
+    """
+    return " ".join([sent_tokens[pred_id].text
+                     for pred_id in get_predicate_indices(tags)])
+
+def check_predicates_subsumed(tags1: List[str], tags2: List[str]) -> bool:
+    """
+    Tests whether the predicate in BIO tags1 are subsumed in
+    those of tags2.
+    """
+    # Get predicate word indices from both predictions
+    pred_ind1, pred_ind2 = map(get_predicate_indices,
+                               [tags1, tags2])
+
+    # Return if pred_ind1 is contained in pred_ind2
+    return (set(pred_ind1) < set(pred_ind2))
+
+def merge_predictions(tags1: List[str], tags2: List[str]) -> List[str]:
+    """
+    Merge two predictions into one. Assumes the predicate in tags1 are contained in
+    the predicate of tags2.
+    """
+    # Allow tags1 to add elements to tags2
+    return [tag2 if (tag2 != 'O')\
+            else tag1
+            for (tag1, tag2) in zip(tags1, tags2)]
 
 @Predictor.register('open-information-extraction')
 class OpenIePredictor(Predictor):
@@ -122,7 +160,29 @@ class OpenIePredictor(Predictor):
         # Build and return output dictionary
         results = {"verbs": [], "words": sent_tokens}
 
-        for tags, pred_id in zip(outputs, pred_ids):
+        # Merge predicates
+        pred_dict = {}
+        merged_outputs = list(map(join_mwp, outputs))
+        for tags1 in merged_outputs:
+            # A flag indicating whether to add tags1 to predictions
+            add_to_prediction = True
+            pred1_text = get_predicate_text(sent_tokens, tags1)
+            if pred1_text in pred_dict:
+                # We already added this predicate
+                continue
+
+            # Else - see if this predicate was subsumed by another predicate
+            for tags2 in outputs:
+                if (tags1 != tags2) and check_predicates_subsumed(tags1, tags2):
+                    # tags1 is contained in tags2
+                    pred2_text = get_predicate_text(sent_tokens, tags2)
+                    pred_dict[pred2_text] = merge_predictions(tags1, tags2)
+                    add_to_prediction = False
+
+            if add_to_prediction:
+                pred_dict[pred1_text] = tags1
+
+        for pred_text, tags in pred_dict.items():
             # Join multi-word predicates
             tags = join_mwp(tags)
 
@@ -131,9 +191,9 @@ class OpenIePredictor(Predictor):
 
             # Add a predicate prediction to the return dictionary.
             results["verbs"].append({
-                    "verb": sent_tokens[pred_id].text,
-                    "description": description,
-                    "tags": tags,
+                "verb": pred_text,
+                "description": description,
+                "tags": tags,
             })
 
         return sanitize(results)
