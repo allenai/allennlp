3285cea0dad3671cb19e02f3932a726aa60566f9
Mark Neumann <markng@allenai.org>
2018-09-14 10:01:20 -0700

    Revert "undo specific predictor types, add esim model"
    This reverts commit 6ca8921eb319d7e3b0d314a6aa4c81e6925051e2.

--- allennlp/pretrained.py
+++ allennlp/pretrained.py
@@ -1,5 +1,6 @@
 # pylint: disable=line-too-long,invalid-name
 
+from allennlp import predictors
 from allennlp.predictors import Predictor
 from allennlp.models.archival import load_archive
 
@@ -19,59 +20,54 @@ class PretrainedModel:
 
 
 #### Models in the demo ####
-def srl_with_elmo_luheng_2018() -> Predictor:
+def srl_with_elmo_luheng_2018() -> predictors.SemanticRoleLabelerPredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/srl-model-2018.05.25.tar.gz',
                             'semantic-role-labeling')
     return model.predictor()
 
-def bidirectional_attention_flow_seo_2017() -> Predictor:
+def bidirectional_attention_flow_seo_2017() -> predictors.BidafPredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/bidaf-model-2017.09.15-charpad.tar.gz',
                             'machine-comprehension')
     return model.predictor()
 
-def open_information_extraction_stanovsky_2018() -> Predictor:
+def open_information_extraction_stanovsky_2018() -> predictors.OpenIePredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/openie-model.2018-08-20.tar.gz',
                             'open-information-extraction')
     return model.predictor()
 
-def decomposable_attention_with_elmo_parikh_2017() -> Predictor:
+def decomposable_attention_with_elmo_parikh_2017() -> predictors.DecomposableAttentionPredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/decomposable-attention-elmo-2018.02.19.tar.gz',
                             'textual-entailment')
     return model.predictor()
 
-def neural_coreference_resolution_lee_2017() -> Predictor:
+def neural_coreference_resolution_lee_2017() -> predictors.CorefPredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/coref-model-2018.02.05.tar.gz',
                             'coreference-resolution')
     return model.predictor()
 
-def named_entity_recognition_with_elmo_peters_2018() -> Predictor:
+def named_entity_recognition_with_elmo_peters_2018() -> predictors.SentenceTaggerPredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/ner-model-2018.04.30.tar.gz',
                             'sentence-tagger')
     return model.predictor()
 
-def fine_grained_named_entity_recognition_with_elmo_peters_2018() -> Predictor:
+def fine_grained_named_entity_recognition_with_elmo_peters_2018() -> predictors.SentenceTaggerPredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/fine-grained-ner-model-elmo-2018.08.31.tar.gz',
                             'sentence-tagger')
     return model.predictor()
 
-def span_based_constituency_parsing_with_elmo_joshi_2018() -> Predictor:
+def span_based_constituency_parsing_with_elmo_joshi_2018() -> predictors.ConstituencyParserPredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/elmo-constituency-parser-2018.03.14.tar.gz',
                             'constituency-parser')
     return model.predictor()
 
-def biaffine_parser_stanford_dependencies_todzat_2017() -> Predictor:
+def biaffine_parser_stanford_dependencies_todzat_2017() -> predictors.BiaffineDependencyParserPredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/biaffine-dependency-parser-ptb-2018.08.23.tar.gz',
                             'biaffine-dependency-parser')
     return model.predictor()
 
 
 #### Models not in the demo ####
-def biaffine_parser_universal_dependencies_todzat_2017() -> Predictor:
+def biaffine_parser_universal_dependencies_todzat_2017() -> predictors.BiaffineDependencyParserPredictor:
     model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/biaffine-dependency-parser-ud-2018.08.23.tar.gz',
                             'biaffine-dependency-parser')
-    return model.predictor()
-
-def esim_nli_with_elmo_chen_2017() -> Predictor:
-    model = PretrainedModel('https://s3-us-west-2.amazonaws.com/allennlp/models/esim-elmo-2018.05.17.tar.gz',
-                            'textual-entailment')
     return model.predictor()
