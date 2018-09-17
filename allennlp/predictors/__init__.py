f536a781a246e8a635c9f25d49bbcd5a90cadca7
Brendan Roof <brendanr@allenai.org>
2018-09-17 13:51:30 -0700

    Add a predictor for Event2Mind.

--- allennlp/predictors/__init__.py
+++ allennlp/predictors/__init__.py
@@ -7,15 +7,16 @@ want to serve up a model through the web service
 a ``Predictor`` that wraps it.
 """
 from allennlp.predictors.predictor import Predictor
-from allennlp.predictors.bidaf import BidafPredictor
-from allennlp.predictors.dialog_qa import DialogQAPredictor
-from allennlp.predictors.constituency_parser import ConstituencyParserPredictor
 from allennlp.predictors.biaffine_dependency_parser import BiaffineDependencyParserPredictor
+from allennlp.predictors.bidaf import BidafPredictor
+from allennlp.predictors.constituency_parser import ConstituencyParserPredictor
 from allennlp.predictors.coref import CorefPredictor
 from allennlp.predictors.decomposable_attention import DecomposableAttentionPredictor
+from allennlp.predictors.dialog_qa import DialogQAPredictor
+from allennlp.predictors.event2mind import Event2MindPredictor
+from allennlp.predictors.nlvr_parser import NlvrParserPredictor
+from allennlp.predictors.open_information_extraction import OpenIePredictor
 from allennlp.predictors.semantic_role_labeler import SemanticRoleLabelerPredictor
 from allennlp.predictors.sentence_tagger import SentenceTaggerPredictor
 from allennlp.predictors.simple_seq2seq import SimpleSeq2SeqPredictor
 from allennlp.predictors.wikitables_parser import WikiTablesParserPredictor
-from allennlp.predictors.nlvr_parser import NlvrParserPredictor
-from allennlp.predictors.open_information_extraction import OpenIePredictor
--- allennlp/tests/predictors/event2mind_test.py
+++ allennlp/tests/predictors/event2mind_test.py
@@ -4,19 +4,26 @@ from allennlp.models.archival import load_archive
 from allennlp.predictors import Predictor
 
 
-class TestSimpleSeq2SeqPredictor(AllenNlpTestCase):
+class TestEvent2MindPredictor(AllenNlpTestCase):
     def test_uses_named_inputs(self):
         inputs = {
-                "source": "What kind of test succeeded on its first attempt?",
+                "source": "personx gave persony a present",
         }
 
-        archive = load_archive(self.FIXTURES_ROOT / 'encoder_decoder' / 'simple_seq2seq' /
+        archive = load_archive(self.FIXTURES_ROOT / 'event2mind' /
                                'serialization' / 'model.tar.gz')
-        predictor = Predictor.from_archive(archive, 'simple_seq2seq')
+        predictor = Predictor.from_archive(archive, 'event2mind')
 
         result = predictor.predict_json(inputs)
 
-        predicted_tokens = result.get("predicted_tokens")
-        assert predicted_tokens is not None
-        assert isinstance(predicted_tokens, list)
-        assert all(isinstance(x, str) for x in predicted_tokens)
+        token_names = [
+                'xintent_top_k_predicted_tokens',
+                'xreact_top_k_predicted_tokens',
+                'oreact_top_k_predicted_tokens'
+        ]
+
+        for token_name in token_names:
+            all_predicted_tokens = result.get(token_name)
+            for predicted_tokens in all_predicted_tokens:
+                assert isinstance(predicted_tokens, list)
+                assert all(isinstance(x, str) for x in predicted_tokens)
