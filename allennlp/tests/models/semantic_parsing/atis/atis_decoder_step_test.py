7ac5bb188e3407905c894d94a0d604093e245c47
Kevin Lin <kevinl@kevinl.local>
2018-09-18 12:59:01 -0700

    add a flag for reversing productions in grammar statelet

--- allennlp/tests/models/semantic_parsing/atis/atis_decoder_step_test.py
+++ allennlp/tests/models/semantic_parsing/atis/atis_decoder_step_test.py
@@ -5,19 +5,39 @@ import torch
 from allennlp.common import Params
 from allennlp.common.testing import AllenNlpTestCase
 from allennlp.modules import SimilarityFunction
-from allennlp.state_machines.states import AtisGrammarBasedState, GrammarStatelet, RnnStatelet
+from allennlp.state_machines.states import AtisGrammarBasedState, GrammarStatelet, RnnStatelet, is_nonterminal
+from allennlp.semparse.worlds import AtisWorld
 
 class AtisDecoderStepTest(AllenNlpTestCase):
     def setUp(self):
+        self.database_directory = self.FIXTURES_ROOT / "data" / "atis" / "atis.db"
         super().setUp()
 
     def test_atis_grammar_state(self):
         valid_actions = None
+        world = AtisWorld([("give me all flights from boston to "
+                            "philadelphia next week arriving after lunch")],
+                          database_directory=str(self.database_directory))
 
-        action_seq = ['stmt -> [query, ";"]', 'query -> [lparen, "SELECT", distinct, select_results, "FROM", table_refs, where_clause, rparen]', 'rparen -> [")"]', 'where_clause -> ["WHERE", lparen, conditions, rparen]', 'rparen -> [")"]', 'conditions -> [condition]', 'condition -> [biexpr]', 'biexpr -> [col_ref, binaryop, value]', 'value -> [pos_value]', 'pos_value -> [string]', 'string -> ["\'BOSTON\'"]', 'binaryop -> ["="]', 'col_ref -> ["city", ".", "city_name"]', 'lparen -> ["("]', 'table_refs -> [table_name]', 'table_name -> ["city"]', 'select_results -> [col_refs]', 'col_refs -> [col_ref, ",", col_refs]', 'col_refs -> [col_ref]', 'col_ref -> ["city", ".", "city_name"]', 'col_ref -> ["city", ".", "city_code"]', 'distinct -> ["DISTINCT"]', 'lparen -> ["("]']        
+        action_seq = ['statement -> [query, ";"]',
+           'query -> ["(", "SELECT", distinct, select_results, "FROM", table_refs, '
+           'where_clause, ")"]',
+           'where_clause -> ["WHERE", "(", conditions, ")"]',
+           'conditions -> [condition]',
+           'condition -> [biexpr]',
+           'biexpr -> ["city", ".", "city_name", binaryop, city_city_name_string]',
+           'city_city_name_string -> ["\'BOSTON\'"]',
+           'binaryop -> ["="]',
+           'table_refs -> [table_name]',
+           'table_name -> ["city"]',
+           'select_results -> [col_refs]',
+           'col_refs -> [col_ref, ",", col_refs]',
+           'col_refs -> [col_ref]',
+           'col_ref -> ["city", ".", "city_name"]',
+           'col_ref -> ["city", ".", "city_code"]',
+           'distinct -> ["DISTINCT"]']
 
-        grammar_state = AtisGrammarBasedState(['stmt'], {}, valid_actions, {}, is_nonterminal)
+        grammar_state = GrammarStatelet(['statement'], {}, world.valid_actions, {}, is_nonterminal, reverse_productions=False)
         for action in action_seq:
             grammar_state = grammar_state.take_action(action)
-            print('Stack', grammar_state._nonterminal_stack)
         assert grammar_state._nonterminal_stack == []
