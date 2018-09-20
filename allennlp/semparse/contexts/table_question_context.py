b04e19fd470a0728539a23df708108ccf568246a
Shikhar Murty <shikhar.murty@gmail.com>
2018-09-20 01:08:56 -0400

    add first set of comments

--- allennlp/semparse/contexts/table_question_context.py
+++ allennlp/semparse/contexts/table_question_context.py
@@ -1,4 +1,8 @@
-import re, itertools,nltk, json
+import re
+import itertools
+import nltk
+import json
+import csv
 from collections import defaultdict
 from typing import Any, DefaultDict, Dict, List, Tuple, Union, Set
 
@@ -34,51 +38,50 @@ class ContextGenerator:
     def __init__(self, 
                  cell_values : Set[str], 
                  column_type_statistics : List[Dict[str,int]],
-                 column_idx_to_name : Dict[int,str]) -> None:
+                 column_index_to_name : Dict[int,str]) -> None:
         self.cell_values = cell_values
-        self.column_types = { column_idx_to_name[column_idx] : max(column_type_statistics[column_idx]) 
-                              for column_idx in column_idx_to_name }
+        self.column_types = { column_index_to_name[column_index] : max(column_type_statistics[column_index]) for column_index in column_index_to_name }
             
        
 
     @classmethod
     def parse_file(cls, filename: str, question: List[Token], max_tokens_for_num_cell : int) -> 'TableQuestionKnowledgeGraph':
-        f = open(filename, 'r')
-        reader = csv.reader(f,  delimiter='\t', quoting=csv.QUOTE_NONE)
-        # obtain column information
-        lines = [line for line in reader]
-        column_idx_to_name = {}
+        with open(filename, 'r') as file_pointer:
+        	reader = csv.reader(file_pointer,  delimiter='\t', quoting=csv.QUOTE_NONE)
+        	# obtain column information
+        	lines = [line for line in reader]
+        	column_index_to_name = {}
 
-        header = lines[0] # the first line is the header
-        idx = 1
-        while lines[idx] == '-1':
-        	# column names start with fb:row.row. 
-        	curr_line = lines[idx]
-        	column_name_sempre = curr_line[2]
-        	column_idx = curr_line[1]
-        	column_name = column_name_sempre[11:]
-        	column_idx_to_name[column_idx] = column_name
-            idx += 1
+        	header = lines[0] # the first line is the header
+        	index = 1
+        	while lines[index][0] == '-1':
+        		# column names start with fb:row.row. 
+        		curr_line = lines[index]
+        		column_name_sempre = curr_line[2]
+        		column_index = curr_line[1]
+        		column_name = column_name_sempre.replace('fb:row.row.', '')
+        		column_index_to_name[column_index] = column_name
+            	index += 1
 
         
-        column_node_type_info = [{'string' : 0, 'number' : 0, 'date' : 0} 
-                                 for col in column_idx_to_name]
-        cell_values = set()
-        while idx < len(lines):
-        	curr_line = lines[idx]
-            column_idx = curr_line[1]
-            node_info = zip(header, curr_line)
-            cell_values.add(cls._normalize_string(node_info['content']))  
-            if node_info['date']:
-            	column_node_type_info[column_idx]['date'] += 1 
-            # If cell contains too many tokens, then likely not number
-            elif node_info['number'] and num_tokens < max_tokens_for_num_cell:
-            	column_node_type_info[column_idx]['number'] += 1 
-            else:
-            	column_node_type_info[column_idx]['string'] += 1
+        	column_node_type_info = [{'string' : 0, 'number' : 0, 'date' : 0} 
+                                 for col in column_index_to_name]
+        	cell_values = set()
+        	while index < len(lines):
+        		curr_line = lines[index]
+            	column_index = curr_line[1]
+            	node_info = dict(zip(header, curr_line))
+                cell_values.add(cls._normalize_string(node_info['content']))  
+            	if node_info['date']:
+            		column_node_type_info[column_index]['date'] += 1 
+            	# If cell contains too many tokens, then likely not number
+            	elif node_info['number'] and num_tokens < max_tokens_for_num_cell:
+            		column_node_type_info[column_index]['number'] += 1 
+            	else:
+            		column_node_type_info[column_index]['string'] += 1
 
 
-        return cls(cell_values, column_node_type_info, column_idx_to_name)
+        	return cls(cell_values, column_node_type_info, column_index_to_name)
 
 
     def get_entities_from_question(self, question : List[str]):
@@ -125,6 +128,6 @@ class ContextGenerator:
                 else: 
                     break
 
-            new_ents.append({'token_start' : curr_st, 'token_end' : curr_en 'value' : curr_tok})
+            new_ents.append({'token_start' : curr_st, 'token_end' : curr_en, 'value' : curr_tok})
 
         return new_ents
