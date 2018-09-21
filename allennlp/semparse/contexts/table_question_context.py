21b706003a9706af46dd9044cc33e44a6c2669bd
Shikhar Murty <shikhar.murty@gmail.com>
2018-09-21 04:15:59 -0400

    add numbers, change interface to input tokenized question

--- allennlp/semparse/contexts/table_question_context.py
+++ allennlp/semparse/contexts/table_question_context.py
@@ -1,9 +1,9 @@
 import re
 import csv
-from typing import Dict, List, Set
+from typing import Dict, List, Set, Tuple
 
 from unidecode import unidecode
-
+from allennlp.data.tokenizers import Token
 
 # == stop words that will be omitted by ContextGenerator
 STOP_WORDS = ["", "", "all", "being", "-", "over", "through", "yourselves", "its", "before",\
@@ -20,6 +20,58 @@ STOP_WORDS = ["", "", "all", "being", "-", "over", "through", "yourselves", "its
              "no", "that", "when", "same", "how", "other", "which", "you", "many", "shan", "'t", "'s", "our",
   "after","most", "'d", "such", "'m", "why", "a", "off", "i", "yours", "so", "the", "having", "once"]
 
+DEFAULT_NUMBERS = ['-1', '0', '1']
+NUMBER_CHARACTERS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-'}
+MONTH_NUMBERS = {
+        'january': 1,
+        'jan': 1,
+        'february': 2,
+        'feb': 2,
+        'march': 3,
+        'mar': 3,
+        'april': 4,
+        'apr': 4,
+        'may': 5,
+        'june': 6,
+        'jun': 6,
+        'july': 7,
+        'jul': 7,
+        'august': 8,
+        'aug': 8,
+        'september': 9,
+        'sep': 9,
+        'october': 10,
+        'oct': 10,
+        'november': 11,
+        'nov': 11,
+        'december': 12,
+        'dec': 12,
+        }
+ORDER_OF_MAGNITUDE_WORDS = {'hundred': 100, 'thousand': 1000, 'million': 1000000}
+NUMBER_WORDS = {
+        'zero': 0,
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+        'ten': 10,
+        'first': 1,
+        'second': 2,
+        'third': 3,
+        'fourth': 4,
+        'fifth': 5,
+        'sixth': 6,
+        'seventh': 7,
+        'eighth': 8,
+        'ninth': 9,
+        'tenth': 10,
+        **MONTH_NUMBERS,
+        }
 
 
 class TableQuestionContext:
@@ -80,23 +132,96 @@ class TableQuestionContext:
 
 
 
-    def get_entities_from_question(self, question: List[str]):
+    def get_entities_from_question(self, question: List[Token]):
         entity_data = []
         for i, token in enumerate(question):
-            if token in STOP_WORDS:
+            token_text = token.text
+            if token_text in STOP_WORDS:
                 continue
-            normalized_token = self._normalize_string(token)
-            if not normalized_token:
+            normalized_token_text = self._normalize_string(token_text)
+            if not normalized_token_text:
                 continue
-            if self._string_in_table(normalized_token):
-                curr_data = {'value' : normalized_token, 'token_start' : i, 'token_end' : i+1}
+            if self._string_in_table(normalized_token_text):
+                curr_data = {'value' : normalized_token_text, 'token_start' : i, 'token_end' : i+1}
                 entity_data.append(curr_data)
 
+        extracted_numbers = self._get_numbers_from_tokens(question)
+        
+        # filter out number entities to avoid repitition
+        if len(extracted_numbers) > 0:
+            _ , number_token_text  = list(zip(*extracted_numbers))
+            number_token_text = list(number_token_text)
+            expanded_string_entities = [] 
+            for ent in entity_data:
+                if ent['value'] not in number_token_text:
+                    expanded_string_entities.append(ent)
+            
+        expanded_entities = [ ent['value'] for ent in self._expand_entities(question, entity_data) ]
+        return expanded_entities, extracted_numbers  #TODO(shikhar) Handle conjunctions
 
-        expanded_entities = self._expand_entities(question, entity_data)
-        return expanded_entities #TODO(shikhar) Handle conjunctions
 
+    @staticmethod
+    def _get_numbers_from_tokens(tokens: List[Token]) -> List[Tuple[str, str]]:
+        """
+        Finds numbers in the input tokens and returns them as strings.  We do some simple heuristic
+        number recognition, finding ordinals and cardinals expressed as text ("one", "first",
+        etc.), as well as numerals ("7th", "3rd"), months (mapping "july" to 7), and units
+        ("1ghz").
 
+        We also handle year ranges expressed as decade or centuries ("1800s" or "1950s"), adding
+        the endpoints of the range as possible numbers to generate.
+
+        We return a list of tuples, where each tuple is the (number_string, token_text) for a
+        number found in the input tokens.
+        """
+        numbers = []
+        for i, token in enumerate(tokens):
+            number: Union[int, float] = None
+            token_text = token.text
+            text = token.text.replace(',', '').lower()
+            if text in NUMBER_WORDS:
+                number = NUMBER_WORDS[text]
+
+            magnitude = 1
+            if i < len(tokens) - 1:
+                next_token = tokens[i + 1].text.lower()
+                if next_token in ORDER_OF_MAGNITUDE_WORDS:
+                    magnitude = ORDER_OF_MAGNITUDE_WORDS[next_token]
+                    token_text += ' ' + tokens[i + 1].text
+
+            is_range = False
+            if len(text) > 1 and text[-1] == 's' and text[-2] == '0':
+                is_range = True
+                text = text[:-1]
+
+            # We strip out any non-digit characters, to capture things like '7th', or '1ghz'.  The
+            # way we're doing this could lead to false positives for something like '1e2', but
+            # we'll take that risk.  It shouldn't be a big deal.
+            text = ''.join(text[i] for i, char in enumerate(text) if char in NUMBER_CHARACTERS)
+
+            try:
+                # We'll use a check for float(text) to find numbers, because text.isdigit() doesn't
+                # catch things like "-3" or "0.07".
+                number = float(text)
+            except ValueError:
+                pass
+
+            if number is not None:
+                number = number * magnitude
+                if '.' in text:
+                    number_string = '%.3f' % number
+                else:
+                    number_string = '%d' % number
+                numbers.append((number_string, token_text))
+                if is_range:
+                    # TODO(mattg): both numbers in the range will have the same text, and so the
+                    # linking score won't have any way to differentiate them...  We should figure
+                    # out a better way to handle this.
+                    num_zeros = 1
+                    while text[-(num_zeros + 1)] == '0':
+                        num_zeros += 1
+                    numbers.append((str(int(number + 10 ** num_zeros)), token_text))
+        return numbers
 
     def _string_in_table(self, candidate: str) -> bool:
         for cell_value in self.cell_values:
@@ -110,25 +235,28 @@ class TableQuestionContext:
 
     def _expand_entities(self, question, entity_data):
         new_ents = []
-        for ent in entity_data:
+        for entity in entity_data:
             # to ensure the same strings are not used over and over
-            if new_ents and ent['token_end'] <= new_ents[-1]['token_end']:
+            if new_ents and entity['token_end'] <= new_ents[-1]['token_end']:
                 continue
-            curr_st = ent['token_start']
-            curr_en = ent['token_end']
-            curr_tok = ent['value']
+            current_start = entity['token_start']
+            current_end = entity['token_end']
+            current_token = entity['value']
 
-            while curr_en < len(question):
-                next_tok = question[curr_en]
-                next_tok_normalized = self._normalize_string(next_tok)
-                candidate = "%s_%s" %(curr_tok, next_tok_normalized)
+            while current_end < len(question):
+                next_token = question[current_end].text
+                next_token_normalized = self._normalize_string(next_token)
+                if next_token_normalized == "":
+                    current_end += 1
+                    continue
+                candidate = "%s_%s" %(current_token, next_token_normalized)
                 if self._string_in_table(candidate):
-                    curr_en += 1
-                    curr_tok = candidate
+                    current_end += 1
+                    current_token = candidate
                 else:
                     break
 
-            new_ents.append({'token_start' : curr_st, 'token_end' : curr_en, 'value' : curr_tok})
+            new_ents.append({'token_start' : current_start, 'token_end' : current_end, 'value' : current_token})
 
         return new_ents
 
