962b224e576952460e32b5a712c1754e037f6ac5
WrRan <wrran@outlook.com>
2018-10-23 15:49:51 +0800

    avoid RuntimeError (Dimension out of range)
    consider all text_field_tensors with smallest_dim in method `get_text_field_mask`

--- allennlp/nn/util.py
+++ allennlp/nn/util.py
@@ -466,9 +466,17 @@ def get_text_field_mask(text_field_tensors: Dict[str, torch.Tensor],
     smallest_dim = tensor_dims[0][0] - num_wrapping_dims
     if smallest_dim == 2:
         token_tensor = tensor_dims[0][1]
+        for ix in range(1, len(tensor_dims)):
+            if smallest_dim != tensor_dims[ix][0] - num_wrapping_dims:
+                break
+            token_tensor += tensor_dims[ix][1]
         return (token_tensor != 0).long()
     elif smallest_dim == 3:
         character_tensor = tensor_dims[0][1]
+        for ix in range(1, len(tensor_dims)):
+            if smallest_dim != tensor_dims[ix][0] - num_wrapping_dims:
+                break
+            character_tensor += tensor_dims[ix][1]
         return ((character_tensor > 0).long().sum(dim=-1) > 0).long()
     else:
         raise ValueError("Expected a tensor with dimension 2 or 3, found {}".format(smallest_dim))
