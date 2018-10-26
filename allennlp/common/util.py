5aa1c8f0cc770493ba843f5cb7a4ac3221cddc3a
Matt Gardner <mattg@allenai.org>
2018-10-26 14:08:06 -0700

    Fix for import_submodules (#1976)
    * Fix for import_submodules
    
    * Use != instead of "not in"
    
    * Pylint

--- allennlp/common/util.py
+++ allennlp/common/util.py
@@ -287,9 +287,14 @@ def import_submodules(package_name: str) -> None:
     # Import at top level
     module = importlib.import_module(package_name)
     path = getattr(module, '__path__', [])
+    path_string = '' if not path else path[0]
 
     # walk_packages only finds immediate children, so need to recurse.
-    for _, name, _ in pkgutil.walk_packages(path):
+    for module_finder, name, _ in pkgutil.walk_packages(path):
+        # Sometimes when you import third-party libraries that are on your path,
+        # `pkgutil.walk_packages` returns those too, so we need to skip them.
+        if path_string and module_finder.path != path_string:
+            continue
         subpackage = f"{package_name}.{name}"
         import_submodules(subpackage)
 
