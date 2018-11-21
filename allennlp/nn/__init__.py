78afffc9e4a116f71338293028b761617b1ed9b7
Daniel Deutsch <danfdeutsch@gmail.com>
2018-11-20 23:31:55 -0500

    Removing PretrainedModelInitializer from imports

--- allennlp/nn/__init__.py
+++ allennlp/nn/__init__.py
@@ -1,3 +1,3 @@
 from allennlp.nn.activations import Activation
-from allennlp.nn.initializers import Initializer, InitializerApplicator, PretrainedModelInitializer
+from allennlp.nn.initializers import Initializer, InitializerApplicator
 from allennlp.nn.regularizers import RegularizerApplicator
