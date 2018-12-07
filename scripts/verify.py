d0aedf3fe110e7b8e4fc09eefe9cdd795aab77af
Kathryn Chan <kathrync@kathrync.local>
2018-12-07 15:00:10 -0800

    put test in verify.py instead of hook

--- scripts/verify.py
+++ scripts/verify.py
@@ -47,12 +47,17 @@ def main(checks):
             run("./scripts/check_requirements_and_setup.py", shell=True, check=True)
             print("check requirements passed")
 
+        if "check-files-sizes" in checks:
+            print("Checking all added files have size <= 2MB", flush=True)
+            run("./scripts/check_files_sizes.sh", shell=True, check=True)
+            print("check files' sizes passed")
+
     except CalledProcessError:
         # squelch the exception stacktrace
         sys.exit(1)
 
 if __name__ == "__main__":
-    checks = ['pytest', 'pylint', 'mypy', 'build-docs', 'check-docs', 'check-links', 'check-requirements']
+    checks = ['pytest', 'pylint', 'mypy', 'build-docs', 'check-docs', 'check-links', 'check-requirements', 'check-files-sizes']
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--checks', type=str, required=False, nargs='+', choices=checks)
