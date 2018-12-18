5ecf2f6216fc3fa431b8ec0384a63e33405f27e5
alontalmor <salont@gmail.com>
2018-12-18 14:37:49 +0200

    minor change to still support torch 0.4.0

--- allennlp/modules/seq2seq_encoders/pytorch_seq2seq_wrapper.py
+++ allennlp/modules/seq2seq_encoders/pytorch_seq2seq_wrapper.py
@@ -120,8 +120,12 @@ class PytorchSeq2SeqWrapper(Seq2SeqEncoder):
             self._update_states(final_states, restoration_indices)
 
         # Restore the original indices and return the sequence.
-        if torch.cuda.is_available():
-            with torch.cuda.device(restoration_indices.device.index):
-                return unpacked_sequence_tensor.index_select(0, restoration_indices.type(torch.cuda.LongTensor(device=restoration_indices.device)))
+        if torch.__version__ == '0.4.0':
+            if torch.cuda.is_available():
+                with torch.cuda.device(restoration_indices.device.index):
+                    return unpacked_sequence_tensor.index_select(0, \
+                            restoration_indices.type(torch.cuda.LongTensor(device=restoration_indices.device)))
+            else:
+                return unpacked_sequence_tensor.index_select(0, restoration_indices.type(torch.LongTensor))
         else:
-            return unpacked_sequence_tensor.index_select(0, restoration_indices.type(torch.LongTensor))
+            return unpacked_sequence_tensor.index_select(0, restoration_indices)
