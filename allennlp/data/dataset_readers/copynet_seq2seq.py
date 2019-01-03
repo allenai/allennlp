88ff913934d233a1dd360aded119cc3e6d99c95e
epwalsh <epwalsh10@gmail.com>
2019-01-03 10:00:46 -0800

    Improve dataset reader docstring

--- allennlp/data/dataset_readers/copynet_seq2seq.py
+++ allennlp/data/dataset_readers/copynet_seq2seq.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List, Dict, Tuple, Optional
+from typing import List, Dict
 
 import numpy as np
 from overrides import overrides
@@ -23,9 +23,38 @@ class CopyNetDatasetReader(DatasetReader):
     Read a tsv file containing paired sequences, and create a dataset suitable for a
     ``CopyNet`` model, or any model with a matching API.
 
-    Expected format for each input line: <source_sequence_string><tab><target_sequence_string>.
+    The expected format for each input line is: <source_sequence_string><tab><target_sequence_string>.
+    An instance produced by ``CopyNetDatasetReader`` will containing at least the following fields:
 
-    `START_SYMBOL` and `END_SYMBOL` tokens are added to the source and target sequences.
+    - ``source_tokens``: a ``TextField`` containing the tokenized source sentence,
+       including the ``START_SYMBOL`` and ``END_SYMBOL``.
+       This will result in a tensor of shape ``(batch_size, source_length)``.
+
+    - ``source_token_ids``: an ``ArrayField`` of size ``(batch_size, trimmed_source_length)``
+      that contains an ID for each token in the source sentence. Tokens that
+      match at the lowercase level will share the same ID. If ``target_tokens``
+      is passed as well, these IDs will also correspond to the ``target_token_ids``
+      field, i.e. any tokens that match at the lowercase level in both
+      the source and target sentences will share the same ID. Note that these IDs
+      have no correlation with the token indices from the corresponding
+      vocabulary namespaces.
+
+    - ``source_to_target``: a ``CopyMapField`` that keeps track of the index
+      of the target token that matches each token in the source sentence.
+      When there is no matching target token, the OOV index is used.
+      This will result in a tensor of shape ``(batch_size, trimmed_source_length)``.
+
+    - ``metadata``: a ``MetadataField`` which contains the source tokens and
+      potentially target tokens as lists of strings.
+
+    When ``target_string`` is passed, the instance will also contain these fields:
+
+    - ``target_tokens``: a ``TextField`` containing the tokenized target sentence,
+      including the ``START_SYMBOL`` and ``END_SYMBOL``. This will result in
+      a tensor of shape ``(batch_size, target_length)``.
+
+    - ``target_token_ids``: an ``ArrayField`` of size ``(batch_size, target_length)``.
+      This is calculated in the same way as ``source_token_ids``.
 
     Parameters
     ----------
@@ -44,6 +73,20 @@ class CopyNetDatasetReader(DatasetReader):
     target_token_indexers : ``Dict[str, TokenIndexer]``, optional
         Indexers used to define output (target side) token representations. Defaults to
         ``source_token_indexers``.
+
+    Notes
+    -----
+    By ``source_length`` we are referring to the number of tokens in the source
+    sentence including the ``START_SYMBOL`` and ``END_SYMBOL``, while
+    ``trimmed_source_length`` refers to the number of tokens in the source sentence
+    *excluding* the ``START_SYMBOL`` and ``END_SYMBOL``, i.e.
+    ``trimmed_source_length = source_length - 2``.
+
+    On the other hand, ``target_length`` is the number of tokens in the target sentence
+    *including* the ``START_SYMBOL`` and ``END_SYMBOL``.
+
+    In the context where there is a ``batch_size`` dimension, the above refer
+    to the maximum of their individual values across the batch.
     """
 
     def __init__(self,
@@ -60,23 +103,18 @@ class CopyNetDatasetReader(DatasetReader):
         self._source_token_indexers = source_token_indexers or {"tokens": SingleIdTokenIndexer()}
         self._target_token_indexers = target_token_indexers or self._source_token_indexers
 
-    @staticmethod
-    def _read_line(line_num: int, line: str) -> Tuple[Optional[str], Optional[str]]:
-        line = line.strip("\n")
-        if not line:
-            return None, None
-        line_parts = line.split('\t')
-        if len(line_parts) != 2:
-            raise ConfigurationError("Invalid line format: %s (line number %d)" % (line, line_num + 1))
-        source_sequence, target_sequence = line_parts
-        return source_sequence, target_sequence
-
     @overrides
     def _read(self, file_path):
         with open(cached_path(file_path), "r") as data_file:
             logger.info("Reading instances from lines in file at: %s", file_path)
             for line_num, line in enumerate(data_file):
-                source_sequence, target_sequence = self._read_line(line_num, line)
+                line = line.strip("\n")
+                if not line:
+                    continue
+                line_parts = line.split('\t')
+                if len(line_parts) != 2:
+                    raise ConfigurationError("Invalid line format: %s (line number %d)" % (line, line_num + 1))
+                source_sequence, target_sequence = line_parts
                 if not source_sequence:
                     continue
                 yield self.text_to_instance(source_sequence, target_sequence)
@@ -92,61 +130,17 @@ class CopyNetDatasetReader(DatasetReader):
     @overrides
     def text_to_instance(self, source_string: str, target_string: str = None) -> Instance:  # type: ignore
         """
-        Turn raw source string and target string into an `Instance`.
+        Turn raw source string and target string into an ``Instance``.
 
         Parameters
         ----------
         source_string : ``str``, required
-        target_string : ``str``, optional
+        target_string : ``str``, optional (default = None)
 
         Returns
         -------
         Instance
-            An Instance containing at least the following fields:
-
-            - `source_tokens`: a `TextField` containing the tokenized source sentence,
-               including the `START_SYMBOL` and `END_SYMBOL`.
-               This will result in a tensor of shape `(batch_size, source_length)`.
-
-            - `source_token_ids`: an `ArrayField` of size `(batch_size, trimmed_source_length)`
-              that contains an ID for each token in the source sentence. Tokens that
-              match at the lowercase level will share the same ID. If `target_tokens`
-              is passed as well, these IDs will also correspond to the `target_token_ids`
-              field, i.e. any tokens that match at the lowercase level in both
-              the source and target sentences will share the same ID. Note that these IDs
-              have no correlation with the token indices from the corresponding
-              vocabulary namespaces.
-
-            - `source_to_target`: a `CopyMapField` that keeps track of the index
-              of the target token that matches each token in the source sentence.
-              When there is no matching target token, the OOV index is used.
-              This will result in a tensor of shape `(batch_size, trimmed_source_length)`.
-
-            - `metadata`: a `MetadataField` which contains the source tokens and
-              potentially target tokens as lists of strings.
-
-            When `target_string` is passed, the instance will also contain these fields:
-
-            - `target_tokens`: a `TextField` containing the tokenized target sentence,
-              including the `START_SYMBOL` and `END_SYMBOL`. This will result in
-              a tensor of shape `(batch_size, target_length)`.
-
-            - `target_token_ids`: an `ArrayField` of size `(batch_size, target_length)`.
-              This is calculated in the same way as `source_token_ids`.
-
-        Notes
-        -----
-        By `source_length` we are referring to the number of tokens in the source
-        sentence including the `START_SYMBOL` and `END_SYMBOL`, while
-        `trimmed_source_length` refers to the number of tokens in the source sentence
-        *excluding* the `START_SYMBOL` and `END_SYMBOL`, i.e.
-        `trimmed_source_length = source_length - 2`.
-
-        On the other hand, `target_length` is the number of tokens in the target sentence
-        *including* the `START_SYMBOL` and `END_SYMBOL`.
-
-        In the context where there is a `batch_size` dimension, the above refer
-        to the maximum of their individual values across the batch.
+            See the above for a description of the fields that the instance will contain.
         """
         # pylint: disable=arguments-differ
         tokenized_source = self._source_tokenizer.tokenize(source_string)
