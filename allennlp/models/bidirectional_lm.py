1a6ff85b8a3f362cc75c2222fe0286d604840361
Nelson Liu <nfliu@cs.washington.edu>
2019-01-03 13:49:05 -0500

    Properly deprecate BidirectionalLM* classes

---
+++ allennlp/models/bidirectional_lm.py
@@ -0,0 +1,66 @@
+from typing import Union
+import warnings
+
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.models.shuffled_sentence_lm import ShuffledSentenceLanguageModel
+from allennlp.models.model import Model
+from allennlp.modules.text_field_embedders import TextFieldEmbedder
+from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder
+from allennlp.nn import InitializerApplicator
+
+
+@Model.register('bidirectional_language_model')
+class BidirectionalLanguageModel(ShuffledSentenceLanguageModel):
+    """
+    The ``BidirectionalLanguageModel`` applies a bidirectional "contextualizing"
+    ``Seq2SeqEncoder`` to uncontextualized embeddings, using a ``SoftmaxLoss``
+    module (defined above) to compute the language modeling loss.
+    It is IMPORTANT that your bidirectional ``Seq2SeqEncoder`` does not do any
+    "peeking ahead". That is, for its forward direction it should only consider
+    embeddings at previous timesteps, and for its backward direction only embeddings
+    at subsequent timesteps. If this condition is not met, your language model is
+    cheating.
+    Parameters
+    ----------
+    vocab: ``Vocabulary``
+    text_field_embedder: ``TextFieldEmbedder``
+        Used to embed the indexed tokens we get in ``forward``.
+    contextualizer: ``Seq2SeqEncoder``
+        Used to "contextualize" the embeddings. As described above,
+        this encoder must not cheat by peeking ahead.
+    dropout: ``float``, optional (default: None)
+        If specified, dropout is applied to the contextualized embeddings before computation of
+        the softmax. The contextualized embeddings themselves are returned without dropout.
+    loss_scale: ``Union[float, str]``, optional (default: 1.0)
+        This scaling factor is applied to the average language model loss.
+        You can also specify ``"n_samples"`` in which case we compute total
+        loss across all predictions.
+    num_samples: ``int``, optional (default: None)
+        If provided, the model will use ``SampledSoftmaxLoss``
+        with the specified number of samples. Otherwise, it will use
+        the full ``_SoftmaxLoss`` defined above.
+    sparse_embeddings: ``bool``, optional (default: False)
+        Passed on to ``SampledSoftmaxLoss`` if True.
+    """
+    def __init__(self,
+                 vocab: Vocabulary,
+                 text_field_embedder: TextFieldEmbedder,
+                 contextualizer: Seq2SeqEncoder,
+                 dropout: float = None,
+                 loss_scale: Union[float, str] = 1.0,
+                 num_samples: int = None,
+                 sparse_embeddings: bool = False,
+                 initializer: InitializerApplicator = None) -> None:
+        warnings.warn('BidirectionalLanguageModel is deprecated, '
+                      'please use the ShuffledSentenceLanguageModel (registered under '
+                      '"shuffled_sentence_language_model") with bidirectional=True, ',
+                      DeprecationWarning)
+        super().__init__(vocab=vocab,
+                         text_field_embedder=text_field_embedder,
+                         contextualizer=contextualizer,
+                         dropout=dropout,
+                         loss_scale=loss_scale,
+                         num_samples=num_samples,
+                         sparse_embeddings=sparse_embeddings,
+                         bidirectional=True,
+                         initializer=initializer)
---
+++ allennlp/modules/token_embedders/bidirectional_language_model_token_embedder.py
@@ -0,0 +1,64 @@
+import json
+from typing import Dict, Tuple, TYPE_CHECKING
+import warnings
+
+import torch
+
+from allennlp.common.checks import ConfigurationError
+from allennlp.data import TokenIndexer, Token
+from allennlp.modules.scalar_mix import ScalarMix
+from allennlp.modules.token_embedders.token_embedder import TokenEmbedder
+from allennlp.nn.util import remove_sentence_boundaries, get_text_field_mask, add_sentence_boundary_token_ids
+
+# Importing at runtime results in a circular dependency.
+if TYPE_CHECKING:
+    from allennlp.models.bidirectional_lm import BidirectionalLanguageModel
+
+
+@TokenEmbedder.register('bidirectional_lm_token_embedder')
+class BidirectionalLanguageModelTokenEmbedder(TokenEmbedder):
+    """
+    Compute a single layer of representations from a bidirectional language model. This is done
+    by computing a learned scalar average of the layers from the LM. Typically the LM's weights
+    will be fixed, but they can be fine tuned by setting ``requires_grad``.
+
+    Parameters
+    ----------
+    archive_file : ``str``, required
+        An archive file, typically model.tar.gz, from a BidirectionalLanguageModel. The
+        contextualizer used by the LM must satisfy two requirements:
+
+        1. It must have a num_layers field.
+        2. It must take a boolean return_all_layers parameter in its constructor.
+
+        See BidirectionalLanguageModelTransformer for their definitions.
+
+    dropout : ``float``, optional.
+        The dropout value to be applied to the representations.
+    bos_eos_tokens : ``Tuple[str, str]``, optional (default=``("<S>", "</S>")``)
+        These will be indexed and placed around the indexed tokens. Necessary if the language model
+        was trained with them, but they were injected external to an indexer.
+    remove_bos_eos: ``bool``, optional (default: True)
+        Typically the provided token indexes will be augmented with begin-sentence and end-sentence
+        tokens. (Alternatively, you can pass bos_eos_tokens.) If this flag is True the
+        corresponding embeddings will be removed from the return values.
+
+        Warning: This only removes a single start and single end token!
+    requires_grad : ``bool``, optional (default: False)
+        If True, compute gradient of bidirectional language model parameters for fine tuning.
+    """
+    def __init__(self,
+                 archive_file: str,
+                 dropout: float = None,
+                 bos_eos_tokens: Tuple[str, str] = ("<S>", "</S>"),
+                 remove_bos_eos: bool = True,
+                 requires_grad: bool = False) -> None:
+                warnings.warn('BidirectionalLanguageModelTokenEmbedder is deprecated, '
+                              'please use the ShuffledSentenceLanguageModelTokenEmbedder '
+                              '(registered under "shuffled_sentence_lm_token_embedder").',
+                      DeprecationWarning)
+                super().__init__(archive_file=archive_file,
+                                 dropout=dropout,
+                                 bos_eos_tokens=bos_eos_tokens,
+                                 remove_bos_eos=remove_bos_eos,
+                                 requires_grad=requires_grad)
