dbb9aa797353dafa358d95d387794c70faec9010
Nelson Liu <nfliu@cs.washington.edu>
2019-01-04 00:48:24 -0500

    Add test for bidirectional_language_model_token_embedder

---
+++ allennlp/tests/modules/token_embedders/bidirectional_language_model_token_embedder_test.py
@@ -0,0 +1,20 @@
+# pylint: disable=no-self-use,invalid-name
+from allennlp.common.testing import ModelTestCase
+from allennlp.data.dataset import Batch
+
+from .language_model_token_embedder_test import TestLanguageModelTokenEmbedder
+
+
+class TestBidirectionalLanguageModelTokenEmbedder(TestLanguageModelTokenEmbedder):
+    def setUp(self):
+        super().setUp()
+        self.set_up_model(self.FIXTURES_ROOT / 'language_model' /
+                          'bidirectional_lm_characters_token_embedder.jsonnet',
+                          self.FIXTURES_ROOT / 'data' / 'conll2003.txt')
+
+class TestBidirectionalLanguageModelTokenEmbedderWithoutBosEos(TestLanguageModelTokenEmbedder):
+    def setUp(self):
+        super().setUp()
+        self.set_up_model(self.FIXTURES_ROOT / 'language_model' /
+                          'bidirectional_lm_characters_token_embedder_without_bos_eos.jsonnet',
+                          self.FIXTURES_ROOT / 'data' / 'conll2003.txt')
