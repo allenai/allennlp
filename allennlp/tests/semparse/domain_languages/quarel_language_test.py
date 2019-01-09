546550a281b0ccc92582030fdee8f5fb7e01c6c6
Kevin Lin <kevinl@kevinl.local>
2019-01-08 17:25:44 -0800

    add and and quaval

--- allennlp/tests/semparse/domain_languages/quarel_language_test.py
+++ allennlp/tests/semparse/domain_languages/quarel_language_test.py
@@ -6,10 +6,6 @@ from allennlp.semparse import DomainLanguage, ExecutionError, ParsingError, pred
 
 from pprint import pprint
 
-class Number():
-    def __init__(self, name):
-        self.name = name 
-
 class Property():
     def __init__(self, name):
         self.name = name 
@@ -30,10 +26,12 @@ class QuaRelType():
 
 class QuaRel(DomainLanguage):
     def __init__(self):
-        super().__init__(start_types={Number}, allowed_constants={'world1': World(1),
-                                                                  'world2': World(2),
-                                                                  'higher': Direction(1),
-                                                                  'lower': Direction(-1)})
+        super().__init__(start_types={int}, allowed_constants={'world1': World(1),
+                                                               'world2': World(2),
+                                                               'higher': Direction(1),
+                                                               'lower': Direction(-1),
+                                                               'high': Direction(1),
+                                                               'low': Direction(-1)})
         self.default_theories = [
             {"friction": 1, "speed": -1, "smoothness": -1, "distance": -1, "heat": 1},
             {"speed": 1, "time": -1},
@@ -52,32 +50,44 @@ class QuaRel(DomainLanguage):
                      "weight"]:
             func = self.make_property_predicate(prop)
             self.add_predicate(prop, func)
+        
+        # ``and`` is a reserved word, so we add it as a predicate here instead of using the decorator. 
+        def and_function(quaval_1: QuaRelType, quaval_2: QuaRelType) -> QuaRelType:
+            if self._check_compatible(quaval_1, quaval_2):
+                return quaval_1
+            else:
+                return None
+        self.add_predicate('and', and_function)
 
 
     def make_property_predicate(self, property_name: str) -> Callable[[Direction, World], QuaRelType]:
         def property_function(direction: Direction, world: World) -> QuaRelType:
             return QuaRelType(Property(property_name), direction, world)
         return property_function
+
+    def _check_compatible(self, quarel_0: QuaRelType, quarel_1: QuaRelType) -> bool:
+        for theory in self.default_theories:
+            if quarel_0.prop.name in theory and quarel_1.prop.name in theory:
+                world_same = 1 if quarel_0.world.number == quarel_1.world.number else -1
+                direction_same = 1 if quarel_0.direction.number == quarel_1.direction.number else -1
+                if theory[quarel_0.prop.name] * theory[quarel_1.prop.name] * world_same * direction_same == 1:
+                    return True
+                else:
+                    return False
     
     @predicate
-    def infer(self, question: QuaRelType, answer_1: QuaRelType, answer_2: QuaRelType) -> Number:
-        for theory in self.default_theories:
-            if question.prop.name in theory and answer_1.prop.name in theory:
-                world_same = 1 if question.world.number == answer_1.world.number else -1
-                direction_same = 1 if question.direction.number == answer_1.direction.number else -1
-                if theory[question.prop.name] * theory[answer_1.prop.name] * world_same * direction_same == 1:
-                    return Number(1)
-                else:
-                    return Number(2)
-            
-            if question.prop.name in theory and answer_2.prop.name in theory:
-                world_same = 1 if question.world.number == answer_2.world.number else -1
-                direction_same = 1 if question.direction.number == answer_2.direction.number else -1
-                if theory[question.prop.name] * theory[answer_2.prop.name] * world_same * direction_same == 1:
-                    return Number(2)
-                else:
-                    return Number(1)
-        return Number(1)
+    def infer(self, question: QuaRelType, answer_0: QuaRelType, answer_1: QuaRelType) -> int:
+        if self._check_compatible(question, answer_0):
+            if self._check_compatible(question, answer_1):
+                # Found two answers
+                return -2
+            else:
+                return 0
+        elif self._check_compatible(question, answer_1):
+            return 1
+        else:
+            return -1
+
 
 class QuaRelLanguageTest(AllenNlpTestCase):
     def setUp(self):
@@ -87,9 +97,17 @@ class QuaRelLanguageTest(AllenNlpTestCase):
     def test_constant_logical_form(self):
         print('execute', self.language.execute('world1'))
             
-    def test_infer_logical_form(self):
-        self.language.execute('(infer (speed higher world1) (friction higher world1) (friction lower world1))')
+    def test_infer_quarel(self):
+        assert self.language.execute('(infer (speed higher world1) (friction higher world1) (friction lower world1))') == 1
         print(self.language.logical_form_to_action_sequence('(infer (speed higher world1) (friction higher world1) (friction lower world1))'))
+
+        assert self.language.execute('(infer (speed higher world2) (friction higher world1) (friction lower world1))') == 0
+
+    def test_infer_quaval(self):
+        assert self.language.execute('(infer (and (thickness low world1) (thickness high world2)) (strength lower world1) (strength lower world2))') == 0
+        print(self.language.logical_form_to_action_sequence('(infer (and (thickness low world1) (thickness high world2)) (strength lower world1) (strength lower world2))'))
+
+
 
 
 
