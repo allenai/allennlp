9547b2c54de2360e5cfe7707a047fab6122cb2c6
vidurj <vidur94@gmail.com>
2019-01-08 16:18:08 -0800

    removed trailing white space

--- allennlp/tests/nn/util_test.py
+++ allennlp/tests/nn/util_test.py
@@ -17,7 +17,7 @@ class TestNnUtil(AllenNlpTestCase):
                                         [1, 0, 0, 0, 0, 0]])
         lengths = util.get_lengths_from_binary_sequence_mask(binary_mask)
         numpy.testing.assert_array_equal(lengths.numpy(), numpy.array([3, 2, 6, 1]))
-    
+
     def test_get_mask_from_sequence_lengths(self):
         sequence_lengths = torch.LongTensor([4, 3, 1, 4, 2])
         mask = util.get_mask_from_sequence_lengths(sequence_lengths, 5).data.numpy()
@@ -309,7 +309,7 @@ class TestNnUtil(AllenNlpTestCase):
                 "tokens": torch.LongTensor([[3, 4, 5, 0, 0], [1, 2, 0, 0, 0]]),
                 "token_characters": torch.LongTensor([[[1, 2], [3, 0], [2, 0], [0, 0], [0, 0]],
                                                       [[5, 0], [4, 6], [0, 0], [0, 0], [0, 0]]])
-                }
+        }
         assert_almost_equal(util.get_text_field_mask(text_field_tensors).numpy(),
                             [[1, 1, 1, 0, 0], [1, 1, 0, 0, 0]])
 
@@ -317,7 +317,7 @@ class TestNnUtil(AllenNlpTestCase):
         text_field_tensors = {
                 "token_characters": torch.LongTensor([[[1, 2, 3], [3, 0, 1], [2, 1, 0], [0, 0, 0]],
                                                       [[5, 5, 5], [4, 6, 0], [0, 0, 0], [0, 0, 0]]])
-                }
+        }
         assert_almost_equal(util.get_text_field_mask(text_field_tensors).numpy(),
                             [[1, 1, 1, 0], [1, 1, 0, 0]])
 
@@ -325,7 +325,7 @@ class TestNnUtil(AllenNlpTestCase):
         text_field_tensors = {
                 "list_tokens": torch.LongTensor([[[1, 2], [3, 0], [2, 0], [0, 0], [0, 0]],
                                                  [[5, 0], [4, 6], [0, 0], [0, 0], [0, 0]]])
-                }
+        }
         actual_mask = util.get_text_field_mask(text_field_tensors, num_wrapping_dims=1).numpy()
         expected_mask = (text_field_tensors['list_tokens'].numpy() > 0).astype('int32')
         assert_almost_equal(actual_mask, expected_mask)
@@ -334,7 +334,7 @@ class TestNnUtil(AllenNlpTestCase):
         text_field_tensors = {
                 "tokens": torch.LongTensor([[3, 4, 5, 0, 0], [1, 2, 0, 0, 0]]),
                 "mask": torch.LongTensor([[0, 0, 1]])
-                }
+        }
         assert_almost_equal(util.get_text_field_mask(text_field_tensors).numpy(),
                             [[0, 0, 1]])
 
@@ -518,7 +518,7 @@ class TestNnUtil(AllenNlpTestCase):
             prediction = torch.nn.functional.log_softmax(prediction, dim=-1)
             correct_loss += prediction[label] * 0.9
             # incorrect elements
-            correct_loss += prediction.sum() * 0.1/4
+            correct_loss += prediction.sum() * 0.1 / 4
         # Average over sequence.
         correct_loss = - correct_loss / 3
         numpy.testing.assert_array_almost_equal(loss.data.numpy(), correct_loss.data.numpy())
@@ -860,20 +860,20 @@ class TestNnUtil(AllenNlpTestCase):
         assert not has_tensor(3)
 
         assert has_tensor({
-            "x": [
-                0,
-                {
-                    "inside": {
-                        "double_inside": [
-                            3,
-                            [
-                                10,
-                                tensor
-                            ]
-                        ]
-                    }
-                }
-            ]
+                "x": [
+                        0,
+                        {
+                                "inside": {
+                                        "double_inside": [
+                                                3,
+                                                [
+                                                        10,
+                                                        tensor
+                                                ]
+                                        ]
+                                }
+                        }
+                ]
         })
 
     def test_combine_initial_dims(self):
