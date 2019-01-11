a91ca1a1712b42a7b1c956387e0e1cbeeb7537c2
Matt Gardner <mattg@allenai.org>
2019-01-11 12:48:17 -0800

    Fix problem introduced by rebase

--- allennlp/semparse/domain_languages/__init__.py
+++ allennlp/semparse/domain_languages/__init__.py
@@ -2,4 +2,4 @@ from allennlp.semparse.domain_languages.domain_language import (DomainLanguage, 
                                                                 ExecutionError, predicate,
                                                                 START_SYMBOL)
 from allennlp.semparse.domain_languages.nlvr_language import NlvrLanguage
-:rom allennlp.semparse.domain_languages.quarel_language import QuaRelLanguage
+from allennlp.semparse.domain_languages.quarel_language import QuaRelLanguage
