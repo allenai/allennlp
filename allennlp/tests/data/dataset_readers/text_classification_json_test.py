68e9a1c1e6ec85c939aad5af98ae90b5bbe9f6a5
Suchin Gururangan <github@suchin.io>
2019-01-15 16:44:16 -0800

    added files

---
+++ allennlp/tests/data/dataset_readers/text_classification_json_test.py
@@ -0,0 +1,46 @@
+# pylint: disable=no-self-use,invalid-name
+import pytest
+
+from allennlp.data.dataset_readers import TextClassificationJSONReader
+from allennlp.common.util import ensure_list
+from allennlp.common.testing import AllenNlpTestCase
+
+
+class TestTextClassificationJSONReader():
+
+    @pytest.mark.parametrize("lazy", (True, False))
+    def test_read_from_file_ag(self, lazy):
+        reader = TextClassificationJSONReader(lazy=lazy)
+        ag_path = AllenNlpTestCase.FIXTURES_ROOT / "data" / "text_classification_json" / "ag.jsonl"
+        instances = reader.read(ag_path)
+        instances = ensure_list(instances)
+
+        instance1 = {"tokens": ['Memphis', 'Rout', 'Still', 'Stings', 'for', 'No', '.', '14',
+                                'Louisville', ';', 'Coach', 'Petrino', 'Vows', 'to', 'Have',
+                                'Team', 'Better', 'Prepared', '.', 'NASHVILLE', ',', 'Tenn.',
+                                'Nov', '3', ',', '2004', '-', 'Louisville', '#', '39;s', '30-point',
+                                'loss', 'at', 'home', 'to', 'Memphis', 'last', 'season', 'is', 'still',
+                                'a', 'painful', 'memory', 'for', 'the', 'Cardinals', '.'],
+                     "label": "2"}
+        instance2 = {"tokens": ['AP', '-', 'Eli', 'Manning', 'has', 'replaced', 'Kurt', 'Warner',
+                                'as', 'the', 'New', 'York', 'Giants', "'", 'starting',
+                                'quarterback', '.'],
+                     "label": "2"}
+        instance3 = {"tokens": ['A', 'conference', 'dedicated', 'to', 'online', 'journalism',
+                                'explores', 'the', 'effect', 'blogs', 'have', 'on', 'news',
+                                'reporting', '.', 'Some', 'say', 'they', 'draw', 'attention',
+                                'to', 'under', '-', 'reported', 'stories', '.', 'Others',
+                                'struggle', 'to', 'establish', 'the', 'credibility',
+                                'enjoyed', 'by', 'professionals', '.'],
+                     "label": "4"}
+
+        assert len(instances) == 3
+        fields = instances[0].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance1["tokens"]
+        assert fields["label"].label == instance1["label"]
+        fields = instances[1].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance2["tokens"]
+        assert fields["label"].label == instance2["label"]
+        fields = instances[2].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance3["tokens"]
+        assert fields["label"].label == instance3["label"]
