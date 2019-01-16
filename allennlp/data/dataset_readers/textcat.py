bf3dbef987f53998bf54a78a2f267d7f78c6b0b7
Suchin Gururangan <github@suchin.io>
2019-01-15 16:15:01 -0800

    addressed comments on textcat reader

--- allennlp/data/dataset_readers/textcat.py
+++ allennlp/data/dataset_readers/textcat.py
@@ -15,8 +15,8 @@ from allennlp.data.tokenizers.sentence_splitter import SpacySentenceSplitter
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
-@DatasetReader.register("textcat")
-class TextCatReader(DatasetReader):
+@DatasetReader.register("text_classification_json")
+class TextClassificationJSONReader(DatasetReader):
     """
     Reads tokens and their labels from a labeled text classification dataset.
     Expects a "tokens" field and a "category" field in JSON format.
@@ -31,7 +31,7 @@ class TextCatReader(DatasetReader):
         optional (default=``{"tokens": SingleIdTokenIndexer()}``)
         We use this to define the input representation for the text.
         See :class:`TokenIndexer`.
-    word_tokenizer : ``Tokenizer``, optional (default = ``{"tokens": WordTokenizer()}``)
+    tokenizer : ``Tokenizer``, optional (default = ``{"tokens": WordTokenizer()}``)
         Tokenizer to use to split the input text into words or other kinds of tokens.
     segment_sentences: ``bool``, optional (default = ``False``)
         If true, we will first segment the text into sentences using Spacy and then tokenize words.
@@ -39,23 +39,24 @@ class TextCatReader(DatasetReader):
         like the Hierarchical Attention Network.
     sequence_length: ``int``, optional (default = ``None``)
         If specified, will truncate tokens to specified maximum length.
-    debug : ``bool``, optional (default = ``False``)
-        If true, will only read 100 instances from file(s), so data can be read quickly during debugging.
+    skip_label_indexing: ``bool``, optional (default = ``False``)
+        Whether or not to skip label indexing. You might want to skip label indexing if your
+        labels are numbers, so the dataset reader doesn't re-number them starting from 0.
     lazy : ``bool``, optional, (default = ``False``)
         Whether or not instances can be read lazily.
     """
     def __init__(self,
                  token_indexers: Dict[str, TokenIndexer] = None,
-                 word_tokenizer: Tokenizer = None,
+                 tokenizer: Tokenizer = None,
                  segment_sentences: bool = False,
                  sequence_length: int = None,
-                 debug: bool = False,
+                 skip_label_indexing: bool = False,
                  lazy: bool = False) -> None:
         super().__init__(lazy=lazy)
-        self.debug = debug
-        self._word_tokenizer = word_tokenizer or WordTokenizer()
+        self.tokenizer = tokenizer or WordTokenizer()
         self._segment_sentences = segment_sentences
         self._sequence_length = sequence_length
+        self._skip_label_indexing = skip_label_indexing
         self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
         if self._segment_sentences:
             self._sentence_segmenter = SpacySentenceSplitter()
@@ -63,19 +64,23 @@ class TextCatReader(DatasetReader):
     @overrides
     def _read(self, file_path):
         with open(cached_path(file_path), "r") as data_file:
-            if self.debug:
-                lines = np.random.choice(data_file.readlines(), 100)
-            else:
-                lines = data_file.readlines()
-        for line in lines:
-            if not line:
-                continue
-            items = json.loads(line)
-            text = items["text"]
-            label = str(items["label"])
-            instance = self.text_to_instance(text=text, label=label)
-            if instance is not None:
-                yield instance
+            for line in data_file.readlines():
+                if not line:
+                    continue
+                items = json.loads(line)
+                text = items["text"]
+                label = str(items["label"])
+                instance = self.text_to_instance(text=text, label=label)
+                if instance is not None:
+                    yield instance
+
+    def _truncate(self, tokens):
+        """
+        truncate a set of tokens using the provided sequence length
+        """"
+        if len(tokens) > self._sequence_length:
+            tokens = tokens[:self._sequence_length]
+        return tokens
 
     @overrides
     def text_to_instance(self, text: str, label: str = None) -> Instance:  # type: ignore
@@ -98,32 +103,21 @@ class TextCatReader(DatasetReader):
         """
         # pylint: disable=arguments-differ
         fields: Dict[str, Field] = {}
-        text_fields: List[Field] = []
         if self._segment_sentences:
+            sentences: List[Field] = []
             sentence_tokens = self._sentence_segmenter.split_sentences(text)
-            if not sentence_tokens:
-                return None
             for sentence in sentence_tokens:
-                word_tokens = self._word_tokenizer.tokenize(sentence)
+                word_tokens = self._tokenizer.tokenize(sentence)
                 if self._sequence_length is not None:
-                    if len(word_tokens) > self._sequence_length:
-                        word_tokens = word_tokens[:self._sequence_length]
-                    else:
-                        padding = [Token("@@PADDING")] * (self._sequence_length - len(word_tokens))
-                        word_tokens = word_tokens + padding
-                text_fields.append(TextField(word_tokens, self._token_indexers))
-            fields['tokens'] = ListField(text_fields)
+                    self._truncate(tokens)
+                sentences.append(TextField(word_tokens, self._token_indexers))
+            fields['tokens'] = ListField(sentences)
         else:
-            tokens = self._word_tokenizer.tokenize(text)
-            if not tokens:
-                return None
+            tokens = self._tokenizer.tokenize(text)
             if self._sequence_length is not None:
-                if len(tokens) > self._sequence_length:
-                    tokens = tokens[:self._sequence_length]
-                else:
-                    padding = [Token("@@PADDING")] * (self._sequence_length - len(tokens))
-                    tokens = tokens + padding
+                self._truncate(tokens)
             fields['tokens'] = TextField(tokens, self._token_indexers)
         if label is not None:
-            fields['label'] = LabelField(label)
+            fields['label'] = LabelField(label,
+                                         skip_indexing=self._skip_label_indexing)
         return Instance(fields)
