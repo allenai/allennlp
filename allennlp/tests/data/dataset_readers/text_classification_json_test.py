5ec17edc02706db9d1a3810428a12711928d5341
Suchin Gururangan <github@suchin.io>
2019-01-16 13:30:04 -0800

    linting fixes

--- allennlp/tests/data/dataset_readers/text_classification_json_test.py
+++ allennlp/tests/data/dataset_readers/text_classification_json_test.py
@@ -51,7 +51,7 @@ class TestTextClassificationJsonReader():
         ag_path = AllenNlpTestCase.FIXTURES_ROOT / "data" / "text_classification_json" / "ag_news_corpus.jsonl"
         instances = reader.read(ag_path)
         instances = ensure_list(instances)
-        
+
         instance1 = {"tokens": ['Memphis', 'Rout', 'Still', 'Stings', 'for'],
                      "label": "2"}
         instance2 = {"tokens": ['AP', '-', 'Eli', 'Manning', 'has'],
@@ -77,23 +77,26 @@ class TestTextClassificationJsonReader():
         instances = reader.read(ag_path)
         instances = ensure_list(instances)
 
-        instance1 = {"tokens": [['Memphis', 'Rout'], ['Still', 'Stings', 'for', 'No', '.', '14',
-                                'Louisville', ';', 'Coach', 'Petrino', 'Vows', 'to', 'Have',
-                                'Team', 'Better', 'Prepared', '.'], ['NASHVILLE', ',', 'Tenn.',
-                                'Nov', '3', ',', '2004', '-', 'Louisville', '#', '39;s', '30-point',
-                                'loss', 'at', 'home', 'to', 'Memphis', 'last', 'season', 'is', 'still',
-                                'a', 'painful', 'memory', 'for', 'the', 'Cardinals', '.']],
+        instance1 = {"tokens": [['Memphis', 'Rout'],
+                                ['Still', 'Stings', 'for', 'No', '.', '14',
+                                 'Louisville', ';', 'Coach', 'Petrino', 'Vows', 'to', 'Have',
+                                 'Team', 'Better', 'Prepared', '.'],
+                                ['NASHVILLE', ',', 'Tenn.', 'Nov', '3', ',', '2004', '-',
+                                 'Louisville', '#', '39;s', '30-point', 'loss', 'at', 'home',
+                                 'to', 'Memphis', 'last', 'season', 'is', 'still', 'a', 'painful',
+                                 'memory', 'for', 'the', 'Cardinals', '.']],
                      "label": "2"}
         instance2 = {"tokens": [['AP', '-', 'Eli', 'Manning', 'has', 'replaced', 'Kurt', 'Warner',
-                                'as', 'the', 'New', 'York', 'Giants', "'", 'starting',
-                                'quarterback', '.']],
+                                 'as', 'the', 'New', 'York', 'Giants', "'", 'starting',
+                                 'quarterback', '.']],
                      "label": "2"}
         instance3 = {"tokens": [['A', 'conference', 'dedicated', 'to', 'online', 'journalism',
-                                'explores', 'the', 'effect', 'blogs', 'have', 'on', 'news',
-                                'reporting', '.'], ['Some', 'say', 'they', 'draw', 'attention',
-                                'to', 'under', '-', 'reported', 'stories', '.'], ['Others',
-                                'struggle', 'to', 'establish', 'the', 'credibility',
-                                'enjoyed', 'by', 'professionals', '.']],
+                                 'explores', 'the', 'effect', 'blogs', 'have', 'on', 'news',
+                                 'reporting', '.'],
+                                ['Some', 'say', 'they', 'draw', 'attention',
+                                 'to', 'under', '-', 'reported', 'stories', '.'],
+                                ['Others', 'struggle', 'to', 'establish', 'the',
+                                 'credibility', 'enjoyed', 'by', 'professionals', '.']],
                      "label": "4"}
 
         assert len(instances) == 3
