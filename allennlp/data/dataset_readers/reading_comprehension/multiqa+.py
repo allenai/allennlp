4d80dfd643537d90c4b3971f427b0a15f6e21be7
alontalmor <salont@gmail.com>
2019-01-20 16:41:19 +0200

    Calculating the optimal tensor size

--- allennlp/data/dataset_readers/reading_comprehension/multiqa+.py
+++ allennlp/data/dataset_readers/reading_comprehension/multiqa+.py
@@ -87,7 +87,7 @@ class MultiQAReader(DatasetReader):
 
 
 
-        if True:
+        if False:
             # bucketing by QuestionID
             instance_list = contexts
             instance_list = sorted(instance_list, key=lambda x: x['metadata']['question_id'])
