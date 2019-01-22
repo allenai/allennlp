f43a1cceea38c5730f52f775331a18aecf214dd4
Nelson Liu <nfliu@cs.washington.edu>
2019-01-21 16:10:34 -0800

    Fix LanguageModelingReader to generate instances for proper contiguous text lm

--- allennlp/data/dataset_readers/language_modeling.py
+++ allennlp/data/dataset_readers/language_modeling.py
@@ -1,18 +1,18 @@
-from typing import Dict
+from typing import Dict, List
 import logging
 
 from overrides import overrides
+import numpy
 
+from allennlp.common.checks import ConfigurationError
 from allennlp.common.file_utils import cached_path
-from allennlp.common.tqdm import Tqdm
 from allennlp.data.instance import Instance
 from allennlp.data.tokenizers.tokenizer import Tokenizer
-from allennlp.data.tokenizers import WordTokenizer
+from allennlp.data.tokenizers import Token, WordTokenizer
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from allennlp.data.token_indexers.token_indexer import TokenIndexer
-from allennlp.data.fields import TextField
+from allennlp.data.fields import ListField, TextField
 from allennlp.data.token_indexers import SingleIdTokenIndexer
-
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
@@ -20,78 +20,117 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 @DatasetReader.register("language_modeling")
 class LanguageModelingReader(DatasetReader):
     """
-    Reads a text file and converts it into a ``Dataset`` suitable for training a language model.
-
-    Note that there's one issue that needs to be fixed before this is actually usable for language
-    modeling - the way start and end tokens for sentences are handled is not correct; we need to
-    add a sentence splitter before this will be done right.
+    Reads a text file and converts it into a ``Dataset`` suitable for training
+    a language model.
 
     Parameters
     ----------
-    tokens_per_instance : ``int``, optional (default=``None``)
-        If this is ``None``, we will have each training instance be a single sentence.  If this is
-        not ``None``, we will instead take all sentences, including their start and stop tokens,
-        line them up, and split the tokens into groups of this number, for more efficient training
-        of the language model.
+    batch_size : ``int``, optional (default=``20``)
+        Batch size to use in language modeling.
+    truncated_bptt_size : ``int``, optional (default=``35``)
+        The sequence length to use for truncated backpropagation through time.
+    fuzz_truncated_bptt_size : ``bool``, optional (default=``True``)
+        If True, randomly perturb the truncated_bptt_size between batches.
     tokenizer : ``Tokenizer``, optional (default=``WordTokenizer()``)
         We use this ``Tokenizer`` for the text.  See :class:`Tokenizer`.
     token_indexers : ``Dict[str, TokenIndexer]``, optional (default=``{"tokens": SingleIdTokenIndexer()}``)
-        We use this to define the input representation for the text.  See :class:`TokenIndexer`.
-        Note that the `output` representation will always be single token IDs - if you've specified
-        a ``SingleIdTokenIndexer`` here, we use the first one you specify.  Otherwise, we create
-        one with default parameters.
+        We use this to define the input representation for the text.
+        See :class:`TokenIndexer`.
+    start_tokens : ``List[str]``, optional (default=``None``)
+        These are prepended to each line read by the dataset reader.
+    end_tokens : ``List[str]``, optional (default=``None``)
+        These are appended to each line read by the dataset reader.
     """
     def __init__(self,
-                 tokens_per_instance: int = None,
+                 batch_size: int = 20,
+                 truncated_bptt_size: int = 35,
+                 fuzz_truncated_bptt_size: bool = True,
                  tokenizer: Tokenizer = None,
                  token_indexers: Dict[str, TokenIndexer] = None,
-                 lazy: bool = False) -> None:
-        super().__init__(lazy)
+                 start_tokens: List[str] = None,
+                 end_tokens: List[str] = ["</S>"]) -> None:
+        super().__init__(lazy=False)
+        self._batch_size = batch_size
+        if truncated_bptt_size < 2:
+            raise ConfigurationError("truncated_bptt_size cannot be less than 2.")
+        self._truncated_bptt_size = truncated_bptt_size
+        self._fuzz_truncated_bptt_size = fuzz_truncated_bptt_size
         self._tokenizer = tokenizer or WordTokenizer()
         self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
-        self._tokens_per_instance = tokens_per_instance
 
-        # No matter how you want to represent the input, we'll always represent the output as a
-        # single token id.  This code lets you learn a language model that concatenates word
-        # embeddings with character-level encoders, in order to predict the word token that comes
-        # next.
-        self._output_indexer: Dict[str, TokenIndexer] = None
-        for name, indexer in self._token_indexers.items():
-            if isinstance(indexer, SingleIdTokenIndexer):
-                self._output_indexer = {name: indexer}
-                break
-        else:
-            self._output_indexer = {"tokens": SingleIdTokenIndexer()}
+        self._start_tokens = [Token(st) for st in (start_tokens or [])]
+        self._end_tokens = [Token(et) for et in (end_tokens or [])]
+        # Cache the batched tokens we read from data files.
+        self._all_batched_file_tokens = {}
 
     @overrides
     def _read(self, file_path: str):
-        # if `file_path` is a URL, redirect to the cache
-        file_path = cached_path(file_path)
+        if file_path not in self._all_batched_file_tokens:
+            logger.info('Loading data from %s', file_path)
+            # if `file_path` is a URL, redirect to the cache
+            file_path = cached_path(file_path)
 
-        with open(file_path, "r") as text_file:
-            instance_strings = text_file.readlines()
+            # Read the contents of the file into one long list of tokens,
+            # adding start and/or end tokens as necessary.
+            file_tokens = []
+            file_tokens.extend(self._start_tokens)
+            with open(file_path, "r") as text_file:
+                for line in text_file:
+                    tokenized_line = self._tokenizer.tokenize(line)
+                    file_tokens.extend(tokenized_line)
+                    file_tokens.extend(self._end_tokens)
 
-        if self._tokens_per_instance is not None:
-            all_text = " ".join([x.replace("\n", " ").strip() for x in instance_strings])
-            tokenized_text = self._tokenizer.tokenize(all_text)
-            num_tokens = self._tokens_per_instance + 1
-            tokenized_strings = []
-            logger.info("Creating dataset from all text in file: %s", file_path)
-            for index in Tqdm.tqdm(range(0, len(tokenized_text) - num_tokens, num_tokens - 1)):
-                tokenized_strings.append(tokenized_text[index:(index + num_tokens)])
+            # Divide file_tokens into batch_size lists
+            # Work out how we can evenly split the dataset into batch_size parts
+            total_num_tokens_per_batch = len(file_tokens) // self._batch_size
+            # Trim off the remainder from file_tokens, so we can evenly divide it
+            # into batch_size lists.
+            file_tokens_for_even_split = file_tokens[:total_num_tokens_per_batch *
+                                                     self._batch_size]
+            # Evenly divide the data into batch_size lists.
+            batched_file_tokens = [
+                    file_tokens_for_even_split[i:i + total_num_tokens_per_batch] for i in
+                    range(0, len(file_tokens_for_even_split), total_num_tokens_per_batch)]
+            # Cache the tokens of the dataset we've just read.
+            self._all_batched_file_tokens[file_path] = batched_file_tokens
         else:
-            tokenized_strings = [self._tokenizer.tokenize(s) for s in instance_strings]
+            batched_file_tokens = self._all_batched_file_tokens[file_path]
 
-        for tokenized_string in tokenized_strings:
-            input_field = TextField(tokenized_string[:-1], self._token_indexers)
-            output_field = TextField(tokenized_string[1:], self._output_indexer)
-            yield Instance({'input_tokens': input_field,
-                            'output_tokens': output_field})
+        # Iterate over the batched_file_tokens, yielding batches
+        batch_start_index = 0
+        # The max value of batch_start_index is len(batched_file_tokens[0]) - 2,
+        # leaving room for the target even when the final batch is size 1.
+        while batch_start_index < len(batched_file_tokens[0]) - 1:
+            if self._fuzz_truncated_bptt_size:
+                # This randomization is taken from the code for training the AWD-LSTM.
+                # (matrices of size (batch_size, truncated_bptt_size))
+                fuzzy_truncated_bptt_size = (
+                        self._truncated_bptt_size if numpy.random.random() < 0.95 else
+                        self._truncated_bptt_size / 2.)
+                # Prevent excessively small or negative sequence length
+                sequence_length = max(5,
+                                      int(numpy.random.normal(fuzzy_truncated_bptt_size, 5)))
+                # There's a very small chance that it could select a very long sequence
+                # length, resulting in OOM. So we cap it at no more than
+                # self._truncated_bptt_size + 10
+                sequence_length = min(sequence_length, self._truncated_bptt_size + 10)
+            else:
+                sequence_length = self._truncated_bptt_size
 
-    @overrides
-    def text_to_instance(self, sentence: str) -> Instance:  # type: ignore
-        # pylint: disable=arguments-differ
-        tokenized_string = self._tokenizer.tokenize(sentence)
-        input_field = TextField(tokenized_string[:-1], self._token_indexers)
-        output_field = TextField(tokenized_string[1:], self._output_indexer)
-        return Instance({'input_tokens': input_field, 'output_tokens': output_field})
+            # We need to constrain the sequence_length to ensure that
+            # the targets don't reach beyond the length of our dataset
+            sequence_length = min(sequence_length,
+                                  len(batched_file_tokens[0]) - batch_start_index - 1)
+            batch_inputs = [single_batch[batch_start_index:batch_start_index + sequence_length]
+                            for single_batch in batched_file_tokens]
+            batch_targets = [single_batch[batch_start_index + 1:batch_start_index + 1 + sequence_length]
+                             for single_batch in batched_file_tokens]
+
+            # Take the examples between batch_start_index and sequence_length
+            yield Instance({
+                    "inputs": ListField([TextField(single_batch, self._token_indexers) for
+                                         single_batch in batch_inputs]),
+                    "targets": ListField([TextField(single_batch, self._token_indexers) for
+                                          single_batch in batch_targets])
+            })
+            batch_start_index += sequence_length
--- allennlp/tests/data/dataset_readers/language_modeling_dataset_test.py
+++ allennlp/tests/data/dataset_readers/language_modeling_dataset_test.py
@@ -1,31 +1,137 @@
 # pylint: disable=no-self-use,invalid-name
-import pytest
-
 from allennlp.data.dataset_readers import LanguageModelingReader
 from allennlp.common.util import ensure_list
 from allennlp.common.testing import AllenNlpTestCase
 
+import numpy
+
+
 class TestLanguageModelingDatasetReader:
-    @pytest.mark.parametrize("lazy", (True, False))
-    def test_read_from_file(self, lazy):
-        reader = LanguageModelingReader(tokens_per_instance=3, lazy=lazy)
+    def test_read_from_file_no_fuzz_is_deterministic(self):
+        """
+        The dataset is split into 4 batches, becoming:
 
-        instances = ensure_list(reader.read(AllenNlpTestCase.FIXTURES_ROOT / 'data' / 'language_modeling.txt'))
-        # The last potential instance is left out, which is ok, because we don't have an end token
-        # in here, anyway.
-        assert len(instances) == 5
+        [[This, is, a, sentence],
+         [for, language, modelling, .],
+         [</S>, Here, 's, another],
+         [one, for, extra, language]]
 
-        assert [t.text for t in instances[0].fields["input_tokens"].tokens] == ["This", "is", "a"]
-        assert [t.text for t in instances[0].fields["output_tokens"].tokens] == ["is", "a", "sentence"]
+        Then, since our truncated bptt size is 2, our the inputs of our first batch
+        consists of:
+        [[This, is],
+         [for, language],
+         [</S>, Here],
+         [one, for]]
 
-        assert [t.text for t in instances[1].fields["input_tokens"].tokens] == ["sentence", "for", "language"]
-        assert [t.text for t in instances[1].fields["output_tokens"].tokens] == ["for", "language", "modelling"]
+        The second batch consists of:
+        [[a],
+         [modelling],
+         ['s],
+         [extra]]
 
-        assert [t.text for t in instances[2].fields["input_tokens"].tokens] == ["modelling", ".", "Here"]
-        assert [t.text for t in instances[2].fields["output_tokens"].tokens] == [".", "Here", "'s"]
+        Note that the second batch has a shorter sequence length of 1, and we do not
+        predict labels for the final words in the batch.
+        """
+        # Results should be identical if we run twice.
+        for i in range(2):
+            reader = LanguageModelingReader(batch_size=4,
+                                            truncated_bptt_size=2,
+                                            fuzz_truncated_bptt_size=False)
+            instances = ensure_list(reader.read(AllenNlpTestCase.FIXTURES_ROOT / 'data' /
+                                                'language_modeling.txt'))
+            # This should match the batch size
+            assert len(instances[0].fields["inputs"].field_list) == 4
+            assert len(instances[0].fields["targets"].field_list) == 4
+            # This is the number of batches generated
+            assert len(instances) == 2
 
-        assert [t.text for t in instances[3].fields["input_tokens"].tokens] == ["'s", "another", "one"]
-        assert [t.text for t in instances[3].fields["output_tokens"].tokens] == ["another", "one", "for"]
+            first_instance_inputs = [["This", "is"],
+                                     ["for", "language"],
+                                     ["</S>", "Here"],
+                                     ["one", "for"]]
+            first_instance_targets = [["is", "a"],
+                                      ["language", "modelling"],
+                                      ["Here", "'s"],
+                                      ["for", "extra"]]
 
-        assert [t.text for t in instances[4].fields["input_tokens"].tokens] == ["for", "extra", "language"]
-        assert [t.text for t in instances[4].fields["output_tokens"].tokens] == ["extra", "language", "modelling"]
+            first_instance_generated_inputs = [
+                [x.text for x in instances[0].fields["inputs"].field_list[i].tokens] for
+                i in range(len(instances[0].fields["inputs"].field_list))]
+            assert first_instance_generated_inputs == first_instance_inputs
+            first_instance_generated_targets = [
+                [x.text for x in instances[0].fields["targets"].field_list[i].tokens] for
+                i in range(len(instances[0].fields["targets"].field_list))]
+            assert first_instance_generated_targets == first_instance_targets
+
+            second_instance_inputs = [["a"],
+                                      ["modelling"],
+                                      ["'s"],
+                                      ["extra"]]
+            second_instance_targets = [["sentence"],
+                                       ["."],
+                                       ["another"],
+                                       ["language"]]
+            second_instance_generated_inputs = [
+                [x.text for x in instances[1].fields["inputs"].field_list[i].tokens] for
+                i in range(len(instances[1].fields["inputs"].field_list))]
+            assert second_instance_generated_inputs == second_instance_inputs
+            second_instance_generated_targets = [
+                [x.text for x in instances[1].fields["targets"].field_list[i].tokens] for
+                i in range(len(instances[1].fields["targets"].field_list))]
+            assert second_instance_generated_targets == second_instance_targets
+
+    def test_read_from_file(self):
+        """
+        The dataset is split into 2 batches, becoming:
+
+        [[This, is, a, sentence, for, language, modelling, ., </S>],
+         [Here, 's, another, one, for, extra, language, modelling, .]]
+
+        Our truncated bptt size is 2, but fuzz_truncated_bptt_size is True. So
+        the sequence length is randomly perturbed, becoming 5. As a result,
+        the inputs are:
+
+        [[This, is, a, sentence, for],
+         [Here, 's, another, one, for]]
+
+        The second batch consists of:
+        [[language, modelling, .],
+         [extra, language, modelling]]
+
+        Note that the second batch has a shorter sequence length of 3, and we do not
+        predict labels for the final words in the batch.
+        """
+        numpy.random.seed(seed=0)
+        reader = LanguageModelingReader(batch_size=2, truncated_bptt_size=2)
+        instances = ensure_list(reader.read(AllenNlpTestCase.FIXTURES_ROOT / 'data' /
+                                            'language_modeling.txt'))
+        # This should match the batch size
+        assert len(instances[0].fields["inputs"].field_list) == 2
+        # This is the number of batches generated
+        assert len(instances) == 2
+
+        first_instance_inputs = [["This", "is", "a", "sentence", "for"],
+                                 ["Here", "'s", "another", "one", "for"]]
+        first_instance_targets = [["is", "a", "sentence", "for", "language"],
+                                  ["'s", "another", "one", "for", "extra"]]
+        first_instance_generated_inputs = [
+            [x.text for x in instances[0].fields["inputs"].field_list[i].tokens] for
+            i in range(len(instances[0].fields["inputs"].field_list))]
+        assert first_instance_generated_inputs == first_instance_inputs
+        first_instance_generated_targets = [
+            [x.text for x in instances[0].fields["targets"].field_list[i].tokens] for
+            i in range(len(instances[0].fields["targets"].field_list))]
+        assert first_instance_generated_targets == first_instance_targets
+
+        second_instance_inputs = [["language", "modelling", "."],
+                                  ["extra", "language", "modelling"]]
+        second_instance_targets = [["modelling", ".", "</S>"],
+                                   ["language", "modelling", "."]]
+        second_instance_generated_inputs = [
+            [x.text for x in instances[1].fields["inputs"].field_list[i].tokens] for
+            i in range(len(instances[1].fields["inputs"].field_list))]
+        assert second_instance_generated_inputs == second_instance_inputs
+        second_instance_generated_targets = [
+            [x.text for x in instances[1].fields["targets"].field_list[i].tokens] for
+            i in range(len(instances[1].fields["targets"].field_list))]
+        assert second_instance_generated_targets == second_instance_targets
