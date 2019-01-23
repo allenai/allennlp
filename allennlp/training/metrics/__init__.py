9ac2de689d8a178e4c27a1a00170db97ad48a57f
Harsh Trivedi <harshjtrivedi94@gmail.com>
2019-01-23 10:38:49 +0800

    Add AUC metric. (#2358)
    * Add AUC metric.
    
    * add masking.
    
    * fix whitespace.
    
    * fix docs.
    
    * Incorporate PR feedback.
    
    (cherry picked from commit cb9651a4c77c10cbd2d76f79b85c6453386dc229)

--- allennlp/training/metrics/__init__.py
+++ allennlp/training/metrics/__init__.py
@@ -23,3 +23,4 @@ from allennlp.training.metrics.span_based_f1_measure import SpanBasedF1Measure
 from allennlp.training.metrics.squad_em_and_f1 import SquadEmAndF1
 from allennlp.training.metrics.unigram_recall import UnigramRecall
 from allennlp.training.metrics.hamming_loss import HammingLoss
+from allennlp.training.metrics.auc import Auc
