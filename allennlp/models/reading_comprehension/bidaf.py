238d518f06c519d7f8f9b97ce9b602575d38709c
MajorChina <535370561@qq.com>
2019-01-31 12:17:53 +0800

    faster(GPU) get_best_span in bidaf

--- allennlp/models/reading_comprehension/bidaf.py
+++ allennlp/models/reading_comprehension/bidaf.py
@@ -302,24 +302,11 @@ class BidirectionalAttentionFlow(Model):
         if span_start_logits.dim() != 2 or span_end_logits.dim() != 2:
             raise ValueError("Input shapes must be (batch_size, passage_length)")
         batch_size, passage_length = span_start_logits.size()
-        max_span_log_prob = [-1e20] * batch_size
-        span_start_argmax = [0] * batch_size
-        best_word_span = span_start_logits.new_zeros((batch_size, 2), dtype=torch.long)
-
-        span_start_logits = span_start_logits.detach().cpu().numpy()
-        span_end_logits = span_end_logits.detach().cpu().numpy()
-
-        for b in range(batch_size):  # pylint: disable=invalid-name
-            for j in range(passage_length):
-                val1 = span_start_logits[b, span_start_argmax[b]]
-                if val1 < span_start_logits[b, j]:
-                    span_start_argmax[b] = j
-                    val1 = span_start_logits[b, j]
-
-                val2 = span_end_logits[b, j]
-
-                if val1 + val2 > max_span_log_prob[b]:
-                    best_word_span[b, 0] = span_start_argmax[b]
-                    best_word_span[b, 1] = j
-                    max_span_log_prob[b] = val1 + val2
-        return best_word_span
+        device = span_start_logits.device
+        probs_product = span_start_logits.unsqueeze(2) + span_end_logits.unsqueeze(1)
+        probs_product_triu = probs_product * torch.triu(torch.ones((passage_length, passage_length),
+                                                                   device=device)).unsqueeze(0)
+        res = probs_product_triu.view(batch_size, -1).argmax(-1)
+        span_start_idx = res // passage_length
+        span_end_idx = res % passage_length
+        return torch.stack([span_start_idx, span_end_idx], dim=-1)
