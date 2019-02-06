ce83cb41bfec1f9c8398f6c77db78bf5a5b3adb5
Andrew Moore <andrew.p.moore94@gmail.com>
2019-02-06 17:30:32 +0000

    Variational dropout in Augmented LSTM (#2344)
    This pull requests fixes #2320 I have done point 2 on the list within the issue. Apart from the points made in the issue I have also added more detail to the doc strings in the [Stacked Bi-Directional LSTM](https://github.com/allenai/allennlp/blob/master/allennlp/modules/stacked_bidirectional_lstm.py) such as the corrected shape of the `final_states` returned form the forward pass and the correct shape required for the `initial_state` argument for the forward pass. I have also included the returned Type of the forward pass for the [Stacked Bi-Directional LSTM](https://github.com/allenai/allennlp/blob/master/allennlp/modules/stacked_bidirectional_lstm.py).
    
    Screen shot of the changed doc strings for the augmented LSTM is below: Only change is the wording of the `recurrent_dropout_probability` argument within the constructor:
    ![augmented doc string](https://user-images.githubusercontent.com/13574854/51051958-34cf7180-15cd-11e9-9a48-7bbb039ae504.png)
    
    Screen shots of the changed doc strings for the Stacked Bi-Directional LSTM are below: Changes are those stated above with regards to the forward pass and the wording of the `recurrent_dropout_probability` and `layer_dropout_probability` within the constructor as well as the text of the constructor:
    ![stacked constructor doc string](https://user-images.githubusercontent.com/13574854/51051945-2719ec00-15cd-11e9-9809-4a603a8e1a66.png)
    ![stacked forward doc string](https://user-images.githubusercontent.com/13574854/51051952-2d0fcd00-15cd-11e9-906b-7a1cf3ab7b25.png)

--- allennlp/tests/modules/stacked_bidirectional_lstm_test.py
+++ allennlp/tests/modules/stacked_bidirectional_lstm_test.py
@@ -1,16 +1,17 @@
 # pylint: disable=no-self-use,invalid-name
 import numpy
+import pytest
 import torch
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 from allennlp.modules.stacked_bidirectional_lstm import StackedBidirectionalLstm
 from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder
 from allennlp.modules.seq2vec_encoders import Seq2VecEncoder
-from allennlp.common.testing import AllenNlpTestCase
 from allennlp.common.params import Params
+from allennlp.nn import InitializerApplicator, Initializer
+from allennlp.nn.util import sort_batch_by_length
 
-
-class TestStackedBidirectionalLstm(AllenNlpTestCase):
+class TestStackedBidirectionalLstm():
     def test_stacked_bidirectional_lstm_completes_forward_pass(self):
         input_tensor = torch.rand(4, 5, 3)
         input_tensor[1, 4:, :] = 0.
@@ -55,3 +56,51 @@ class TestStackedBidirectionalLstm(AllenNlpTestCase):
         mask = torch.ones(4, 5)
         output = encoder(input_tensor, mask)
         assert output.detach().numpy().shape == (4, 18)
+
+
+    @pytest.mark.parametrize("dropout_name", ('layer_dropout_probability',
+                                              'recurrent_dropout_probability'))
+    def test_stacked_bidirectional_lstm_dropout_version_is_different(self, dropout_name: str):
+        stacked_lstm = StackedBidirectionalLstm(input_size=10, hidden_size=11,
+                                                num_layers=3)
+        if dropout_name == 'layer_dropout_probability':
+            dropped_stacked_lstm = StackedBidirectionalLstm(input_size=10, hidden_size=11,
+                                                            num_layers=3,
+                                                            layer_dropout_probability=0.9)
+        elif dropout_name == 'recurrent_dropout_probability':
+            dropped_stacked_lstm = StackedBidirectionalLstm(input_size=10, hidden_size=11,
+                                                            num_layers=3,
+                                                            recurrent_dropout_probability=0.9)
+        else:
+            raise ValueError('Do not recognise the following dropout name '
+                             f'{dropout_name}')
+        # Initialize all weights to be == 1.
+        constant_init = Initializer.from_params(Params({"type": "constant", "val": 0.5}))
+        initializer = InitializerApplicator([(".*", constant_init)])
+        initializer(stacked_lstm)
+        initializer(dropped_stacked_lstm)
+
+        initial_state = torch.randn([3, 5, 11])
+        initial_memory = torch.randn([3, 5, 11])
+
+        tensor = torch.rand([5, 7, 10])
+        sequence_lengths = torch.LongTensor([7, 7, 7, 7, 7])
+
+        sorted_tensor, sorted_sequence, _, _ = sort_batch_by_length(tensor, sequence_lengths)
+        lstm_input = pack_padded_sequence(sorted_tensor, sorted_sequence.data.tolist(), batch_first=True)
+
+        stacked_output, stacked_state = stacked_lstm(lstm_input, (initial_state, initial_memory))
+        dropped_output, dropped_state = dropped_stacked_lstm(lstm_input, (initial_state, initial_memory))
+        dropped_output_sequence, _ = pad_packed_sequence(dropped_output, batch_first=True)
+        stacked_output_sequence, _ = pad_packed_sequence(stacked_output, batch_first=True)
+        if dropout_name == 'layer_dropout_probability':
+            with pytest.raises(AssertionError):
+                numpy.testing.assert_array_almost_equal(dropped_output_sequence.data.numpy(),
+                                                        stacked_output_sequence.data.numpy(), decimal=4)
+        if dropout_name == 'recurrent_dropout_probability':
+            with pytest.raises(AssertionError):
+                numpy.testing.assert_array_almost_equal(dropped_state[0].data.numpy(),
+                                                        stacked_state[0].data.numpy(), decimal=4)
+            with pytest.raises(AssertionError):
+                numpy.testing.assert_array_almost_equal(dropped_state[1].data.numpy(),
+                                                        stacked_state[1].data.numpy(), decimal=4)
