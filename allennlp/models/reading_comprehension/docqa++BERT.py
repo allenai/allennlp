7bfa972db0e2cb944740bdbbf287efa03d7d7291
alontalmor <salont@gmail.com>
2019-02-26 22:33:35 +0200

    4GPU support

--- allennlp/models/reading_comprehension/docqa++BERT.py
+++ allennlp/models/reading_comprehension/docqa++BERT.py
@@ -333,7 +333,7 @@ class DocQAPlusBERT(Model):
         # but we would like to see if we still get F1 score for it...
         # so in evaluation our loss is not Accurate! (however the question with no answer tokens will
         # remain the same number so it is relatively accuracy)
-        if not self.training and 'loss' not in output_dict:
+        if 'loss' not in output_dict:
             output_dict["loss"] = torch.cuda.FloatTensor([0], device=span_end_logits.device) \
                 if torch.cuda.is_available() else torch.FloatTensor([0])
 
