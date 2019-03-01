d55c1707df77b354433749b8316c1dab64df719e
Matt Gardner <mattg@allenai.org>
2019-03-01 12:53:33 -0800

    Updated eval script

--- allennlp/tools/drop_eval.py
+++ allennlp/tools/drop_eval.py
@@ -1,54 +1,63 @@
 #!/usr/bin/python
 
+from collections import defaultdict
+from typing import Any, Dict, List, Set, Tuple, Union
 import json
 import sys
 import argparse
 import string
-import numpy as np
 import re
 
+import numpy as np
 
-# Originally copied from:
+
+# From here through _normalize_answer was originally copied from:
 # https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
-# Then cleaned up a bit.
-def remove_articles(text):
+# Then cleaned up and modified a bit.
+def _remove_articles(text: str) -> str:
     regex = re.compile(r'\b(a|an|the)\b', re.UNICODE)
     return re.sub(regex, ' ', text)
 
-def white_space_fix(text):
+def _white_space_fix(text: str) -> str:
     return ' '.join(text.split())
 
-def is_number(s):
+EXCLUDE = set(string.punctuation)
+def _remove_punc(text: str) -> str:
+    if not _is_number(text):
+        return ''.join(ch for ch in text if ch not in EXCLUDE)
+    else:
+        return text
+
+def _lower(text: str) -> str:
+    return text.lower()
+
+def _tokenize(text: str) -> List[str]:
+    return re.split(" |-", text)
+
+def _normalize_answer(string: str) -> str:
+    """Lower text and remove punctuation, articles and extra whitespace."""
+
+    parts = [_white_space_fix(_remove_articles(_normalize_number(_remove_punc(_lower(token)))))
+             for token in _tokenize(string)]
+    parts = [part for part in parts if part.strip()]
+    normalized = ' '.join(parts).strip()
+    return normalized
+
+def _is_number(string: str) -> bool:
     try:
-        float(s)
+        float(string)
         return True
     except ValueError:
         return False
 
-def remove_punc(text):
-    if not is_number(text):
-        exclude = set(string.punctuation)
-        return ''.join(ch for ch in text if ch not in exclude)
-    else:
-        return text
-
-def lower(text):
-    return text.lower()
-
-# use the number instead of string, if it is one
-def norm_number(text):
-    if is_number(text):
+def _normalize_number(text: str) -> str:
+    if _is_number(text):
         return str(float(text))
     else:
         return text
 
-def normalize_answer(s):
-    """Lower text and remove punctuation, articles and extra whitespace."""
-    sp = ' '.join([white_space_fix(remove_articles(norm_number(remove_punc(lower(tok))))) for tok in s.split()])
-    return sp
 
-
-def answer_to_bags(answer):
+def _answer_to_bags(answer: Union[str, List[str], Tuple[str]]) -> Tuple[Set[str], List[Set[str]]]:
     span_bag = set()
     raw_spans = []
     if isinstance(answer, list) or isinstance(answer, tuple):
@@ -56,35 +65,92 @@ def answer_to_bags(answer):
     if isinstance(answer, str):
         raw_spans = [answer]
     span_bag = set()
-    token_bag = set()
+    token_bag = []
     for raw_span in raw_spans:
-        span = normalize_answer(raw_span)
+        span = _normalize_answer(raw_span)
         span_bag.add(span)
-        token_bag.update(span.split())
+        token_bag.append(set(span.split()))
     return span_bag, token_bag
 
 
-def get_metrics(predicted, gold):
-    predicted_bags = answer_to_bags(predicted)
-    gold_bags = answer_to_bags(gold)
+def _align_bags(predicted: List[Set[str]], gold: List[Set[str]]) -> List[float]:
+    f1_scores = []
+    for gold_index in range(len(gold)):
+        gold_item = gold[gold_index]
+        max_f1 = 0.0
+        max_index = None
+        best_alignment = (set([]),set([]))
+        for pred_index in range(len(predicted)):
+            pred_item = predicted[pred_index]
+            current_f1 = _compute_f1(pred_item, gold_item)
+            if current_f1 >= max_f1:
+                best_alignment = (gold_item, pred_item)
+                max_f1 = current_f1
+                max_index = pred_index
+        match_flag = _match_numbers_if_present(*best_alignment)
+        if match_flag:
+            f1_scores.append(max_f1)
+        else:
+            f1_scores.append(0.0)
+        gold[gold_index] = {}
+        predicted[max_index] = {}
+    return f1_scores
 
-    exact_match = 1 if predicted_bags[0] == gold_bags[0] else 0
 
-    intersection = len(gold_bags[1].intersection(predicted_bags[1]))
-    if len(predicted_bags[1]) == 0:
+def _compute_f1(predicted_bag: Set[str], gold_bag: Set[str]) -> float:
+    intersection = len(gold_bag.intersection(predicted_bag))
+    if len(predicted_bag) == 0:
         precision = 1.0
     else:
-        precision = intersection / len(predicted_bags[1])
-    if len(gold_bags[1]) == 0:
+        precision = intersection / float(len(predicted_bag))
+    if len(gold_bag) == 0:
         recall = 1.0
     else:
-        recall = intersection / len(gold_bags[1])
-    f1 = (2 * precision * recall) / (precision + recall) if not (precision==0 and recall==0) else 0
+        recall = intersection / float(len(gold_bag))
+    f1 = (2 * precision * recall) / (precision + recall) if not (precision == 0.0 and recall == 0.0) else 0.0
+    return f1
 
+
+def _match_numbers_if_present(gold_bag: Set[str], predicted_bag: Set[str]) -> bool:
+    gold_numbers = set()
+    predicted_numbers = set()
+    for word in gold_bag:
+        if _is_number(word):
+            gold_numbers.add(word)
+    for word in predicted_bag:
+        if _is_number(word):
+            predicted_numbers.add(word)
+    if (len(gold_numbers) == 0 or
+        (len(gold_numbers) > 0 and len(gold_numbers.intersection(predicted_numbers)) > 0)):
+        return True
+    return False
+
+
+def get_metrics(predicted: Union[str, List[str], Tuple[str]],
+                gold: Union[str, List[str], Tuple[str]]) -> Tuple[float, float]:
+    """
+    Takes a predicted answer and a gold answer (that are both either a string or a list of
+    strings), and returns exact match and the DROP F1 metric for the prediction.  If you are
+    writing a script for evaluating objects in memory (say, the output of predictions during
+    validation, or while training), this is the function you want to call, after using
+    :func:`answer_json_to_strings` when reading the gold answer from the released data file.
+    """
+    predicted_bags = _answer_to_bags(predicted)
+    gold_bags = _answer_to_bags(gold)
+
+    exact_match = 1.0 if predicted_bags[0] == gold_bags[0] else 0
+
+    f1_per_bag = _align_bags(predicted_bags[1], gold_bags[1])
+    f1 = np.mean(f1_per_bag)
+    f1 = round(f1, 2)
     return exact_match, f1
 
 
-def to_string(answer):
+def answer_json_to_strings(answer: Dict[str, Any]) -> Tuple[Tuple[str, ...], str]:
+    """
+    Takes an answer JSON blob from the DROP data release and converts it into strings used for
+    evaluation.
+    """
     if answer["number"] != "":
         return tuple([str(answer["number"])]), "number"
     elif len(answer["spans"]) > 0:
@@ -95,15 +161,23 @@ def to_string(answer):
                                            answer["date"]["year"])]), "date"
 
 
-def _run_evaluation(annotations, predicted_answers):
+def evaluate_json(annotations: Dict[str, Any], predicted_answers: Dict[str, Any]) -> Tuple[float, float]:
     """
-    Evaluation for programatic use.
+    Takes gold annotations and predicted answers and  evaluates the predictions for each question
+    in the gold annotations.  Both JSON dictionaries must have query_id keys, which are used to
+    match predictions to gold annotations (note that these are somewhat deep in the JSON for the
+    gold annotations, but must be top-level keys in the predicted answers).
+
+    The ``annotations`` are assumed to have the format of the dev set in the DROP data release.
+    The ``predicted_answers`` JSON must be a dictionary keyed by query id, where the value is
+    either a JSON dictionary with an "answer" key, or just a string (or list of strings) that is
+    the answer.
     """
     exact_match = []
     f1 = []
     # for each type as well
-    type_to_em = {}
-    type_to_f1 = {}
+    type_to_em = defaultdict(list)
+    type_to_f1 = defaultdict(list)
     for pid, annotation in annotations.items():
         for qa_pair in annotation["qa_pairs"]:
             query_id = qa_pair["query_id"]
@@ -119,7 +193,7 @@ def _run_evaluation(annotations, predicted_answers):
                 print("Missing prediction for question: {}".format(query_id))
                 predicted = None
             for answer in [qa_pair["answer"]] + qa_pair["validated_answers"]:
-                gold_answer, gold_type = to_string(answer)
+                gold_answer, gold_type = answer_json_to_string(answer)
                 em_score, f1_score = get_metrics(predicted, gold_answer)
                 if gold_answer[0].strip() != "":
                     max_em_score = max(max_em_score, em_score)
@@ -128,38 +202,42 @@ def _run_evaluation(annotations, predicted_answers):
                         max_type = gold_type
             exact_match.append(max_em_score)
             f1.append(max_f1_score)
-            if max_type not in type_to_em:
-                type_to_em[max_type] = []
             type_to_em[max_type].append(max_em_score)
-            if max_type not in type_to_f1:
-                type_to_f1[max_type] = []
             type_to_f1[max_type].append(max_f1_score)
 
     global_em = np.mean(exact_match)
     global_f1 = np.mean(f1)
-    print("Exact-match accuracy {0:.2f}".format(global_em*100))
-    print("F1 score {0:.2f}".format(global_f1*100))
-    print("{0:.2f}   &   {1:.2f}".format(global_em*100, global_f1*100))
+    print("Exact-match accuracy {0:.2f}".format(global_em * 100))
+    print("F1 score {0:.2f}".format(global_f1 * 100))
+    print("{0:.2f}   &   {1:.2f}".format(global_em * 100, global_f1 * 100))
     print("----")
     total = np.sum([len(v) for v in type_to_em.values()])
     for typ in sorted(type_to_em.keys()):
-        print("{0}: {1} ({2:.2f}%)".format(typ, len(type_to_em[typ]), 100.*len(type_to_em[typ])/total))
-        print("  Exact-match accuracy {0:.3f}".format(100.*np.mean(type_to_em[typ])))
-        print("  F1 score {0:.3f}".format(100.*np.mean(type_to_f1[typ])))
+        print("{0}: {1} ({2:.2f}%)".format(typ, len(type_to_em[typ]), 100. * len(type_to_em[typ])/total))
+        print("  Exact-match accuracy {0:.3f}".format(100. * np.mean(type_to_em[typ])))
+        print("  F1 score {0:.3f}".format(100. * np.mean(type_to_f1[typ])))
     return global_em, global_f1
 
 
-def run_evaluation(args):
-    predicted_answers = json.load(open(args.prediction_path, encoding='utf-8'))
-    annotations = json.load(open(args.gold_path, encoding='utf-8'))
-    return _run_evaluation(annotations, predicted_answers)
+def evaluate_prediction_file(prediction_path: str, gold_path: str) -> Tuple[float, float]:
+    """
+    Takes a prediction file and a gold file and evaluates the predictions for each question in the
+    gold file.  Both files must be json formatted and must have query_id keys, which are used to
+    match predictions to gold annotations.  The gold file is assumed to have the format of the dev
+    set in the DROP data release.  The prediction file must be a JSON dictionary keyed by query id,
+    where the value is either a JSON dictionary with an "answer" key, or just a string (or list of
+    strings) that is the answer.
+    """
+    predicted_answers = json.load(open(prediction_path, encoding='utf-8'))
+    annotations = json.load(open(gold_path, encoding='utf-8'))
+    return evalute_json(annotations, predicted_answers)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Evaluate on DROP dataset')
+    parser = argparse.ArgumentParser(description='evaluate on drop dataset')
     parser.add_argument("--gold_path", type=str, required=False, default="drop_dataset_test.gold.json",
                     help='location of the gold file')
-    parser.add_argument("--prediction_path", type=str, required=True,
+    parser.add_argument("--prediction_path", type=str, required=False, default="sample_predictions.json",
                     help='location of the prediction file')
     args = parser.parse_args()
-    run_evaluation(args)
+    evaluate_prediction_file(args.prediction_path, args.gold_path)
