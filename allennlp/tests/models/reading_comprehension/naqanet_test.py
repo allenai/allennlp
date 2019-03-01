48a923e8ffd051986ed31eb8850e46dbc4be4595
Matt Gardner <mattg@allenai.org>
2019-03-01 10:13:09 -0800

    Added NAQANet

---
+++ allennlp/tests/models/reading_comprehension/naqanet_test.py
@@ -0,0 +1,13 @@
+#pylint: disable=unused-import
+from allennlp.common.testing import ModelTestCase
+
+
+class NumericallyAugmentedQaNetTest(ModelTestCase):
+    def setUp(self):
+        super().setUp()
+        print(self.FIXTURES_ROOT)
+        self.set_up_model(self.FIXTURES_ROOT / "naqanet" / "experiment.json",
+                          self.FIXTURES_ROOT / "data" / "drop.json")
+
+    def test_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
