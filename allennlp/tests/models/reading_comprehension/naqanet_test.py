aa3325126b9b558e2229de03f9bc1cb6ef0b6f02
Matt Gardner <mattg@allenai.org>
2019-03-02 12:13:14 -0800

    Mark NAQANet test as flaky

--- allennlp/tests/models/reading_comprehension/naqanet_test.py
+++ allennlp/tests/models/reading_comprehension/naqanet_test.py
@@ -1,4 +1,6 @@
 #pylint: disable=unused-import
+from flaky import flaky
+
 from allennlp.common.testing import ModelTestCase
 
 
@@ -9,5 +11,6 @@ class NumericallyAugmentedQaNetTest(ModelTestCase):
         self.set_up_model(self.FIXTURES_ROOT / "naqanet" / "experiment.json",
                           self.FIXTURES_ROOT / "data" / "drop.json")
 
+    @flaky
     def test_model_can_train_save_and_load(self):
         self.ensure_model_can_train_save_and_load(self.param_file)
