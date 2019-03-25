617c3e2c68f18b28dbe21fbecf207a0be5cf49c0
Mark Neumann <markn@allenai.org>
2019-03-25 12:39:09 -0700

    val not dev

--- allennlp/tests/common/params_test.py
+++ allennlp/tests/common/params_test.py
@@ -207,7 +207,7 @@ class TestParams(AllenNlpTestCase):
             'PTB_TRAIN_PATH', 'PTB_DEV_PATH', 'PTB_TEST_PATH',
 
             # dependency parser
-            'PTB_DEPENDENCIES_TRAIN', 'PTB_DEPENDENCIES_DEV',
+            'PTB_DEPENDENCIES_TRAIN', 'PTB_DEPENDENCIES_VAL',
 
             # srl_elmo_5.5B
             'SRL_TRAIN_DATA_PATH', 'SRL_VALIDATION_DATA_PATH',
