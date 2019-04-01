5ca2297a1fc60ef80c9b3cbc41307e163071f3f2
mike <mross@evidscience.com>
2019-04-01 14:29:09 -0700

    temporary change: non-strict loading of model

--- allennlp/models/model.py
+++ allennlp/models/model.py
@@ -272,8 +272,8 @@ class Model(torch.nn.Module, Registrable):
         # If vocab and model embeddings are in sync, following would be just a no-op.
         model.extend_embedder_vocab()
 
-        model_state = torch.load(weights_file, map_location=util.device_mapping(cuda_device), strict=False)
-        model.load_state_dict(model_state)
+        model_state = torch.load(weights_file, map_location=util.device_mapping(cuda_device))
+        model.load_state_dict(model_state, strict=False)
 
         # Force model to cpu or gpu, as appropriate, to make sure that the embeddings are
         # in sync with the weights
