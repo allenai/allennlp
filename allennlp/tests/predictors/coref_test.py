deae1db203efa9400b1d8b479b4faca3876a907a
Franklin Bradfield <fbradfield1@gmail.com>
2019-04-20 10:13:21 -0400

    Add coref_resolved method to CorefPredictor (#2296)
    * Add coref_resolved method to CorefPredictor
    
      * Resolves coreferences by producing a document that has had
        its coreferences substituted with their main mentions
    
      * Ex:
          Personal
          ========
          "Charlie wants to buy a game, so he can play it with friends."
            -->
          "Charlie wants to buy a game, so Charlie can play a game
           with friends."
    
          Possessive
          ==========
          "Stocks also got a boost after China took steps to encourage
           bank lending and stimulate the country's flagging economy."
           -->
          "Stocks also got a boost after China took steps to encourage
           bank lending and stimulate China's flagging economy."
    
    * Remove redudant Doc import

--- allennlp/tests/predictors/coref_test.py
+++ allennlp/tests/predictors/coref_test.py
@@ -1,8 +1,11 @@
 # pylint: disable=no-self-use,invalid-name
+import spacy
+
 from allennlp.common.testing import AllenNlpTestCase
 
 from allennlp.models.archival import load_archive
 from allennlp.predictors import Predictor
+from allennlp.predictors import CorefPredictor
 
 
 class TestCorefPredictor(AllenNlpTestCase):
@@ -39,3 +42,54 @@ class TestCorefPredictor(AllenNlpTestCase):
                 # Spans should be inside document.
                 assert 0 < mention[0] <= len(document)
                 assert 0 < mention[1] <= len(document)
+
+    def test_coref_resolved(self):
+
+        """Tests I/O of coref_resolved method"""
+
+        document = "This is a test sentence."
+        archive = load_archive(self.FIXTURES_ROOT / 'coref' / 'serialization' / 'model.tar.gz')
+        predictor = Predictor.from_archive(archive, 'coreference-resolution')
+        result = predictor.coref_resolved(document)
+        assert isinstance(result, str)
+
+    def test_replace_corefs(self):
+
+        """Tests core coref replacement logic"""
+
+        nlp = spacy.load("en_core_web_sm")
+
+        inputs = [
+                "This is a sentence with no coreferences.", # No coreferences
+                "Julie wants to buy fruit. That is what she loves.", # Single coreference / personal
+                "Charlie wants to buy a game, so he can play it with friends.", # Multiple coreferences / personal
+                "The woman reading a newspaper sat on the bench with her dog.", # Phrasal mention / possessive
+                "Canada stimulated the country's economy." # Phrasal coreference / possessive
+                ]
+
+        expected_clusters = [
+                [],
+                [[[0, 0], [9, 9]]],
+                [[[0, 0], [8, 8]], [[4, 5], [11, 11]]],
+                [[[0, 4], [10, 10]]],
+                [[[0, 0], [2, 4]]]
+                ]
+
+        expected_outputs = [
+                "This is a sentence with no coreferences.",
+                "Julie wants to buy fruit. That is what Julie loves.",
+                "Charlie wants to buy a game, so Charlie can play a game with friends.",
+                "The woman reading a newspaper sat on the bench with The woman reading a newspaper's dog.",
+                "Canada stimulated Canada's economy."
+                ]
+
+        for i, text in enumerate(inputs):
+            clusters = expected_clusters[i]
+
+            if not clusters:
+                assert text == inputs[i]
+                continue
+
+            doc = nlp(text)
+            output = CorefPredictor.replace_corefs(doc, clusters)
+            assert output == expected_outputs[i]
