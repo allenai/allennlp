69444a193ec6946b9a7dce89887eb12fb4587237
Matthew Peters <matthewp@allenai.org>
2019-04-22 15:43:43 -0700

    grad accumulation with fp16

--- allennlp/training/trainer.py
+++ allennlp/training/trainer.py
@@ -371,7 +371,7 @@ class Trainer(TrainerBase):
                     raise ValueError("nan loss encountered")
 
                 if self.fp16:
-                    self.optimizer.backward(loss)
+                    self.optimizer.backward(loss, update_master_grads=False)
                 else:
                     loss.backward()
 
@@ -404,6 +404,8 @@ class Trainer(TrainerBase):
                     self._tensorboard.add_train_scalar("gradient_update/" + name,
                                                        update_norm / (param_norm + 1e-7))
             else:
+                if self.fp16:
+                    self.optimizer.update_master_grads()
                 self.optimizer.step()
 
             # Update moving averages
