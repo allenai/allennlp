12cee9222cd9df4d284a21d9e4d778acc24cdaa0
Mark Neumann <markn@allenai.org>
2019-04-24 14:08:43 -0700

    lowercase stopword set in word filter (#2749)
    * lowercase stopword set in word filter
    
    * indentation

--- allennlp/data/tokenizers/word_filter.py
+++ allennlp/data/tokenizers/word_filter.py
@@ -61,6 +61,7 @@ class StopwordFilter(WordFilter):
     """
     A ``StopwordFilter`` uses a list of stopwords to filter.
     If no file is specified, a default list of stopwords is used.
+    Words and stopwords are lowercased for comparison.
 
     Parameters
     ----------
@@ -74,33 +75,34 @@ class StopwordFilter(WordFilter):
                  tokens_to_add: List[str] = None) -> None:
         self._tokens_to_add = tokens_to_add or []
         if stopword_file is not None:
-            self.stopwords = read_set_from_file(stopword_file)
+            self.stopwords = {token.lower() for token in read_set_from_file(stopword_file)}
         else:
-            self.stopwords = set(['I', 'a', 'aboard', 'about', 'above', 'accordance', 'according',
-                                  'across', 'after', 'against', 'along', 'alongside', 'also', 'am',
-                                  'amid', 'amidst', 'an', 'and', 'apart', 'are', 'around', 'as',
-                                  'aside', 'astride', 'at', 'atop', 'back', 'be', 'because', 'before',
-                                  'behind', 'below', 'beneath', 'beside', 'besides', 'between',
-                                  'beyond', 'but', 'by', 'concerning', 'do', 'down', 'due', 'during',
-                                  'either', 'except', 'exclusive', 'false', 'for', 'from', 'happen',
-                                  'he', 'her', 'hers', 'herself', 'him', 'himself', 'his', 'how',
-                                  'how many', 'how much', 'i', 'if', 'in', 'including', 'inside',
-                                  'instead', 'into', 'irrespective', 'is', 'it', 'its', 'itself',
-                                  'less', 'me', 'mine', 'minus', 'my', 'myself', 'neither', 'next',
-                                  'not', 'occur', 'of', 'off', 'on', 'onto', 'opposite', 'or', 'our',
-                                  'ours', 'ourselves', 'out', 'out of', 'outside', 'over', 'owing',
-                                  'per', 'prepatory', 'previous', 'prior', 'pursuant', 'regarding',
-                                  's', 'sans', 'she', 'subsequent', 'such', 'than', 'thanks', 'that',
-                                  'the', 'their', 'theirs', 'them', 'themselves', 'then', 'these',
-                                  'they', 'this', 'those', 'through', 'throughout', 'thru', 'till',
-                                  'to', 'together', 'top', 'toward', 'towards', 'true', 'under',
-                                  'underneath', 'unlike', 'until', 'up', 'upon', 'us', 'using',
-                                  'versus', 'via', 'was', 'we', 'were', 'what', 'when', 'where',
-                                  'which', 'who', 'why', 'will', 'with', 'within', 'without', 'you',
-                                  'your', 'yours', 'yourself', 'yourselves', ",", '.', ':', '!', ';',
-                                  "'", '"', '&', '$', '#', '@', '(', ')', '?'])
+            self.stopwords = set([token.lower() for token in
+                                  ['I', 'a', 'aboard', 'about', 'above', 'accordance', 'according',
+                                   'across', 'after', 'against', 'along', 'alongside', 'also', 'am',
+                                   'amid', 'amidst', 'an', 'and', 'apart', 'are', 'around', 'as',
+                                   'aside', 'astride', 'at', 'atop', 'back', 'be', 'because', 'before',
+                                   'behind', 'below', 'beneath', 'beside', 'besides', 'between',
+                                   'beyond', 'but', 'by', 'concerning', 'do', 'down', 'due', 'during',
+                                   'either', 'except', 'exclusive', 'false', 'for', 'from', 'happen',
+                                   'he', 'her', 'hers', 'herself', 'him', 'himself', 'his', 'how',
+                                   'how many', 'how much', 'i', 'if', 'in', 'including', 'inside',
+                                   'instead', 'into', 'irrespective', 'is', 'it', 'its', 'itself',
+                                   'less', 'me', 'mine', 'minus', 'my', 'myself', 'neither', 'next',
+                                   'not', 'occur', 'of', 'off', 'on', 'onto', 'opposite', 'or', 'our',
+                                   'ours', 'ourselves', 'out', 'out of', 'outside', 'over', 'owing',
+                                   'per', 'prepatory', 'previous', 'prior', 'pursuant', 'regarding',
+                                   's', 'sans', 'she', 'subsequent', 'such', 'than', 'thanks', 'that',
+                                   'the', 'their', 'theirs', 'them', 'themselves', 'then', 'these',
+                                   'they', 'this', 'those', 'through', 'throughout', 'thru', 'till',
+                                   'to', 'together', 'top', 'toward', 'towards', 'true', 'under',
+                                   'underneath', 'unlike', 'until', 'up', 'upon', 'us', 'using',
+                                   'versus', 'via', 'was', 'we', 'were', 'what', 'when', 'where',
+                                   'which', 'who', 'why', 'will', 'with', 'within', 'without', 'you',
+                                   'your', 'yours', 'yourself', 'yourselves', ",", '.', ':', '!', ';',
+                                   "'", '"', '&', '$', '#', '@', '(', ')', '?']])
         for token in self._tokens_to_add:
-            self.stopwords.add(token)
+            self.stopwords.add(token.lower())
 
     @overrides
     def filter_words(self, words: List[Token]) -> List[Token]:
