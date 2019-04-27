57f56731e600fb89b473ea2c97729ac23153b30e
ryan <ryanliu1991@gmail.com>
2019-04-27 15:07:45 +0800

    fix batched_index_select

--- allennlp/nn/util.py
+++ allennlp/nn/util.py
@@ -983,8 +983,8 @@ def flatten_and_batch_shift_indices(indices: torch.Tensor,
     offset_indices : ``torch.LongTensor``
     """
     # Shape: (batch_size)
-    if torch.max(indices) >= sequence_length:
-        raise ConfigurationError("Any elem in indices should not exceed sequence length.")
+    if torch.max(indices) >= sequence_length or torch.min(indices) < 0:
+        raise ConfigurationError("Any elem in indices should be in a range of [0, " + str(sequence_length-1) + "].")
     offsets = get_range_vector(indices.size(0), get_device_of(indices)) * sequence_length
     for _ in range(len(indices.size()) - 1):
         offsets = offsets.unsqueeze(1)
