06f67c212304e0adab7dede68b090c59f7a11f45
Brendan Roof <brendanr@allenai.org>
2019-05-01 03:00:16 +0000

    fixes

--- allennlp/tests/models/language_model_test.py
+++ allennlp/tests/models/language_model_test.py
@@ -77,8 +77,8 @@ class TestUnidirectionalLanguageModelTransformer(TestUnidirectionalLanguageModel
         # they are not used.
         self.ensure_model_can_train_save_and_load(
                 self.param_file, gradients_to_ignore={
-                        "_contextualizer.feedforward_layer_norm_0.gamma",
-                        "_contextualizer.feedforward_layer_norm_0.beta"})
+                        "_contextualizer._feed_forward_layer_norm_layers.0.gamma",
+                        "_contextualizer._feed_forward_layer_norm_layers.0.beta"})
 
 class TestBidirectionalLanguageModel(TestUnidirectionalLanguageModel):
     def setUp(self):
