f8576cfaba4e5fef372fe768f4a9285be363f5a2
ryan <ryanliu1991@gmail.com>
2019-05-01 11:00:30 +0800

    add unit test

--- allennlp/nn/util.py
+++ allennlp/nn/util.py
@@ -984,7 +984,7 @@ def flatten_and_batch_shift_indices(indices: torch.Tensor,
     """
     # Shape: (batch_size)
     if torch.max(indices) >= sequence_length or torch.min(indices) < 0:
-        raise ConfigurationError("Any elem in indices should be in a range of [0, " + str(sequence_length-1) + "].")
+        raise ConfigurationError(f"All elements in indices should be in range (0, {sequence_length - 1})")
     offsets = get_range_vector(indices.size(0), get_device_of(indices)) * sequence_length
     for _ in range(len(indices.size()) - 1):
         offsets = offsets.unsqueeze(1)
