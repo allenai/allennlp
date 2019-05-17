5993275418d0edc10ef28f1ff0da9b6cfcadb007
Gabi Stanovsky <gabis@julianm.local>
2019-05-17 16:29:22 -0700

    removing dev null

--- allennlp/tools/drop_eval.py
+++ allennlp/tools/drop_eval.py
@@ -217,7 +217,7 @@ def evaluate_json(annotations: Dict[str, Any], predicted_answers: Dict[str, Any]
     return global_em, global_f1
 
 
-def evaluate_prediction_file(prediction_path: str, gold_path: str, output_path: Optional[str]) -> Tuple[float, float]:
+def evaluate_prediction_file(prediction_path: str, gold_path: str, output_path: Optional[str] = None) -> Tuple[float, float]:
     """
     Takes a prediction file and a gold file and evaluates the predictions for each question in the
     gold file.  Both files must be json formatted and must have query_id keys, which are used to
@@ -225,17 +225,17 @@ def evaluate_prediction_file(prediction_path: str, gold_path: str, output_path: 
     set in the DROP data release.  The prediction file must be a JSON dictionary keyed by query id,
     where the value is either a JSON dictionary with an "answer" key, or just a string (or list of
     strings) that is the answer. Writes a json with global_em and global_f1 metrics to file at
-    the specified output path.
+    the specified output path, unless None is passed as output path.
     """
     predicted_answers = json.load(open(prediction_path, encoding='utf-8'))
     annotations = json.load(open(gold_path, encoding='utf-8'))
     global_em, global_f1 = evaluate_json(annotations, predicted_answers)
 
-    # Output predictions to file
-    output_dict = {"global_em": global_em,
-                   "global_f1": global_f1}
-
+    # Output predictions to file if an output path is given
     if output_path is not None:
+        output_dict = {"global_em": global_em,
+                       "global_f1": global_f1}
+
         with open(output_path, "w", encoding="utf8") as outfile:
             json.dump(output_dict, outfile)
 
