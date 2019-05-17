c0acac9878456b16e7d879628e04acdfdc4c9c0f
Gabi Stanovsky <gabis@julianm.local>
2019-05-17 16:11:41 -0700

    pylint

--- allennlp/tools/drop_eval.py
+++ allennlp/tools/drop_eval.py
@@ -235,7 +235,7 @@ def evaluate_prediction_file(prediction_path: str, gold_path: str, output_path: 
     output_dict = {"global_em": global_em,
                    "global_f1": global_f1}
 
-    with open(output_path, "w", encoding = "utf8") as outfile:
+    with open(output_path, "w", encoding="utf8") as outfile:
         json.dump(output_dict, outfile)
 
     return (global_em, global_f1)
