93e86a961c31467885905280b7f9b2a444e371b4
Gabriel Stanovsky <gabriel.satanovsky@gmail.com>
2019-05-19 18:21:16 -0700

    Add output path to drop eval (#2860)
    * adding output path to drop eval
    
    * checking codecov issue
    
    * checking codecov issue
    
    * checking codecov issue
    
    * pylint
    
    * removing dev null
    
    * removing dev null
    
    * pylint

--- allennlp/tools/drop_eval.py
+++ allennlp/tools/drop_eval.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from collections import defaultdict
-from typing import Any, Dict, List, Set, Tuple, Union
+from typing import Any, Dict, List, Set, Tuple, Union, Optional
 import json
 import argparse
 import string
@@ -216,18 +216,30 @@ def evaluate_json(annotations: Dict[str, Any], predicted_answers: Dict[str, Any]
     return global_em, global_f1
 
 
-def evaluate_prediction_file(prediction_path: str, gold_path: str) -> Tuple[float, float]:
+def evaluate_prediction_file(prediction_path: str, gold_path: str,
+                             output_path: Optional[str] = None) -> Tuple[float, float]:
     """
     Takes a prediction file and a gold file and evaluates the predictions for each question in the
     gold file.  Both files must be json formatted and must have query_id keys, which are used to
     match predictions to gold annotations.  The gold file is assumed to have the format of the dev
     set in the DROP data release.  The prediction file must be a JSON dictionary keyed by query id,
     where the value is either a JSON dictionary with an "answer" key, or just a string (or list of
-    strings) that is the answer.
+    strings) that is the answer. Writes a json with global_em and global_f1 metrics to file at
+    the specified output path, unless None is passed as output path.
     """
     predicted_answers = json.load(open(prediction_path, encoding='utf-8'))
     annotations = json.load(open(gold_path, encoding='utf-8'))
-    return evaluate_json(annotations, predicted_answers)
+    global_em, global_f1 = evaluate_json(annotations, predicted_answers)
+
+    # Output predictions to file if an output path is given
+    if output_path is not None:
+        output_dict = {"global_em": global_em,
+                       "global_f1": global_f1}
+
+        with open(output_path, "w", encoding="utf8") as outfile:
+            json.dump(output_dict, outfile)
+
+    return (global_em, global_f1)
 
 
 if __name__ == "__main__":
@@ -243,5 +255,11 @@ if __name__ == "__main__":
                         required=False,
                         default="sample_predictions.json",
                         help='location of the prediction file')
+    parser.add_argument("--output_path",
+                        type=str,
+                        required=False,
+                        default=None,
+                        help='location of the output metrics file')
+
     args = parser.parse_args()
-    evaluate_prediction_file(args.prediction_path, args.gold_path)
+    evaluate_prediction_file(args.prediction_path, args.gold_path, args.output_path)
