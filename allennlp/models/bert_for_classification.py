c3786bfe98882312e4c961341182c1fc1487b6c5
sbhaktha <sumithrab@allenai.org>
2019-06-06 16:27:14 -0700

    Changes per PR comments

--- allennlp/models/bert_for_classification.py
+++ allennlp/models/bert_for_classification.py
@@ -68,7 +68,7 @@ class BertForClassification(Model):
         if num_labels:
             out_features = num_labels
         else:
-            out_features = vocab.get_vocab_size(namespace=self._label_namespace)
+            out_features = vocab.get_vocab_size(namespace=label_namespace)
 
         self._dropout = torch.nn.Dropout(p=dropout)
 
@@ -141,8 +141,8 @@ class BertForClassification(Model):
         classes = []
         for prediction in predictions_list:
             label_idx = prediction.argmax(dim=-1).item()
-            label_str = \
-                self.vocab.get_index_to_token_vocabulary(self._label_namespace).get(label_idx, str(label_idx))
+            label_str = (self.vocab.get_index_to_token_vocabulary(self._label_namespace)
+                .get(label_idx, str(label_idx)))
             classes.append(label_str)
         output_dict["label"] = classes
         return output_dict
