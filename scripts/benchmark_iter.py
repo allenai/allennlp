20064ee4fbf3c5051e9a587abb77a57ca8aeb5be
Brendan Roof <brendanr@allenai.org>
2019-06-05 19:21:37 -0700

    pass loop

--- scripts/benchmark_iter.py
+++ scripts/benchmark_iter.py
@@ -2,12 +2,8 @@
 import argparse
 import logging
 
-from allennlp.common import Params
+from allennlp.common import Params, Tqdm
 from allennlp.training.trainer import TrainerPieces
-
-#Needed?
-#sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
-
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
@@ -19,3 +15,11 @@ if __name__ == "__main__":
 
     params = Params.from_file(args.config)
     pieces = TrainerPieces.from_params(params, args.serialization_dir)
+
+    # Get tqdm for the training batches
+    raw_generator = pieces.iterator(pieces.train_dataset,
+                                      num_epochs=1,
+                                      shuffle=True)
+    generator_tqdm = Tqdm.tqdm(raw_generator)
+    for batch in generator_tqdm:
+        pass
