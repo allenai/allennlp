77511de77a2bd2b10a02640e20d76e34fe73d520
alontalmor <salont@gmail.com>
2019-06-21 11:02:16 +0300

    binary class classifier

---
+++ allennlp/models/bert_binary_class.py
@@ -0,0 +1,126 @@
+from typing import Dict, Optional, List, Any
+
+import logging
+from overrides import overrides
+from pytorch_pretrained_bert.modeling import BertModel, gelu
+import re
+import torch
+from torch.nn.modules.linear import Linear
+from allennlp.modules import TextFieldEmbedder
+from allennlp.data import Vocabulary
+from allennlp.models.archival import load_archive
+from allennlp.models.model import Model
+from allennlp.modules.scalar_mix import ScalarMix
+from allennlp.nn import RegularizerApplicator
+import allennlp.nn.util as util
+from allennlp.training.metrics import BooleanAccuracy, CategoricalAccuracy
+
+
+@Model.register("bert_binary_class")
+class BertBinClass(Model):
+    """
+    """
+    def __init__(self,
+                 vocab: Vocabulary,
+                 pretrained_model: str = None,
+                 requires_grad: bool = True,
+                 top_layer_only: bool = True,
+                 bert_weights_model: str = None,
+                 per_choice_loss: bool = False,
+                 layer_freeze_regexes: List[str] = None,
+                 regularizer: Optional[RegularizerApplicator] = None) -> None:
+        super().__init__(vocab, regularizer)
+
+        if bert_weights_model:
+            logging.info(f"Loading BERT weights model from {bert_weights_model}")
+            bert_model_loaded = load_archive(bert_weights_model)
+            self._bert_model = bert_model_loaded.model._bert_model
+        else:
+            self._bert_model = BertModel.from_pretrained(pretrained_model)
+
+        for param in self._bert_model.parameters():
+            param.requires_grad = requires_grad
+        #for name, param in self._bert_model.named_parameters():
+        #    grad = requires_grad
+        #    if layer_freeze_regexes and grad:
+        #        grad = not any([bool(re.search(r, name)) for r in layer_freeze_regexes])
+        #    param.requires_grad = grad
+
+        bert_config = self._bert_model.config
+        self._output_dim = bert_config.hidden_size
+        self._dropout = torch.nn.Dropout(bert_config.hidden_dropout_prob)
+        self._per_choice_loss = per_choice_loss
+        # TODO: Check dimensions before replacing
+        if bert_weights_model and hasattr(bert_model_loaded.model, "_classifier"):
+            self._classifier = bert_model_loaded.model._classifier
+        else:
+            final_output_dim = 1
+            self._classifier = Linear(self._output_dim, final_output_dim)
+            self._classifier.apply(self._bert_model.init_bert_weights)
+        self._all_layers = not top_layer_only
+        if self._all_layers:
+            if bert_weights_model and hasattr(bert_model_loaded.model, "_scalar_mix") \
+                    and bert_model_loaded.model._scalar_mix is not None:
+                self._scalar_mix = bert_model_loaded.model._scalar_mix
+            else:
+                num_layers = bert_config.num_hidden_layers
+                initial_scalar_parameters = num_layers * [0.0]
+                initial_scalar_parameters[-1] = 5.0  # Starts with most mass on last layer
+                self._scalar_mix = ScalarMix(bert_config.num_hidden_layers,
+                                             initial_scalar_parameters=initial_scalar_parameters,
+                                             do_layer_norm=False)
+        else:
+            self._scalar_mix = None
+
+
+        self._accuracy = BooleanAccuracy()
+        self._loss = torch.nn.BCEWithLogitsLoss()
+
+        self._debug = -1
+
+
+    def forward(self,
+                    question: Dict[str, torch.LongTensor],
+                    segment_ids: torch.LongTensor = None,
+                    label: torch.LongTensor = None,
+                    metadata: List[Dict[str, Any]] = None) -> torch.Tensor:
+
+        self._debug -= 1
+        input_ids = question['bert']
+        batch_size, num_choices, _  = question['bert'].size()
+        question_mask = (input_ids != 0).long()
+        token_type_ids = torch.zeros_like(input_ids)
+
+        encoded_layers, pooled_output = self._bert_model(input_ids=util.combine_initial_dims(input_ids),
+                                            token_type_ids=util.combine_initial_dims(token_type_ids),
+                                            attention_mask=util.combine_initial_dims(question_mask),
+                                            output_all_encoded_layers=self._all_layers)
+
+        if self._all_layers:
+            mixed_layer = self._scalar_mix(encoded_layers, question_mask)
+            pooled_output = self._bert_model.pooler(mixed_layer)
+
+        pooled_output = self._dropout(pooled_output)
+        label_logits = self._classifier(pooled_output)
+        label_logits_flat = label_logits.squeeze(1)
+        label_logits = label_logits.view(-1, num_choices)
+
+        output_dict = {}
+        output_dict['label_logits'] = label_logits
+        output_dict['label_probs'] = torch.sigmoid(label_logits_flat)
+        output_dict['answer_index'] = label_logits_flat > 0
+
+
+        if label is not None:
+            loss = self._loss(label_logits, label.view(-1,1).float())
+            self._accuracy(label_logits > 0 , label.view(-1,1).byte())
+            output_dict["loss"] = loss
+
+        return output_dict
+
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {
+            'EM': self._accuracy.get_metric(reset),
+        }
+
