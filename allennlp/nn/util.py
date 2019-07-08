Fix some problem with decimal precision problems

allennlp/nn/util.py
@@ -684,6 +684,8 @@ def sequence_cross_entropy_with_logits(logits: torch.FloatTensor,
         raise ValueError("Got average f{average}, expected one of "
                          "None, 'token', or 'batch'")
 
+    # make sure weights are float
+    weights = weights.float()
     # shape : (batch * sequence_length, num_classes)
     logits_flat = logits.view(-1, logits.size(-1))
     # shape : (batch * sequence_length, num_classes)
@@ -703,19 +705,19 @@ def sequence_cross_entropy_with_logits(logits: torch.FloatTensor,
         focal_factor = 1.
     if alpha is not None:
         # shape : () / (num_classes,)
-        if isinstance(alpha, float):
+        if isinstance(alpha, (float, int)):
             # pylint: disable=not-callable
             # shape : (2,)
-            alpha_factor = torch.tensor([1. - alpha, alpha], device=targets_flat.device)
+            alpha_factor = torch.tensor([1. - float(alpha), float(alpha)], dtype=weights.dtype, device=weights.device)
             # pylint: enable=not-callable
-        elif isinstance(alpha, list):
+        elif isinstance(alpha, (list, numpy.ndarray)):
             # pylint: disable=not-callable
             # shape : (c,)
-            alpha_factor = torch.tensor(alpha, device=targets_flat.device)
+            alpha_factor = torch.tensor(alpha, dtype=weights.dtype, device=weights.device)
             # pylint: enable=not-callable
         elif isinstance(alpha, torch.Tensor):
             # shape : () / (num_classes,)
-            alpha = alpha.clone().detach().to(device=targets_flat.device)
+            alpha = alpha.clone(dtype=weights.dtype).detach().to(device=weights.device)
             if alpha.size():
                 # shape : (num_classes,)
                 alpha_factor = alpha
@@ -727,7 +729,7 @@ def sequence_cross_entropy_with_logits(logits: torch.FloatTensor,
         else:
             raise TypeError(('alpha must be float, list of float, or torch.FloatTensor, '
                              '{} provided.').format(type(alpha)))
-        # shape : (batch * max_len,)
+        # shape : (batch, max_len)
         alpha_factor = torch.gather(alpha_factor, dim=0, index=targets_flat.view(-1)).view(*targets.size())
         weights = weights * alpha_factor
     else:
@@ -752,18 +754,18 @@ def sequence_cross_entropy_with_logits(logits: torch.FloatTensor,
     # shape : (batch, sequence_length)
     negative_log_likelihood = negative_log_likelihood_flat.view(*targets.size())
     # shape : (batch, sequence_length)
-    negative_log_likelihood = negative_log_likelihood * weights.float()
+    negative_log_likelihood = negative_log_likelihood * weights
 
     if average == "batch":
         # shape : (batch_size,)
-        per_batch_loss = negative_log_likelihood.sum(1) / (weights.sum(1).float() + 1e-13)
+        per_batch_loss = negative_log_likelihood.sum(1) / (weights.sum(1) + 1e-13)
         num_non_empty_sequences = ((weights.sum(1) > 0).float().sum() + 1e-13)
         return per_batch_loss.sum() / num_non_empty_sequences
     elif average == "token":
         return negative_log_likelihood.sum() / (weights.sum().float() + 1e-13)
     else:
         # shape : (batch_size,)
-        per_batch_loss = negative_log_likelihood.sum(1) / (weights.sum(1).float() + 1e-13)
+        per_batch_loss = negative_log_likelihood.sum(1) / (weights.sum(1) + 1e-13)
         return per_batch_loss
 
 