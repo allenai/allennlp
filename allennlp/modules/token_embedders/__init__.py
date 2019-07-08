f3b13d0a9e977511b3ccd03d86225cd163d6f53c
Mark Neumann <markng@allenai.org>
2019-07-08 15:06:58 +0200

    pass through token embedder

--- allennlp/modules/token_embedders/__init__.py
+++ allennlp/modules/token_embedders/__init__.py
@@ -15,3 +15,4 @@ from allennlp.modules.token_embedders.bidirectional_language_model_token_embedde
 from allennlp.modules.token_embedders.language_model_token_embedder import \
         LanguageModelTokenEmbedder
 from allennlp.modules.token_embedders.bag_of_word_counts_token_embedder import BagOfWordCountsTokenEmbedder
+from allennlp.modules.token_embedders.pass_through_token_embedder import PassThroughTokenEmbedder
---
+++ allennlp/modules/token_embedders/pass_through_token_embedder.py
@@ -0,0 +1,25 @@
+import torch
+from allennlp.modules.token_embedders.token_embedder import TokenEmbedder
+
+
+@TokenEmbedder.register("pass_through")
+class PassThroughTokenEmbedder(TokenEmbedder):
+    """
+    Assumes that the input is already vectorized in some way,
+    and just returns it.
+
+    Parameters
+    ----------
+    hidden_dim : `int`, required.
+
+    """
+    def __init__(self, hidden_dim: int) -> None:
+        self.hidden_dim = hidden_dim
+        super().__init__()
+
+    def get_output_dim(self):
+        return self.hidden_dim
+
+    def forward(self,  # pylint: disable=arguments-differ
+                inputs: torch.Tensor) -> torch.Tensor:
+        return inputs
