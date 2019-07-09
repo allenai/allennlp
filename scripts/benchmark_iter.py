fb5bc922b9795715a52521530b2dfd08ac67bd72
Brendan Roof <brendanr@allenai.org>
2019-07-09 14:28:47 -0700

    fix

--- scripts/benchmark_iter.py
+++ scripts/benchmark_iter.py
@@ -9,6 +9,26 @@ from allennlp.training.trainer_pieces import TrainerPieces
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 BATCH_INTERVAL = 100
+# On subset of 1b word corpus
+MEAN_BATCH_SIZE = 66.0
+
+
+def time_iterable(iterable, get_size):
+    start = time.perf_counter()
+    last = start
+    batch_count = 0
+    cumulative_batch_size = 0
+    for batch in iterable:
+        batch_count += 1
+        cumulative_batch_size += get_size(batch)
+
+        if int(batch_count) % BATCH_INTERVAL == 0:
+            end = time.perf_counter()
+            print(f"b/s total: {(end - start)/batch_count} b/s last: {(end - last)/BATCH_INTERVAL} mean batch size: {cumulative_batch_size/batch_count}")
+            last = end
+
+        pass
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -24,18 +44,4 @@ if __name__ == "__main__":
                                       num_epochs=1,
                                       shuffle=True)
     generator_tqdm = Tqdm.tqdm(raw_generator)
-
-    start = time.perf_counter()
-    last = start
-    batch_count = 0
-    cumulative_batch_size = 0
-    for batch in generator_tqdm:
-        batch_count += 1
-        cumulative_batch_size += batch['source']['tokens'].size(0)
-
-        if batch_count % BATCH_INTERVAL == 0:
-            end = time.perf_counter()
-            print(f"b/s total: {(end - start)/batch_count} b/s last: {(end - last)/BATCH_INTERVAL} mean batch size: {cumulative_batch_size/batch_count}")
-            last = end
-
-        pass
+    time_iterable(generator_tqdm, lambda batch: batch['source']['tokens'].size(0))
