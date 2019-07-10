2dd3b514492a1a22b7e0bfe72b4011275f649ad0
Brendan Roof <brendanr@allenai.org>
2019-07-09 19:18:37 -0700

    fix

--- scripts/benchmark_iter.py
+++ scripts/benchmark_iter.py
@@ -13,28 +13,24 @@ BATCH_INTERVAL = 100
 MEAN_BATCH_SIZE = 66.0
 
 
-def time_iterable(iterable, get_items_per_batch, batches_per_interval):
+def time_iterable(iterable, get_items_per_batch):
     start = time.perf_counter()
     last = start
 
-    batch_count = 0
     item_count = 0
     for batch in iterable:
-        batch_count += 1
         item_count += get_items_per_batch(batch)
+        adjusted_batch_count = item_count / MEAN_BATCH_SIZE
 
-        if batch_count % batches_per_interval == 0:
+        if 0 <= adjusted_batch_count % BATCH_INTERVAL < 1:
             end = time.perf_counter()
-            interval_count = batch_count / batches_per_interval
-            approximate_batch_count = item_count / MEAN_BATCH_SIZE
 
-            msg = (f"s/b total: {(end - start) / approximate_batch_count} " +
-                   f"s/b last: {(end - last) / batches_per_interval} "
-                   f"mean interval size: {item_count / interval_count}")
+            msg = (f"s/b total: {(end - start) / adjusted_batch_count} " +
+                   f"s/b last: {(end - last) / BATCH_INTERVAL} "
+                   f"adjusted batch count: {adjusted_batch_count}")
             print(msg)
-            last = end
 
-        pass
+            last = end
 
 
 if __name__ == "__main__":
@@ -47,12 +43,12 @@ if __name__ == "__main__":
     pieces = TrainerPieces.from_params(params, args.serialization_dir)
 
     # Time just the reader.
-    #time_iterable(pieces.train_dataset, lambda batch: 1, BATCH_INTERVAL * MEAN_BATCH_SIZE)
+    #time_iterable(pieces.train_dataset, lambda batch: 1)
 
     # Get tqdm for the training batches
     raw_generator = pieces.iterator(pieces.train_dataset,
                                       num_epochs=1,
                                       shuffle=True)
     generator_tqdm = Tqdm.tqdm(raw_generator)
-    time_iterable(generator_tqdm, lambda batch: batch['source']['tokens'].size(0), BATCH_INTERVAL)
+    time_iterable(generator_tqdm, lambda batch: batch['source']['tokens'].size(0))
 
