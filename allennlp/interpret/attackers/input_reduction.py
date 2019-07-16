fix smooth gradient

allennlp/interpret/attackers/input_reduction.py
@@ -41,7 +41,7 @@ class InputReduction(Attacker):
 
             # Set num_ignore_tokens for NER and build token mask
             else:
-                num_ignore_tokens, tag_mask, original_tags = get_ner_tags_and_mask(current_instance,
+                num_ignore_tokens, tag_mask, original_tags = _get_ner_tags_and_mask(current_instance,
                                                                                    input_field_to_attack,
                                                                                    ignore_tokens)
 
@@ -66,7 +66,6 @@ class InputReduction(Attacker):
                 # special case for sentence tagging (we have tested NER)
                 else:
                     if smallest_idx != -1:
-                        print("delete tag mask)")
                         del tag_mask[smallest_idx]
                     cur_tags = [outputs["tags"][x] for x in range(len(outputs["tags"])) if tag_mask[x]]
                     if cur_tags != original_tags:
@@ -74,76 +73,73 @@ class InputReduction(Attacker):
 
                 # remove a token from the input
                 current_tokens = deepcopy(current_text_field.tokens)
-                print(current_instance)
-                current_instance, smallest_idx = \
-                    remove_one_token(current_instance,
-                                     input_field_to_attack,
-                                     grads[grad_input_field],
-                                     ignore_tokens)
-                print(current_instance)
+                current_instance, smallest_idx = _remove_one_token(current_instance,
+                                                                  input_field_to_attack,
+                                                                  grads[grad_input_field],
+                                                                  ignore_tokens)
 
             final_tokens.append(current_tokens)
         return sanitize({"final": final_tokens, "original": original_tokens})
 
-def remove_one_token(_instance: Instance,
-                     _input_field_to_attack: str,
-                     _grads: np.ndarray,
-                     _ignore_tokens: List[str]) -> Tuple[Instance, int]:
+def _remove_one_token(instance: Instance,
+                     input_field_to_attack: str,
+                     grads: np.ndarray,
+                     ignore_tokens: List[str]) -> Tuple[Instance, int]:
     """
     Finds the token with the smallest gradient and removes it.
     """
     # Compute L2 norm of all grads.
-    grads_mag = [np.sqrt(grad.dot(grad)) for grad in _grads]
+    grads_mag = [np.sqrt(grad.dot(grad)) for grad in grads]
 
     # Skip all ignore_tokens by setting grad to infinity
-    text_field: TextField = _instance[_input_field_to_attack]  # type: ignore
+    text_field: TextField = instance[input_field_to_attack]  # type: ignore
     for tok_idx, tok in enumerate(text_field.tokens):
-        if tok in _ignore_tokens:
+        if tok in ignore_tokens:
             grads_mag[tok_idx] = float("inf")
 
     # For NER, skip all tokens that are not in outside
-    if "tags" in _instance:
-        tag_field: SequenceLabelField = _instance["tags"] # type: ignore
+    if "tags" in instance:
+        tag_field: SequenceLabelField = instance["tags"] # type: ignore
         for idx, label in enumerate(tag_field):
             if label != "O":
                 grads_mag[idx] = float("inf")
 
     smallest = np.argmin(grads_mag)
     if smallest == float("inf"): # if all are ignored tokens, return.
-        return _instance, smallest
+        return instance, smallest
 
     # remove smallest
     inputs_before_smallest = text_field.tokens[0:smallest]
     inputs_after_smallest = text_field.tokens[smallest + 1:]
     text_field.tokens = inputs_before_smallest + inputs_after_smallest
 
-    if "tags" in _instance:
+    if "tags" in instance:
         tag_field_before_smallest = tag_field.labels[0:smallest]
         tag_field_after_smallest = tag_field.labels[smallest + 1:]
         tag_field.labels = tag_field_before_smallest + tag_field_after_smallest
         tag_field.sequence_field = TextField(text_field, text_field._token_indexers)
 
-    _instance.indexed = False
-    return _instance, smallest
+    instance.indexed = False
+    return instance, smallest
 
-def get_ner_tags_and_mask(_current_instance: Instance,
-                          _input_field_to_attack: str,
-                          _ignore_tokens: List[str]):
+def _get_ner_tags_and_mask(current_instance: Instance,
+                          input_field_to_attack: str,
+                          ignore_tokens: List[str]):
     """
     Used for the NER task. Sets the num_ignore tokens, saves the original
     predicted tag and a 0/1 mask in the position of the tags
     """
     # Set num_ignore_tokens
     num_ignore_tokens = 0
-    input_field = _current_instance[_input_field_to_attack]
+    input_field = current_instance[input_field_to_attack]
     for token in input_field.tokens:
-        if str(token) in _ignore_tokens:
+        if str(token) in ignore_tokens:
             num_ignore_tokens += 1
 
     # save the original tags and a 0/1 mask where the tags are
     tag_mask = []
     original_tags = []
-    tag_field: SequenceLabelField = _current_instance["tags"] # type: ignore
+    tag_field: SequenceLabelField = current_instance["tags"] # type: ignore
     for label in tag_field:
         if label != "O":
             tag_mask.append(1)

allennlp/interpret/saliency_interpreters/smooth_gradient.py
@@ -9,14 +9,15 @@ from allennlp.common.util import JsonDict, sanitize
 from allennlp.interpret.saliency_interpreters.saliency_interpreter import SaliencyInterpreter
 from allennlp.modules.text_field_embedders import TextFieldEmbedder
 from allennlp.data import Instance
-
+from allennlp.predictors import Predictor
 
 @SaliencyInterpreter.register('smooth-gradient')
 class SmoothGradient(SaliencyInterpreter):
     """
     Interprets the prediction using SmoothGrad (https://arxiv.org/abs/1706.03825)
     """
-    def __init__(self) -> None:
+    def __init__(self, predictor: Predictor) -> None:
+        self.predictor = predictor
         # Hyperparameters
         self.stdev = 0.01
         self.num_samples = 25
@@ -50,7 +51,7 @@ class SmoothGradient(SaliencyInterpreter):
         """
         def forward_hook(module, inp, output): # pylint: disable=unused-argument
             # Random noise = N(0, stdev * (max-min))
-            scale = output.detach.max() - output.detach.min()
+            scale = output.detach().max() - output.detach().min()
             noise = torch.randn(output.shape).to(output.device) * stdev * scale
 
             # Add the random noise