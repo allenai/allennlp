c2c934cf80404f68da94e995d2f1b75ffb1aab41
hawkeoni <iliyadimov@icloud.com>
2019-07-17 19:23:47 +0300

    Improve dict missing key code

--- allennlp/data/dataset_readers/penn_tree_bank.py
+++ allennlp/data/dataset_readers/penn_tree_bank.py
@@ -132,12 +132,9 @@ class PennTreeBankConstituencySpanDatasetReader(DatasetReader):
             gold_spans = None
         for start, end in enumerate_spans(tokens):
             spans.append(SpanField(start, end, text_field))
-
+            
             if gold_spans is not None:
-                if (start, end) in gold_spans.keys():
-                    gold_labels.append(gold_spans[(start, end)])
-                else:
-                    gold_labels.append("NO-LABEL")
+                gold_labels.append(gold_spans.get((start, end), "NO-LABEL"))
 
         metadata = {"tokens": tokens}
         if gold_tree:
