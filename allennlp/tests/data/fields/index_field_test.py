a1476c0414ad1e1412515c53d9e1f27b69039ead
Eric Wallace <eswallace@comcast.net>
2019-07-18 09:46:13 -0700

    add equality check for index field; allennlp interpret (#3073)
    * add equality check for index field; allennlp interpret
    
    * add test
    
    * change hotflip to use equals method
    
    * tests per matt
    
    * newline
    
    * change input reduction to eq also
    
    * undo
    
    * add newline
    
    * fix pylutn

--- allennlp/tests/data/fields/index_field_test.py
+++ allennlp/tests/data/fields/index_field_test.py
@@ -35,7 +35,13 @@ class TestIndexField(AllenNlpTestCase):
     def test_equality(self):
         index_field1 = IndexField(4, self.text)
         index_field2 = IndexField(4, self.text)
+        index_field3 = IndexField(4, TextField([Token(t) for t in ["AllenNLP", "is", "the", "bomb", "!"]],
+                                               {"words": SingleIdTokenIndexer("words")}))
 
         assert index_field1 == 4
         assert index_field1 == index_field1
-        assert index_field1 != index_field2
+        assert index_field1 == index_field2
+
+        assert index_field1 != index_field3
+        assert index_field2 != index_field3
+        assert index_field3 == index_field3
