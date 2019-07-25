dcecc74f5b21e723e5d04c54d393ca0681bb75d2
sanjays <sanjays@dhcp-v006-182.mobile.uci.edu>
2019-07-25 11:35:08 -0700

    predictions_to_labeled_instances for next_token_lm

--- allennlp/predictors/next_token_lm.py
+++ allennlp/predictors/next_token_lm.py
@@ -6,9 +6,16 @@ from allennlp.predictors.predictor import Predictor
 
 
 @Predictor.register('next_token_lm')
-class NextTokenLMPredictor(Predictor):    
+class NextTokenLMPredictor(Predictor):
     def predict(self, sentence: str, target: str) -> JsonDict:
         return self.predict_json({"sentence" : sentence, "target": target})
+
+    def predictions_to_labeled_instances(self,
+                                         instance: Instance,
+                                         outputs: Dict[str, numpy.ndarray]):
+        new_instance = deepcopy(instance)
+        new_instance.add_field('target_ids', LabelField(int(outputs['top_indices'][0])), skip_indexing=True)
+        return [new_instance]
 
     @overrides
     def _json_to_instance(self, json_dict: JsonDict) -> Instance:
