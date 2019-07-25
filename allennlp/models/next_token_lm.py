e15216532746e560d0e84cc6baad7499c810ece5
Eric Wallace <eswallace@comcast.net>
2019-07-25 11:36:33 -0700

    add decode

--- allennlp/models/next_token_lm.py
+++ allennlp/models/next_token_lm.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Tuple, Union
 
+from overrides import overrides
 import torch
 
 from allennlp.common.checks import check_dimensions_match, ConfigurationError
@@ -24,6 +25,7 @@ class NextTokenLM(Model):
         super().__init__(vocab)
         self._text_field_embedder = text_field_embedder
         self._contextualizer = contextualizer
+        self._target_namespace = target_namespace
         if contextualizer:
             softmax_dim = contextualizer.get_output_dim()
             check_dimensions_match(text_field_embedder.get_output_dim(), contextualizer.get_input_dim(),
@@ -42,14 +44,8 @@ class NextTokenLM(Model):
                 tokens: Dict[str, torch.LongTensor],                
                 target_ids: Dict[str, torch.LongTensor] = None) -> Dict[str, torch.Tensor]:        
         # pylint: disable=arguments-differ        
-        if len(target_ids) != 1:
-            raise ValueError(f"Found {len(target_ids)} indexers for target_ids, not sure what to do")
-        target_ids = list(target_ids.values())[0]        
         batch_size = tokens['tokens'].size()[0]
-        if target_ids is not None and target_ids.size()[0] != tokens['tokens'].size()[0]:
-            raise ValueError(f"Number of targets ({target_ids.size()}) should match batch size "
-                             f"({tokens['tokens'].size()[0]}) are not equal")
-
+        
          # Shape: (batch_size, num_tokens, embedding_dim)
         embeddings = self._text_field_embedder(tokens)
 
@@ -73,6 +69,7 @@ class NextTokenLM(Model):
         output_dict = {"top_probs": top_probs, "top_indices": top_indices}
 
         if target_ids is not None:
+            target_ids = list(target_ids.values())[0]        
             target_logits = target_logits.view(batch_size, vocab_size)
             target_ids = target_ids.view(batch_size)
             loss = torch.nn.functional.cross_entropy(target_logits, target_ids)
@@ -83,3 +80,18 @@ class NextTokenLM(Model):
 
     def get_metrics(self, reset: bool = False):
         return {"perplexity": self._perplexity.get_metric(reset=reset)}
+
+    @overrides
+    def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        """
+        Converts the tag ids to the actual target_ids.
+        ``output_dict["target_ids"]`` is a list of lists of tag_ids,
+        so we use an ugly nested list comprehension.
+        """
+        output_dict["target_ids"] = [
+                [self.vocab.get_token_from_index(target_id, namespace=self._target_namespace)
+                 for target_id in target_ids]
+                for target_ids in output_dict["target_ids"]
+        ]
+
+        return output_dict
