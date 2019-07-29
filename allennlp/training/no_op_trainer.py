6746d12c51e26ea25c326b40cda4bcf4109b808d
Evan Pete Walsh <epwalsh10@gmail.com>
2019-07-29 08:33:40 -0700

    pass cache_directory and cache_prefix to non-default trainers (#3077)
    * pass cache_directory and prefix to other trainers
    
    * fixes

--- allennlp/training/no_op_trainer.py
+++ allennlp/training/no_op_trainer.py
@@ -23,9 +23,11 @@ class NoOpTrainer(TrainerBase):
     def from_params(cls,   # type: ignore
                     params: Params,
                     serialization_dir: str,
-                    recover: bool = False):
+                    recover: bool = False,
+                    cache_directory: str = None,
+                    cache_prefix: str = None):
         # pylint: disable=arguments-differ
-        pieces = TrainerPieces.from_params(params, serialization_dir, recover)  # pylint: disable=no-member
+        pieces = TrainerPieces.from_params(params, serialization_dir, recover, cache_directory, cache_prefix)  # pylint: disable=no-member
         return NoOpTrainer(serialization_dir, pieces.model)
 
     def train(self) -> Dict[str, Any]:
