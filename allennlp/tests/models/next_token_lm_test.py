0a567097064406a83525003f6b84f11b81bc9ec6
Matt Gardner <mattg@allenai.org>
2019-08-26 12:20:25 -0700

    Fix test

--- allennlp/tests/models/next_token_lm_test.py
+++ allennlp/tests/models/next_token_lm_test.py
@@ -10,7 +10,7 @@ from allennlp.models import Model
 class TestNextTokenLanguageModel(ModelTestCase):
     def setUp(self):
         super().setUp()
-        self.set_up_model(self.FIXTURES_ROOT / 'next_token_lm' / 'experiment.jsonnet',
+        self.set_up_model(self.FIXTURES_ROOT / 'next_token_lm' / 'experiment.json',
                           self.FIXTURES_ROOT / 'language_model' / 'sentences.txt')
 
     def test_model_can_train_save_and_load(self):
