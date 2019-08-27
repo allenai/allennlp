028a3e68dd1f0e336a3d3ba6f5383811dd0e3ca7
Matt Gardner <mattg@allenai.org>
2019-08-27 12:31:36 -0700

    fix import...

--- allennlp/modules/language_model_heads/__init__.py
+++ allennlp/modules/language_model_heads/__init__.py
@@ -1,4 +1,3 @@
 from allennlp.modules.language_model_heads.language_model_head import LanguageModelHead
 from allennlp.modules.language_model_heads.bert import BertLanguageModelHead
-from allennlp.modules.language_model_heads.linear import LinearLanguageModelHead
 from allennlp.modules.language_model_heads.gpt2 import Gpt2LanguageModelHead
