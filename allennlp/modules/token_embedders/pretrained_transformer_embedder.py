030e28c9e11d85ab674442d635d0e2f66548f211
Ana <anam@allennlp-server4.corp.ai2>
2019-08-26 18:46:32 -0700

    Revert "Revert "Merge branch 'matt-gardner-transformer-embedder'""
    This reverts commit 6e1e3713155c74e041cde8bb8f23535b5c84ec9c.

---
+++ allennlp/modules/token_embedders/pretrained_transformer_embedder.py
@@ -0,0 +1,26 @@
+from overrides import overrides
+from pytorch_transformers.modeling_auto import AutoModel
+import torch
+
+from allennlp.modules.token_embedders.token_embedder import TokenEmbedder
+
+
+@TokenEmbedder.register("pretrained_transformer")
+class PretrainedTransformerEmbedder(TokenEmbedder):
+    """
+    Uses a pretrained model from ``pytorch-transformers`` as a ``TokenEmbedder``.
+    """
+    def __init__(self, model_name: str) -> None:
+        super().__init__()
+        self.transformer_model = AutoModel.from_pretrained(model_name)
+        # I'm not sure if this works for all models; open an issue on github if you find a case
+        # where it doesn't work.
+        self.output_dim = self.transformer_model.config.hidden_size
+
+    @overrides
+    def get_output_dim(self):
+        return self.output_dim
+
+    def forward(self, token_ids: torch.LongTensor) -> torch.Tensor:  # type: ignore
+        # pylint: disable=arguments-differ
+        return self.transformer_model(token_ids)[0]
