bbb8ef2c2a36c9010431cedd2fef9c495bf2cc96
Matt Gardner <mattg@allenai.org>
2019-08-27 17:27:13 -0700

    Fix module imports, remove more tests

--- allennlp/data/dataset_readers/__init__.py
+++ allennlp/data/dataset_readers/__init__.py
@@ -42,16 +42,7 @@ from allennlp.data.dataset_readers.stanford_sentiment_tree_bank import (
     StanfordSentimentTreeBankDatasetReader,
 )
 from allennlp.data.dataset_readers.quora_paraphrase import QuoraParaphraseDatasetReader
-from allennlp.data.dataset_readers.semantic_parsing import (
-    WikiTablesDatasetReader,
-    AtisDatasetReader,
-    NlvrDatasetReader,
-    TemplateText2SqlDatasetReader,
-)
-from allennlp.data.dataset_readers.semantic_parsing.quarel import QuarelDatasetReader
-from allennlp.data.dataset_readers.simple_language_modeling import (
-    SimpleLanguageModelingDatasetReader,
-)
+from allennlp.data.dataset_readers.simple_language_modeling import SimpleLanguageModelingDatasetReader
 from allennlp.data.dataset_readers.babi import BabiReader
 from allennlp.data.dataset_readers.copynet_seq2seq import CopyNetDatasetReader
 from allennlp.data.dataset_readers.text_classification_json import TextClassificationJsonReader
--- allennlp/data/fields/__init__.py
+++ allennlp/data/fields/__init__.py
@@ -7,12 +7,10 @@ from allennlp.data.fields.field import Field
 from allennlp.data.fields.array_field import ArrayField
 from allennlp.data.fields.adjacency_field import AdjacencyField
 from allennlp.data.fields.index_field import IndexField
-from allennlp.data.fields.knowledge_graph_field import KnowledgeGraphField
 from allennlp.data.fields.label_field import LabelField
 from allennlp.data.fields.multilabel_field import MultiLabelField
 from allennlp.data.fields.list_field import ListField
 from allennlp.data.fields.metadata_field import MetadataField
-from allennlp.data.fields.production_rule_field import ProductionRuleField
 from allennlp.data.fields.sequence_field import SequenceField
 from allennlp.data.fields.sequence_label_field import SequenceLabelField
 from allennlp.data.fields.span_field import SpanField
