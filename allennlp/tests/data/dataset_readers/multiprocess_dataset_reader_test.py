e5e885e5758210cc4c191f87f6d33010b3979258
Brendan Roof <brendanr@allenai.org>
2019-08-29 01:58:36 +0000

    Replace code

--- allennlp/tests/data/dataset_readers/multiprocess_dataset_reader_test.py
+++ allennlp/tests/data/dataset_readers/multiprocess_dataset_reader_test.py
@@ -55,26 +55,26 @@ class TestMultiprocessDatasetReader(AllenNlpTestCase):
         # For some of the tests we need a vocab, we'll just use the base_reader for that.
         self.vocab = Vocabulary.from_instances(self.base_reader.read(str(base_file_path)))
 
-#    def test_multiprocess_read(self):
-#        reader = MultiprocessDatasetReader(base_reader=self.base_reader, num_workers=4)
-#
-#        all_instances = []
-#
-#        for instance in reader.read(self.identical_files_glob):
-#            all_instances.append(instance)
-#
-#        # 100 files * 4 sentences / file
-#        assert len(all_instances) == 100 * 4
-#
-#        counts = Counter(fingerprint(instance) for instance in all_instances)
-#
-#        # should have the exact same data 100 times
-#        assert len(counts) == 4
-#        assert counts[("cats", "are", "animals", ".", "N", "V", "N", "N")] == 100
-#        assert counts[("dogs", "are", "animals", ".", "N", "V", "N", "N")] == 100
-#        assert counts[("snakes", "are", "animals", ".", "N", "V", "N", "N")] == 100
-#        assert counts[("birds", "are", "animals", ".", "N", "V", "N", "N")] == 100
-#
+    def test_multiprocess_read(self):
+        reader = MultiprocessDatasetReader(base_reader=self.base_reader, num_workers=4)
+
+        all_instances = []
+
+        for instance in reader.read(self.identical_files_glob):
+            all_instances.append(instance)
+
+        # 100 files * 4 sentences / file
+        assert len(all_instances) == 100 * 4
+
+        counts = Counter(fingerprint(instance) for instance in all_instances)
+
+        # should have the exact same data 100 times
+        assert len(counts) == 4
+        assert counts[("cats", "are", "animals", ".", "N", "V", "N", "N")] == 100
+        assert counts[("dogs", "are", "animals", ".", "N", "V", "N", "N")] == 100
+        assert counts[("snakes", "are", "animals", ".", "N", "V", "N", "N")] == 100
+        assert counts[("birds", "are", "animals", ".", "N", "V", "N", "N")] == 100
+
     def test_multiprocess_read_partial_does_not_hang(self):
         # Use a small queue size such that the processes generating the data will block.
         reader = MultiprocessDatasetReader(base_reader=self.base_reader, num_workers=4, output_queue_size=10)
@@ -127,68 +127,68 @@ class TestMultiprocessDatasetReader(AllenNlpTestCase):
         assert counts[("snakes", "are", "animals", ".", "N", "V", "N", "N")] == 100
         assert counts[("birds", "are", "animals", ".", "N", "V", "N", "N")] == 100
 
-#    def test_multiprocess_read_in_subprocess_is_deterministic(self):
-#        reader = MultiprocessDatasetReader(base_reader=self.base_reader, num_workers=1)
-#        q = Queue()
-#        def read():
-#            for instance in reader.read(self.distinct_files_glob):
-#                q.put(fingerprint(instance))
-#
-#        # Ensure deterministic shuffling.
-#        np.random.seed(0)
-#        p = Process(target=read)
-#        p.start()
-#        p.join()
-#
-#        # Convert queue to list.
-#        actual_fingerprints = []
-#        while not q.empty():
-#            actual_fingerprints.append(q.get(block=False))
-#
-#        assert len(actual_fingerprints) == 100
-#
-#        expected_fingerprints = []
-#        for instance in self.base_reader.read(self.all_distinct_path):
-#            expected_fingerprints.append(fingerprint(instance))
-#
-#        np.random.seed(0)
-#        expected_fingerprints.sort()
-#        # This should be shuffled into exactly the same order as actual_fingerprints.
-#        np.random.shuffle(expected_fingerprints)
-#
-#        assert actual_fingerprints == expected_fingerprints
-#
-#    def test_multiple_epochs(self):
-#        reader = MultiprocessDatasetReader(base_reader=self.base_reader,
-#                                           num_workers=2,
-#                                           epochs_per_read=3)
-#
-#        all_instances = []
-#
-#        for instance in reader.read(self.identical_files_glob):
-#            all_instances.append(instance)
-#
-#        # 100 files * 4 sentences per file * 3 epochs
-#        assert len(all_instances) == 100 * 4 * 3
-#
-#        counts = Counter(fingerprint(instance) for instance in all_instances)
-#
-#        # should have the exact same data 100 * 3 times
-#        assert len(counts) == 4
-#        assert counts[("cats", "are", "animals", ".", "N", "V", "N", "N")] == 300
-#        assert counts[("dogs", "are", "animals", ".", "N", "V", "N", "N")] == 300
-#        assert counts[("snakes", "are", "animals", ".", "N", "V", "N", "N")] == 300
-#        assert counts[("birds", "are", "animals", ".", "N", "V", "N", "N")] == 300
-#
-#    def test_with_iterator(self):
-#        reader = MultiprocessDatasetReader(base_reader=self.base_reader, num_workers=2)
-#        instances = reader.read(self.identical_files_glob)
-#
-#        iterator = BasicIterator(batch_size=32)
-#        iterator.index_with(self.vocab)
-#
-#        batches = [batch for batch in iterator(instances, num_epochs=1)]
-#
-#        # 400 instances / batch_size 32 = 12 full batches + 1 batch of 16
-#        sizes = sorted([len(batch['tags']) for batch in batches])
-#        assert sizes == [16] + 12 * [32]
+    def test_multiprocess_read_in_subprocess_is_deterministic(self):
+        reader = MultiprocessDatasetReader(base_reader=self.base_reader, num_workers=1)
+        q = Queue()
+        def read():
+            for instance in reader.read(self.distinct_files_glob):
+                q.put(fingerprint(instance))
+
+        # Ensure deterministic shuffling.
+        np.random.seed(0)
+        p = Process(target=read)
+        p.start()
+        p.join()
+
+        # Convert queue to list.
+        actual_fingerprints = []
+        while not q.empty():
+            actual_fingerprints.append(q.get(block=False))
+
+        assert len(actual_fingerprints) == 100
+
+        expected_fingerprints = []
+        for instance in self.base_reader.read(self.all_distinct_path):
+            expected_fingerprints.append(fingerprint(instance))
+
+        np.random.seed(0)
+        expected_fingerprints.sort()
+        # This should be shuffled into exactly the same order as actual_fingerprints.
+        np.random.shuffle(expected_fingerprints)
+
+        assert actual_fingerprints == expected_fingerprints
+
+    def test_multiple_epochs(self):
+        reader = MultiprocessDatasetReader(base_reader=self.base_reader,
+                                           num_workers=2,
+                                           epochs_per_read=3)
+
+        all_instances = []
+
+        for instance in reader.read(self.identical_files_glob):
+            all_instances.append(instance)
+
+        # 100 files * 4 sentences per file * 3 epochs
+        assert len(all_instances) == 100 * 4 * 3
+
+        counts = Counter(fingerprint(instance) for instance in all_instances)
+
+        # should have the exact same data 100 * 3 times
+        assert len(counts) == 4
+        assert counts[("cats", "are", "animals", ".", "N", "V", "N", "N")] == 300
+        assert counts[("dogs", "are", "animals", ".", "N", "V", "N", "N")] == 300
+        assert counts[("snakes", "are", "animals", ".", "N", "V", "N", "N")] == 300
+        assert counts[("birds", "are", "animals", ".", "N", "V", "N", "N")] == 300
+
+    def test_with_iterator(self):
+        reader = MultiprocessDatasetReader(base_reader=self.base_reader, num_workers=2)
+        instances = reader.read(self.identical_files_glob)
+
+        iterator = BasicIterator(batch_size=32)
+        iterator.index_with(self.vocab)
+
+        batches = [batch for batch in iterator(instances, num_epochs=1)]
+
+        # 400 instances / batch_size 32 = 12 full batches + 1 batch of 16
+        sizes = sorted([len(batch['tags']) for batch in batches])
+        assert sizes == [16] + 12 * [32]
