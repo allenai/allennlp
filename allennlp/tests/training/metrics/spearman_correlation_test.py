349b0f247c290c6e7f17f138e79ffe4241c43557
JackKuo666 <41313632+JackKuo666@users.noreply.github.com>
2019-09-08 19:20:54 +0800

    Update spearman_correlation_test.py

--- allennlp/tests/training/metrics/spearman_correlation_test.py
+++ allennlp/tests/training/metrics/spearman_correlation_test.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import math
 from numpy.testing import assert_allclose
 
 from allennlp.common.testing import AllenNlpTestCase
@@ -7,7 +8,11 @@ from allennlp.training.metrics import SpearmanCorrelation
 
 # pylint: disable=no-self-use
 
-def spearman_corrcoef(predictions, labels, mask=None):
+def spearman_formula(predictions, labels, mask=None):
+    """
+    This function is spearman formula from:
+        https://en.wikipedia.org/wiki/Spearman%27s_rank_correlation_coefficient
+    """
     if mask is not None:
         predictions = predictions * mask
         labels = labels * mask
@@ -38,8 +43,8 @@ def spearman_corrcoef(predictions, labels, mask=None):
 
 class SpearmanCorrelationTest(AllenNlpTestCase):
     def test_unmasked_computation(self):
-        spearman_correlation = SpearmanCorrelation()
-        batch_size = 100
+                spearman_correlation = SpearmanCorrelation()
+        batch_size = 10
         num_labels = 10
         predictions1 = np.random.randn(batch_size, num_labels).astype("float32")
         labels1 = 0.5 * predictions1 + np.random.randn(batch_size, num_labels).astype("float32")
@@ -53,28 +58,15 @@ class SpearmanCorrelationTest(AllenNlpTestCase):
         # but in a few cases, for example, predictions_2, the data of such a batch is exactly the same.
         predictions_labels_ = [(predictions1, labels1), (predictions2, labels2)]
 
-        stride = 10
-
         for predictions, labels in predictions_labels_:
             spearman_correlation.reset()
-            for i in range(batch_size // stride):
-                timestep_predictions = torch.FloatTensor(predictions[stride * i:stride * (i+1), :])
-                timestep_labels = torch.FloatTensor(labels[stride * i:stride * (i+1), :])
-                expected_spearman_correlation = spearman_corrcoef(predictions[:stride * (i + 1), :].reshape(-1),
-                                                                  labels[:stride * (i + 1), :].reshape(-1))
-
-                spearman_correlation(timestep_predictions, timestep_labels)
-                assert_allclose(expected_spearman_correlation, spearman_correlation.get_metric(), rtol=1e-5)
-
-            # Test reset
-            spearman_correlation.reset()
             spearman_correlation(torch.FloatTensor(predictions), torch.FloatTensor(labels))
-            assert_allclose(spearman_corrcoef(predictions.reshape(-1), labels.reshape(-1)),
+            assert_allclose(spearman_formula(predictions.reshape(-1), labels.reshape(-1)),
                             spearman_correlation.get_metric(), rtol=1e-5)
-
+            
     def test_masked_computation(self):
         spearman_correlation = SpearmanCorrelation()
-        batch_size = 100
+        batch_size = 10
         num_labels = 10
         predictions1 = np.random.randn(batch_size, num_labels).astype("float32")
         labels1 = 0.5 * predictions1 + np.random.randn(batch_size, num_labels).astype("float32")
@@ -90,32 +82,40 @@ class SpearmanCorrelationTest(AllenNlpTestCase):
 
         # Random binary mask
         mask = np.random.randint(0, 2, size=(batch_size, num_labels)).astype("float32")
-        stride = 10
 
         for predictions, labels in predictions_labels_:
             spearman_correlation.reset()
-            for i in range(batch_size // stride):
-                timestep_predictions = torch.FloatTensor(predictions[stride * i:stride * (i+1), :])
-                timestep_labels = torch.FloatTensor(labels[stride * i:stride * (i+1), :])
-                timestep_mask = torch.FloatTensor(mask[stride * i:stride * (i+1), :])
+            spearman_correlation(torch.FloatTensor(predictions), torch.FloatTensor(labels), torch.FloatTensor(mask))
+            expected_spearman_correlation = spearman_formula(predictions.reshape(-1), labels.reshape(-1),
+                                                             mask=mask.reshape(-1))
 
-                expected_spearman_correlation = spearman_corrcoef(predictions[:stride * (i+1), :].reshape(-1),
-                                                                  labels[:stride * (i+1), :].reshape(-1),
-                                                                  mask=mask[:stride * (i+1), :].reshape(-1))
-
-                spearman_correlation(timestep_predictions, timestep_labels, timestep_mask)
-
-                # because add mask, a batch of predictions or labels will have many 0,
-                # spearman correlation algorithm will dependence the sorting position of a set of numbers,
-                # too many identical numbers will result in different calculation results each time
-                # but the positive and negative results are the same,
-                # so here we only test the positive and negative results of the results.
-                assert (expected_spearman_correlation * spearman_correlation.get_metric()) > 0
-
-            # Test reset
-            spearman_correlation.reset()
-            spearman_correlation(torch.FloatTensor(predictions),
-                                 torch.FloatTensor(labels), torch.FloatTensor(mask))
-            expected_spearman_correlation = spearman_corrcoef(predictions.reshape(-1), labels.reshape(-1),
-                                                              mask=mask.reshape(-1))
+            # because add mask, a batch of predictions or labels will have many 0,
+            # spearman correlation algorithm will dependence the sorting position of a set of numbers,
+            # too many identical numbers will result in different calculation results each time
+            # but the positive and negative results are the same,
+            # so here we only test the positive and negative results of the results.
             assert (expected_spearman_correlation * spearman_correlation.get_metric()) > 0
+    
+    def test_reset(self):
+        spearman_correlation = SpearmanCorrelation()
+        batch_size = 10
+        num_labels = 10
+        predictions = np.random.randn(batch_size, num_labels).astype("float32")
+        labels = 0.5 * predictions + np.random.randn(batch_size, num_labels).astype("float32")
+
+        # 1.test spearman_correlation.reset()
+        spearman_correlation.reset()
+        spearman_correlation(torch.FloatTensor(predictions), torch.FloatTensor(labels))
+        temp = spearman_correlation.get_metric()
+        spearman_correlation.reset()
+        spearman_correlation(torch.FloatTensor(predictions), torch.FloatTensor(labels))
+        assert spearman_correlation.get_metric() == temp
+
+        # 2.test spearman_correlation.reset()
+        spearman_correlation.reset()
+        spearman_correlation(torch.FloatTensor(predictions), torch.FloatTensor(labels))
+
+        spearman_correlation.get_metric(reset=False)
+        assert spearman_correlation.get_metric() != np.nan
+        spearman_correlation.get_metric(reset=True)
+        assert math.isnan(spearman_correlation.get_metric())
