e439c649e957de7b5e501efedcca1d0f1ac26ba3
rloganiv <rloganiv@gmail.com>
2019-09-11 18:39:26 -0700

    Generalized WeightDrop implementation

--- allennlp/modules/weight_drop.py
+++ allennlp/modules/weight_drop.py
@@ -1,32 +1,99 @@
-import torch.nn as nn
+from typing import Iterator, List
+from collections import namedtuple
+import re
+
+import torch
 import torch.nn.functional as F
 
-class WeightDropout(nn.Module):
-    "A module that warps another layer in which some weights will be replaced by 0 during training."
 
-    def __init__(self, module: nn.Module, weight_p: float, layer_names=['weight_hh_l0']):
+class _Match:
+    """
+    Helper object that stores ``Parameter`` objects along with their parent ``Module`` objects
+    and associated names.
+    """
+    def __init__(self,
+                 module_name: str,
+                 module: torch.nn.Module,
+                 parameter_name: str,
+                 parameter: torch.nn.Parameter) -> None:
+        self.module_name = module_name
+        self.module = module
+        self.parameter_name = parameter_name
+        self.parameter = parameter
+
+    @property
+    def raw_parameter_name(self):
+        prefix = self.module_name.replace('.', '_')  # PyTorch will complain if the name contains a '.'
+        return '_'.join((prefix, self.parameter_name)) + '_raw'
+
+
+def _module_search(regex: str, module: torch.nn.Module) -> Iterator[_Match]:
+    """
+    Generates _Match objects for all parameters in a given module whose names match the provided
+    regular expression.
+    """
+    for submodule_name, submodule in module.named_modules():
+        for parameter_name, parameter in submodule.named_parameters():
+            if re.search(regex, parameter_name):
+                yield _Match(submodule_name, module, parameter_name, parameter)
+
+
+class WeightDropout(torch.nn.Module):
+    """
+    Weight dropout (a.k.a DropConnect) module described in: `"Regularization of Neural Networks using DropConnect"
+    <https://www.semanticscholar.org/paper/Regularization-of-Neural-Networks-using-DropConnect-Wan-Zeiler/38f35dd624cd1cf827416e31ac5e0e0454028eca>`_
+    by Wan et al., 2013.
+
+    Parameters
+    ==========
+    module : ``torch.nn.Module``
+        Module to apply weight dropout to.
+    parameter_regex : ``str``
+        Regular expression identifying which parameters to apply weight dropout to.
+    dropout : ``float``, optional (default = 0.0)
+        Probability that a given weight is dropped.
+    """
+    def __init__(self,
+                 module: torch.nn.Module,
+                 parameter_regex: str,
+                 dropout: float = 0.0) -> None:
         super().__init__()
-        self.wrapper = module
-        self.module, self.weight_p, self.layer_names = module._modules['_module'], weight_p, layer_names
-        for layer in self.layer_names:
-            #Makes a copy of the weights of the selected layers.
-            w = getattr(self.module, layer)
-            self.register_parameter(f'{layer}_raw', nn.Parameter(w.data))
-            self.module._parameters[layer] = F.dropout(w, p=self.weight_p, training=False)
-
-    def _setweights(self):
-        "Apply dropout to the raw weights."
-        for layer in self.layer_names:
-            raw_w = getattr(self, f'{layer}_raw')
-            self.module._parameters[layer] = F.dropout(raw_w, p=self.weight_p, training=self.training)
+        self._module = module
+        self._parameter_regex = parameter_regex
+        self._dropout = dropout
+        # Find all of the parameters that match the regular expression and cache them, the
+        # submodule they belong to, as well as their names. We need to know the specific submodule
+        # so that we can place the dropped out tensors into the correct Module._parameters
+        # dictionaries.
+        self._matches = list(_module_search(self._parameter_regex, self._module))
+        # Register the raw parameters (e.g., the parameters before dropout is applied) to this
+        # module and remove them from their parent module. This avoids double counting when using
+        # the Module.parameters() method. If we didn't do this the matched parameters would be
+        # updated twice when calling Optimizer.step().
+        for match in self._matches:
+            self.register_parameter(match.raw_parameter_name, match.parameter)
+            delattr(match.module, match.parameter_name)
 
     def forward(self, *args):
-        self._setweights()
-        return self.wrapper.forward(*args)
+        # pylint: disable=protected-access
+
+        # Apply dropout to all of the matching parameters, and force them into their original
+        # parent module's _parameter dictionary
+        for match in self._matches:
+            raw_parameter = getattr(self, match.raw_parameter_name)
+            match.module._parameters[match.parameter_name] = F.dropout(raw_parameter,
+                                                                       p=self._dropout,
+                                                                       training=self.training)
+        # Call the top-level module on the inputs.
+        output = self._module(*args)
+        # Lastly, remove the dropped out parameters from their parent module's _parameter
+        # dictionary. If not PyTorch might raise errors due to using non-leaf tensors as
+        # parameters.
+        for match in self._matches:
+            delattr(match.module, match.parameter_name)
+
+        return output
 
     def reset(self):
-        for layer in self.layer_names:
-            raw_w = getattr(self, f'{layer}_raw')
-            self.module._parameters[layer] = F.dropout(raw_w, p=self.weight_p, training=False)
         if hasattr(self.module, 'reset'):
             self.module.reset()
