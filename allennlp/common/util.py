088b2883de1a6ab03d808de4dd809041d088c5ea
12seetharaman <53142037+12seetharaman@users.noreply.github.com>
2019-09-27 18:54:06 +0530

    sanitize function fix to convert numpy.bool_ to bool (#3290)

--- allennlp/common/util.py
+++ allennlp/common/util.py
@@ -65,6 +65,9 @@ def sanitize(x: Any) -> Any:  # pylint: disable=invalid-name,too-many-return-sta
     elif isinstance(x, dict):
         # Dicts need their values sanitized
         return {key: sanitize(value) for key, value in x.items()}
+    elif isinstance(x, numpy.bool_):
+        # Numpy bool_ need to be converted to python bool.
+        return bool(x)
     elif isinstance(x, (spacy.tokens.Token, allennlp.data.Token)):
         # Tokens get sanitized to just their text.
         return x.text
