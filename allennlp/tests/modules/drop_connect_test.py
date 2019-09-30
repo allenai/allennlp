426710658d4adce29e1623c5e9db17e18a6cb67f
Joel Grus <joelgrus@gmail.com>
2019-09-30 16:19:49 -0700

    more flake8

--- allennlp/tests/modules/drop_connect_test.py
+++ allennlp/tests/modules/drop_connect_test.py
@@ -6,15 +6,12 @@ from allennlp.modules.drop_connect import DropConnect
 
 
 class DropConnectTest(AllenNlpTestCase):
-
     @flaky(max_runs=10, min_passes=1)
     def test_linear_outputs(self):
         # Check that weights are (probably) being dropped out properly. There's an extremely small
         # chance (p < 1e-86) that this test fails.
         input_tensor = torch.ones(10, dtype=torch.float32)
-        dropped_linear = DropConnect(torch.nn.Linear(10, 10),
-                                     parameter_regex='weight',
-                                     dropout=0.9)
+        dropped_linear = DropConnect(torch.nn.Linear(10, 10), parameter_regex="weight", dropout=0.9)
 
         # Check that outputs differ if module is in training mode
         dropped_linear.train()
@@ -34,7 +31,7 @@ class DropConnectTest(AllenNlpTestCase):
         # small chance (p < 1e-86) that this test fails.
         input_tensor = torch.ones(1, 2, 10, dtype=torch.float32)  # shape: (batch, seq_length, dim)
         lstm = torch.nn.LSTM(input_size=10, hidden_size=10, batch_first=True)
-        dropped_lstm = DropConnect(module=lstm, parameter_regex='weight_hh', dropout=0.9)
+        dropped_lstm = DropConnect(module=lstm, parameter_regex="weight_hh", dropout=0.9)
 
         # Check that outputs differ if module is in training mode. Since only hidden-to-hidden
         # weights are masked, the first outputs should be the same.
@@ -61,10 +58,10 @@ class DropConnectTest(AllenNlpTestCase):
                 self.linear = torch.nn.Linear(1, 1)
                 # Use identical names to linear params to test that we don't run into name conflicts.
                 self.weight = torch.nn.Parameter(torch.FloatTensor([1, 2]))
-                self.bias = torch.nn.Parameter(torch.FloatTensor([1., 2]))
+                self.bias = torch.nn.Parameter(torch.FloatTensor([1.0, 2]))
 
         module = Mock()
-        dropped_module = DropConnect(module=module, parameter_regex='weight', dropout=0.9)
+        dropped_module = DropConnect(module=module, parameter_regex="weight", dropout=0.9)
 
         # We are ensuring the following:
         #   1. That there is a '_raw' version of the matched parameters associated to the top-level module.
@@ -72,10 +69,10 @@ class DropConnectTest(AllenNlpTestCase):
         #   3. That nothing has happened to the unnmatched parameters.
         parameter_names = {name for name, _ in dropped_module.named_parameters()}
         expected_parameter_names = {
-                '_module_weight_raw',
-                '_module.bias',
-                '_module_linear_weight_raw',
-                '_module.linear.bias'
+            "_module_weight_raw",
+            "_module.bias",
+            "_module_linear_weight_raw",
+            "_module.linear.bias",
         }
         self.assertSetEqual(parameter_names, expected_parameter_names)
 
@@ -83,9 +80,9 @@ class DropConnectTest(AllenNlpTestCase):
         # Checks that WeightDrop parameters are always leaf tensors.
 
         # Case 1: After initialization
-        weight_dropped_linear = DropConnect(torch.nn.Linear(10, 10),
-                                            parameter_regex='weight',
-                                            dropout=0.9)
+        weight_dropped_linear = DropConnect(
+            torch.nn.Linear(10, 10), parameter_regex="weight", dropout=0.9
+        )
         assert all(parameter.is_leaf for parameter in weight_dropped_linear.parameters())
 
         # Case 2: When in training mode
