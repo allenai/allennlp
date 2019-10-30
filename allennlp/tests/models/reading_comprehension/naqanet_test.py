c70219364dfe65ee3a9460d47ec9132f6994cc78
Mark Neumann <markn@allenai.org>
2019-10-30 12:43:53 -0700

    try to fix naqanet spurious failure

--- allennlp/tests/models/reading_comprehension/naqanet_test.py
+++ allennlp/tests/models/reading_comprehension/naqanet_test.py
@@ -14,4 +14,7 @@ class NumericallyAugmentedQaNetTest(ModelTestCase):
 
     @flaky(max_runs=3, min_passes=1)
     def test_model_can_train_save_and_load(self):
-        self.ensure_model_can_train_save_and_load(self.param_file)
+        self.ensure_model_can_train_save_and_load(
+            self.param_file,
+            gradients_to_ignore={"_passage_span_end_predictor._linear_layers.1.bias"}
+        )
