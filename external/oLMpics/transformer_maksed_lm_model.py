512731c916636c90e835ec29092324be6d33c322
alontalmor <salont@gmail.com>
2019-11-02 08:19:24 +0200

    Multi choice training in masked LM

--- external/oLMpics/transformer_maksed_lm_model.py
+++ external/oLMpics/transformer_maksed_lm_model.py
@@ -11,6 +11,7 @@ import re
 import torch
 from torch.nn.modules.linear import Linear
 from torch.nn.functional import binary_cross_entropy_with_logits
+from torch.nn import CrossEntropyLoss
 
 from allennlp.common.params import Params
 from allennlp.data import Vocabulary
@@ -19,6 +20,31 @@ from allennlp.models.model import Model
 from allennlp.models.reading_comprehension.util import get_best_span
 from allennlp.nn import RegularizerApplicator, util
 from allennlp.training.metrics import BooleanAccuracy, CategoricalAccuracy, SquadEmAndF1
+
+
+class BertForMultiChoiceMaskedLM(BertForMaskedLM):
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, masked_lm_labels=None,
+                position_ids=None, head_mask=None, all_masked_index_ids = None, label = None):
+        outputs = self.bert(input_ids, position_ids=position_ids, token_type_ids=token_type_ids,
+                            attention_mask=attention_mask, head_mask=head_mask)
+
+        sequence_output = outputs[0]
+        prediction_scores = self.cls(sequence_output)
+
+        outputs = (prediction_scores,) + outputs[2:]  # Add hidden states and attention if they are here
+        if masked_lm_labels is not None:
+            loss_fct = CrossEntropyLoss(ignore_index=-1)
+
+            # choosing prediction with all_masked_index_ids
+            masked_lm_loss = 0
+            for i,e in enumerate(all_masked_index_ids):
+                masked_lm_loss += \
+                    loss_fct(prediction_scores[i,e[0][0][0],[e[0][0][1],e[1][0][1],e[2][0][1]]].unsqueeze(0),label[i].unsqueeze(0))
+
+            #masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), masked_lm_labels.view(-1))
+            outputs = (masked_lm_loss,) + outputs
+
+        return outputs  # (masked_lm_loss), prediction_scores, (hidden_states), (attentions)
 
 
 @Model.register("transformer_masked_lm")
@@ -49,7 +75,7 @@ class TransformerMaskedLMModel(Model):
             self._padding_value = 5  # The index of the XLNet padding token
             self._transformer_model = XLNetLMHeadModel.from_pretrained(pretrained_model)
         elif 'bert' in pretrained_model:
-            self._transformer_model = BertForMaskedLM.from_pretrained(pretrained_model)
+            self._transformer_model = BertForMultiChoiceMaskedLM.from_pretrained(pretrained_model)
             self._padding_value = 0  # The index of the BERT padding token
         else:
             assert (ValueError)
@@ -98,7 +124,8 @@ class TransformerMaskedLMModel(Model):
 
         # Segment ids are not used by RoBERTa
         if 'roberta' in self._pretrained_model or 'bert' in self._pretrained_model:
-            outputs = self._transformer_model(input_ids=util.combine_initial_dims(input_ids), masked_lm_labels=masked_labels)
+            outputs = self._transformer_model(input_ids=util.combine_initial_dims(input_ids), masked_lm_labels=masked_labels,
+                                              all_masked_index_ids=[e['all_masked_index_ids'] for e in metadata],label=label)
             loss, predictions = outputs[:2]
         elif 'xlnet' in self._pretrained_model:
             transformer_outputs = self._transformer_model(input_ids=util.combine_initial_dims(input_ids),
