569a6d728f929c5a0ce62b3f22b70b36982a88e2
Maksym Del <max.del.edu@gmail.com>
2019-11-14 01:47:47 +0200

    Downgrade black

--- allennlp/tests/data/dataset_readers/next_token_lm_test.py
+++ allennlp/tests/data/dataset_readers/next_token_lm_test.py
@@ -27,13 +27,7 @@ class TestNextTokenLmReader(AllenNlpTestCase):
         token_indexer = PretrainedTransformerIndexer("bert-base-cased")
         reader = NextTokenLmReader(tokenizer, {"bert": token_indexer})
         instance = reader.text_to_instance(sentence="AllenNLP is very", target="very")
-        assert [t.text for t in instance["tokens"]] == [
-            "Allen",
-            "##NL",
-            "##P",
-            "is",
-            "very",
-        ]
+        assert [t.text for t in instance["tokens"]] == ["Allen", "##NL", "##P", "is", "very"]
         assert [t.text for t in instance["target_ids"]] == ["very"]
 
         vocab = Vocabulary()
