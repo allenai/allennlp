de5ab861ee5a6869c223216d175f863154f40a29
Diganta Misra <mishradiganta91@gmail.com>
2019-11-20 00:07:39 +0530

    Added type ignore statement for mypy check

--- allennlp/nn/activations.py
+++ allennlp/nn/activations.py
@@ -8,6 +8,8 @@ Here we provide a thin wrapper to allow registering them and instantiating them 
 The available activation functions are
 
 * "linear"
+* `"mish" <https://arxiv.org/abs/1908.08681>`_
+* `"swish" <https://arxiv.org/abs/1710.05941>`_
 * `"relu" <https://pytorch.org/docs/master/nn.html#torch.nn.ReLU>`_
 * `"relu6" <https://pytorch.org/docs/master/nn.html#torch.nn.ReLU6>`_
 * `"elu" <https://pytorch.org/docs/master/nn.html#torch.nn.ELU>`_
@@ -55,8 +57,8 @@ class Activation(Registrable):
 # Activation.by_name('relu')()
 Registrable._registry[Activation] = {
     "linear": lambda: lambda x: x,  # type: ignore
-    "mish": lambda: lambda x: x * torch.tanh(torch.nn.functional.softplus(x)), 
-    "swish": lambda: lambda x: x * torch.sigmoid(x),
+    "mish": lambda: lambda x: x * torch.tanh(torch.nn.functional.softplus(x)),   # type: ignore
+    "swish": lambda: lambda x: x * torch.sigmoid(x),   # type: ignore
     "relu": torch.nn.ReLU,
     "relu6": torch.nn.ReLU6,
     "elu": torch.nn.ELU,
