e7d9bdd3eb884c9ef88d8c8df26740497f25a6b6
Mark Neumann <markng@allenai.org>
2019-12-12 14:41:02 -0800

    torch mp uses it's own context, no need to set default

--- allennlp/run.py
+++ allennlp/run.py
@@ -16,17 +16,10 @@ logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s
 from allennlp.commands import main  # noqa
 
 
+
 def run():
     main(prog="allennlp")
 
 
 if __name__ == "__main__":
-    # First, let Pytorch's multiprocessing module know how to create child processes.
-    # Refer https://docs.python.org/3.7/library/multiprocessing.html#multiprocessing.set_start_method
-    # We use force=True because this is guarded by the if __name__ ... block, which
-    # means that it only runs if allennlp is being run as a binary.
-    # Without this, if another library that allennlp uses has set the start method,
-    # this line would crash.
-    torch.multiprocessing.set_start_method("spawn", force=True)
-
     run()
