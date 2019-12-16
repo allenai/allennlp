bab03472585e51ef0faf02d40ed6803441522afd
Elad Segal <eladsegal@users.noreply.github.com>
2019-12-16 02:24:37 +0200

    add more coverage

--- allennlp/tests/data/tokenizers/huggingface_transformers_tokenizer_test.py
+++ allennlp/tests/data/tokenizers/huggingface_transformers_tokenizer_test.py
@@ -55,3 +55,62 @@ class TestHuggingTransformersTokenizer(AllenNlpTestCase):
         )
         assert tokens == expected_tokens
         assert offsets == [0, 1, 3, 10, 15, 17, 19, 27]
+
+    def test_tokenize_with_offsets_prefix_issue(self):
+        """ For words that have a wordpiece tokenization that
+            doesn't contain the tokenization of its prefixes.
+            Example for XLNet:
+            text = "1603"
+            tokens = ["▁16", "03"]
+            tokenization for "160": ["▁160"]
+        """
+        sentence = "1603"
+        expected_tokens = ["▁16", "03"]
+        tokenizer = HuggingfaceTransformersTokenizer("xlnet-base-cased")
+
+        # tokenize with offsets
+        tokens, offsets = list(
+            map(list, zip(*[(t.text, t.idx) for t in tokenizer.tokenize_with_offsets(sentence)]))
+        )
+        assert tokens == expected_tokens
+        assert offsets == [0, 2]
+
+    def test_tokenize_with_offsets_additional_char_issue(self):
+        """ For cases in which the current token won't be produced
+            without an additional character that is only part of the
+            text that corresponds to the next tokens.
+            Example for XLNet:
+            text = "How many points did the buccaneers need to tie in the first?"
+            tokens = [..., '▁the', '▁', 'bu', 'cca', 'ne', 'ers', ...]
+            target_tokens = ['▁']
+            comparison_tokens = ['▁', 'b']
+            prev_comparison_tokens = ['']
+        """
+        sentence = "How many points did the buccaneers need to tie in the first?"
+        expected_tokens = [
+            "▁How",
+            "▁many",
+            "▁points",
+            "▁did",
+            "▁the",
+            "▁",
+            "bu",
+            "cca",
+            "ne",
+            "ers",
+            "▁need",
+            "▁to",
+            "▁tie",
+            "▁in",
+            "▁the",
+            "▁first",
+            "?",
+        ]
+        tokenizer = HuggingfaceTransformersTokenizer("xlnet-base-cased")
+
+        # tokenize with offsets
+        tokens, offsets = list(
+            map(list, zip(*[(t.text, t.idx) for t in tokenizer.tokenize_with_offsets(sentence)]))
+        )
+        assert tokens == expected_tokens
+        assert offsets == [0, 4, 9, 16, 20, 24, 24, 26, 29, 31, 35, 40, 43, 47, 50, 54, 59]
