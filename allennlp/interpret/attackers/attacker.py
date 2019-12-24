6d8d22ea180f9ae82459477c7b70d07ec0879227
c4n <udomc.can@gmail.com>
2019-12-24 17:38:38 +0700

    Revert attacker to allennlp original version

--- allennlp/interpret/attackers/attacker.py
+++ allennlp/interpret/attackers/attacker.py
@@ -3,7 +3,6 @@ from typing import List
 from allennlp.common import Registrable
 from allennlp.common.util import JsonDict
 from allennlp.predictors import Predictor
-from allennlp.data import Instance
 
 
 class Attacker(Registrable):
@@ -39,43 +38,6 @@ class Attacker(Registrable):
         inputs : ``JsonDict``
             The input you want to attack (the same as the argument to a Predictor, e.g.,
             predict_json()).
-        input_field_to_attack : ``str``
-            The key in the inputs JsonDict you want to attack, e.g., ``tokens``.
-        grad_input_field : ``str``
-            The field in the gradients dictionary that contains the input gradients.  For example,
-            `grad_input_1` will be the field for single input tasks. See get_gradients() in
-            `Predictor` for more information on field names.
-        target : ``JsonDict``
-            If given, this is a `targeted` attack, trying to change the prediction to a particular
-            value, instead of just changing it from its original prediction.  Subclasses are not
-            required to accept this argument, as not all attacks make sense as targeted attacks.
-            Perhaps that means we should make the API more crisp, but adding another class is not
-            worth it.
-
-        Returns
-        -------
-        reduced_input : ``JsonDict``
-            Contains the final, sanitized input after adversarial modification.
-        """
-        raise NotImplementedError()
-
-    def attack_from_instance(
-        self,
-        instance: Instance,
-        input_field_to_attack: str,
-        grad_input_field: str,
-        ignore_tokens: List[str],
-        target: JsonDict,
-    ) -> JsonDict:
-        """
-        This function finds a modification to the input instance that would change the model's
-        prediction in some desired manner (e.g., an adversarial attack).
-
-        Parameters
-        ----------
-        instance : ``Instance``
-            The instance you want to attack (the same as the argument to a Predictor, e.g.,
-            predict_instance()).
         input_field_to_attack : ``str``
             The key in the inputs JsonDict you want to attack, e.g., ``tokens``.
         grad_input_field : ``str``
