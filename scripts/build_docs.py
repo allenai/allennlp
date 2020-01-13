49fefee5066f911cc61c4e160c40385dec5369ad
Mark Neumann <markn@allenai.org>
2020-01-13 09:00:06 -0800

    PR comments

--- scripts/build_docs.py
+++ scripts/build_docs.py
@@ -2,7 +2,6 @@ from typing import Dict
 import os
 from pathlib import Path
 from subprocess import check_output
-import argparse
 
 from ruamel.yaml import YAML
 
@@ -10,10 +9,12 @@ from ruamel.yaml import YAML
 exclude_files = [".DS_Store", "__init__.py", "__init__.pyc", "README.md", "version.py", "run.py"]
 
 
-def render_docs(relative_src_path: str, src_file: str, to_file: str, modifier="++") -> None:
+def render_file(relative_src_path: str, src_file: str, to_file: str, modifier="++") -> None:
     """
     Shells out to pydocmd, which creates a .md file from the docstrings of python functions and classes in
-    the file we specify.
+    the file we specify. The modifer specifies the depth at which to generate docs for classes and functions
+    in the file. More information here: https://pypi.org/project/pydoc-markdown/
+
     """
     relative_src_namespace = relative_src_path.replace("/", ".")
     src_base = src_file.replace(".py", "")
@@ -31,26 +32,21 @@ def render_docs(relative_src_path: str, src_file: str, to_file: str, modifier="+
     print(f"Built docs for {src_file}: {to_file}")
 
 
-def build_docs_for_file(
-    relative_path: str, file_name: str, docs_dir: str, check: bool = False
-) -> Dict[str, str]:
+def build_docs_for_file(relative_path: str, file_name: str, docs_dir: str,) -> Dict[str, str]:
     """
-    Build docs for an individual python file. If `check` is passed, we don't generate the docs themselves,
-    but instead we just generate what the corresponding mkdocs YAML would be, so we can compare it to the
-    current version and see if it has changed (which would mean the docs need re-building).
+    Build docs for an individual python file.
     """
     clean_filename = file_name.replace(".py", "")
     markdown_filename = f"{clean_filename}.md"
 
     output_path = os.path.join(docs_dir, relative_path, markdown_filename)
     nav_path = os.path.join("api", relative_path, markdown_filename)
-    if not check:
-        render_docs(relative_path, file_name, output_path)
+    render_file(relative_path, file_name, output_path)
 
     return {os.path.basename(clean_filename): nav_path}
 
 
-def build_docs(root_path: str, docs_dir: str, check: bool = False):
+def build_docs(root_path: str, docs_dir: str):
 
     nav_root = []
 
@@ -70,7 +66,7 @@ def build_docs(root_path: str, docs_dir: str, check: bool = False):
             os.mkdir(target_dir)
 
         if os.path.isdir(relative_path):
-            nav_subsection = build_docs(relative_path, docs_dir, check)
+            nav_subsection = build_docs(relative_path, docs_dir)
             if not nav_subsection:
                 continue
             nav_root.append({child: nav_subsection})
@@ -79,7 +75,7 @@ def build_docs(root_path: str, docs_dir: str, check: bool = False):
             if child in exclude_files or not child.endswith(".py"):
                 continue
 
-            nav = build_docs_for_file(without_allennlp, child, docs_dir, check)
+            nav = build_docs_for_file(without_allennlp, child, docs_dir)
             nav_root.append(nav)
 
     return nav_root
@@ -87,24 +83,16 @@ def build_docs(root_path: str, docs_dir: str, check: bool = False):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Builds the Markdown docs for allennlp.")
-    parser.add_argument(
-        "--check", help="If passed, just check if the docs need rebuilding.", action="store_true"
-    )
-
-    args = parser.parse_args()
-    if args.check:
-        print("Checking if changes are required to the auto-generated documentation.")
-    else:
-        print("Building the docs.")
-
+    print("Building the docs.")
     parent_folder_path = Path(__file__).parent.parent
     yaml_path = parent_folder_path / "mkdocs.yml"
     source_path = parent_folder_path / "allennlp"
     docs_dir = str(parent_folder_path / "docs" / "api")
+    if not os.path.exists(docs_dir):
+        os.mkdir(docs_dir)
     yaml = YAML()
 
-    nav_entries = build_docs(source_path, docs_dir, check=args.check)
+    nav_entries = build_docs(source_path, docs_dir)
     nav_entries.sort(key=lambda x: list(x)[0], reverse=False)
 
     mkdocs_yaml = yaml.load(yaml_path)
@@ -116,15 +104,7 @@ if __name__ == "__main__":
         if docs_key in nav_obj:
             break
 
-    if args.check:
-        if nav_obj[docs_key] != nav_entries:
-            raise ValueError(
-                "Changes are required to the API documentation. Run `./scripts/build_docs.py` and commit the changes."
-            )
-        else:
-            print("No changes required!")
-    else:
-        nav_obj[docs_key] = nav_entries
+    nav_obj[docs_key] = nav_entries
 
     with open(yaml_path, "w") as f:
         yaml.dump(mkdocs_yaml, f)
