9704ba55f27440d65e16c3b9e53a83da1a3e6e78
Mark Neumann <markn@allenai.org>
2020-01-14 16:53:31 -0800

    remove deprecated language modelling reader (#3620)
    * remove deprecated language modelling reader
    
    * catch another LM reader ref

--- allennlp/tests/common/registrable_test.py
+++ allennlp/tests/common/registrable_test.py
@@ -83,7 +83,6 @@ class TestRegistrable(AllenNlpTestCase):
     def test_registry_has_builtin_dataset_readers(self):
         assert DatasetReader.by_name("snli").__name__ == "SnliReader"
         assert DatasetReader.by_name("sequence_tagging").__name__ == "SequenceTaggingDatasetReader"
-        assert DatasetReader.by_name("language_modeling").__name__ == "LanguageModelingReader"
 
     def test_registry_has_builtin_iterators(self):
         assert DataIterator.by_name("basic").__name__ == "BasicIterator"
