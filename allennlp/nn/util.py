8dabdac7a5d3b8ff66503e12450e94be354d0fd0
Zhaofeng Wu <wuzhaofeng1997@gmail.com>
2020-01-17 15:05:10 -0800

    Factor out batched_span_select code

--- allennlp/nn/util.py
+++ allennlp/nn/util.py
@@ -1260,6 +1260,73 @@ def batched_index_select(
     return selected_targets
 
 
+def batched_span_select(target: torch.Tensor, spans: torch.LongTensor) -> torch.Tensor:
+    """
+    The given ``spans`` of size ``(batch_size, num_spans, 2)`` indexes into the sequence
+    dimension (dimension 2) of the target, which has size ``(batch_size, sequence_length,
+    embedding_size)``.
+
+    This function returns segmented spans in the target with respect to the provided span indices.
+    It does not guarantee element order within each span.
+
+    # Parameters
+
+    target : ``torch.Tensor``, required.
+        A 3 dimensional tensor of shape (batch_size, sequence_length, embedding_size).
+        This is the tensor to be indexed.
+    indices : ``torch.LongTensor``
+        A 3 dimensional tensor of shape (batch_size, num_spans, 2) representing start and end
+        indices (both inclusive) into the ``sequence_length`` dimension of the ``target`` tensor.
+
+    # Returns
+
+    span_embeddings : ``torch.Tensor``
+        A tensor with shape (batch_size, num_spans, max_batch_span_width, embedding_size]
+        representing the embedded spans extracted from the batch flattened target tensor.
+    span_mask: ``torch.LongTensor``
+        A tensor with shape (batch_size, num_spans, max_batch_span_width) representing the mask on
+        the returned span embeddings.
+    """
+    # both of shape (batch_size, num_spans, 1)
+    span_starts, span_ends = spans.split(1, dim=-1)
+
+    # shape (batch_size, num_spans, 1)
+    # These span widths are off by 1, because the span ends are `inclusive`.
+    span_widths = span_ends - span_starts
+
+    # We need to know the maximum span width so we can
+    # generate indices to extract the spans from the sequence tensor.
+    # These indices will then get masked below, such that if the length
+    # of a given span is smaller than the max, the rest of the values
+    # are masked.
+    max_batch_span_width = span_widths.max().item() + 1
+
+    # Shape: (1, 1, max_batch_span_width)
+    max_span_range_indices = get_range_vector(
+        max_batch_span_width, get_device_of(target)
+    ).view(1, 1, -1)
+    # Shape: (batch_size, num_spans, max_batch_span_width)
+    # This is a broadcasted comparison - for each span we are considering,
+    # we are creating a range vector of size max_span_width, but masking values
+    # which are greater than the actual length of the span.
+    #
+    # We're using <= here (and for the mask below) because the span ends are
+    # inclusive, so we want to include indices which are equal to span_widths rather
+    # than using it as a non-inclusive upper bound.
+    span_mask = (max_span_range_indices <= span_widths).float()
+    raw_span_indices = span_ends - max_span_range_indices
+    # We also don't want to include span indices which are less than zero,
+    # which happens because some spans near the beginning of the sequence
+    # have an end index < max_batch_span_width, so we add this to the mask here.
+    span_mask = span_mask * (raw_span_indices >= 0).float()
+    span_indices = torch.nn.functional.relu(raw_span_indices.float()).long()
+
+    # Shape: (batch_size, num_spans, max_batch_span_width, embedding_dim)
+    span_embeddings = batched_index_select(target, span_indices)
+
+    return span_embeddings, span_mask
+
+
 def flattened_index_select(target: torch.Tensor, indices: torch.LongTensor) -> torch.Tensor:
     """
     The given `indices` of size `(set_size, subset_size)` specifies subsets of the `target`
--- allennlp/tests/nn/util_test.py
+++ allennlp/tests/nn/util_test.py
@@ -1053,6 +1053,42 @@ class TestNnUtil(AllenNlpTestCase):
         with pytest.raises(ConfigurationError):
             util.batched_index_select(targets, indices)
 
+    def test_batched_span_select(self):
+        # Each element is a vector of it's index.
+        targets = torch.ones([3, 12, 2]).cumsum(1) - 1
+        spans = torch.LongTensor([
+            [[0, 0], [1, 2], [5, 8], [10, 10]],
+            [[i, i] for i in range(3, -1, -1)],
+            [[0, 3], [1, 4], [2, 5], [10, 11]],
+        ])
+        selected, mask = util.batched_span_select(targets, spans)
+
+        selected = torch.where(
+            mask.unsqueeze(-1).bool(), selected, torch.empty_like(selected).fill_(-1)
+        )
+
+        # breakpoint()
+        numpy.testing.assert_array_equal(
+            selected,
+            [
+                [
+                    [[0, 0], [-1, -1], [-1, -1], [-1, -1]],
+                    [[2, 2], [1, 1], [-1, -1], [-1, -1]],
+                    [[8, 8], [7, 7], [6, 6], [5, 5]],
+                    [[10, 10], [-1, -1], [-1, -1], [-1, -1]],
+                ],
+                [
+                    [[i, i], [-1, -1], [-1, -1], [-1, -1]] for i in range(3, -1, -1)
+                ],
+                [
+                    [[3, 3], [2, 2], [1, 1], [0, 0]],
+                    [[4, 4], [3, 3], [2, 2], [1, 1]],
+                    [[5, 5], [4, 4], [3, 3], [2, 2]],
+                    [[11, 11], [10, 10], [-1, -1], [-1, -1]],
+                ],
+            ]
+        )
+
     def test_flattened_index_select(self):
         indices = numpy.array([[1, 2], [3, 4]])
         targets = torch.ones([2, 6, 3]).cumsum(1) - 1
