a80bc62979dc4b8681c5c0ac28ed31421e85cf97
Zhaofeng Wu <wuzhaofeng1997@gmail.com>
2020-01-17 16:09:28 -0800

    Use util.batch_span_select to simply embedder

--- allennlp/modules/token_embedders/pretrained_transformer_pretokenized_embedder.py
+++ allennlp/modules/token_embedders/pretrained_transformer_pretokenized_embedder.py
@@ -2,6 +2,7 @@ from overrides import overrides
 import torch
 
 from allennlp.modules.token_embedders import PretrainedTransformerEmbedder, TokenEmbedder
+from allennlp.nn import util
 
 
 @TokenEmbedder.register("pretrained_transformer_pretokenized")
@@ -9,7 +10,8 @@ class PretrainedTransformerPretokenizedEmbedder(PretrainedTransformerEmbedder):
     """
     Use this embedder when input comes from pre-tokenized text, `PretrainedTransformerTokenizer` was
     not used in the dataset loader, and `PretrainedTransformerPretokenizedIndexer` independently
-    tokenized each word into subword wordpieces.
+    tokenized each word into subword wordpieces. This embedder sums the wordpiece representations
+    to get word-level representations.
     """
 
     def forward(
@@ -38,26 +40,18 @@ class PretrainedTransformerPretokenizedEmbedder(PretrainedTransformerEmbedder):
 
         Shape: [batch_size, num_orig_tokens, embedding_size].
         """
-        device = token_ids.device
-
         # Shape: [batch_size, num_wordpieces, embedding_size].
         embeddings = super().forward(token_ids, wordpiece_mask)
 
-        batch_size, _, embedding_size = embeddings.size()
-        num_orig_tokens = offsets.size(1)
+        # span_embeddings: (batch_size, num_orig_tokens, max_span_length, embedding_size)
+        # span_mask: (batch_size, num_orig_tokens, max_span_length)
+        span_embeddings, span_mask = util.batched_span_select(embeddings, offsets)
+        span_mask = span_mask.unsqueeze(-1)
+        span_embeddings *= span_mask  # zero out paddings
 
-        orig_embeddings = torch.FloatTensor(
-            batch_size, num_orig_tokens, embedding_size
-        ).to(device)
-        for batch_idx in range(batch_size):  # TODO: do we have to use loops?
-            for token_idx in range(num_orig_tokens):
-                if not mask[batch_idx, token_idx]:
-                    continue
-
-                start_offset, end_offset = offsets[batch_idx, token_idx]
-                end_offset += 1  # inclusive to exclusive
-                assert wordpiece_mask[batch_idx, start_offset:end_offset].bool().all()
-                embedding = embeddings[batch_idx, start_offset:end_offset].mean(0)
-                orig_embeddings[batch_idx, token_idx] = embedding
+        span_embeddings_sum = span_embeddings.sum(2)
+        span_embeddings_len = span_mask.sum(2)
+        # Shape: (batch_size, num_orig_tokens, embedding_size)
+        orig_embeddings = span_embeddings_sum / span_embeddings_len
 
         return orig_embeddings
