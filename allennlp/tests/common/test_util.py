0e847d2cee98353026d89793adbdf26025ba8116
Santiago Castro <sacastro@umich.edu>
2020-01-23 19:57:42 -0500

    Test with installed packages

--- allennlp/tests/common/test_util.py
+++ allennlp/tests/common/test_util.py
@@ -5,10 +5,11 @@ import sys
 import tempfile
 from collections import OrderedDict
 from os import PathLike
-from typing import Generator, Union
+from typing import Generator, TypeVar, Union
 
 import pytest
 import torch
+from pip._internal.cli.main import main as pip_main
 
 from allennlp.commands import Subcommand
 from allennlp.common import util
@@ -24,12 +25,24 @@ class Sanitizable:
         return {"sanitizable": True}
 
 
+PathType = Union[bytes, PathLike, str]
+T = TypeVar("T")
+ContextManagerFunctionReturnType = Generator[T, None, None]
+
+
 @contextlib.contextmanager
-def pushd(new_dir: Union[bytes, PathLike, str]) -> Generator[None, None, None]:
+def pushd(new_dir: PathType) -> ContextManagerFunctionReturnType[None]:
     previous_dir = os.getcwd()
     os.chdir(new_dir)
     yield
     os.chdir(previous_dir)
+
+
+@contextlib.contextmanager
+def pip_install(path: PathType, package_name: str) -> ContextManagerFunctionReturnType[None]:
+    pip_main(["install", str(path)])
+    yield
+    pip_main(["uninstall", "-y", package_name])
 
 
 class TestCommonUtils(AllenNlpTestCase):
@@ -108,25 +121,28 @@ class TestCommonUtils(AllenNlpTestCase):
         plugins_root = self.FIXTURES_ROOT / "plugins"
         project_a_fixtures_root = plugins_root / "project_a"
         project_b_fixtures_root = plugins_root / "project_b"
+        project_c_fixtures_root = plugins_root / "project_c"
 
-        with tempfile.TemporaryDirectory() as temp_dir_a, tempfile.TemporaryDirectory() as temp_dir_b:
+        with tempfile.TemporaryDirectory() as temp_dir_a, tempfile.TemporaryDirectory() as temp_dir_b, tempfile.TemporaryDirectory() as temp_dir_c:
             distutils.dir_util.copy_tree(project_a_fixtures_root, temp_dir_a)
             distutils.dir_util.copy_tree(project_b_fixtures_root, temp_dir_b)
+            distutils.dir_util.copy_tree(project_c_fixtures_root, temp_dir_c)
 
-            # We make one plugin available in the path, in another directory as if it were another project.
-            sys.path.append(temp_dir_a)
-
-            # We move to another directory with a different plugin, as if it were other project.
-            with pushd(temp_dir_b):
+            # We make plugins "a" and "c" available as packages, each from other directories, as if they were
+            # separate installed projects ("global" usage of the plugins).
+            # We move to another directory with a different plugin "b", as if it were another separate project
+            # which is not installed ("local" usage of the plugins).
+            with pip_install(temp_dir_a, "a"), pushd(temp_dir_b), pip_install(temp_dir_c, "c"):
                 # In general when we run scripts or commands in a project, the current directory is the root of it
                 # and is part of the path. So we emulate this here.
                 sys.path.append(".")
 
                 available_plugins = list(util.discover_plugins())
-                self.assertEqual(len(available_plugins), 2)
+                self.assertEqual(len(available_plugins), 3)
 
                 util.import_plugins()
                 # As a secondary effect of importing, the new subcommands should be available.
                 subcommands_available = {t.__name__ for t in Subcommand.__subclasses__()}
                 self.assertIn("A", subcommands_available)
                 self.assertIn("B", subcommands_available)
+                self.assertIn("C", subcommands_available)
