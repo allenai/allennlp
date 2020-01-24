1b462d3507d33a42322a6034054130eaf6f3341e
Santiago Castro <sacastro@umich.edu>
2020-01-23 21:18:31 -0500

    Add plugin discoverability from a file

--- allennlp/common/util.py
+++ allennlp/common/util.py
@@ -585,9 +585,22 @@ def discover_namespace_plugins() -> Iterable[pkgutil.ModuleInfo]:
         return []
 
 
+def discover_file_plugins() -> Iterable[str]:
+    plugins_filename = ".allennlp_plugins"
+    if os.path.isfile(plugins_filename):
+        with open(plugins_filename) as file_:
+            for module_name in file_.readlines():
+                module_name = module_name.strip()
+                if module_name:
+                    yield module_name
+    else:
+        return []
+
+
 def discover_plugins() -> Iterable[str]:
     for module_info in discover_namespace_plugins():
         yield module_info.name
+    yield from discover_file_plugins()
 
 
 def import_plugins() -> None:
