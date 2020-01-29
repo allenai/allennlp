f44d3dbdbe77e93476192fa74aae651449e4e4ad
Matt Gardner <mattg@allenai.org>
2020-01-29 15:14:05 -0800

    Fix handling of TokenCharactersIndexer in Hotflip (#3695)

--- allennlp/tests/interpret/hotflip_test.py
+++ allennlp/tests/interpret/hotflip_test.py
@@ -1,5 +1,7 @@
 from allennlp.common.testing import AllenNlpTestCase
+from allennlp.data.token_indexers import TokenCharactersIndexer
 from allennlp.models.archival import load_archive
+from allennlp.modules.token_embedders import EmptyEmbedder
 from allennlp.predictors import Predictor
 from allennlp.interpret.attackers import Hotflip
 from allennlp.interpret.attackers.hotflip import DEFAULT_IGNORE_TOKENS
@@ -18,6 +20,33 @@ class TestHotflip(AllenNlpTestCase):
             self.FIXTURES_ROOT / "decomposable_attention" / "serialization" / "model.tar.gz"
         )
         predictor = Predictor.from_archive(archive, "textual-entailment")
+
+        hotflipper = Hotflip(predictor)
+        hotflipper.initialize()
+        attack = hotflipper.attack_from_json(inputs, "hypothesis", "grad_input_1")
+        assert attack is not None
+        assert "final" in attack
+        assert "original" in attack
+        assert "outputs" in attack
+        assert len(attack["final"][0]) == len(
+            attack["original"]
+        )  # hotflip replaces words without removing
+
+    def test_with_token_characters_indexer(self):
+
+        inputs = {
+            "premise": "I always write unit tests for my code.",
+            "hypothesis": "One time I didn't write any unit tests for my code.",
+        }
+
+        archive = load_archive(
+            self.FIXTURES_ROOT / "decomposable_attention" / "serialization" / "model.tar.gz"
+        )
+        predictor = Predictor.from_archive(archive, "textual-entailment")
+        predictor._dataset_reader._token_indexers["chars"] = TokenCharactersIndexer(
+            min_padding_length=1
+        )
+        predictor._model._text_field_embedder._token_embedders["chars"] = EmptyEmbedder()
 
         hotflipper = Hotflip(predictor)
         hotflipper.initialize()
