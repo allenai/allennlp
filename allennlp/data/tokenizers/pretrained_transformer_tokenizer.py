b1d754ec6820e7f71755fa21d08a40c689445a7d
Zhaofeng Wu <wuzhaofeng1997@gmail.com>
2020-01-31 15:51:28 -0800

    Formatting

--- allennlp/data/tokenizers/pretrained_transformer_tokenizer.py
+++ allennlp/data/tokenizers/pretrained_transformer_tokenizer.py
@@ -50,7 +50,8 @@ class PretrainedTransformerTokenizer(Tokenizer):
         - 'do_not_truncate': Do not truncate (raise an error if the input sequence is longer than max_length)
     calculate_character_offsets : `bool`, optional (default=False)
         Attempts to reconstruct character offsets for the instances of Token that this tokenizer produces.
-    tokenizer_kwargs: 'Dict[str, Any]' - dictionary with additional arguments for AutoTokenizer.from_pretrained.
+    tokenizer_kwargs: 'Dict[str, Any]'
+        Dictionary with additional arguments for `AutoTokenizer.from_pretrained`.
 
     Argument descriptions are from
     https://github.com/huggingface/transformers/blob/155c782a2ccd103cf63ad48a2becd7c76a7d2115/transformers/tokenization_utils.py#L691
@@ -66,9 +67,8 @@ class PretrainedTransformerTokenizer(Tokenizer):
         calculate_character_offsets: bool = False,
         tokenizer_kwargs: Dict[str, Any] = None,
     ) -> None:
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            model_name, **(tokenizer_kwargs if tokenizer_kwargs is not None else {})
-        )
+        tokenizer_kwargs = tokenizer_kwargs or {}
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_kwargs)
 
         # Huggingface tokenizers have different ways of remembering whether they lowercase or not. Detecting it
         # this way seems like the least brittle way to do it.
