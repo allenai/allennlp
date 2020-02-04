9d6ea54231e1eccdd28ea8af5ee377bb2b2a65ab
Xianyang Chen <xchen002@ets.org>
2020-02-04 13:22:35 -0500

    fix MultiLabelField

--- allennlp/data/fields/multilabel_field.py
+++ allennlp/data/fields/multilabel_field.py
@@ -128,7 +128,7 @@ class MultiLabelField(Field[torch.Tensor]):
 
     @overrides
     def empty_field(self):
-        return MultiLabelField([], self._label_namespace, skip_indexing=True)
+        return MultiLabelField([], self._label_namespace, skip_indexing=True, num_labels=self._num_labels)
 
     def __str__(self) -> str:
         return (
