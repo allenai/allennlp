d7274802e0806bf052a87af2783103d3b9302eaa
Dirk Groeneveld <dirkg@allenai.org>
2020-02-03 16:04:05 -0800

    Makes taking the new parameters optional

--- allennlp/training/learning_rate_schedulers/learning_rate_scheduler.py
+++ allennlp/training/learning_rate_schedulers/learning_rate_scheduler.py
@@ -26,9 +26,15 @@ class LearningRateScheduler(Scheduler, Registrable):
         params: Params,
     ):  # type: ignore
         scheduler_type = params.pop_choice("type", LearningRateScheduler.list_available())
-        construct_params = {"num_epochs": num_epochs, "batches_per_epoch": batches_per_epoch}
-        construct_params.update(params.as_dict())
-        scheduler = LearningRateScheduler.by_name(scheduler_type)(optimizer, **construct_params)  # type: ignore
+        constructor = LearningRateScheduler.by_name(scheduler_type)
+        from inspect import signature
+        constructor_parameters = signature(constructor).parameters.keys()
+        params = params.as_dict()
+        if "num_epochs" in constructor_parameters and "num_epochs" not in params:
+            params["num_epochs"] = num_epochs
+        if "batches_per_epoch" in constructor_parameters and "batches_per_epoch" not in params:
+            params["batches_per_epoch"] = batches_per_epoch
+        scheduler = constructor(optimizer, **params)  # type: ignore
         if isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
             return _PyTorchLearningRateSchedulerWithMetricsWrapper(scheduler)
         elif isinstance(scheduler, torch.optim.lr_scheduler._LRScheduler):
