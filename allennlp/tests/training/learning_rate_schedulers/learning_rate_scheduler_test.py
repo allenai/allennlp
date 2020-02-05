346f388608a2ef20cf49dbc7ebeafa94dee7214a
Matt Gardner <mattg@allenai.org>
2020-02-05 12:46:08 -0800

    black

--- allennlp/tests/training/learning_rate_schedulers/learning_rate_scheduler_test.py
+++ allennlp/tests/training/learning_rate_schedulers/learning_rate_scheduler_test.py
@@ -15,20 +15,26 @@ class LearningRateSchedulersTest(AllenNlpTestCase):
     def test_reduce_on_plateau_error_throw_when_no_metrics_exist(self):
         with self.assertRaises(ConfigurationError) as context:
             LearningRateScheduler.from_params(
-                optimizer=Optimizer.from_params(self.model.named_parameters(), Params({"type": "adam"})),
+                optimizer=Optimizer.from_params(
+                    self.model.named_parameters(), Params({"type": "adam"})
+                ),
                 params=Params({"type": "reduce_on_plateau"}),
             ).step(None, None)
         assert "learning rate scheduler requires a validation metric" in str(context.exception)
 
     def test_reduce_on_plateau_works_when_metrics_exist(self):
         LearningRateScheduler.from_params(
-            optimizer=Optimizer.from_params(self.model.named_parameters(), Params({"type": "adam"})),
+            optimizer=Optimizer.from_params(
+                self.model.named_parameters(), Params({"type": "adam"})
+            ),
             params=Params({"type": "reduce_on_plateau"}),
         ).step(10, None)
 
     def test_no_metric_wrapper_can_support_none_for_metrics(self):
         lrs = LearningRateScheduler.from_params(
-            optimizer=Optimizer.from_params(self.model.named_parameters(), Params({"type": "adam"})),
+            optimizer=Optimizer.from_params(
+                self.model.named_parameters(), Params({"type": "adam"})
+            ),
             params=Params({"type": "step", "step_size": 1}),
         )
         lrs.lr_scheduler.optimizer.step()  # to avoid a pytorch warning
@@ -36,7 +42,9 @@ class LearningRateSchedulersTest(AllenNlpTestCase):
 
     def test_noam_learning_rate_schedule_does_not_crash(self):
         lrs = LearningRateScheduler.from_params(
-            optimizer=Optimizer.from_params(self.model.named_parameters(), Params({"type": "adam"})),
+            optimizer=Optimizer.from_params(
+                self.model.named_parameters(), Params({"type": "adam"})
+            ),
             params=Params({"type": "noam", "model_size": 10, "warmup_steps": 2000}),
         )
         lrs.step(None, None)
