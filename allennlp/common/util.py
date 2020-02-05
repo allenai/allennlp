b83ce23b6921342f9053ed6f15d52571cbb93f52
Guo Quan <guoquanscu@gmail.com>
2020-02-04 21:49:35 -0500

    Update gpu_memory_mb() not to log at error level
    According to the discussion in #2167 , there are known occasional failures in this non-critical function.
    Log as a warning with the stack trace and error information.

--- allennlp/common/util.py
+++ allennlp/common/util.py
@@ -513,7 +513,8 @@ def gpu_memory_mb() -> Dict[int, int]:
     except:  # noqa
         # Catch *all* exceptions, because this memory check is a nice-to-have
         # and we'd never want a training run to fail because of it.
-        logger.exception("unable to check gpu_memory_mb(), continuing")
+        logger.warning("unable to check gpu_memory_mb() due to "
+                       "occasional failure, continuing", exc_info=True)
         return {}
 
 
