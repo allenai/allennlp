a39fec9ca092b3cbc889ade4ba3bb157965e49e5
Mark Neumann <markn@allenai.org>
2020-02-12 09:50:54 -0800

    santiago's suggested change
    Co-Authored-By: Santiago Castro <sacastro@umich.edu>

--- allennlp/version.py
+++ allennlp/version.py
@@ -5,7 +5,7 @@ _MINOR = "9"
 
 # We check the environment for the revision here, because it
 # enables us to set it to a git sha when doing automated nightly releases.
-_REVISION = os.environ.get("ALLENNLP_REVISION", None) or "1-unreleased"
+_REVISION = os.environ.get("ALLENNLP_REVISION", "1-unreleased")
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
 VERSION = "{0}.{1}.{2}".format(_MAJOR, _MINOR, _REVISION)
