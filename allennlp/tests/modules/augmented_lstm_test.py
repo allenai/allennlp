5bb5e6418c84c8937c25e65b30ed804622679860
Bruno Cabral <bruno@potelo.com.br>
2020-02-13 00:47:28 -0300

    Rewrite the augmented LSTM to use the improvments from PyText rewrite. Fixes #2482

--- allennlp/tests/modules/augmented_lstm_test.py
+++ allennlp/tests/modules/augmented_lstm_test.py
@@ -8,7 +8,7 @@ from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 from allennlp.common.checks import ConfigurationError
 from allennlp.common.params import Params
 from allennlp.common.testing import AllenNlpTestCase
-from allennlp.modules.augmented_lstm import AugmentedLstm
+from allennlp.modules.augmented_lstm import AugmentedLstm, AugmentedLSTMCell, AugmentedLSTMUnidirectional
 from allennlp.nn import InitializerApplicator, Initializer
 from allennlp.nn.util import sort_batch_by_length
 
@@ -48,7 +48,7 @@ class TestAugmentedLSTM(AllenNlpTestCase):
         tensor = pack_padded_sequence(
             sorted_tensor, sorted_sequence.data.tolist(), batch_first=True
         )
-        lstm = AugmentedLstm(10, 11, go_forward=False)
+        lstm = AugmentedLSTMUnidirectional(10, 11, go_forward=False)
         output, _ = lstm(tensor)
         output_sequence, _ = pad_packed_sequence(output, batch_first=True)
 
@@ -111,12 +111,12 @@ class TestAugmentedLSTM(AllenNlpTestCase):
             lstm(tensor)
 
     def test_augmented_lstm_is_initialized_with_correct_biases(self):
-        lstm = AugmentedLstm(2, 3)
+        lstm = AugmentedLSTMCell(2, 3)
         true_state_bias = numpy.array([0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         numpy.testing.assert_array_equal(lstm.state_linearity.bias.data.numpy(), true_state_bias)
 
         # Non-highway case.
-        lstm = AugmentedLstm(2, 3, use_highway=False)
+        lstm = AugmentedLSTMCell(2, 3, use_highway=False)
         true_state_bias = numpy.array([0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0])
         numpy.testing.assert_array_equal(lstm.state_linearity.bias.data.numpy(), true_state_bias)
 
@@ -127,7 +127,8 @@ class TestAugmentedLSTM(AllenNlpTestCase):
         tensor = pack_padded_sequence(
             sorted_tensor, sorted_sequence.data.tolist(), batch_first=True
         )
-        lstm = AugmentedLstm(10, 11, recurrent_dropout_probability=0.5)
+        lstm = AugmentedLSTMUnidirectional(10, 11, recurrent_dropout_probability=0.5)
+
         output, (hidden_state, _) = lstm(tensor)
         output_sequence, _ = pad_packed_sequence(output, batch_first=True)
         # Test returned output sequence
@@ -143,8 +144,8 @@ class TestAugmentedLSTM(AllenNlpTestCase):
         assert not num_hidden_dims_zero_across_timesteps
 
     def test_dropout_version_is_different_to_no_dropout(self):
-        augmented_lstm = AugmentedLstm(10, 11)
-        dropped_augmented_lstm = AugmentedLstm(10, 11, recurrent_dropout_probability=0.9)
+        augmented_lstm = AugmentedLSTMUnidirectional(10, 11)
+        dropped_augmented_lstm = AugmentedLSTMUnidirectional(10, 11, recurrent_dropout_probability=0.9)
         # Initialize all weights to be == 1.
         constant_init = Initializer.from_params(Params({"type": "constant", "val": 0.5}))
         initializer = InitializerApplicator([(".*", constant_init)])
