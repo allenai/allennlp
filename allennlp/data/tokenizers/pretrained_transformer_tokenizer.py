592362b21545b6d8bc9813b770aac235e24cbfad
Dirk Groeneveld <dirkg@allenai.org>
2020-02-27 16:08:49 -0800

    Use the new tokenizers

--- allennlp/data/tokenizers/pretrained_transformer_tokenizer.py
+++ allennlp/data/tokenizers/pretrained_transformer_tokenizer.py
@@ -1,7 +1,6 @@
 import logging
 from typing import Any, Dict, List, Optional, Tuple
 
-from allennlp.common.util import sanitize_wordpiece
 from overrides import overrides
 from transformers.tokenization_auto import AutoTokenizer
 
@@ -64,25 +63,17 @@ class PretrainedTransformerTokenizer(Tokenizer):
         max_length: int = None,
         stride: int = 0,
         truncation_strategy: str = "longest_first",
-        calculate_character_offsets: bool = False,
         tokenizer_kwargs: Dict[str, Any] = None,
     ) -> None:
         tokenizer_kwargs = tokenizer_kwargs or {}
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_kwargs)
-
-        # Huggingface tokenizers have different ways of remembering whether they lowercase or not. Detecting it
-        # this way seems like the least brittle way to do it.
-        tokenized = self.tokenizer.tokenize(
-            "A"
-        )  # Use a single character that won't be cut into word pieces.
-        detokenized = " ".join(tokenized)
-        self._tokenizer_lowercases = "a" in detokenized
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name, use_fast=True, **tokenizer_kwargs
+        )
 
         self._add_special_tokens = add_special_tokens
         self._max_length = max_length
         self._stride = stride
         self._truncation_strategy = truncation_strategy
-        self._calculate_character_offsets = calculate_character_offsets
 
         (
             self.num_added_start_tokens,
@@ -90,7 +81,7 @@ class PretrainedTransformerTokenizer(Tokenizer):
             self.num_added_end_tokens,
         ) = self._determine_num_special_tokens_added()
 
-    def _tokenize(self, sentence_1: str, sentence_2: str = None):
+    def _tokenize(self, sentence_1: str, sentence_2: str = None) -> List[Token]:
         """
         This method works on both sentence and sentence pair.
         """
@@ -103,70 +94,19 @@ class PretrainedTransformerTokenizer(Tokenizer):
             stride=self._stride,
             truncation_strategy=self._truncation_strategy,
             return_tensors=None,
+            return_offsets_mapping=True,
         )
         # token_ids contains a final list with ids for both regular and special tokens
-        token_ids, token_type_ids = encoded_tokens["input_ids"], encoded_tokens["token_type_ids"]
+        token_ids, token_type_ids, token_offsets = (
+            encoded_tokens["input_ids"],
+            encoded_tokens["token_type_ids"],
+            encoded_tokens["offset_mappings"],
+        )
 
         tokens = []
-        for token_id, token_type_id in zip(token_ids, token_type_ids):
+        for token_id, token_type_id, (start, end) in zip(token_ids, token_type_ids, token_offsets):
             token_str = self.tokenizer.convert_ids_to_tokens(token_id, skip_special_tokens=False)
-            tokens.append(Token(text=token_str, text_id=token_id, type_id=token_type_id))
-
-        if self._calculate_character_offsets:
-            # The huggingface tokenizers produce tokens that may or may not be slices from the
-            # original text.  Differences arise from lowercasing, Unicode normalization, and other
-            # kinds of normalization, as well as special characters that are included to denote
-            # various situations, such as "##" in BERT for word pieces from the middle of a word, or
-            # "Ġ" in RoBERTa for the beginning of words not at the start of a sentence.
-
-            # This code attempts to calculate character offsets while being tolerant to these
-            # differences. It scans through the text and the tokens in parallel, trying to match up
-            # positions in both. If it gets out of sync, it backs off to not adding any token
-            # indices, and attempts to catch back up afterwards. This procedure is approximate.
-            # Don't rely on precise results, especially in non-English languages that are far more
-            # affected by Unicode normalization.
-
-            whole_text = sentence_1
-            if sentence_2 is not None:
-                whole_text += sentence_2  # Calculating character offsets with sentence pairs is sketchy at best.
-            if self._tokenizer_lowercases:
-                whole_text = whole_text.lower()
-
-            min_allowed_skipped_whitespace = 3
-            allowed_skipped_whitespace = min_allowed_skipped_whitespace
-
-            text_index = 0
-            token_index = 0
-            while text_index < len(whole_text) and token_index < len(tokens):
-                token_text = tokens[token_index].text
-                if self._tokenizer_lowercases:
-                    token_text = token_text.lower()
-                token_text = sanitize_wordpiece(token_text)
-                token_start_index = whole_text.find(token_text, text_index)
-
-                # Did we not find it at all?
-                if token_start_index < 0:
-                    token_index += 1
-                    # When we skip a token, we increase our tolerance, so we have a chance of catching back up.
-                    allowed_skipped_whitespace += 1 + min_allowed_skipped_whitespace
-                    continue
-
-                # Did we jump too far?
-                non_whitespace_chars_skipped = sum(
-                    1 for c in whole_text[text_index:token_start_index] if not c.isspace()
-                )
-                if non_whitespace_chars_skipped > allowed_skipped_whitespace:
-                    # Too many skipped characters. Something is wrong. Ignore this token.
-                    token_index += 1
-                    # When we skip a token, we increase our tolerance, so we have a chance of catching back up.
-                    allowed_skipped_whitespace += 1 + min_allowed_skipped_whitespace
-                    continue
-                allowed_skipped_whitespace = min_allowed_skipped_whitespace
-
-                tokens[token_index].idx = token_start_index
-                text_index = token_start_index + len(token_text)
-                token_index += 1
-
+            tokens.append(Token(text=token_str, text_id=token_id, type_id=token_type_id, idx=start))
         return tokens
 
     def tokenize_sentence_pair(self, sentence_1: str, sentence_2: str) -> List[Token]:
