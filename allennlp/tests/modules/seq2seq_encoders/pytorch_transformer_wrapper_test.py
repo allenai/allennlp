9494aa1e5300ec0dc86ae4bd82845e8d0491f9e7
Dirk Groeneveld <groeneveld@gmail.com>
2020-03-05 14:52:48 -0800

    Makes test succeed

--- allennlp/tests/modules/seq2seq_encoders/pytorch_transformer_wrapper_test.py
+++ allennlp/tests/modules/seq2seq_encoders/pytorch_transformer_wrapper_test.py
@@ -13,6 +13,7 @@ def test_positional_embeddings(positional_encoding: Optional[str]):
     n_head = 5
     dims = 11 * n_head
     transformer = PytorchTransformer(dims, 3, positional_encoding=positional_encoding, num_attention_heads=n_head)
+    transformer.eval()
 
     inputs = torch.randn(batch_size, max_seq_len, dims)
     mask = torch.ones(batch_size, max_seq_len, dtype=torch.bool)
@@ -35,17 +36,26 @@ def test_mask_works(positional_encoding: Optional[str]):
     n_head = 2
     dims = 7 * n_head
     transformer = PytorchTransformer(dims, 2, positional_encoding=positional_encoding, num_attention_heads=n_head)
-
-    inputs = torch.randn(batch_size, max_seq_len, dims)
-    mask = torch.ones(batch_size, max_seq_len, dtype=torch.bool)
-    for b in range(batch_size):
-        mask[b, max_seq_len-b:] = False
-
     transformer.eval()
-    outputs1 = transformer(inputs, mask).detach()
-    # change masked values and run it again
-    inputs += (~mask).unsqueeze(2) * 10.0
-    outputs2 = transformer(inputs, mask).detach()
 
-    assert torch.allclose(outputs1, outputs2, equal_nan=True)
+    with torch.no_grad():
+        # Construct inputs and masks
+        inputs = torch.randn(batch_size, max_seq_len, dims)
+        all_ones_mask = torch.ones(batch_size, max_seq_len, dtype=torch.bool)
+        mask = all_ones_mask.clone()
+        for b in range(batch_size):
+            mask[b, max_seq_len-b:] = False
+        altered_inputs = inputs + (~mask).unsqueeze(2) * 10.0
+
+        # Make sure there is a difference without the mask
+        assert(
+            not torch.allclose(
+                transformer(inputs, all_ones_mask),
+                transformer(altered_inputs, all_ones_mask)))
+
+        # Make sure there is no difference with the mask
+        assert(
+            torch.allclose(
+                torch.masked_select(transformer(inputs, mask), mask.unsqueeze(2)),
+                torch.masked_select(transformer(altered_inputs, mask), mask.unsqueeze(2))))
 
