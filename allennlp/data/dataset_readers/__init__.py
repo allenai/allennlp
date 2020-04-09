0ba71cbceb8ade707142b569b4f8f456d2ef2878
Matt Gardner <mattg@allenai.org>
2020-04-09 16:46:47 -0700

    fix dataset reader tests

--- allennlp/data/dataset_readers/__init__.py
+++ allennlp/data/dataset_readers/__init__.py
@@ -15,7 +15,6 @@ from allennlp.data.dataset_readers.semantic_dependency_parsing import (
 )
 from allennlp.data.dataset_readers.sequence_tagging import SequenceTaggingDatasetReader
 from allennlp.data.dataset_readers.sharded_dataset_reader import ShardedDatasetReader
-from allennlp.data.dataset_readers.snli import SnliReader
 from allennlp.data.dataset_readers.stanford_sentiment_tree_bank import (
     StanfordSentimentTreeBankDatasetReader,
 )
