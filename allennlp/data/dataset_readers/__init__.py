1268ce031f77798e6eba8a73ea2dd1614663388c
Dirk Groeneveld <dirkg@allenai.org>
2020-04-16 11:30:25 -0700

    Moves the graph parser

--- allennlp/data/dataset_readers/__init__.py
+++ allennlp/data/dataset_readers/__init__.py
@@ -10,9 +10,6 @@ and produce datasets in the formats required by specific models.
 from allennlp.data.dataset_readers.conll2003 import Conll2003DatasetReader
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from allennlp.data.dataset_readers.interleaving_dataset_reader import InterleavingDatasetReader
-from allennlp.data.dataset_readers.semantic_dependency_parsing import (
-    SemanticDependenciesDatasetReader,
-)
 from allennlp.data.dataset_readers.sequence_tagging import SequenceTaggingDatasetReader
 from allennlp.data.dataset_readers.sharded_dataset_reader import ShardedDatasetReader
 from allennlp.data.dataset_readers.babi import BabiReader
--- allennlp/models/__init__.py
+++ allennlp/models/__init__.py
@@ -6,5 +6,4 @@ all of which are subclasses of `Model`.
 from allennlp.models.model import Model
 from allennlp.models.archival import archive_model, load_archive, Archive
 from allennlp.models.simple_tagger import SimpleTagger
-from allennlp.models.graph_parser import GraphParser
 from allennlp.models.basic_classifier import BasicClassifier
