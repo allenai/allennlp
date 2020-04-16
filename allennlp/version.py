59aca849f78eaf62b3b506027c9f72b2405195c1
Dirk Groeneveld <dirkg@allenai.org>
2020-04-16 15:30:09 -0700

    bump version number to v1.0.0.rc1

--- allennlp/version.py
+++ allennlp/version.py
@@ -1,15 +1,15 @@
 import os
 
-_MAJOR = "0"
-_MINOR = "9"
+_MAJOR = "1"
+_MINOR = "0"
 # On master and in a nightly release the patch should be one ahead of the last
 # released build.
-_PATCH = "1"
+_PATCH = "0"
 # For pre-release and build metadata. In an official release this must be the
 # empty string. On master we will default to "-unreleased" while in our nightly
 # builds this will have the syntax ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
-_SUFFIX = os.environ.get("ALLENNLP_VERSION_SUFFIX", "-unreleased")
+_SUFFIX = os.environ.get("ALLENNLP_VERSION_SUFFIX", ".rc1")
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
 VERSION = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
