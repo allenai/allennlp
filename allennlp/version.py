3f5eabb2cb4d551c71e592c46e9617ddbddba98d
Dirk Groeneveld <dirkg@allenai.org>
2020-04-16 15:58:00 -0700

    Bump version numbers to v1.0.0.rc2-unreleased

--- allennlp/version.py
+++ allennlp/version.py
@@ -9,7 +9,7 @@ _PATCH = "0"
 # empty string. On master we will default to "-unreleased" while in our nightly
 # builds this will have the syntax ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
-_SUFFIX = os.environ.get("ALLENNLP_VERSION_SUFFIX", ".rc1")
+_SUFFIX = os.environ.get("ALLENNLP_VERSION_SUFFIX", ".rc2-unreleased")
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
 VERSION = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
