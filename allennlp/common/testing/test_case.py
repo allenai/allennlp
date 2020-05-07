29160536185e3fef6b26aa6f9bcac34bed197085
epwalsh <epwalsh10@gmail.com>
2020-05-07 11:17:58 -0700

    ensure multi_device tests are also gpu marked

--- allennlp/common/testing/test_case.py
+++ allennlp/common/testing/test_case.py
@@ -61,7 +61,7 @@ class AllenNlpTestCase:
 
 _available_devices = ["cpu"] + (["cuda"] if torch.cuda.is_available() else [])
 
-multi_device = pytest.mark.parametrize("device", _available_devices)
+multi_device = lambda f: pytest.mark.parametrize("device", _available_devices)(pytest.mark.gpu(f))
 """
 Decorator that provides an argument `device` of type `str` for each available PyTorch device.
 """
