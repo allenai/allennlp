dacd14ab003e46fd7a7710c1d435af9ccbbb196d
epwalsh <epwalsh10@gmail.com>
2020-05-27 08:52:52 -0700

    add a test

--- tests/data/instance_test.py
+++ tests/data/instance_test.py
@@ -1,6 +1,7 @@
 from allennlp.common.testing import AllenNlpTestCase
 from allennlp.data import Instance
 from allennlp.data.fields import TextField, LabelField
+from allennlp.data.token_indexers import PretrainedTransformerIndexer
 from allennlp.data.tokenizers import Token
 
 
@@ -20,3 +21,22 @@ class TestInstance(AllenNlpTestCase):
         values = [v for k, v in instance.items()]
         assert words_field in values
         assert label_field in values
+
+    def test_duplicate(self):
+        # Verify the `duplicate()` method works with a `PretrainedTransformerIndexer` in
+        # a `TextField`. See https://github.com/allenai/allennlp/issues/4270.
+        instance = Instance(
+            {
+                "words": TextField(
+                    [Token("hello")], {"tokens": PretrainedTransformerIndexer("bert-base-uncased")}
+                )
+            }
+        )
+
+        other = instance.duplicate()
+        assert other == instance
+
+        # Adding new fields to the original instance should not effect the duplicate.
+        instance.add_field("labels", LabelField("some_label"))
+        assert "labels" not in other.fields
+        assert other != instance  # sanity check on the '__eq__' method.
