e7e38e461c06a11e46529a932dee006b6e5a4be3
Dirk Groeneveld <dirkg@allenai.org>
2020-05-28 18:13:12 -0700

    Deals with the case where a word doesn't have any word pieces assigned

--- allennlp/data/token_indexers/pretrained_transformer_mismatched_indexer.py
+++ allennlp/data/token_indexers/pretrained_transformer_mismatched_indexer.py
@@ -63,6 +63,11 @@ class PretrainedTransformerMismatchedIndexer(TokenIndexer):
         self._matched_indexer._add_encoding_to_vocabulary_if_needed(vocabulary)
 
         wordpieces, offsets = self._allennlp_tokenizer.intra_word_tokenize([t.text for t in tokens])
+
+        # For tokens that don't correspond to any word pieces, we put (-1, -1) into the offsets.
+        # That results in the embedding for the token to be all zeros.
+        offsets = [x if x is not None else (-1, -1) for x in offsets]
+
         output: IndexedTokenList = {
             "token_ids": [t.text_id for t in wordpieces],
             "mask": [True] * len(tokens),  # for original tokens (i.e. word-level)
