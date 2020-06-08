17b62d2ed3ffd8ab102e389dda9af4b7c772990e
epwalsh <epwalsh10@gmail.com>
2020-06-08 15:56:49 -0700

    fix test name

--- tests/data/dataloader_test.py
+++ tests/data/dataloader_test.py
@@ -4,6 +4,6 @@ from allennlp.data.dataloader import DataLoader
 from allennlp.data.dataset_readers.dataset_reader import _LazyInstances
 
 
-def test_multi_processing_with_lazy_dataset_raises():
+def test_multi_processing_with_lazy_dataset_warns():
     with pytest.warns(UserWarning, match=r".*'num_workers'.*"):
         DataLoader(_LazyInstances(iter([]), "nonexistent_file"), num_workers=1)
