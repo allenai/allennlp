940e4ed7e36a2586960ea110d6f5beb36fdf0930
Dirk Groeneveld <dirkg@allenai.org>
2020-07-16 06:12:19 -0700

    Remove detectron reader and field

--- allennlp/data/dataset_readers/__init__.py
+++ allennlp/data/dataset_readers/__init__.py
@@ -19,4 +19,3 @@ from allennlp.data.dataset_readers.sharded_dataset_reader import ShardedDatasetR
 from allennlp.data.dataset_readers.babi import BabiReader
 from allennlp.data.dataset_readers.text_classification_json import TextClassificationJsonReader
 from allennlp.data.dataset_readers.nlvr2 import Nlvr2Reader
-from allennlp.data.dataset_readers.detectron_reader import DetectronDatasetReader
