3bd87530bef52d9657e1c38f80f7e2e2572b7b53
Matt Gardner <mattg@allenai.org>
2020-07-28 14:31:04 -0700

    working on grid embedder

--- allennlp/modules/vision/grid_embedder.py
+++ allennlp/modules/vision/grid_embedder.py
@@ -8,6 +8,7 @@ class GridEmbedder(nn.Module, Registrable):
     A `GridEmbedder` takes a batch of images as a tensor with the dimensions
     (Batch, Color, Height, Width), and returns a tensor in the format
     (Batch, Features, new_height, new_width).
+
     For every image, it embeds a patch of the image, and returns the embedding
     of the patch. The size of the image might change during this operation.
     """
@@ -23,13 +24,55 @@ class GridEmbedder(nn.Module, Registrable):
         """
         raise NotImplementedError
 
+    def get_grid_size(self) -> Tuple[int, int]:
+        """
+        Returns the dimensionality of the grid that's computed by this `GridEmbedder`.  This is the
+        last two dimensions of the output of this modules.
+        """
+        raise NotImplementedError
 
-@GridEmbedder.register("null")
-class NullGridEmbedder(GridEmbedder):
-    """A `GridEmbedder` that returns the input image as given."""
 
-    def forward(self, images: FloatTensor) -> FloatTensor:
-        return images
+@GridEmbedder.register("resnet_backbone")
+class ResnetBackbone(Image2ImageModule):
+    """Runs an image through resnet, as implemented by Detectron."""
+
+    def __init__(self,
+        meta_architecture: str = "GeneralizedRCNN",
+        device: str = "cpu",
+        weights: str = "RCNN-X152-C4-2020-07-18",
+
+        attribute_on: bool = True,  # not in detectron2 default config
+        max_attr_per_ins: int = 16,  # not in detectron2 default config
+
+        stride_in_1x1: bool = False,  # different from default (True)
+        num_groups: int = 32,  # different from default (1)
+        width_per_group: int = 8,  # different from default (64)
+        depth: int = 152,  # different from default (50)
+    ):
+        super().__init__()
+        from allennlp.common import detectron
+
+        flat_parameters = detectron.DetectronFlatParameters(
+            max_attr_per_ins=max_attr_per_ins,
+            device=device,
+            weights=weights,
+            meta_architecture=meta_architecture,
+            attribute_on=attribute_on,
+            stride_in_1x1=stride_in_1x1,
+            num_groups=num_groups,
+            width_per_group=width_per_group,
+            depth=depth)
+
+        pipeline = detectron.get_pipeline_from_flat_parameters(flat_parameters, make_copy=False)
+        self.backbone = pipeline.model.backbone
+
+    def forward(self, images: FloatTensor, sizes: IntTensor) -> FloatTensor:
+        result = self.backbone(images)
+        assert len(result) == 1
+        return next(iter(result.values()))
 
     def get_output_dim(self) -> int:
-        return 3
+        raise NotImplementedError
+
+    def get_grid_size(self) -> int:
+        raise NotImplementedError
