8e0160f9c592754362dba44c511494249a7525c8
Akshita Bhagia <akshita23bhagia@gmail.com>
2020-08-03 11:26:21 -0400

    adding distributed boolean accuracy

--- allennlp/training/metrics/boolean_accuracy.py
+++ allennlp/training/metrics/boolean_accuracy.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import Optional, Union
 
 from overrides import overrides
 import torch
+import torch.distributed as dist
 
 from allennlp.training.metrics.metric import Metric
 
@@ -85,7 +86,12 @@ class BooleanAccuracy(Metric):
         self._correct_count += (correct * keep).sum()
         self._total_count += keep.sum()
 
-    def get_metric(self, reset: bool = False):
+    def get_metric(
+        self,
+        reset: bool = False,
+        world_size: int = 1,
+        cuda_device: Union[int, torch.device] = torch.device("cpu"),
+    ):
         """
         # Returns
 
@@ -95,9 +101,13 @@ class BooleanAccuracy(Metric):
             accuracy = float(self._correct_count) / float(self._total_count)
         else:
             accuracy = 0.0
+        if world_size > 1:
+            accuracy = torch.tensor(accuracy).to(cuda_device)
+            dist.all_reduce(accuracy, op=dist.ReduceOp.SUM)
+            accuracy = accuracy.item() / world_size
         if reset:
             self.reset()
-        return accuracy
+        return {"accuracy": accuracy}
 
     @overrides
     def reset(self):
