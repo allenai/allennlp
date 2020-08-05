da2d277e8968c0aada8141532e7e9a7a08ff3522
Nitish Gupta <gnnitish@gmail.com>
2020-08-05 12:02:07 -0500

    minor lint

--- allennlp/training/trainer.py
+++ allennlp/training/trainer.py
@@ -179,9 +179,10 @@ EpochCallback.register("null")(EpochCallback)
 @EpochCallback.register("track_epoch_callback")
 class TrackEpochCallback:
     """
-    A callback that you can pass to the `GradientDescentTrainer` to access the current epoch number in progress in the
-    model as a class member `model.epoch`. Since the EpochCallback passes `epoch=-1` at the start of the training,
-    we set `model.epoch = epoch + 1` which now denotes the number of completed epochs at a given training state.
+    A callback that you can pass to the `GradientDescentTrainer` to access the current epoch number
+    in progress in the model as a class member `model.epoch`. Since the EpochCallback passes `epoch=-1`
+    at the start of the training, we set `model.epoch = epoch + 1` which now denotes the number of
+    completed epochs at a given training state.
     """
 
     def __init__(self):
