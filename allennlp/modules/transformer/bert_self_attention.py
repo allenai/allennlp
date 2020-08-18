7cdc7b99bfc490ff45581ee68373bfa824ac13a5
Akshita Bhagia <akshita23bhagia@gmail.com>
2020-08-18 18:48:01 -0400

    transformer toolkit: BertSelfAttention

---
+++ allennlp/modules/transformer/bert_self_attention.py
@@ -0,0 +1,64 @@
+import math
+
+import torch
+
+from allennlp.common import FromParams
+
+
+class BertSelfAttention(torch.nn.Module, FromParams):
+    def __init__(
+        self, hidden_size: int, num_attention_heads: int, dropout: float = 0.0,
+    ):
+        super().__init__()
+        if hidden_size % num_attention_heads != 0:
+            raise ValueError(
+                "The hidden size (%d) is not a multiple of the number of attention "
+                "heads (%d)" % (hidden_size, num_attention_heads)
+            )
+        self.num_attention_heads = num_attention_heads
+        self.attention_head_size = int(hidden_size / num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = torch.nn.Linear(hidden_size, self.all_head_size)
+        self.key = torch.nn.Linear(hidden_size, self.all_head_size)
+        self.value = torch.nn.Linear(hidden_size, self.all_head_size)
+
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size,)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(
+        self, hidden_states: torch.Tensor, attention_mask: torch.Tensor,
+    ):
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        # Take the dot product between "query" and "key" to get the raw
+        # attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        # Apply the attention mask is (precomputed for all layers in
+        # BertModel forward() function)
+        attention_scores = attention_scores + attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = torch.nn.Softmax(dim=-1)(attention_scores)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs = self.dropout(attention_probs)
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+        return context_layer
---
+++ tests/modules/transformer/bert_self_attention_test.py
@@ -0,0 +1,46 @@
+import copy
+import torch
+
+import pytest
+
+from allennlp.common import Params
+from allennlp.modules.transformer import BertSelfAttention
+from allennlp.common.testing import AllenNlpTestCase
+
+
+class TestBertSelfAttention(AllenNlpTestCase):
+    def setup_method(self):
+        super().setup_method()
+
+        self.params_dict = {
+            "hidden_size": 6,
+            "num_attention_heads": 2,
+            "dropout": 0.0,
+        }
+
+        params = Params(copy.deepcopy(self.params_dict))
+
+        self.bert_self_attention = BertSelfAttention.from_params(params)
+
+    def test_can_construct_from_params(self):
+        assert (
+            self.bert_self_attention.num_attention_heads == self.params_dict["num_attention_heads"]
+        )
+        assert self.bert_self_attention.attention_head_size == int(
+            self.params_dict["hidden_size"] / self.params_dict["num_attention_heads"]
+        )
+
+        assert (
+            self.bert_self_attention.all_head_size
+            == self.params_dict["num_attention_heads"]
+            * self.bert_self_attention.attention_head_size
+        )
+
+        assert self.bert_self_attention.query.in_features == self.params_dict["hidden_size"]
+        assert self.bert_self_attention.key.in_features == self.params_dict["hidden_size"]
+        assert self.bert_self_attention.value.in_features == self.params_dict["hidden_size"]
+
+        assert self.bert_self_attention.dropout.p == self.params_dict["dropout"]
+
+    def test_forward_runs(self):
+        self.bert_self_attention.forward(torch.randn(2, 3, 6), torch.randn(2, 2, 3, 3))
