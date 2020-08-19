03efd7d3fdcf1acab08d3141eca43873a5e305bf
sanjays <sanjays@ip-10-0-0-157.us-west-2.compute.internal>
2020-08-19 16:00:32 -0500

    Standalone script for caching image features

---
+++ scripts/cache_vision_features.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import argparse
+from tqdm import tqdm
+
+import torch
+
+from allennlp.common.file_utils import LmdbCache, cached_path
+from allennlp.data import DetectronImageLoader
+from allennlp.modules.vision import ResnetBackbone, FasterRcnnRegionDetector
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_dir", type=str)
+    parser.add_argument("--cache_path", type=str)
+    parser.add_argument("--use-cuda", action="store_true", help="use GPU if one is available")
+    parser.add_argument("--batch-size", type=int, default=32)
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+    feature_cache = LmdbCache(args.cache_path, read_only=False)
+    image_paths = list(glob.iglob(os.path.join(args.image_dir, "**", "*.png"), recursive=True))
+    image_keys = [os.path.basename(filename) for filename in image_paths]
+
+    image_loader = DetectronImageLoader()
+    image_featurizer = ResnetBackbone()
+    region_detector = FasterRcnnRegionDetector()
+    if torch.cuda.is_available() and args.use_cuda:
+        backbone.cuda()
+        region_detector.cuda()
+
+    images, sizes = image_loader(image_paths)
+    for index in tqdm(range(0, images.shape[0], args.batch_size)):
+        end = min(index+args.batch_size, images.shape[0])
+        batch_images = images[index:end].to(image_featurizer.device)
+        batch_shapes = sizes[index:end]
+        featurized_images = image_featurizer(batch_images)
+        detector_results = region_detector(batch_images, batch_shapes, featurized_images)
+        features = detector_results["features"]
+        coordinates = detector_results["coordinates"]
+        for subindex in range(index, end):
+            feature_cache[image_paths[subindex]] = (features[subindex-index], coordinates[subindex-index])
