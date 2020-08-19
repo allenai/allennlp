812b2763910bb5537a6e6b6a0d4dab64c29f7c88
Akshita Bhagia <akshita23bhagia@gmail.com>
2020-08-19 16:14:16 -0400

    transformer toolkit: BertBiAttention

---
+++ allennlp/modules/transformer/bert_biattention.py
@@ -0,0 +1,122 @@
+import math
+
+import torch
+
+from allennlp.common import FromParams
+
+
+class BertBiAttention(torch.nn.Module, FromParams):
+    def __init__(
+        self,
+        hidden_size1: int,
+        hidden_size2: int,
+        combined_hidden_size: int,
+        num_attention_heads: int,
+        dropout1: float,
+        dropout2: float,
+    ):
+        super().__init__()
+        if combined_hidden_size % num_attention_heads != 0:
+            raise ValueError(
+                "The hidden size (%d) is not a multiple of the number of attention "
+                "heads (%d)" % (combined_hidden_size, num_attention_heads)
+            )
+
+        self.num_attention_heads = num_attention_heads
+        self.attention_head_size = int(combined_hidden_size / num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query1 = torch.nn.Linear(hidden_size1, self.all_head_size)
+        self.key1 = torch.nn.Linear(hidden_size1, self.all_head_size)
+        self.value1 = torch.nn.Linear(hidden_size1, self.all_head_size)
+
+        self.dropout1 = torch.nn.Dropout(dropout1)
+
+        self.query2 = torch.nn.Linear(hidden_size2, self.all_head_size)
+        self.key2 = torch.nn.Linear(hidden_size2, self.all_head_size)
+        self.value2 = torch.nn.Linear(hidden_size2, self.all_head_size)
+
+        self.dropout2 = torch.nn.Dropout(dropout2)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size,)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(
+        self,
+        input_tensor1,
+        attention_mask1,
+        input_tensor2,
+        attention_mask2,
+        co_attention_mask=None,
+        use_co_attention_mask=False,
+    ):
+
+        # for vision input.
+        mixed_query_layer1 = self.query1(input_tensor1)
+        mixed_key_layer1 = self.key1(input_tensor1)
+        mixed_value_layer1 = self.value1(input_tensor1)
+        # mixed_logit_layer1 = self.logit1(input_tensor1)
+
+        query_layer1 = self.transpose_for_scores(mixed_query_layer1)
+        key_layer1 = self.transpose_for_scores(mixed_key_layer1)
+        value_layer1 = self.transpose_for_scores(mixed_value_layer1)
+        # logit_layer1 = self.transpose_for_logits(mixed_logit_layer1)
+
+        # for text input:
+        mixed_query_layer2 = self.query2(input_tensor2)
+        mixed_key_layer2 = self.key2(input_tensor2)
+        mixed_value_layer2 = self.value2(input_tensor2)
+        # mixed_logit_layer2 = self.logit2(input_tensor2)
+
+        query_layer2 = self.transpose_for_scores(mixed_query_layer2)
+        key_layer2 = self.transpose_for_scores(mixed_key_layer2)
+        value_layer2 = self.transpose_for_scores(mixed_value_layer2)
+        # logit_layer2 = self.transpose_for_logits(mixed_logit_layer2)
+
+        # Take the dot product between "query2" and "key1" to get the raw
+        # attention scores for value 1.
+        attention_scores1 = torch.matmul(query_layer2, key_layer1.transpose(-1, -2))
+        attention_scores1 = attention_scores1 / math.sqrt(self.attention_head_size)
+        attention_scores1 = attention_scores1 + attention_mask1
+        if use_co_attention_mask:
+            attention_scores1 = attention_scores1 + co_attention_mask.permute(0, 1, 3, 2)
+
+        # Normalize the attention scores to probabilities.
+        attention_probs1 = torch.nn.Softmax(dim=-1)(attention_scores1)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs1 = self.dropout1(attention_probs1)
+
+        context_layer1 = torch.matmul(attention_probs1, value_layer1)
+        context_layer1 = context_layer1.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape1 = context_layer1.size()[:-2] + (self.all_head_size,)
+        context_layer1 = context_layer1.view(*new_context_layer_shape1)
+
+        # Take the dot product between "query1" and "key2" to get the
+        # raw attention scores for value 2.
+        attention_scores2 = torch.matmul(query_layer1, key_layer2.transpose(-1, -2))
+        attention_scores2 = attention_scores2 / math.sqrt(self.attention_head_size)
+        # Apply the attention mask is (precomputed for all layers in BertModel
+        # forward() function)
+
+        # we can comment this line for single flow.
+        attention_scores2 = attention_scores2 + attention_mask2
+        if use_co_attention_mask:
+            attention_scores2 = attention_scores2 + co_attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs2 = torch.nn.Softmax(dim=-1)(attention_scores2)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs2 = self.dropout2(attention_probs2)
+
+        context_layer2 = torch.matmul(attention_probs2, value_layer2)
+        context_layer2 = context_layer2.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape2 = context_layer2.size()[:-2] + (self.all_head_size,)
+        context_layer2 = context_layer2.view(*new_context_layer_shape2)
+
+        return context_layer1, context_layer2
