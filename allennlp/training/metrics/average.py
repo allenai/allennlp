fix Average metric

allennlp/training/metrics/average.py
@@ -31,9 +31,8 @@ class Average(Metric):
         _total_value = list(self.detach_tensors(value))[0]
         _count = 1
         if is_distributed():
-            device = torch.device("cpu")
-            count = torch.tensor(_count).to(device)
-            total_value = torch.tensor(_total_value).to(device)
+            count = torch.tensor(_count)
+            total_value = torch.tensor(_total_value)
             dist.all_reduce(count, op=dist.ReduceOp.SUM)
             dist.all_reduce(total_value, op=dist.ReduceOp.SUM)
             _count = count.item()

allennlp/training/metrics/evalb_bracketing_scorer.py
@@ -154,11 +154,9 @@ class EvalbBracketingScorer(Metric):
         shutil.rmtree(tempdir)
 
         if is_distributed():
-            # Setting the device to CPU since this metric is not expected to run on GPUs.
-            device = torch.device("cpu")
-            correct_predicted_brackets = torch.tensor(_correct_predicted_brackets).to(device)
-            predicted_brackets = torch.tensor(_predicted_brackets).to(device)
-            gold_brackets = torch.tensor(_gold_brackets).to(device)
+            correct_predicted_brackets = torch.tensor(_correct_predicted_brackets)
+            predicted_brackets = torch.tensor(_predicted_brackets)
+            gold_brackets = torch.tensor(_gold_brackets)
             dist.all_reduce(correct_predicted_brackets, op=dist.ReduceOp.SUM)
             dist.all_reduce(predicted_brackets, op=dist.ReduceOp.SUM)
             dist.all_reduce(gold_brackets, op=dist.ReduceOp.SUM)