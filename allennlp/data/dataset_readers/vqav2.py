30f34804e2bdb4156a6329665c5b0cd828658c34
Dirk Groeneveld <dirkg@allenai.org>
2020-09-16 16:13:10 -0700

    Refactored VQA reader

--- allennlp/data/dataset_readers/vqav2.py
+++ allennlp/data/dataset_readers/vqav2.py
@@ -1,6 +1,8 @@
+import glob
+import itertools
 from collections import defaultdict
 from os import PathLike
-from typing import Dict, List, Union, Optional, MutableMapping
+from typing import Dict, List, Union, Optional, MutableMapping, NamedTuple, Tuple, Iterable
 import json
 import os
 import re
@@ -8,9 +10,10 @@ import re
 from overrides import overrides
 import torch
 from torch import Tensor
+from tqdm import tqdm
 
 from allennlp.common.checks import check_for_gpu
-from allennlp.common.util import int_to_device
+from allennlp.common.util import int_to_device, lazy_groups_of
 from allennlp.common.file_utils import cached_path, TensorCache
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from allennlp.data.fields import ArrayField, LabelField, ListField, TextField
@@ -266,11 +269,11 @@ class VQAv2Reader(DatasetReader):
         region_detector: RegionDetector,
         *,
         feature_cache_dir: Optional[Union[str, PathLike]] = None,
-        data_dir: Union[str, PathLike] = None,
         tokenizer: Tokenizer = None,
         token_indexers: Dict[str, TokenIndexer] = None,
         cuda_device: Optional[Union[int, torch.device]] = None,
         max_instances: Optional[int] = None,
+        image_processing_batch_size: int = 3# TODO: change this back to 8
     ) -> None:
         super().__init__(
             max_instances=max_instances,
@@ -286,14 +289,12 @@ class VQAv2Reader(DatasetReader):
         check_for_gpu(cuda_device)
         self.cuda_device = int_to_device(cuda_device)
 
-        self._image_dir = image_dir
-
-        if not data_dir:
-            data_dir = "TODO"  # TODO(mattg): upload combined files to some central place
-        self.splits = {
-            "train2014": f"{data_dir}/combined_train2014.json",
-            "val2014": f"{data_dir}/combined_val2014.json",
-            "test2015": f"{data_dir}/combined_test2015.json",
+        self.images = {
+            os.path.basename(filename): filename
+            for filename in tqdm(
+                glob.iglob(os.path.join(image_dir, "**", "*.jpg"), recursive=True),
+                desc="Discovering images",
+            )
         }
 
         # tokenizers and indexers
@@ -318,44 +319,93 @@ class VQAv2Reader(DatasetReader):
             self._features_cache = TensorCache(os.path.join(feature_cache_dir, "features"))
             self._coordinates_cache = TensorCache(os.path.join(feature_cache_dir, "coordinates"))
 
+        self.image_processing_batch_size = image_processing_batch_size
+
     @overrides
     def _read(self, split: str):
-        if split not in self.splits:
+        class Split(NamedTuple):
+            annotations: Optional[str]
+            questions: str
+
+        splits = {
+            "balanced_real_train": Split(
+                "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip!v2_mscoco_train2014_annotations.json",
+                "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip!v2_OpenEnded_mscoco_train2014_questions.json"
+            ),
+            "balanced_real_val": Split(
+                "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip!v2_mscoco_val2014_annotations.json",
+                "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip!v2_OpenEnded_mscoco_val2014_questions.json"
+            ),
+            "balanced_real_test": Split(
+                None,
+                "https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Test_mscoco.zip!v2_OpenEnded_mscoco_test2015_questions.json"
+            ),
+            "balanced_bas_train": Split(        # "bas" is Binary Abstract Scenes
+                "https://s3.amazonaws.com/cvmlp/vqa/abstract_v002/vqa/Annotations_Binary_Train2017_abstract_v002.zip!abstract_v002_train2017_annotations.json",
+                "https://s3.amazonaws.com/cvmlp/vqa/abstract_v002/vqa/Questions_Binary_Train2017_abstract_v002.zip!OpenEnded_abstract_v002_train2017_questions.json"
+            ),
+            "balanced_bas_val": Split(
+                "https://s3.amazonaws.com/cvmlp/vqa/abstract_v002/vqa/Annotations_Binary_Val2017_abstract_v002.zip!abstract_v002_val2017_annotations.json",
+                "https://s3.amazonaws.com/cvmlp/vqa/abstract_v002/vqa/Questions_Binary_Val2017_abstract_v002.zip!OpenEnded_abstract_v002_val2017_questions.json"
+            ),
+            "abstract_scenes_train": Split(
+                "https://s3.amazonaws.com/cvmlp/vqa/abstract_v002/vqa/Annotations_Train_abstract_v002.zip!abstract_v002_train2015_annotations.json",
+                "https://s3.amazonaws.com/cvmlp/vqa/abstract_v002/vqa/Questions_Train_abstract_v002.zip!OpenEnded_abstract_v002_train2015_questions.json"
+            ),
+            "abstract_scenes_val": Split(
+                "https://s3.amazonaws.com/cvmlp/vqa/abstract_v002/vqa/Annotations_Val_abstract_v002.zip!abstract_v002_val2015_annotations.json",
+                "https://s3.amazonaws.com/cvmlp/vqa/abstract_v002/vqa/Questions_Val_abstract_v002.zip!OpenEnded_abstract_v002_val2015_questions.json"
+            ),
+            "abstract_scenes_test": Split(
+                None,
+                "https://s3.amazonaws.com/cvmlp/vqa/abstract_v002/vqa/Questions_Test_abstract_v002.zip!OpenEnded_abstract_v002_test2015_questions.json"
+            )
+        }
+
+        try:
+            split = splits[split]
+        except KeyError:
             raise ValueError(
                 f"Unrecognized split: {split}. We require a split, not a filename, for VQA "
                 "because the image filenames require using the split."
             )
-        filename = self.splits[split]
 
-        json_file_path = cached_path(filename)
+        if split.annotations is None:
+            annotations_by_question_id = {}
+        else:
+            with open(cached_path(split.annotations, extract_archive=True)) as f:
+                annotations = json.load(f)
+                annotations_by_question_id = {
+                    a["question_id"]: a
+                    for a in annotations["annotations"]
+                }
+        with open(cached_path(split.questions, extract_archive=True)) as f:
+            questions = json.load(f)
 
-        with open(json_file_path) as json_file:
-            data = json.load(json_file)
+        # It would be much easier to just process one image at a time, but it's faster to process
+        # them in batches. So this code gathers up instances until it has enough to fill up a batch
+        # that needs processing, and then processes them all.
+        question_dicts = self.shard_iterable(questions["questions"])
+        question_dicts, processed_images = itertools.tee(question_dicts)
+        processed_images = self._process_image_paths(
+            self.images[f"{question_dict['image_id']:012d}.jpg"]
+            for question_dict in question_dicts
+        )
 
-        for question_dict in self.shard_iterable(data["questions"]):
-            image_id = question_dict["image_id"]
-            image_path = os.path.join(self._image_dir, split, f"COCO_{split}_{image_id:012d}.jpg")
-            yield self.text_to_instance(
-                question=question_dict["question"],
-                image_path=image_path,
-                answers=question_dict.get("answers"),
-            )
+        for question_dict, processed_image in zip(question_dicts, processed_images):
+            answers = annotations_by_question_id.get(question_dict["question_id"])
+            if answers is not None:
+                answers = answers["answers"]
+            yield self.text_to_instance(question_dict["question"], processed_image, answers)
 
-    @overrides
-    def text_to_instance(
-        self,  # type: ignore
-        question: str,
-        image_path: str,
-        answers: List[Dict[str, str]] = None,
-    ) -> Instance:
-        tokenized_question = self._tokenizer.tokenize(question)
-        question_field = TextField(tokenized_question, self._token_indexers)
+    def _process_image_paths(self, image_paths: Iterable[str]) -> Iterable[Tuple[Tensor, Tensor]]:
+        batch = []
+        unprocessed_paths = set()
 
-        # Load images
-        name = os.path.basename(image_path)
-
-        if name not in self._features_cache or name not in self._coordinates_cache:
-            images, sizes = self.image_loader([image_path])
+        def yield_batch():
+            # process the images
+            paths = list(unprocessed_paths)
+            images, sizes = self.image_loader(paths)
             with torch.no_grad():
                 images = images.to(self.cuda_device)
                 sizes = sizes.to(self.cuda_device)
@@ -364,11 +414,59 @@ class VQAv2Reader(DatasetReader):
                 features = detector_results["features"]
                 coordinates = detector_results["coordinates"]
 
-            self._features_cache[os.path.basename(image_path)] = features[0].cpu()
-            self._coordinates_cache[os.path.basename(image_path)] = coordinates[0].cpu()
+            # store the processed results in memory, so we can complete the batch
+            paths_to_tensors = {
+                path: (features[i], coordinates[i])
+                for i, path in enumerate(paths)
+            }
 
-        features = self._features_cache[name]
-        coords = self._coordinates_cache[name]
+            # store the processed results in the cache
+            for path, (features, coordinates) in paths_to_tensors.items():
+                basename = os.path.basename(path)
+                self._features_cache[basename] = features
+                self._coordinates_cache[basename] = coordinates
+
+            # yield the batch
+            for b in batch:
+                if isinstance(b, str):
+                    yield paths_to_tensors[b]
+                else:
+                    yield b
+
+        for image_path in image_paths:
+            basename = os.path.basename(image_path)
+            try:
+                features = self._features_cache[basename]
+                coordinates = self._coordinates_cache[basename]
+                if len(batch) <= 0:
+                    yield features, coordinates
+                else:
+                    batch.append((features, coordinates))
+            except KeyError:
+                batch.append(image_path)
+                unprocessed_paths.add(image_path)
+                if len(unprocessed_paths) >= self.image_processing_batch_size:
+                    yield from yield_batch()
+                    batch = []
+                    unprocessed_paths = set()
+
+        if len(batch) > 0:
+            yield from yield_batch()
+
+
+    @overrides
+    def text_to_instance(
+        self,  # type: ignore
+        question: str,
+        image: Union[str, Tuple[Tensor, Tensor]],
+        answers: List[Dict[str, str]] = None,
+    ) -> Instance:
+        tokenized_question = self._tokenizer.tokenize(question)
+        question_field = TextField(tokenized_question, self._token_indexers)
+        if isinstance(image, str):
+            features, coords = next(self._process_image_paths([image]))
+        else:
+            features, coords = image
 
         fields = {
             "box_features": ArrayField(features),
