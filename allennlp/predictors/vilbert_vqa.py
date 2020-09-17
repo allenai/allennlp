8e05f92e17c65f74e20cbd385338c5426547b112
Dirk Groeneveld <dirkg@allenai.org>
2020-09-17 16:42:54 -0700

    Adds a basic Predictor for VQAv2

---
+++ allennlp/predictors/vilbert_vqa.py
@@ -0,0 +1,28 @@
+from typing import List, Dict
+
+from overrides import overrides
+import numpy
+
+from allennlp.common.file_utils import cached_path
+from allennlp.common.util import JsonDict
+from allennlp.data import Instance
+from allennlp.predictors.predictor import Predictor
+
+
+@Predictor.register("vilbert_vqa")
+class VilbertVqaPredictor(Predictor):
+    def predict(self, image: str, sentence: str) -> JsonDict:
+        image = cached_path(image)
+        return self.predict_json({"question": sentence, "image": image})
+
+    @overrides
+    def _json_to_instance(self, json_dict: JsonDict) -> Instance:
+        question = json_dict["question"]
+        image = cached_path(json_dict["image"])
+        return self._dataset_reader.text_to_instance(question, image)
+
+    @overrides
+    def predictions_to_labeled_instances(
+        self, instance: Instance, outputs: Dict[str, numpy.ndarray]
+    ) -> List[Instance]:
+        return [instance]   # TODO
