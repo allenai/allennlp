940cd62fb9c81d07e9826d47415e934b2e236d96
Karen Hambardzumyan <mahnerak@gmail.com>
2020-09-29 01:24:20 +0400

    Remove unnecessary code
    Co-authored-by: Dirk Groeneveld <groeneveld@gmail.com>

--- allennlp/commands/train.py
+++ allennlp/commands/train.py
@@ -502,8 +502,8 @@ class TrainModel(Registrable):
         self.batch_weight_key = batch_weight_key
 
     def run(self) -> Dict[str, Any]:
-        with self.trainer as trainer:
-            return trainer.train()
+        with self.trainer:
+            return self.trainer.train()
 
     def finish(self, metrics: Dict[str, Any]):
         if self.evaluation_data_loader is not None and self.evaluate_on_test:
