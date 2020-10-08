4d8e9eef46b98523f9c997d15d0f580dbcb3dff7
Jackson Stokes <jacksons@Jacksons-MacBook-Pro.local>
2020-10-08 09:34:56 -0700

    pushing again

--- allennlp/nn/beam_search.py
+++ allennlp/nn/beam_search.py
@@ -419,30 +419,8 @@ class BeamSearch(Registrable):
         per_node_beam_size: int = None,
     ) -> "BeamSearch":
         """
-        Given an index of the end token in target vocabulary, return a BeamSearch object
-        that can be used to find `beam_size` candidate sequences, found by sampling from
-        the top `k` tokens to choose from at each step.
-
-        # Parameters
-
-        end_index : `int`
-            The index of the "stop" or "end" token in the target vocabulary.
-        max_steps : `int`, optional (default = `50`)
-            The maximum number of decoding steps to take, i.e. the maximum length
-            of the predicted sequences.
-        beam_size : `int`, optional (default = `10`)
-            The width of the beam used.
-        per_node_beam_size : `int`, optional (default = `beam_size`)
-            The maximum number of candidates to consider per node, at each step in the search.
-            If not given, this just defaults to `beam_size`. Setting this parameter
-            to a number smaller than `beam_size` may give better results, as it can introduce
-            more diversity into the search. See
-            [*Beam Search Strategies for Neural Machine Translation*, Freitag and Al-Onaizan, 2017]
-            (https://arxiv.org/abs/1702.01806).
-
-        # Returns
-
-        `BeamSearch` object with the defined parameters
+        Given an index of the end token in target vocabulary, return a `BeamSearch` object
+        that can be used to find `beam_size` candidate sequences.
         """
         return cls(
             end_index=end_index,
@@ -461,29 +439,9 @@ class BeamSearch(Registrable):
         temperature: float = 1.0,
     ) -> "BeamSearch":
         """
-        Given an index of the end token in target vocabulary, return a BeamSearch object
-        that can be used to find `beam_size` candidate sequences.
-
-        # Parameters
-
-        end_index : `int`
-            The index of the "stop" or "end" token in the target vocabulary.
-        max_steps : `int`, optional (default = `50`)
-            The maximum number of decoding steps to take, i.e. the maximum length
-            of the predicted sequences.
-        beam_size : `int`, optional (default = `10`)
-            The width of the beam used.
-        k : `int`, optional (default = `1`)
-            The number of top next tokens to sample from.
-        temperature : `float`, optional (default = `1.0`)
-            The 'temperature' of the probability distribution to be sampled
-            from, lowering below 1.0 creates a sharper distribution.
-
-        # Returns
-
-        `BeamSearch` object with the defined `end_index`, `max_steps`, and
-        `beam_size`, with subsequent tokens sampled using a `TopKSampler` with
-        the specified `k` and `temperature`
+        Given an index of the end token in target vocabulary, return a `BeamSearch` object
+        that can be used to find `beam_size` candidate sequences, found by sampling from
+        the top `k` tokens to choose from at each step.
         """
         # Make sure `k` is a valid threshold
         if type(k) is not int or k < 1:
@@ -510,49 +468,10 @@ class BeamSearch(Registrable):
         temperature: float = 1.0,
     ) -> "BeamSearch":
         """
-        Given an index of the end token in target vocabulary, return a BeamSearch object
-        that can be used to find `beam_size` candidate sequences.
-        # Parameters
-
-        end_index : `int`
-            The index of the "stop" or "end" token in the target vocabulary.
-        start_predictions : `torch.Tensor`
-            A tensor containing the initial predictions with shape `(batch_size,)`.
-            Usually the initial predictions are just the index of the "start" token
-            in the target vocabulary.
-        start_state : `StateType`
-            The initial state passed to the `step` function. Each value of the state dict
-            should be a tensor of shape `(batch_size, *)`, where `*` means any other
-            number of dimensions.
-        step : `StepFunctionType`
-            A function that is responsible for computing the next most likely tokens,
-            given the current state and the predictions from the last time step.
-            The function should accept two arguments. The first being a tensor
-            of shape `(group_size,)`, representing the index of the predicted
-            tokens from the last time step, and the second being the current state.
-            The `group_size` will be `batch_size * beam_size`, except in the initial
-            step, for which it will just be `batch_size`.
-            The function is expected to return a tuple, where the first element
-            is a tensor of shape `(group_size, target_vocab_size)` containing
-            the log probabilities of the tokens for the next step, and the second
-            element is the updated state. The tensor in the state should have shape
-            `(group_size, *)`, where `*` means any other number of dimensions.
-        max_steps : `int`, optional (default = `50`)
-            The maximum number of decoding steps to take, i.e. the maximum length
-            of the predicted sequences.
-        beam_size : `int`, optional (default = `10`)
-            The width of the beam used.
-        p : `float`, optional (default = `0.9`)
-            The cumulative probability of top next tokens to select from.
-        temperature : `float`, optional (default = `1.0`)
-            The 'temperature' of the probability distribution to be sampled
-            from, lowering below 1.0 creates a sharper distribution.
-
-        # Returns
-
-        `BeamSearch` object with the defined `end_index`, `max_steps`, and
-        `beam_size`, with subsequent tokens sampled using a `TopPSampler` with
-        the specified `p` and `temperature`
+        Given an index of the end token in target vocabulary, return a `BeamSearch` object
+        that can be used to find `beam_size` candidate sequences, found by sampling from
+        tokens which cumulatively make up the top `p` probability of the total tokens to
+        choose from.
         """
         # Make sure `p` is a valid cumulative probability threshold.
         if type(p) is not float or p < 0.0 or p > 1.0:
