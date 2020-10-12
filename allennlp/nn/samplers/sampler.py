5a754c119b3cfb3a4696bafbf7c2bd9e1a203450
Jackson Stokes <jacksons@Jacksons-MacBook-Pro.local>
2020-10-12 11:18:24 -0700

    ran formatter

--- allennlp/nn/samplers/sampler.py
+++ allennlp/nn/samplers/sampler.py
@@ -8,7 +8,5 @@ class Sampler(Registrable):
     An abstract class representing a multinomial sampler
     """
 
-    def __call__(
-        self, logits: torch.Tensor, num_samples: int = 1
-    ) -> torch.Tensor:
+    def __call__(self, logits: torch.Tensor, num_samples: int = 1) -> torch.Tensor:
         raise NotImplementedError
