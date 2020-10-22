3431a22412aa371af01ac780c9d0bab6e8e72b77
MichalMalyska <malyskamichal@gmail.com>
2020-10-22 15:34:30 -0400

    Add test on respecting masking

--- tests/modules/seq2vec_encoders/cnn_encoder_test.py
+++ tests/modules/seq2vec_encoders/cnn_encoder_test.py
@@ -7,7 +7,6 @@ from allennlp.modules.seq2vec_encoders import CnnEncoder
 from allennlp.nn import InitializerApplicator, Initializer
 from allennlp.common.testing import AllenNlpTestCase
 
-
 class TestCnnEncoder(AllenNlpTestCase):
     def test_get_dimension_is_correct(self):
         encoder = CnnEncoder(embedding_dim=5, num_filters=4, ngram_filter_sizes=(3, 5))
@@ -46,3 +45,19 @@ class TestCnnEncoder(AllenNlpTestCase):
         )
         tensor = torch.rand(4, 8, 7)
         assert encoder(tensor, None).size() == (4, 30)
+
+    def test_forward_respects_masking(self):
+        # seed 1 fails on the old cnn encoder code
+        torch.manual_seed(1)
+        encoder = CnnEncoder(
+            embedding_dim=7, num_filters=13, ngram_filter_sizes=(1, 2, 3, 4, 5)
+        )
+        init = Initializer.from_params(Params({"type": "normal", "mean": 0.0, "std": 10}))
+        initializer = InitializerApplicator([(".*", init)])
+        initializer(encoder)
+        tokens = torch.ones(4,8,7)
+        padded_tokens = torch.nn.functional.pad(tokens.transpose(1,2), (0,2),value=5).transpose(1,2)
+        mask = torch.where(padded_tokens == 5, torch.zeros_like(padded_tokens), torch.ones_like(padded_tokens)).bool().any(dim=2)
+        regular_output = encoder.forward(tokens=tokens, mask=None)
+        masked_output = encoder.forward(tokens=padded_tokens, mask=mask)
+        assert_almost_equal(regular_output.data.numpy(), masked_output.data.numpy(), decimal=6)
