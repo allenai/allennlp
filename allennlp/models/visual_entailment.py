abacc01b57f06423686e6484184ea60f5011866c
Akshita Bhagia <akshita23bhagia@gmail.com>
2020-12-23 14:44:38 -0800

    Adding f1 score (#4890)
    * adding f1 score
    
    * updated config

--- allennlp/models/visual_entailment.py
+++ allennlp/models/visual_entailment.py
@@ -13,6 +13,7 @@ from allennlp.modules.transformer import (
     BiModalEncoder,
 )
 from allennlp.training.metrics import CategoricalAccuracy
+from allennlp.training.metrics import FBetaMeasure
 
 
 from allennlp.models.vision_text_model import VisionTextModel
@@ -68,6 +69,7 @@ class VisualEntailmentModel(VisionTextModel):
         )
 
         self.accuracy = CategoricalAccuracy()
+        self.fbeta = FBetaMeasure(beta=1.0, average="macro")
 
     @overrides
     def forward(
@@ -102,12 +104,15 @@ class VisualEntailmentModel(VisionTextModel):
                 torch.nn.functional.cross_entropy(outputs["logits"], label) / batch_size
             )
             self.accuracy(outputs["logits"], label)
+            self.fbeta(outputs["probs"], label)
         return outputs
 
     @overrides
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
-        result = self.accuracy.get_metric(reset)
-        return {"accuracy": result}
+        metrics = self.fbeta.get_metric(reset)
+        accuracy = self.accuracy.get_metric(reset)
+        metrics.update({"accuracy": accuracy})
+        return metrics
 
     @overrides
     def make_output_human_readable(
