d2cdf623b23b9b6176923ffc9ff0ea7e8b07f50b
johngiorgi <johnmgiorgi@gmail.com>
2021-01-06 12:43:49 -0500

    Add scaled dot-product attention

--- allennlp/modules/attention/__init__.py
+++ allennlp/modules/attention/__init__.py
@@ -1,4 +1,5 @@
 from allennlp.modules.attention.attention import Attention
+from allennlp.modules.attention.scaled_dot_product_attention import ScaledDotProductAttention
 from allennlp.modules.attention.bilinear_attention import BilinearAttention
 from allennlp.modules.attention.additive_attention import AdditiveAttention
 from allennlp.modules.attention.cosine_attention import CosineAttention
---
+++ allennlp/modules/attention/scaled_dot_product_attention.py
@@ -0,0 +1,20 @@
+import torch
+from overrides import overrides
+from allennlp.modules.attention.attention import Attention
+import math
+
+
+@Attention.register("scaled_dot_product")
+class ScaledDotProductAttention(Attention):
+    """
+    Computes attention between a vector and a matrix using scaled dot product.
+
+    This attention is often referred to as "Scaled Dot-Product Attention". It was introduced in
+    <https://arxiv.org/abs/1706.03762> by Vaswani et al.
+
+    Registered as an `Attention` with name "scaled_dot_product".
+    """
+
+    @overrides
+    def _forward_internal(self, vector: torch.Tensor, matrix: torch.Tensor) -> torch.Tensor:
+        return matrix.bmm(vector.unsqueeze(-1)).squeeze(-1) / math.sqrt(vector.size(-1))
