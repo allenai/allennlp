de720d4a9aa1e0ff388306ba6f58ed2035285139
himkt <himkt@cookpad.com>
2021-01-23 23:35:31 +0900

    Remove test that the experimentcouldn't be replicated

--- tests/interpret/input_reduction_test.py
+++ tests/interpret/input_reduction_test.py
@@ -25,22 +25,3 @@ class TestInputReduction(AllenNlpTestCase):
         )  # input reduction removes tokens
         for word in reduced["final"][0]:  # no new words entered
             assert word in reduced["original"]
-
-        # test using NER model (tests different underlying logic)
-        inputs = {"sentence": "Eric Wallace was an intern at AI2"}
-
-        archive = load_archive(
-            self.FIXTURES_ROOT / "simple_tagger" / "serialization" / "model.tar.gz"
-        )
-        predictor = Predictor.from_archive(archive, "sentence_tagger")
-
-        reducer = InputReduction(predictor)
-        reduced = reducer.attack_from_json(inputs, "tokens", "grad_input_1")
-        assert reduced is not None
-        assert "final" in reduced
-        assert "original" in reduced
-        for reduced_input in reduced["final"]:
-            assert reduced_input  # always at least one token
-            assert len(reduced_input) <= len(reduced["original"])  # input reduction removes tokens
-            for word in reduced_input:  # no new words entered
-                assert word in reduced["original"]
