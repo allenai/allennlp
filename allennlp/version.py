00e3ff27b312b121aa2b4b199acbffa31ebf3e07
epwalsh <epwalsh10@gmail.com>
2021-01-26 12:06:26 -0800

    tick version for nightly release

--- allennlp/version.py
+++ allennlp/version.py
@@ -7,7 +7,7 @@ _MINOR = "0"
 _PATCH = "0"
 # This is mainly for nightly builds which have the suffix ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
-_SUFFIX = os.environ.get("ALLENNLP_VERSION_SUFFIX", "rc1")
+_SUFFIX = os.environ.get("ALLENNLP_VERSION_SUFFIX", "rc2")
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
 VERSION = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
