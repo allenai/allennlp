e7b88252aa7344584635714301402b98b162279a
Jacob Danovitch <jacobgdt@gmail.com>
2021-02-18 14:04:17 -0500

    checking in sparse attention

--- allennlp/training/deepspeed/__init__.py
+++ allennlp/training/deepspeed/__init__.py
@@ -1,2 +1,3 @@
 from allennlp.training.deepspeed.trainer import DeepspeedTrainer
 from allennlp.training.deepspeed import optimizers
+from allennlp.training.deepspeed import modules
---
+++ allennlp/training/deepspeed/modules/__init__.py
@@ -0,0 +1,1 @@
+from allennlp.training.deepspeed.modules.sparse_transformer import SparseTransformerEmbedder
