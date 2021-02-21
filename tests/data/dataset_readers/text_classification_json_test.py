815f04ef6972a52c27714bcdfdd2cc1193310000
RyujiTamaki <e44119rt@gmail.com>
2021-02-21 14:17:11 +0900

    Add text_key and label_key to TextClassificationJsonReader

--- tests/data/dataset_readers/text_classification_json_test.py
+++ tests/data/dataset_readers/text_classification_json_test.py
@@ -324,3 +324,199 @@ class TestTextClassificationJsonReader:
         text = [[token.text for token in sentence.tokens] for sentence in fields["tokens"]]
         assert text == instance3["tokens"]
         assert fields["label"].label == instance3["label"]
+
+    def test_read_from_file_ag_news_corpus_and_test_key_and_label_key(self):
+        reader = TextClassificationJsonReader(text_key="description", label_key="category")
+        ag_path = (
+            AllenNlpTestCase.FIXTURES_ROOT
+            / "data"
+            / "text_classification_json"
+            / "ag_news_corpus_test_key_name.jsonl"
+        )
+        instances = list(reader.read(ag_path))
+        is_version_3 = spacy.__version__ >= "3.0"
+
+        instance1 = {
+            "tokens": [
+                "Memphis",
+                "Rout",
+                "Still",
+                "Stings",
+                "for",
+                "No",
+                ".",
+                "14",
+                "Louisville",
+                ";",
+                "Coach",
+                "Petrino",
+                "Vows",
+                "to",
+                "Have",
+                "Team",
+                "Better",
+                "Prepared",
+                ".",
+                "NASHVILLE",
+                ",",
+                "Tenn.",
+                "Nov",
+                "3",
+                ",",
+                "2004",
+                "-",
+                "Louisville",
+                "#",
+                "39;s",
+                "30-point",
+                "loss",
+                "at",
+                "home",
+                "to",
+                "Memphis",
+                "last",
+                "season",
+                "is",
+                "still",
+                "a",
+                "painful",
+                "memory",
+                "for",
+                "the",
+                "Cardinals",
+                ".",
+            ],
+            "label": "2",
+        }
+        instance1_v3 = {
+            "tokens": [
+                "Memphis",
+                "Rout",
+                "Still",
+                "Stings",
+                "for",
+                "No",
+                ".",
+                "14",
+                "Louisville",
+                ";",
+                "Coach",
+                "Petrino",
+                "Vows",
+                "to",
+                "Have",
+                "Team",
+                "Better",
+                "Prepared",
+                ".",
+                "NASHVILLE",
+                ",",
+                "Tenn.",
+                "Nov",
+                "3",
+                ",",
+                "2004",
+                "-",
+                "Louisville",
+                "#",
+                "39;s",
+                "30",
+                "-",
+                "point",
+                "loss",
+                "at",
+                "home",
+                "to",
+                "Memphis",
+                "last",
+                "season",
+                "is",
+                "still",
+                "a",
+                "painful",
+                "memory",
+                "for",
+                "the",
+                "Cardinals",
+                ".",
+            ],
+            "label": "2",
+        }
+
+        instance2 = {
+            "tokens": [
+                "AP",
+                "-",
+                "Eli",
+                "Manning",
+                "has",
+                "replaced",
+                "Kurt",
+                "Warner",
+                "as",
+                "the",
+                "New",
+                "York",
+                "Giants",
+                "'",
+                "starting",
+                "quarterback",
+                ".",
+            ],
+            "label": "2",
+        }
+        instance3 = {
+            "tokens": [
+                "A",
+                "conference",
+                "dedicated",
+                "to",
+                "online",
+                "journalism",
+                "explores",
+                "the",
+                "effect",
+                "blogs",
+                "have",
+                "on",
+                "news",
+                "reporting",
+                ".",
+                "Some",
+                "say",
+                "they",
+                "draw",
+                "attention",
+                "to",
+                "under",
+                "-",
+                "reported",
+                "stories",
+                ".",
+                "Others",
+                "struggle",
+                "to",
+                "establish",
+                "the",
+                "credibility",
+                "enjoyed",
+                "by",
+                "professionals",
+                ".",
+            ],
+            "label": "4",
+        }
+
+        assert len(instances) == 3
+        fields = instances[0].fields
+        if is_version_3:
+            assert [t.text for t in fields["tokens"].tokens] == instance1_v3["tokens"]
+        else:
+            assert [t.text for t in fields["tokens"].tokens] == instance1["tokens"]
+        assert fields["label"].label == instance1["label"]
+        fields = instances[1].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance2["tokens"]
+        assert fields["label"].label == instance2["label"]
+        fields = instances[2].fields
+        assert [t.text for t in fields["tokens"].tokens] == instance3["tokens"]
+        assert fields["label"].label == instance3["label"]
