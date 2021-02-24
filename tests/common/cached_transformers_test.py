4a5898fbc0257b594393807317d495b5c40b277a
Dirk Groeneveld <dirkg@allenai.org>
2021-02-23 16:21:02 -0800

    Apparently there are three versions of this exception 🙄

--- tests/common/cached_transformers_test.py
+++ tests/common/cached_transformers_test.py
@@ -6,7 +6,7 @@ from allennlp.common.testing import AllenNlpTestCase
 
 class TestCachedTransformers(AllenNlpTestCase):
     def test_get_missing_from_cache_local_files_only(self):
-        with pytest.raises((FileNotFoundError, ValueError)):
+        with pytest.raises((OSError, ValueError)):
             cached_transformers.get(
                 "bert-base-uncased",
                 True,
@@ -15,7 +15,7 @@ class TestCachedTransformers(AllenNlpTestCase):
             )
 
     def test_get_tokenizer_missing_from_cache_local_files_only(self):
-        with pytest.raises((FileNotFoundError, ValueError)):
+        with pytest.raises((OSError, ValueError)):
             cached_transformers.get_tokenizer(
                 "bert-base-uncased",
                 cache_dir=self.TEST_DIR,
