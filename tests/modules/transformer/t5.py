f93d137cf2c6b5adc6c3cb3713745f8b562e9d9b
Dirk Groeneveld <dirkg@allenai.org>
2021-02-25 19:18:38 -0800

    Adds one test that passes, and one test that fails

--- tests/modules/transformer/t5.py
+++ tests/modules/transformer/t5.py
@@ -3,3 +3,11 @@ from allennlp.modules.transformer.t5 import *
 
 def test_create_default_t5():
     t5 = T5ForConditionalGeneration()
+
+
+def test_create_t5_with_weights():
+    t5 = T5ForConditionalGeneration.from_pretrained_module("t5-large")
+
+
+def test_create_t5_with_different_size_than_default():
+    t5 = T5ForConditionalGeneration.from_pretrained_module("t5-small")
