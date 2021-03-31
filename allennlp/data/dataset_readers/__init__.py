da778db674bc0cb94f5c444c70f1764f1437ec0e
epwalsh <epwalsh10@gmail.com>
2021-03-31 15:50:07 -0700

    remove useless dataset reader

--- allennlp/data/dataset_readers/__init__.py
+++ allennlp/data/dataset_readers/__init__.py
@@ -19,4 +19,3 @@ from allennlp.data.dataset_readers.multitask import MultiTaskDatasetReader
 from allennlp.data.dataset_readers.sequence_tagging import SequenceTaggingDatasetReader
 from allennlp.data.dataset_readers.sharded_dataset_reader import ShardedDatasetReader
 from allennlp.data.dataset_readers.text_classification_json import TextClassificationJsonReader
-from allennlp.data.dataset_readers.t5 import T5DatasetReader
