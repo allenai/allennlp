b578091a6f8ce962d295375c1e58db2d38afc245
Arjun Subramonian <arjuns@Arjuns-MacBook-Pro.local>
2021-04-01 13:54:34 -0700

    Another major update to test

--- tests/common/cached_transformers_test.py
+++ tests/common/cached_transformers_test.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
 import os
+import json
 
 from allennlp.common import cached_transformers
 from allennlp.common.testing import AllenNlpTestCase
@@ -11,7 +12,7 @@ from transformers import AutoModel, AutoConfig
 class TestCachedTransformers(AllenNlpTestCase):
     def test_get_missing_from_cache_local_files_only(self):
         with pytest.raises((OSError, ValueError)):
-            transformer = cached_transformers.get(
+            cached_transformers.get(
                 "bert-base-uncased",
                 True,
                 cache_dir=self.TEST_DIR,
@@ -31,19 +32,26 @@ class TestCachedTransformers(AllenNlpTestCase):
         save_weights_path = str(self.TEST_DIR) + "/bert_weights.pth"
         torch.save(transformer.state_dict(), save_weights_path)
 
-        pre_download_num_files = len(os.listdir(str(self.TEST_DIR)))
         override_transformer = cached_transformers.get(
             "bert-base-uncased",
             False,
             override_weights_file=save_weights_path,
             cache_dir=self.TEST_DIR,
         )
-        post_download_num_files = len(os.listdir(str(self.TEST_DIR)))
-        # check that only three files were downloaded (.json, .[etag], .lock), for config.json
+        # check that only three files were downloaded (filename.json, filename, filename.lock), for config.json
         # if more than three files were downloaded, then model weights were also (incorrectly) downloaded
         # NOTE: downloaded files are not explicitly detailed in Huggingface's public API,
         # so this assertion could fail in the future
-        assert post_download_num_files - pre_download_num_files == 3
+        json_fnames = [fname for fname in os.listdir(str(self.TEST_DIR)) if fname.endswith(".json")]
+        assert len(json_fnames) == 1
+        json_data = json.load(open(str(self.TEST_DIR) + "/" + json_fnames[0]))
+        assert (
+            json_data["url"] == "https://huggingface.co/bert-base-uncased/resolve/main/config.json"
+        )
+        resource_id = os.path.splitext(json_fnames[0])[0]
+        assert set(os.listdir(str(self.TEST_DIR))) == set(
+            [json_fnames[0], resource_id, resource_id + ".lock", "bert_weights.pth"]
+        )
 
         # check that override weights were loaded correctly
         for p1, p2 in zip(transformer.parameters(), override_transformer.parameters()):
