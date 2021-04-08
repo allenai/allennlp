37373c85def39e908984df724a4f1a7a4ac74b6d
Arjun Subramonian <arjuns@Arjuns-MacBook-Pro.local>
2021-04-08 09:01:40 -0700

    Added DemographicParityWithoutGroundTruth and finished tests

--- allennlp/fairness/fairness_metrics.py
+++ allennlp/fairness/fairness_metrics.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, Union
 
 import torch
 from torch.distributions.categorical import Categorical
@@ -9,13 +9,15 @@ from allennlp.training.metrics.metric import Metric
 
 """
 Fairness metrics are based on:
-1) Barocas, S.; Hardt, M.; Narayanan, A. 2019. Fairness and machine learning. fairmlbook.org.
-1) Zhang, B. H.; Lemoine, B.; and Mitchell, M. 2018. Mitigating unwanted biases with adversarial learning.
+1) Barocas, S.; Hardt, M.; and Narayanan, A. 2019. Fairness and machine learning. fairmlbook.org.
+2) Zhang, B. H.; Lemoine, B.; and Mitchell, M. 2018. Mitigating unwanted biases with adversarial learning.
 In Proceedings of the 2018 AAAI/ACM Conference on AI, Ethics, and Society, 335-340.
-2) Hardt, M.; Price, E.; Srebro, N.; et al. 2016. Equality of opportunity in supervised learning.
+3) Hardt, M.; Price, E.; Srebro, N.; et al. 2016. Equality of opportunity in supervised learning.
 In Advances in Neural Information Processing Systems, 3315–3323.
-3) Beutel, A.; Chen, J.; Zhao, Z.; and Chi, E. H. 2017. Data decisions and theoretical implications when
+4) Beutel, A.; Chen, J.; Zhao, Z.; and Chi, E. H. 2017. Data decisions and theoretical implications when
 adversarially learning fair representations. arXiv preprint arXiv:1707.00075.
+5) Aka, O.; Burke, K.; Bäuerle, A.; Greer, C.; and Mitchell, M. 2021.
+Measuring model biases in the absence of ground truth. arXiv preprint arXiv:2103.03417.
 
 It is provably impossible to satisfy any two of Independence, Separation, and Sufficiency simultaneously,
 except in degenerate cases.
@@ -43,7 +45,7 @@ class Independence(Metric):
         protected_variable_labels : `torch.Tensor`, required.
             A tensor of integer protected variable labels of shape (batch_size, ...). It must be the same
             shape as the `predicted_labels` tensor. Represented as A.
-        num_classes : `torch.Tensor`, optional (default = `None`).
+        num_classes : `int`, optional (default = `None`).
             Number of classes. If not supplied, `num_classes` is inferred from `predicted_labels`.
 
         # Returns
@@ -119,6 +121,10 @@ class Separation(Metric):
         kl_divs : `Dict[int, Dict[int, torch.FloatTensor]]`
             A dictionary mapping each class label y to a dictionary mapping each protected
             variable label a to the KL divergence of P(C | A = a, Y = y) from P(C | Y = y).
+
+            Note: If a class label is not present in Y conditioned on a protected variable label,
+            the expected behavior is that the divergence corresponding to this (class label, protected variable
+            label) pair is NaN.
         """
         predicted_labels, gold_labels, protected_variable_labels = self.detach_tensors(
             predicted_labels, gold_labels, protected_variable_labels
@@ -154,9 +160,6 @@ class Separation(Metric):
                 .histc(bins=num_classes, min=0, max=num_classes - 1)
                 / predicted_labels.nelement()
             )
-            if probs.sum() == 0:
-                kl_divs[y] = {a: torch.zeros(1)[0] for a in range(num_protected_variables)}
-                continue
 
             C_given_y_dist = Categorical(probs)
             kl_divs[y] = {}
@@ -167,8 +170,9 @@ class Separation(Metric):
                     .histc(bins=num_classes, min=0, max=num_classes - 1)
                     / predicted_labels.nelement()
                 )
+                # Implies class label y is not present in Y conditioned on protected variable label a
                 if probs.sum() == 0:
-                    kl_divs[y][a] = torch.zeros(1)[0]
+                    kl_divs[y][a] = torch.tensor(float("nan"))
                     continue
 
                 C_given_a_and_y_dist = Categorical(probs)
@@ -206,6 +210,11 @@ class Sufficiency(Metric):
         kl_divs : `Dict[int, Dict[int, torch.FloatTensor]]`
             A dictionary mapping each class label c to a dictionary mapping each protected
             variable label a to the KL divergence of P(Y | A = a, C = c) from P(Y | C = c).
+
+            Note: If a possible class label is not present in C, the expected behavior is that
+            the divergences corresponding to this class label are NaN. If a possible class label is
+            not present in C conditioned on a protected variable label, the expected behavior is that
+            the divergence corresponding to this (class label, protected variable label) pair is NaN.
         """
         predicted_labels, gold_labels, protected_variable_labels = self.detach_tensors(
             predicted_labels, gold_labels, protected_variable_labels
@@ -243,8 +252,9 @@ class Sufficiency(Metric):
                 .histc(bins=num_classes, min=0, max=num_classes - 1)
                 / gold_labels.nelement()
             )
+            # Implies class label y is not present in Y conditioned on protected variable label a
             if probs.sum() == 0:
-                kl_divs[c] = {a: torch.zeros(1)[0] for a in range(num_protected_variables)}
+                kl_divs[c] = {a: torch.tensor(float("nan")) for a in range(num_protected_variables)}
                 continue
 
             Y_given_c_dist = Categorical(probs)
@@ -257,9 +267,200 @@ class Sufficiency(Metric):
                     / gold_labels.nelement()
                 )
                 if probs.sum() == 0:
-                    kl_divs[c][a] = torch.zeros(1)[0]
+                    kl_divs[c][a] = torch.tensor(float("nan"))
                     continue
 
                 Y_given_a_and_c_dist = Categorical(probs)
                 kl_divs[c][a] = kl_divergence(Y_given_a_and_c_dist, Y_given_c_dist)
         return kl_divs
+
+
+@Metric.register("demographic_parity_without_ground_truth")
+class DemographicParityWithoutGroundTruth(Metric):
+    """
+    Demographic parity without ground truth. Assumes integer predictions, with
+    each item to be classified having a single correct class.
+    From: Aka, O.; Burke, K.; Bäuerle, A.; Greer, C.; and Mitchell, M. 2021.
+    Measuring model biases in the absence of ground truth. arXiv preprint arXiv:2103.03417.
+    """
+
+    def __init__(self, association_metric: str = "npmixy", gap_type: str = "ova") -> None:
+        """
+        # Parameters
+
+        association_metric : `str`, optional (default = `npmixy`).
+            A generic association metric A(x, y), where x is an identity label and y is any other label.
+            Examples include: nPMIxy (`npmixy`), nPMIy (`npmiy`), PMI^2 (`pmisq`), PMI (`pmi`)
+            Empirically, nPMIxy and nPMIy are more capable of capturing labels across a range of
+            marginal frequencies.
+        gap_type : `str`, optional (default = `ova`).
+            Either one-vs-all (`ova`) or pairwise (`pairwise`). One-vs-all gap is equivalent to
+            A(x, y) - E[A(x', y)], where x' is in the set of all protected variable labels setminus {x}.
+            Pairwise gaps are A(x, y) - A(x', y), for all x' in the set of all protected variable labels
+            setminus {x}.
+        """
+        self.IMPLEMENTED_ASSOCIATION_METRICS = set(["npmixy", "npmiy", "pmisq", "pmi"])
+        if association_metric in self.IMPLEMENTED_ASSOCIATION_METRICS:
+            self.association_metric = association_metric
+        else:
+            raise NotImplementedError(
+                f"Association metric {association_metric} has not been implemented!"
+            )
+
+        if gap_type == "ova":
+            self.gap_func = self._ova_gap
+        elif gap_type == "pairwise":
+            self.gap_func = self._pairwise_gaps
+        else:
+            raise NotImplementedError(f"Gap type {gap_type} has not been implemented!")
+
+    def __call__(
+        self,
+        predicted_labels: torch.Tensor,
+        protected_variable_labels: torch.Tensor,
+        num_classes: Optional[int] = None,
+    ) -> Dict[int, Union[torch.FloatTensor, Dict[int, torch.FloatTensor]]]:
+        """
+        # Parameters
+
+        predicted_labels : `torch.Tensor`, required.
+            A tensor of predicted integer class labels of shape (batch_size, ...). Represented as Y.
+        protected_variable_labels : `torch.Tensor`, required.
+            A tensor of integer protected variable labels of shape (batch_size, ...). It must be the same
+            shape as the `predicted_labels` tensor. Represented as X.
+        num_classes : `int`, optional (default = `None`).
+            Number of classes. If not supplied, `num_classes` is inferred from `predicted_labels`.
+
+        # Returns
+
+        gaps : `Dict[int, Union[torch.FloatTensor, Dict[int, torch.FloatTensor]]]`
+            A dictionary mapping each protected variable label x to either:
+            1) a tensor of the one-vs-all gaps (where the gap corresponding to prediction
+            label i is at index i),
+            2) another dictionary mapping protected variable labels x' to a tensor
+            of the pairwise gaps (where the gap corresponding to prediction label i is at index i).
+
+            Note: If a possible class label is not present in Y, the expected behavior is that
+            the gaps corresponding to this class label are NaN. If a possible (class label,
+            protected variable label) pair is not present in the joint of Y and X, the expected
+            behavior is that the gap corresponding to this (class label, protected variable label)
+            pair is NaN.
+        """
+        predicted_labels, protected_variable_labels = self.detach_tensors(
+            predicted_labels, protected_variable_labels
+        )
+
+        # Some sanity checks.
+        if predicted_labels.size() != protected_variable_labels.size():
+            raise ConfigurationError(
+                "protected_variable_labels must be of same size as predicted_labels but "
+                "found tensor of shape: {}".format(protected_variable_labels.size())
+            )
+        if num_classes is not None and (predicted_labels >= num_classes).any():
+            raise ConfigurationError(
+                "A predicted label contains an id >= {}, "
+                "the number of classes.".format(num_classes)
+            )
+
+        if num_classes is None:
+            num_classes = predicted_labels.max() + 1
+        num_protected_variables = protected_variable_labels.max() + 1
+        gaps = {}
+        # Assumes num_protected_variables is small
+        for x in range(num_protected_variables):
+            gaps[x] = self.gap_func(
+                x,
+                protected_variable_labels.flatten(),
+                predicted_labels.flatten(),
+                num_classes,
+                num_protected_variables,
+            )
+        return gaps
+
+    def _ova_gap(
+        self,
+        x: torch.Tensor,
+        X: torch.Tensor,
+        Y: torch.Tensor,
+        num_classes,
+        num_protected_variables: int,
+    ):
+        x_mask = X == x
+        joint = torch.zeros(num_classes)
+        self._joint(x_mask.long(), Y, out=joint)
+        if self.association_metric == "pmisq":
+            torch.square_(joint)
+        prob_y = torch.zeros(num_classes)
+        self._prob_y(Y, out=prob_y)
+        pmi_x = torch.log(torch.div(joint, self._prob_x(x_mask) * prob_y))
+        if self.association_metric == "npmixy":
+            pmi_x.div_(torch.log(joint))
+        elif self.association_metric == "npmiy":
+            pmi_x.div_(torch.log(prob_y))
+
+        joint = torch.zeros(num_classes)
+        self._joint((~x_mask).long(), Y, out=joint)
+        if self.association_metric == "pmisq":
+            torch.square_(joint)
+        pmi_not_x = torch.log(torch.div(joint, self._prob_x(~x_mask) * prob_y)) / (
+            num_protected_variables - 1
+        )
+        if self.association_metric == "npmixy":
+            pmi_not_x.div_(torch.log(joint))
+        elif self.association_metric == "npmiy":
+            pmi_not_x.div_(torch.log(prob_y))
+
+        # Will contain NaN if not all possible class labels are predicted
+        # Will contain NaN if not all possible (class label,
+        # protected variable label) pairs are predicted
+        gap = pmi_x - pmi_not_x
+        return torch.where(~gap.isinf(), gap, torch.tensor(float("nan")))
+
+    def _pairwise_gaps(
+        self,
+        x: torch.Tensor,
+        X: torch.Tensor,
+        Y: torch.Tensor,
+        num_classes,
+        num_protected_variables: int,
+    ):
+        x_mask = X == x
+        joint = torch.zeros(num_classes)
+        self._joint(x_mask.long(), Y, out=joint)
+        if self.association_metric == "pmisq":
+            torch.square_(joint)
+        prob_y = torch.zeros(num_classes)
+        self._prob_y(Y, out=prob_y)
+        pmi_x = torch.log(torch.div(joint, self._prob_x(x_mask) * prob_y))
+        if self.association_metric == "npmixy":
+            pmi_x.div_(torch.log(joint))
+        elif self.association_metric == "npmiy":
+            pmi_x.div_(torch.log(prob_y))
+
+        pairwise_gaps = {}
+        for not_x in range(num_protected_variables):
+            not_x_mask = X == not_x
+            joint = torch.zeros(num_classes)
+            self._joint(not_x_mask.long(), Y, out=joint)
+            if self.association_metric == "pmisq":
+                torch.square_(joint)
+            pmi_not_x = torch.log(torch.div(joint, self._prob_x(not_x_mask) * prob_y))
+            if self.association_metric == "npmixy":
+                pmi_not_x.div_(torch.log(joint))
+            elif self.association_metric == "npmiy":
+                pmi_not_x.div_(torch.log(prob_y))
+
+            gap = pmi_x - pmi_not_x
+            pairwise_gaps[not_x] = torch.where(~gap.isinf(), gap, torch.tensor(float("nan")))
+        return pairwise_gaps
+
+    def _joint(self, x_mask: torch.Tensor, Y: torch.Tensor, out: torch.Tensor):
+        counts = torch.zeros_like(out, dtype=x_mask.dtype).scatter_add_(0, Y, x_mask)
+        torch.div(counts, Y.nelement(), out=out)
+
+    def _prob_x(self, x_mask: torch.Tensor):
+        return x_mask.sum() / x_mask.nelement()
+
+    def _prob_y(self, Y: torch.Tensor, out: torch.Tensor):
+        counts = torch.zeros_like(out, dtype=Y.dtype).scatter_add_(0, Y, torch.ones_like(Y))
+        torch.div(counts, Y.nelement(), out=out)
--- tests/fairness/fairness_metrics_test.py
+++ tests/fairness/fairness_metrics_test.py
@@ -1,9 +1,16 @@
 import pytest
 import torch
+import math
+import numpy as np
 
 from allennlp.common.checks import ConfigurationError
 from allennlp.common.testing import AllenNlpTestCase
-from allennlp.fairness.fairness_metrics import Independence, Separation, Sufficiency
+from allennlp.fairness.fairness_metrics import (
+    Independence,
+    Separation,
+    Sufficiency,
+    DemographicParityWithoutGroundTruth,
+)
 
 
 class IndependenceTest(AllenNlpTestCase):
@@ -79,17 +86,18 @@ class SeparationTest(AllenNlpTestCase):
         # P(C | A = 0, Y = 0) = [1.0, 0.0]
         # P(C | A = 1, Y = 0) = [0.0, 0.0]
         # KL(P(C | A = 0, Y = 0) || P(C | Y = 0)) = 0.0
-        # KL(P(C | A = 1, Y = 0) || P(C | Y = 0)) = 0.0
+        # KL(P(C | A = 1, Y = 0) || P(C | Y = 0)) = NaN
 
         # P(C | Y = 1) = [0.0, 1.0]
         # P(C | A = 0, Y = 1) = [0.0, 0.0]
         # P(C | A = 1, Y = 1) = [0.0, 1.0]
-        # KL(P(C | A = 0, Y = 1) || P(C | Y = 1)) = 0.0
+        # KL(P(C | A = 0, Y = 1) || P(C | Y = 1)) = NaN
         # KL(P(C | A = 1, Y = 1) || P(C | Y = 1)) = 0.0
 
-        expected_kl_divs = {0: {0: 0.0, 1: 0.0}, 1: {0: 0.0, 1: 0.0}}
+        # KL divergence cannot be negative
+        expected_kl_divs = {0: {0: 0.0, 1: np.nan}, 1: {0: np.nan, 1: 0.0}}
         test_kl_divs = {
-            k1: {k2: v2.item() for k2, v2 in v1.items()}
+            k1: {k2: (v2.item() if not math.isnan(v2.item()) else np.nan) for k2, v2 in v1.items()}
             for k1, v1 in self.separation(C, Y, A).items()
         }
         assert expected_kl_divs == test_kl_divs
@@ -129,14 +137,159 @@ class SufficiencyTest(AllenNlpTestCase):
         # P(Y | C = 1) = [0.0, 0.0]
         # P(Y | A = 0, C = 1) = [0.0, 0.0]
         # P(Y | A = 1, C = 1) = [0.0, 0.0]
-        # KL(P(Y | A = 0, C = 1) || P(Y | C = 1)) = 0.0
-        # KL(P(Y | A = 1, C = 1) || P(Y | C = 1)) = 0.0
+        # KL(P(Y | A = 0, C = 1) || P(Y | C = 1)) = NaN
+        # KL(P(Y | A = 1, C = 1) || P(Y | C = 1)) = NaN
 
-        expected_kl_divs = {0: {0: 0.4055, 1: 1.0986}, 1: {0: 0.0, 1: 0.0}}
+        # KL divergence cannot be negative
+        expected_kl_divs = {0: {0: 0.4055, 1: 1.0986}, 1: {0: np.nan, 1: np.nan}}
         test_kl_divs = {
-            k1: {k2: v2.item() for k2, v2 in v1.items()}
+            k1: {k2: (v2.item() if not math.isnan(v2.item()) else np.nan) for k2, v2 in v1.items()}
             for k1, v1 in self.sufficiency(C, Y, A).items()
         }
         assert len(expected_kl_divs) == len(test_kl_divs)
         assert expected_kl_divs[0] == pytest.approx(test_kl_divs[0], abs=1e-3)
-        assert expected_kl_divs[1] == pytest.approx(test_kl_divs[1], abs=1e-3)
+        assert expected_kl_divs[1] == test_kl_divs[1]
+
+
+class DemographicParityWithoutGroundTruthTest(AllenNlpTestCase):
+    def test_invalid_dimensions(self):
+        ova_npmixy = DemographicParityWithoutGroundTruth()
+        Y = torch.eye(3).long()
+        X = torch.eye(4).long()
+        with pytest.raises(ConfigurationError):
+            ova_npmixy(Y, X)
+
+    def test_invalid_num_classes(self):
+        ova_npmixy = DemographicParityWithoutGroundTruth()
+        Y = torch.eye(3).long()
+        X = torch.eye(3).long()
+        with pytest.raises(ConfigurationError):
+            ova_npmixy(Y, X, 1)
+
+    def test_pmi(self):
+        ova_pmi = DemographicParityWithoutGroundTruth("pmi", "ova")
+        pairwise_pmi = DemographicParityWithoutGroundTruth("pmi", "pairwise")
+        Y = torch.ones(3, 3).long()
+        X = torch.eye(3).long()
+
+        # P(X = 0, Y = 0) = 0
+        # P(X = 0, Y = 1) = 2/3
+        # P(X = 1, Y = 0) = 0
+        # P(X = 1, Y = 1) = 1/3
+        # P(X = 0) = 2/3
+        # P(X = 1) = 1/3
+        # P(Y = 0) = 0
+        # P(Y = 1) = 1
+        # G(Y = 0 | X = 0, X = rest, PMI) = NaN
+        # G(Y = 1 | X = 0, X = rest, PMI) = ln(1) - ln(1) = 0.0
+        # G(Y = 0 | X = 1, X = rest, PMI) = NaN
+        # G(Y = 1 | X = 1, X = rest, PMI) = ln(1) - ln(1) = 0.0
+        expected_ova_pmi_gaps = {
+            0: [np.nan, 0.0],
+            1: [np.nan, 0.0],
+        }
+        test_ova_pmi_gaps = {
+            k: [(e if not math.isnan(e) else np.nan) for e in v.tolist()]
+            for k, v in ova_pmi(Y, X).items()
+        }
+        assert expected_ova_pmi_gaps == test_ova_pmi_gaps
+
+        expected_pairwise_pmi_gaps = {
+            0: {0: [np.nan, 0.0], 1: [np.nan, 0.0]},
+            1: {0: [np.nan, 0.0], 1: [np.nan, 0.0]},
+        }
+        test_pairwise_pmi_gaps = {
+            k1: {
+                k2: [(e if not math.isnan(e) else np.nan) for e in v2.tolist()]
+                for k2, v2 in v1.items()
+            }
+            for k1, v1 in pairwise_pmi(Y, X).items()
+        }
+        assert expected_pairwise_pmi_gaps == test_pairwise_pmi_gaps
+
+    def test_pmisq(self):
+        ova_pmisq = DemographicParityWithoutGroundTruth("pmisq", "ova")
+        pairwise_pmisq = DemographicParityWithoutGroundTruth("pmisq", "pairwise")
+        Y = torch.ones(3, 3).long()
+        X = torch.eye(3).long()
+
+        expected_ova_pmisq_gaps = {
+            0: [np.nan, round(math.log(2), 3)],
+            1: [np.nan, round(math.log(0.5), 3)],
+        }
+        test_ova_pmisq_gaps = {
+            k: [(round(e, 3) if not math.isnan(e) else np.nan) for e in v.tolist()]
+            for k, v in ova_pmisq(Y, X).items()
+        }
+        assert expected_ova_pmisq_gaps == test_ova_pmisq_gaps
+
+        expected_pairwise_pmisq_gaps = {
+            0: {0: [np.nan, 0.0], 1: [np.nan, round(math.log(2), 3)]},
+            1: {0: [np.nan, round(math.log(0.5), 3)], 1: [np.nan, 0.0]},
+        }
+        test_pairwise_pmisq_gaps = {
+            k1: {
+                k2: [(round(e, 3) if not math.isnan(e) else np.nan) for e in v2.tolist()]
+                for k2, v2 in v1.items()
+            }
+            for k1, v1 in pairwise_pmisq(Y, X).items()
+        }
+        assert expected_pairwise_pmisq_gaps == test_pairwise_pmisq_gaps
+
+    def test_npmiy(self):
+        ova_npmiy = DemographicParityWithoutGroundTruth("npmiy", "ova")
+        pairwise_npmiy = DemographicParityWithoutGroundTruth("npmiy", "pairwise")
+        Y = torch.ones(3, 3).long()
+        X = torch.eye(3).long()
+
+        expected_ova_npmiy_gaps = {
+            0: [np.nan, np.nan],
+            1: [np.nan, np.nan],
+        }
+        test_ova_npmiy_gaps = {
+            k: [(e if not math.isnan(e) else np.nan) for e in v.tolist()]
+            for k, v in ova_npmiy(Y, X).items()
+        }
+        assert expected_ova_npmiy_gaps == test_ova_npmiy_gaps
+
+        expected_pairwise_npmiy_gaps = {
+            0: {0: [np.nan, np.nan], 1: [np.nan, np.nan]},
+            1: {0: [np.nan, np.nan], 1: [np.nan, np.nan]},
+        }
+        test_pairwise_npmiy_gaps = {
+            k1: {
+                k2: [(e if not math.isnan(e) else np.nan) for e in v2.tolist()]
+                for k2, v2 in v1.items()
+            }
+            for k1, v1 in pairwise_npmiy(Y, X).items()
+        }
+        assert expected_pairwise_npmiy_gaps == test_pairwise_npmiy_gaps
+
+    def test_npmixy(self):
+        ova_npmixy = DemographicParityWithoutGroundTruth("npmixy", "ova")
+        pairwise_npmixy = DemographicParityWithoutGroundTruth("npmixy", "pairwise")
+        Y = torch.ones(3, 3).long()
+        X = torch.eye(3).long()
+
+        expected_ova_npmixy_gaps = {
+            0: [np.nan, 0.0],
+            1: [np.nan, 0.0],
+        }
+        test_ova_npmixy_gaps = {
+            k: [(e if not math.isnan(e) else np.nan) for e in v.tolist()]
+            for k, v in ova_npmixy(Y, X).items()
+        }
+        assert expected_ova_npmixy_gaps == test_ova_npmixy_gaps
+
+        expected_pairwise_npmixy_gaps = {
+            0: {0: [np.nan, 0.0], 1: [np.nan, 0.0]},
+            1: {0: [np.nan, 0.0], 1: [np.nan, 0.0]},
+        }
+        test_pairwise_npmixy_gaps = {
+            k1: {
+                k2: [(e if not math.isnan(e) else np.nan) for e in v2.tolist()]
+                for k2, v2 in v1.items()
+            }
+            for k1, v1 in pairwise_npmixy(Y, X).items()
+        }
+        assert expected_pairwise_npmixy_gaps == test_pairwise_npmixy_gaps
