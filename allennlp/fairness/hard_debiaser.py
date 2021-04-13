79c6c33d287d378849a5c880881f208a43d66873
Arjun Subramonian <arjuns@Arjuns-MacBook-Pro.local>
2021-04-13 01:57:27 -0700

    added linear and hard debiasers

---
+++ allennlp/fairness/hard_debiaser.py
@@ -0,0 +1,34 @@
+import torch
+
+
+class HardDebiaser(torch.nn.Module):
+    """
+    Hard debiaser. Debiases by removing component of embeddings
+    in the bias direction.
+    """
+
+    def forward(
+        self, embeddings: torch.Tensor, bias_direction: torch.Tensor, no_grad: bool = False
+    ):
+        """
+
+        # Parameters
+
+        embeddings : `torch.Tensor`
+            A tensor of size (batch_size, ..., dim).
+        bias_direction : `torch.Tensor`
+            A tensor of size (dim, ).
+        no_grad : `bool`, optional (default=`False`)
+            Option to disable gradient calculation.
+
+        # Returns
+
+        debiased_embeddings : `torch.Tensor`
+            A tensor of the same size as embeddings. debiased_embeddings do not contain a component
+            in bias_direction.
+        """
+
+        with torch.set_grad_enabled(not no_grad):
+            return embeddings - torch.matmul(
+                embeddings, bias_direction.reshape(-1, 1)
+            ) * bias_direction / torch.dot(bias_direction, bias_direction)
