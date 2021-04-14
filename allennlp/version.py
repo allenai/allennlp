f82a060d0cca1d220d09de11d710d03118d2d478
Akshita Bhagia <akshita23bhagia@gmail.com>
2021-04-14 15:48:29 -0700

    Prepare for release v2.3.0

--- allennlp/version.py
+++ allennlp/version.py
@@ -1,10 +1,10 @@
 import os
 
 _MAJOR = "2"
-_MINOR = "2"
+_MINOR = "3"
 # On main and in a nightly release the patch should be one ahead of the last
 # released build.
-_PATCH = "1"
+_PATCH = "0"
 # This is mainly for nightly builds which have the suffix ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
 _SUFFIX = os.environ.get("ALLENNLP_VERSION_SUFFIX", "")
