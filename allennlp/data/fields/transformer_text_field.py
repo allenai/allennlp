5a9a7f42712b92a0bd821178a575b3563db4293e
Dirk Groeneveld <dirkg@allenai.org>
2021-05-11 16:36:22 -0700

    Steps for reading and tokenizing a dataset

---
+++ allennlp/data/fields/transformer_text_field.py
@@ -0,0 +1,82 @@
+from typing import Dict, Optional, List, Any
+
+from overrides import overrides
+import torch
+import torch.nn.functional
+
+from allennlp.data.fields.field import Field
+from allennlp.nn import util
+
+
+class TransformerTextField(Field[torch.Tensor]):
+    """
+    A `TransformerTextField` is a collection of several tensors that are are a representation of text,
+    tokenized and ready to become input to a transformer.
+    """
+
+    __slots__ = [
+        "token_ids",
+        "token_type_ids",
+        "attention_mask",
+        "special_tokens_mask",
+        "offsets_mapping",
+        "padding_token_id"
+    ]
+
+    def __init__(
+        self,
+        token_ids: torch.Tensor,
+        token_type_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        special_tokens_mask: Optional[torch.Tensor] = None,
+        offsets_mapping: Optional[torch.Tensor] = None,
+        padding_token_id: int = 0
+    ) -> None:
+        self.token_ids = token_ids
+        self.token_type_ids = token_type_ids
+        self.attention_mask = attention_mask
+        self.special_tokens_mask = special_tokens_mask
+        self.offsets_mapping = offsets_mapping
+        self.padding_token_id = padding_token_id
+
+    @overrides
+    def get_padding_lengths(self) -> Dict[str, int]:
+        return {
+            name: len(getattr(self, name))
+            for name in self.__slots__
+            if isinstance(getattr(self, name), torch.Tensor)
+        }
+
+    @overrides
+    def as_tensor(self, padding_lengths: Dict[str, int]) -> Dict[str, torch.Tensor]:
+        result = {}
+        for name, padding_length in padding_lengths.items():
+            tensor = getattr(self, name)
+            result[name] = torch.nn.functional.pad(
+                tensor,
+                (0, padding_length - len(tensor)),
+                value=self.padding_token_id if name == "token_ids" else 0)
+        return result
+
+    @overrides
+    def empty_field(self):
+        return TransformerTextField(torch.LongTensor())
+
+    @overrides
+    def batch_tensors(self, tensor_list: List[Dict[str, torch.Tensor]]) -> Dict[str, torch.Tensor]:
+        return util.batch_tensor_dicts(tensor_list)
+
+    def human_readable_repr(self) -> Dict[str, Any]:
+        def readable_tensor(t: torch.Tensor) -> str:
+            if len(t) <= 16:
+                return "[" + ", ".join(map(str, t)) + "]"
+            else:
+                return "[" + \
+                       ", ".join(map(str, t[:8])) + \
+                       ", ".join(map(str, t[-8:])) + \
+                       "]"
+        return {
+            name: readable_tensor(getattr(self, name))
+            for name in self.__slots__
+            if isinstance(getattr(self, name), torch.Tensor)
+        }
--- allennlp/steps/examples.py
+++ allennlp/steps/examples.py
@@ -1,16 +1,31 @@
+import copy
 import dataclasses
-from typing import Set
+import logging
+import re
+from typing import Set, Optional, Union, List, Dict, Collection, Any, Tuple, Iterable
 
 import datasets
+import more_itertools
+import torch.optim
+from datasets import Dataset
+from transformers import AutoTokenizer
 
-from allennlp.steps.dataset import AllenNlpDataset
+from allennlp.data import Batch, BatchSampler, DataLoader, Vocabulary
+from allennlp.data.fields.transformer_text_field import TransformerTextField
+from allennlp.models import Model
+from allennlp.steps.dataset import AllenNlpDataset, AllenNlpBatchedDataset
 from allennlp.steps.step import Step
+from allennlp.training import Checkpointer, TrainerCallback, GradientDescentTrainer
+from allennlp.training.learning_rate_schedulers import LearningRateScheduler
+from allennlp.training.momentum_schedulers import MomentumScheduler
+from allennlp.training.moving_average import MovingAverage
 
 
-@Step.register("huggingface_dataset")
+@Step.register("hf_dataset")
 class HuggingfaceDataset(Step):
     DETERMINISTIC = True
     VERSION = "001"
+    CACHEABLE = False   # These are already cached by huggingface.
 
     def run(self, dataset_name: str) -> AllenNlpDataset:
         return AllenNlpDataset(datasets.load_dataset(dataset_name), None, {"source": "huggingface"})
@@ -33,3 +48,174 @@ class TextOnlyDataset(Step):
                 for split_name, split in input.splits.items()
             },
         )
+
+
+@Step.register("hf_tokenizer")
+class Tokenize(Step):
+    DETERMINISTIC = True
+    VERSION = "001"
+    CACHEABLE = True
+
+    def run(
+        self,
+        tokenizer_name: str,
+        input: AllenNlpDataset,
+        fields_to_tokenize: Optional[List[str]] = None,
+        add_special_tokens: bool = True,
+        max_length: Optional[int] = 512,
+        special_tokens_mask: bool = False,
+        offset_mapping: bool = False
+    ) -> AllenNlpDataset:
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+        assert tokenizer.pad_token_type_id == 0
+
+        field_names_used = set()
+
+        # find all the strings
+        if fields_to_tokenize is None:
+            def should_tokenize_field(_: str) -> bool:
+                return True
+        else:
+            regexes_to_tokenize = [re.compile(r) for r in fields_to_tokenize]
+
+            def should_tokenize_field(field_name: str) -> bool:
+                for r in regexes_to_tokenize:
+                    if r.fullmatch(field_name):
+                        return True
+                return False
+
+        def find_string_objects(o: Any, prefix: str = "") -> Iterable[Tuple[str, str]]:
+            prefix = prefix.lstrip(".")
+            if isinstance(o, str):
+                if should_tokenize_field(prefix):
+                    yield prefix, o
+            elif isinstance(o, List):
+                for i, item in enumerate(o):
+                    yield from find_string_objects(item, f"{prefix}.{i}")
+            elif isinstance(o, Dict):
+                for name, item in o.items():
+                    yield from find_string_objects(item, f"{prefix}.{name}")
+
+        strings = []
+        for split_name, instances in input.splits.items():
+            for instance in instances:
+                for name, string in find_string_objects(instance):
+                    field_names_used.add(name)
+                    strings.append(string)
+
+        for field_name in sorted(field_names_used):
+            logging.info("Tokenizing field %s", field_name)
+
+        # This thing is so complicated because we want to call `batch_encode_plus` with all
+        # the strings at once.
+        encoded = tokenizer.batch_encode_plus(
+            strings,
+            add_special_tokens=add_special_tokens,
+            truncation=max_length is not None,
+            max_length=max_length,
+            return_token_type_ids=True,
+            return_attention_mask=False,
+            return_special_tokens_mask=special_tokens_mask,
+            return_offsets_mapping=offset_mapping)
+
+        # make fields
+        string_to_field = {
+            s: TransformerTextField(
+                torch.tensor(encoded["input_ids"][i], dtype=torch.int32),
+                torch.tensor(encoded["token_type_ids"][i], dtype=torch.int32),
+                torch.tensor(encoded["attention_mask"][i], dtype=torch.bool) if "attention_mask" in encoded else None,
+                torch.tensor(encoded["special_tokens_mask"][i], dtype=torch.bool) if "special_tokens_mask" in encoded else None,
+                torch.tensor(encoded["offset_mapping"][i], dtype=torch.int32) if "offset_mapping" in encoded else None,
+                tokenizer.pad_token_id
+            ) for i, s in enumerate(strings)
+        }
+
+        def replace_string_objects(o: Any) -> Any:
+            if isinstance(o, str):
+                try:
+                    return string_to_field[o]
+                except KeyError:
+                    return o
+            elif isinstance(o, List) or isinstance(o, Dataset):
+                return [replace_string_objects(i) for i in o]
+            elif isinstance(o, Dict):
+                return {
+                    key: replace_string_objects(value)
+                    for key, value in o.items()
+                }
+            else:
+                return o
+
+        new_splits = {
+            split_name: replace_string_objects(split_data)
+            for split_name, split_data in input.splits.items()
+        }
+
+        # make vocab
+        if input.vocab is not None:
+            vocab = copy.deepcopy(input.vocab)
+        else:
+            vocab = Vocabulary.empty()
+
+        for name in field_names_used:
+            vocab.add_transformer_vocab(tokenizer, name)
+
+        return AllenNlpDataset(new_splits, vocab)
+
+
+@Step.register("training")
+class TrainingStep(Step):
+    DETERMINISTIC = True
+    VERSION = "001"
+
+    # TODO: distributed training
+    # TODO: recovery of failed jobs
+
+    def run(
+        self,
+        model: Model,
+        dataset: AllenNlpDataset,
+        data_loader: DataLoader,
+        validation_data_loader: Optional[DataLoader],
+        training_split: str,
+        validation_split: Optional[str],
+        optimizer: torch.optim.Optimizer,
+        patience: Optional[int] = None,
+        validation_metric: Union[str, List[str]] = "-loss",
+        num_epochs: int = 20,
+        checkpointer: Checkpointer = None,
+        cuda_device: Optional[Union[int, torch.device]] = None,
+        grad_norm: Optional[float] = None,
+        grad_clipping: Optional[float] = None,
+        learning_rate_scheduler: Optional[LearningRateScheduler] = None,
+        momentum_scheduler: Optional[MomentumScheduler] = None,
+        moving_average: Optional[MovingAverage] = None,
+        callbacks: List[TrainerCallback] = None,
+        num_gradient_accumulation_steps: int = 1,
+        use_amp: bool = False,
+        enable_default_callbacks: bool = True,
+        run_sanity_checks: bool = True,
+    ) -> Model:
+
+
+        trainer = GradientDescentTrainer(
+            model,
+            optimizer=optimizer,
+            data_loader=None, # dataloader
+            patience=patience,
+            validation_metric=validation_metric,
+            validation_data_loader=None, # validation dataloader
+            num_epochs=num_epochs,
+            serialization_dir=None, # serialization dir
+            checkpointer=checkpointer,
+            cuda_device=cuda_device,
+            grad_norm=grad_norm,
+            grad_clipping=grad_clipping,
+            learning_rate_scheduler=learning_rate_scheduler,
+            momentum_scheduler=momentum_scheduler,
+            moving_average=moving_average,
+            callbacks=callbacks,
+            num_gradient_accumulation_steps=num_gradient_accumulation_steps,
+            use_amp=use_amp,
+            enable_default_callbacks=enable_default_callbacks,
+            run_sanity_checks=run_sanity_checks)
