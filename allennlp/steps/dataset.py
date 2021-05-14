1c005633f66c48e26393449a68e301f72ec92cde
Dirk Groeneveld <dirkg@allenai.org>
2021-05-14 16:04:21 -0700

    Updated dataset definition

--- allennlp/steps/dataset.py
+++ allennlp/steps/dataset.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass, field
-from typing import Mapping, Any, Optional, Collection
+from typing import Mapping, Any, Optional, Sequence
 
-from allennlp.data import Vocabulary, Batch
+from allennlp.data import Vocabulary
 
 
 @dataclass
 class AllenNlpDataset:
-    splits: Mapping[str, Collection[Any]]
+    splits: Mapping[str, Sequence[Any]]
     vocab: Optional[Vocabulary] = None
     metadata: Mapping[str, Any] = field(default_factory=dict)
