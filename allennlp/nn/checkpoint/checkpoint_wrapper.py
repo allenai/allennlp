055a7c91cb4aef7fed6fa50b32ee963d016f0363
epwalsh <epwalsh10@gmail.com>
2021-06-08 20:05:46 -0400

    get FairScale activation/grad checkpointing working

--- allennlp/nn/checkpoint/checkpoint_wrapper.py
+++ allennlp/nn/checkpoint/checkpoint_wrapper.py
@@ -1,10 +1,10 @@
 import functools
-from typing import Optional, Callable, TypeVar
+from typing import Callable, TypeVar
 import weakref
 
 import torch
 import torch.nn as nn
-from torch.utils.checkpoint import checkpoint_sequential
+from torch.utils.checkpoint import CheckpointFunction
 from overrides import overrides
 
 from allennlp.common.registrable import Registrable
@@ -14,17 +14,6 @@ class CheckpointWrapper(Registrable):
     """
     A `CheckpointWrapper` is used to enable activation/gradient checkpointing on modules
     that you wrap via the `.wrap_module()` method.
-
-    !!! Note
-        Some implementations have restrictions on the type of `nn.Module` you can wrap,
-        and the inputs that can be passed to the wrapped module.
-        For instance, the default implementation `TorchCheckpointWrapper` only accepts
-        `nn.Sequential` modules that take a single tensor input.
-
-    !!! Note
-        The provided `CheckpointWrapper` implementations work by patching the `.forward()` method
-        of the module, so there is no easy way to disable checkpointing
-        in a module after `.wrap_module()` is called.
     """
 
     default_implementation = "torch"
@@ -36,22 +25,17 @@ class CheckpointWrapper(Registrable):
 @CheckpointWrapper.register("torch")
 class TorchCheckpointWrapper(CheckpointWrapper):
     @overrides
-    def wrap_module(self, module: nn.Sequential, segments: Optional[int] = None) -> nn.Sequential:
+    def wrap_module(self, module: nn.Module) -> nn.Module:
         """
-        Wrap a `nn.Sequential` module for activation checkpointing.
+        Wrap a module so that the forward method uses PyTorch's [checkpointing functionality]
+        (https://pytorch.org/docs/stable/checkpoint.html).
         """
-        if not isinstance(module, nn.Sequential):
-            raise ValueError(
-                f"TorchCheckpointWrapper can only wrap nn.Sequential modules, but got {module.__class__}"
-            )
-        if segments is None:
-            segments = len(module)
-        # Adapted from FairScale:
+        # Inspired by FairScale:
         #  --> https://github.com/facebookresearch/fairscale/blob/1e4a503cda8571851a68effd6e504a192838ab06/fairscale/nn/checkpoint/checkpoint_activations.py#L145-L153  # noqa: E501
         # We just patch the forward method to avoid having to proxy all the fields and other methods.
         # The use of weakref here is to prevent creating a ref cycle: m -> m.forward -> m.
         module.forward = functools.partial(  # type: ignore[assignment]
-            _checkpointed_forward, type(module).forward, weakref.ref(module), segments
+            _checkpointed_forward, type(module).forward, weakref.ref(module)
         )
         return module
 
@@ -60,10 +44,10 @@ _T = TypeVar("_T")
 
 
 def _checkpointed_forward(
-    original_forward: Callable[[nn.Sequential, torch.Tensor], _T],
+    original_forward: Callable[..., _T],
     weak_self,
-    segments: int,
-    inputs: torch.Tensor,
+    *args,
+    **kwargs,
 ) -> _T:
     module = weak_self()
     assert (
@@ -72,9 +56,14 @@ def _checkpointed_forward(
 
     # If in eval mode, just use the original `.forward()` method.
     if not module.training:
-        return original_forward(module, inputs)
+        return original_forward(module, *args, **kwargs)
 
-    # Annoyingly the input needs a gradient or all gradients will be None.
-    if not inputs.requires_grad:
-        inputs.requires_grad_()
-    return checkpoint_sequential(module, segments, inputs)
+    # Need a dummy tensor with `requires_grad=True` or all gradients will be None,
+    # so we have to wrap the `original_forward` method with a function that takes the
+    # dummy tensor (it doesn't have to do anythning with it).
+
+    def run_function(dummy_tensor, *forward_args, **forward_kwargs):
+        return original_forward(module, *forward_args, **forward_kwargs)
+
+    dummy_tensor = torch.tensor([], requires_grad=True)
+    return CheckpointFunction.apply(run_function, True, dummy_tensor, *args, **kwargs)
--- tests/nn/checkpoint/checkpoint_wrapper_test.py
+++ tests/nn/checkpoint/checkpoint_wrapper_test.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import torch
 import torch.nn as nn
 
@@ -8,32 +6,45 @@ from allennlp.common.testing import multi_device
 from allennlp.nn.checkpoint.checkpoint_wrapper import CheckpointWrapper, TorchCheckpointWrapper
 
 
+forward_calls = 0
+
+
+class LinearForTesting(nn.Linear):
+    def forward(self, *args, **kwargs):
+        global forward_calls
+        forward_calls += 1
+        return super().forward(*args, **kwargs)
+
+
+def setup_function(_):
+    global forward_calls
+    forward_calls = 0
+
+
 class ModuleForTesting(nn.Module):
-    def __init__(self, checkpoint_wrapper: Optional[CheckpointWrapper] = None) -> None:
+    def __init__(self, checkpoint_wrapper: CheckpointWrapper) -> None:
         super().__init__()
         ffn = nn.Sequential(
             nn.Linear(3, 3),
             # Use a nn.Dropout layer to test RNG (random number generator) state save/restore
             nn.Dropout(p=0.5),
+            LinearForTesting(3, 3),
             nn.Linear(3, 3),
         )
-        if checkpoint_wrapper is not None:
-            self.ffn = checkpoint_wrapper.wrap_module(ffn)
-        else:
-            self.ffn = ffn
+        self.ffn = checkpoint_wrapper.wrap_module(ffn)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.ffn(x).sum(-1)
 
 
 @multi_device
-def test_torch_checkpointer_wrapper_forward_backward(device: str):
+def test_torch_checkpoint_wrapper(device: str):
     device_ = torch.device(device)
 
-    checkpointer_wrapper: TorchCheckpointWrapper = CheckpointWrapper.from_params(  # type: ignore[assignment]
+    checkpoint_wrapper: TorchCheckpointWrapper = CheckpointWrapper.from_params(  # type: ignore[assignment]
         Params({"type": "torch"})
     )
-    module = ModuleForTesting(checkpointer_wrapper).to(device_)
+    module = ModuleForTesting(checkpoint_wrapper).to(device_)
     optim = torch.optim.Adam(module.parameters(), lr=0.0001)
 
     # Test forward pass
@@ -41,10 +52,13 @@ def test_torch_checkpointer_wrapper_forward_backward(device: str):
     # Shape (batch_size = 2, 3)
     x = torch.randn(2, 3).to(device_)
     loss = module(x).sum()
+    assert forward_calls == 1, f"wrong number of forward calls: {forward_calls}"
 
     # Test backward pass + optimizer step.
     loss.backward()
-    assert module.ffn[0].weight.grad is not None  # type: ignore[index]
+    assert forward_calls == 2, f"wrong number of forward calls: {forward_calls}"
+    for param in module.parameters():
+        assert param.grad is not None
     optim.step()
     optim.zero_grad(set_to_none=True)
 
