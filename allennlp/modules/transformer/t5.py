c6ec78b0b594d6dd4fde56beaba909d8d298c48f
Dirk Groeneveld <dirkg@allenai.org>
2021-12-06 16:51:30 -0800

    Adds a way to resize the vocabulary in T5

--- allennlp/modules/transformer/t5.py
+++ allennlp/modules/transformer/t5.py
@@ -428,6 +428,36 @@ class T5Stack(TransformerModule, FromParams):
             head_mask = [None] * num_hidden_layers
         return head_mask
 
+    def resize_token_embeddings(
+        self, new_size: int, *, init_fn: Callable = torch.nn.init.normal_
+    ) -> None:
+        old_size, embedding_dim = tuple(self.token_embeddings.weight.shape)
+        if old_size == new_size:
+            return
+        if old_size > new_size:
+            logger.warning(
+                "Shrinking vocabulary from size %d to size %d. This is probably not what you want?",
+                old_size,
+                new_size,
+            )
+
+        result = torch.nn.Embedding(
+            new_size,
+            embedding_dim,
+            self.token_embeddings.padding_idx,
+            self.token_embeddings.max_norm,
+            self.token_embeddings.norm_type,
+            self.token_embeddings.scale_grad_by_freq,
+            self.token_embeddings.sparse,
+            device=self.token_embeddings.weight.device,
+            dtype=self.token_embeddings.weight.dtype,
+        )
+        copy_size = min(old_size, new_size)
+        result.weight.data[:copy_size, ...] = self.token_embeddings.weight.data[:copy_size, ...]
+        if new_size > old_size:
+            init_fn(result.weight.data[copy_size:, ...])
+        self.token_embeddings = result
+
     def forward(
         self,
         input_ids: Optional[torch.IntTensor] = None,
@@ -805,6 +835,35 @@ class T5(TransformerModule, Registrable):
         self.output_all_hidden_states = output_all_hidden_states
 
         self.beam_search = beam_search.construct(end_index=self.eos_token_id)
+
+    def resize_token_embeddings(
+        self, new_size: int, *, init_fn: Callable = torch.nn.init.normal_
+    ) -> None:
+        self.encoder.resize_token_embeddings(new_size, init_fn=init_fn)
+        # If encoder and decoder share embeddings, this is a no-op the second time.
+        self.decoder.resize_token_embeddings(new_size, init_fn=init_fn)
+
+        # resize lm head
+        old_size = self.lm_head.out_features
+        if old_size == new_size:
+            return
+        new_lm_head = torch.nn.Linear(
+            self.lm_head.in_features,
+            new_size,
+            self.lm_head.bias,
+            self.lm_head.weight.device,
+            self.lm_head.weight.dtype,
+        )
+        copy_size = min(old_size, new_size)
+        new_lm_head.weight.data[:copy_size, ...] = self.lm_head.weight.data[:copy_size, ...]
+        if self.lm_head.bias and new_lm_head.bias:
+            new_lm_head.bias.data[:copy_size, ...] = self.lm_head.bias[:copy_size, ...]
+        if new_size > old_size:
+            init_fn(new_lm_head.weight.data[copy_size:, ...])
+            if new_lm_head.bias:
+                init_fn(new_lm_head.bias[copy_size:, ...])
+
+        self.lm_head = new_lm_head
 
     def _post_load_state_dict(
         self, missing_keys: List[str], unexpected_keys: List[str]
