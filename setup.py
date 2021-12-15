c6580f4119760d1a41bd6de64c2611bd31c2295a
Dirk Groeneveld <dirkg@allenai.org>
2021-12-15 08:55:58 -0800

    Use correct pip version string syntax

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         ]
     ),
     install_requires=[
-        "torch>=1.6.0,<1.11.0,!1.10.0",
+        "torch>=1.6.0,<1.11.0,!=1.10.0",
         "torchvision>=0.8.1,<0.12.0",
         "cached-path>=1.0.0,<2.0.0",
         "fairscale==0.4.3",
