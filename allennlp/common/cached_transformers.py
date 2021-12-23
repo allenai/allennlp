440705158893aa39291ae14c0fd37c305f160d85
johngiorgi <johnmgiorgi@gmail.com>
2021-12-23 15:09:26 -0500

    Correct error message to say tuple, not list

--- allennlp/common/cached_transformers.py
+++ allennlp/common/cached_transformers.py
@@ -171,7 +171,9 @@ def get(
                         if re.search(regex, name):
                             module.apply(transformer._init_weights)
             else:
-                raise ValueError("reinit_modules must be either an integer, a list of strings, or a list of integers.")
+                raise ValueError(
+                    "reinit_modules must be either an integer, a tuple of strings, or a tuple of integers."
+                )
         else:
             transformer = AutoModel.from_pretrained(
                 model_name,
