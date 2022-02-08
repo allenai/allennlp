7b098aa712632d3b451b645c6404f46a28f4deb2
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-02-07 16:59:48 -0800

    fix ci, import warning

--- allennlp/confidence_checks/task_checklists/__init__.py
+++ allennlp/confidence_checks/task_checklists/__init__.py
@@ -1,17 +1,20 @@
-from allennlp.confidence_checks.task_checklists.task_suite import TaskSuite
-from allennlp.confidence_checks.task_checklists.sentiment_analysis_suite import (
-    SentimentAnalysisSuite,
-)
-from allennlp.confidence_checks.task_checklists.question_answering_suite import (
-    QuestionAnsweringSuite,
-)
-from allennlp.confidence_checks.task_checklists.textual_entailment_suite import (
-    TextualEntailmentSuite,
-)
-
 import warnings
 
-warnings.warn(
-    'To use this integration you should install ``allennlp`` with the "checklist" extra '
-    "(e.g. ``pip install allennlp[checklist]``) or just install checklist after the fact."
-)
+
+try:
+    from allennlp.confidence_checks.task_checklists.task_suite import TaskSuite
+    from allennlp.confidence_checks.task_checklists.sentiment_analysis_suite import (
+        SentimentAnalysisSuite,
+    )
+    from allennlp.confidence_checks.task_checklists.question_answering_suite import (
+        QuestionAnsweringSuite,
+    )
+    from allennlp.confidence_checks.task_checklists.textual_entailment_suite import (
+        TextualEntailmentSuite,
+    )
+except ImportError:
+    warnings.warn(
+        'To use this integration you should install ``allennlp`` with the "checklist" extra '
+        "(e.g. ``pip install allennlp[checklist]``) or just install checklist after the fact."
+    )
+    raise
