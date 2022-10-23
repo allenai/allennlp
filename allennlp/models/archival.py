4712f23c4857935c7ec2b484f27d9d7cab8fbfdd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 19:42:11 +0000

    Adding tarfile member sanitization to extractall()

--- allennlp/models/archival.py
+++ allennlp/models/archival.py
@@ -299,7 +299,26 @@ def extracted_archive(resolved_archive_file, cleanup=True):
         tempdir = tempfile.mkdtemp()
         logger.info(f"extracting archive file {resolved_archive_file} to temp dir {tempdir}")
         with tarfile.open(resolved_archive_file, "r:gz") as archive:
-            archive.extractall(tempdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, tempdir)
         yield tempdir
     finally:
         if tempdir is not None and cleanup:
